98e7df1c22b4d453549fce2f2d37577b8362f872
Michael Marshall <michaelmarshall220@gmail.com>
2020-01-02 09:07:20 -0600

    renamed modules folder to components as we aren't using modules yet.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { DatabaseService } from './core/database.service';
-import { OrganizationService } from './modules/organization/organization.service';
-import { OrganizationResolver } from './modules/organization/organization.resolver';
-import { LanguageResolver } from './modules/language/language.resolver';
-import { LanguageService } from './modules/language/language.service';
-import { LocationService } from './modules/location/location.service';
-import { LocationResolver } from './modules/location/location.resolver';
+import { OrganizationService } from './components/organization/organization.service';
+import { OrganizationResolver } from './components/organization/organization.resolver';
+import { LanguageResolver } from './components/language/language.resolver';
+import { LanguageService } from './components/language/language.service';
+import { LocationService } from './components/location/location.service';
+import { LocationResolver } from './components/location/location.resolver';
 
 @Module({
   imports: [GraphQLModule.forRoot({ autoSchemaFile: 'schema.gql' })],
