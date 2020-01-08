0caec913ff9204543793fef71cc290e14a58f414
ibohalsinghkoijam <koijam@olivetech.net>
2020-01-08 20:26:48 +0530

    cord-api-master: fixed the test cases.

--- src/components/language/language.ts
+++ src/components/language/language.ts
@@ -1,10 +1,32 @@
-export interface Language {
+import { Field, Int, ObjectType } from 'type-graphql';
+
+@ObjectType()
+export class Language {
+  @Field(type => String)
   id: string;
-  name: string | null;
-  displayName: string | null;
-  beginFiscalYear: number | null;
-  ethnologueName: string | null;
-  ethnologuePopulation: number | null;
-  organizationPopulation: number | null;
-  rodNumber: number | null;
+
+  @Field(type => String)
+  name: string;
+
+  @Field()
+  displayName: string;
+
+  @Field(() => Int)
+  beginFiscalYear: number;
+
+  @Field()
+  ethnologueName: string;
+
+  @Field(() => Int)
+  ethnologuePopulation: number;
+
+  @Field(() => Int)
+  organizationPopulation: number;
+
+  @Field(() => Int)
+  rodNumber: number;
+
+  static from(language: Language) {
+    return Object.assign(new Language(), language);
+  }
 }
