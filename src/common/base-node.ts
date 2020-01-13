aea367df03fc63018b36d5da923f03dba7ec6709
Michael Marshall <michaelmarshall220@gmail.com>
2020-01-13 17:40:11 -0600

    first simple draft of listing organizations. just returns all orgs

--- src/common/base-node.ts
+++ src/common/base-node.ts
@@ -1,17 +1,22 @@
 import { ObjectType, Field, ID } from 'type-graphql';
-import { User } from '../components/user/user';
-import { Organization } from '../components/organization/organization';
+import { User } from 'src/components/user/user';
 
 @ObjectType()
 export class BaseNode {
   @Field(type => ID)
   id: string;
 
-  @Field(type => Organization)
-  owningOrg: Organization;
+  @Field(type => String)
+  modifiedByUserId: string;
 
-  @Field()
-  createdAt: string;
+  @Field(type => String)
+  owningOrgId: string;
+
+  @Field(type => String)
+  createdOn: string;
+
+  @Field(type => String, { nullable: true })
+  deletedOn: string;
 
   @Field(type => User)
   createdBy: User;
--- src/components/organization/organization.ts
+++ src/components/organization/organization.ts
@@ -1,9 +1,10 @@
 import { Field, Int, ObjectType, ID, InputType } from 'type-graphql';
-import { BaseNode } from '../../common/base-node';
+import { BaseNode } from 'src/common/base-node';
 
 @ObjectType()
 @InputType('OrganizationInput')
-export class Organization extends BaseNode {
-  @Field(type => String, { nullable: true })
+export class Organization extends BaseNode{
+
+  @Field(type => String, {nullable: true})
   name: string;
 }
