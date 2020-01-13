0eb12c2aa12573771ca63665906fde448a25323b
Daniel Chen <candleshine@gmail.com>
2020-01-13 15:11:24 -0600

    stubs

---
+++ src/components/budget/budget.resolver.ts
@@ -0,0 +1,4 @@
+import { Resolver } from '@nestjs/graphql';
+
+@Resolver('Budget')
+export class BudgetResolver {}
---
+++ src/components/internship-engagement/internship-engagement.resolver.ts
@@ -0,0 +1,4 @@
+import { Resolver } from '@nestjs/graphql';
+
+@Resolver('InternshipEngagement')
+export class InternshipEngagementResolver {}
