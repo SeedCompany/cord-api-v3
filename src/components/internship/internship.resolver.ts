d009491d1cd8a0fddf384fdbdaba2c7b57113fa9
Daniel Chen <candleshine@gmail.com>
2020-01-14 15:53:28 -0600

    WIP

--- src/components/internship/internship.resolver.ts
+++ src/components/internship/internship.resolver.ts
@@ -1,4 +1,57 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Args, Query, Mutation } from '@nestjs/graphql';
+import { Internship } from './internship';
+import { InternshipService } from './internship.service';
+import {
+  CreateInternshipInputDto,
+  CreateInternshipOutputDto,
+  ReadInternshipInputDto,
+  ReadInternshipOutputDto,
+  UpdateInternshipInputDto,
+  UpdateInternshipOutputDto,
+  DeleteInternshipInputDto,
+  DeleteInternshipOutputDto,
+} from './internship.dto';
 
-@Resolver('Internship')
-export class InternshipResolver {}
+@Resolver(of => Internship)
+export class InternshipResolver {
+  constructor(private readonly internshipService: InternshipService) {
+  }
+
+  @Mutation(returns => CreateInternshipOutputDto, {
+    description: 'Create a Internship',
+  })
+  async createInternship(
+    @Args('input') { internship: input }: CreateInternshipInputDto,
+  ): Promise<CreateInternshipOutputDto> {
+    return await this.internshipService.create(input);
+  }
+
+  @Query(returns => ReadInternshipOutputDto, {
+    description: 'Read one Internship by id',
+  })
+  async readInternship(
+    @Args('input') { internship: input }: ReadInternshipInputDto,
+  ): Promise<ReadInternshipOutputDto> {
+    return await this.internshipService.readOne(input);
+  }
+
+  @Mutation(returns => UpdateInternshipOutputDto, {
+    description: 'Update an Internship',
+  })
+  async updateInternship(
+    @Args('input')
+      { internship: input }: UpdateInternshipInputDto,
+  ): Promise<UpdateInternshipOutputDto> {
+    return await this.internshipService.update(input);
+  }
+
+  @Mutation(returns => DeleteInternshipOutputDto, {
+    description: 'Delete an Internship',
+  })
+  async deleteInternship(
+    @Args('input')
+      { internship: input }: DeleteInternshipInputDto,
+  ): Promise<DeleteInternshipOutputDto> {
+    return await this.internshipService.delete(input);
+  }
+}
--- src/components/internship/internship.ts
+++ src/components/internship/internship.ts
@@ -1,10 +1,8 @@
 import { DateTime } from 'luxon';
 import { Budget } from '../budget/budget';
 import { Location } from '../location/location';
-
 import { Sensitivity } from '../project/sensitivity';
 import { TeamMember } from '../user/team-member';
-
 import { InternshipStatus } from './status';
 import { Partnership } from '../partnership/partnership';
 import { InternshipEngagement } from '../internship-engagement/engagement';
