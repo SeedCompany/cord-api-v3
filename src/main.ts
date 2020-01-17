d69ecb9c1b16af61f54eacdf29d5812f730222de
Carson Full <carson_full@tsco.org>
2020-01-16 18:26:55 -0600

    Register source-map-supports so stack traces reference TS files

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'source-map-support/register';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
