c68ad02c9f8b88ee30b59794abf9d7a3de3584f4
Carson Full <carson_full@tsco.org>
2020-02-03 19:18:23 -0600

    Hook up our logger to neo4j driver as `database:driver`

--- src/core/cypher.factory.ts
+++ src/core/cypher.factory.ts
@@ -2,15 +2,32 @@ import { FactoryProvider } from '@nestjs/common/interfaces';
 import { Connection } from 'cypher-query-builder';
 import { ConfigService } from './config/config.service';
 import { MyTransformer } from './database-transformer';
+import { ILogger, LoggerToken, LogLevel } from './logger';
 import './database/transaction'; // import our transaction augmentation
 
 export const CypherFactory: FactoryProvider<Connection> = {
   provide: Connection,
-  useFactory: (config: ConfigService) => {
+  useFactory: (config: ConfigService, logger: ILogger) => {
     const { url, username, password, driverConfig } = config.neo4j;
-    const conn = new Connection(url, { username, password }, { driverConfig });
+    const conn = new Connection(
+      url,
+      { username, password },
+      {
+        driverConfig: {
+          ...driverConfig,
+          logging: {
+            level: 'debug', // log everything, we'll filter out in our logger
+            logger: (neoLevel, message) => {
+              const level =
+                neoLevel === 'warn' ? LogLevel.WARNING : (neoLevel as LogLevel);
+              logger.log(level, message);
+            },
+          },
+        },
+      },
+    );
     (conn as any).transformer = new MyTransformer();
     return conn;
   },
-  inject: [ConfigService],
+  inject: [ConfigService, LoggerToken('database:driver')],
 };
