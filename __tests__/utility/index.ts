ee152710250a70253d336c665f2201ad660411cc
Michael Marshall <michaelmarshall220@gmail.com>
2020-02-07 15:28:52 -0600

    removed old folder

--- __tests__/utility/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './create-app';
-export * from './create-graphql-client';
-export * from './create-organization';
-export * from './create-user';
-export * from './create-token';
-export * from './fragments';
