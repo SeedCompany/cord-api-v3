3975223f66fcce605e9612145b08ea27ca0d5ca5
subodhsri1 <subodh.s@subcodevs.com>
2020-02-19 16:31:32 -0500

    generified read service and updated in user service

--- src/core/database/property-updater.service.ts
+++ src/core/database/property-updater.service.ts
@@ -6,6 +6,7 @@ import {
   isSecured,
   unwrapSecured,
 } from '../../common';
+import { User } from '../../components/user/dto';
 
 import { DateTime } from 'luxon';
 import { ISession } from '../../components/auth';
@@ -126,15 +127,73 @@ export class PropertyUpdaterService {
       ...object,
       ...(isSecured(object[key])
         ? // replace value in secured object keeping can* properties
-          {
-            [key]: {
-              ...object[key],
-              value,
-            },
-          }
+        {
+          [key]: {
+            ...object[key],
+            value,
+          },
+        }
         : // replace value directly
-          { [key]: value }),
+        { [key]: value }),
     };
+  }
+
+  async readProperties<TObject extends User>({
+    id, session, props, nodevar
+  }: { id: string, session: ISession, props: ReadonlyArray<keyof TObject>, nodevar: string }) {
+    let result: { [Key in keyof TObject]?: UnwrapSecured<TObject[Key]> } = {};
+    for (const prop of props) {
+      const key = prop as string;
+      result[key] = await this.readProperty({ id, session, key, nodevar })
+    }
+    return result
+  }
+
+  async readProperty<TObject extends User, Key extends keyof TObject>({ id, session, key, nodevar, aclReadProp }: {
+    id: string,
+    session: ISession,
+    key: string,
+    nodevar: string,
+    aclReadProp?: string
+  }): Promise<{ value: string, canEdit?: boolean, canRead?: boolean }> {
+    const aclReadPropName = aclReadProp || `canRead${upperFirst(key as string)}`;
+    const aclEditPropName = `canEdit${upperFirst(key as string)}`;
+    const query = `
+    match  (token:Token { 
+      active: true,
+      value: $token
+    })
+    <-[:token { active: true }]-
+    (requestingUser:User { 
+      active: true,
+      id: $userId
+    })
+    with * optional match (user:User {active: true, id: $id, owningOrgId: $owningOrgId})
+    with * optional match  (requestingUser) <- [:member]-(acl:ACL { ${aclReadPropName}:true })
+    with * optional match  (user) <- [:member]-(acl2:ACL { ${aclReadPropName}:true })
+    -[:toNode]->(${nodevar})-[:${key} {active: true}]->(${key})
+    return ${key}.value as value, acl2.${aclReadPropName} as canRead, acl2.${aclEditPropName} as canEdit, user.${key} as ${key}
+    `;
+
+    const result = await this.db
+      .query()
+      .raw(query, {
+        token: session.token,
+        userId: session.userId,
+        owningOrgId: session.owningOrgId,
+        id
+      }).run();
+
+    if (!result.length) {
+      throw new NotFoundException('Could not find requested key');
+    }
+
+    const { value, canRead, canEdit } = result[0];
+    if (value && canRead && canEdit) {
+      return { value, canRead, canEdit }
+    } else {
+      return { value: result[0][key] }
+    }
   }
 
   async deleteNode<TObject extends Resource>({
