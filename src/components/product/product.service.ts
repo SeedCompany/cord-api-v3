77f5d7634227db6e10aeeb49586899e9d98ddd95
Richard Girges <richard@Richards-MacBook-Pro.local>
2020-02-21 10:16:28 -0800

    Progress commit - product service refactor: `create` generic. At the moment the createNode statement is simply returning an empty array and no records are being inserted. May be related to the session token not having the correct canCreateProduct permission - that is currently the leading theory behind the create statement failing.

--- src/components/product/product.service.ts
+++ src/components/product/product.service.ts
@@ -17,195 +17,156 @@ export class ProductService {
 
 
   async create(input: CreateProduct, session: ISession): Promise<Product> {
-    const result = await this.db
-      .query()
-      .raw(
-        `
-        MATCH
-          (token:Token {
-            active: true,
-            value: $token
-          })
-          <-[:token {active: true}]-
-          (user:User {
-            active: true,
-            canCreateProduct: true
-          })
-        CREATE
-          (prod:Product {
-            id: $id,
-            active: true,
-            createdAt: datetime(),
-            owningOrgId: "Seed Company"
-          })
-          -[:type {active: true}]->
-          (type:ProductType {
-            active: true,
-            value: $type
-          }),
-          (prod)-[:books {active: true}]->(books:[BibleBooks] {
-            active: true,
-            value: $books
-          }),
-          (prod)-[:mediums {active: true}]->(mediums:[ProductMedium] {
-            active: true,
-            value: $mediums
-          }),
-          (prod)-[:purposes {active: true}]->(purposes:[ProductPurpose] {
-            active: true,
-            value: $purposes
-          }),
-          (prod)-[:approach {active: true}]->(approach:ProductApproach {
-            active: true,
-            value: $approach
-          }),
-          (prod)-[:methodology {active: true}]->(methodology:ProductMethodology {
-            active: true,
-            value: $methodology
-          }),
-          (user)
-          <-[:member]-
-          (acl:ACL {
-            canReadType: true,
-            canEditType: true,
-            canReadBooks: true,
-            canEditBooks: true,
-            canReadMediums: true,
-            canEditMediums: true,
-            canReadPurposes: true,
-            canEditPurposes: true,
-            canReadApproach: true,
-            canEditApproach: true,
-            canReadMethodology: true,
-            canEditMethodology: true
-          })
-          -[:toNode]->(prod)
-        RETURN
-          product.id as id,
-          product.type as type,
-          product.books as books,
-          product.mediums as mediums,
-          product.purposes as purposes,
-          product.approach as approach,
-          product.methodology as methodology
-        `,
-        {
-          token: session.token,
-          id: generate(),
-          type: input.type,
-          books: input.books,
-          mediums: input.mediums,
-          purposes: input.purposes,
-          approach: input.approach,
-          methodology: input.methodology,
-        },
-      )
-      .first();
+    const id = generate();
+    const acls = {
+      canReadType: true,
+      canEditType: true,
+      canReadBooks: true,
+      canEditBooks: true,
+      canReadMediums: true,
+      canEditMediums: true,
+      canReadPurposes: true,
+      canEditPurposes: true,
+      canReadApproach: true,
+      canEditApproach: true,
+      canReadMethodology: true,
+      canEditMethodology: true,
+    };
 
-      if (!result) {
+    try {
+      await this.propertyUpdater.createNode({
+        session,
+        input: { id, ...input },
+        acls,
+        baseNodeLabel: 'Product',
+        aclEditProp: 'canCreateProduct',
+      });
+    } catch (e) {
+      console.log(e);
       throw new Error('Could not create product');
     }
 
-    return {
-      id: result.id,
-      type: result.type,
-      books: result.books,
-      mediums: result.mediums,
-      purposes: result.purposes,
-      approach: result.approach,
-      methodology: result.methodology,
-      createdAt: result.createdAt,
-      // degree: {
-      //   value: result.degree,
-      //   canRead: result.canReadDegree,
-      //   canEdit: result.canEditDegree,
-      // },
-      // major: {
-      //   value: result.major,
-      //   canRead: result.canReadMajor,
-      //   canEdit: result.canEditMajor,
-      // },
-      // institution: {
-      //   value: result.institution,
-      //   canRead: result.canReadInstitution,
-      //   canEdit: result.canEditInstitution,
-      // },
-    };
+    return await this.readOne(id);
+
+
+    // try {
+    //   const result = await this.db
+    //   .query()
+    //   .raw(
+    //     `
+    //     MATCH
+    //       (token:Token {
+    //         active: true,
+    //         value: $token
+    //       })
+    //       <-[:token {active: true}]-
+    //       (user:User {
+    //         active: true,
+    //         canCreateProduct: true
+    //       })
+    //     CREATE
+    //       (prod:Product {
+    //         id: $id,
+    //         active: true,
+    //         createdAt: datetime(),
+    //         owningOrgId: "Seed Company"
+    //       })
+    //       -[:type {active: true}]->
+    //       (type:Property {
+    //         active: true,
+    //         value: $type
+    //       }),
+    //       (prod)-[:books {active: true}]->(books:Property {
+    //         active: true,
+    //         value: $books
+    //       }),
+    //       (prod)-[:mediums {active: true}]->(mediums:Property {
+    //         active: true,
+    //         value: $mediums
+    //       }),
+    //       (prod)-[:purposes {active: true}]->(purposes:Property {
+    //         active: true,
+    //         value: $purposes
+    //       }),
+    //       (prod)-[:approach {active: true}]->(approach:Property {
+    //         active: true,
+    //         value: $approach
+    //       }),
+    //       (prod)-[:methodology {active: true}]->(methodology:Property {
+    //         active: true,
+    //         value: $methodology
+    //       }),
+    //       (user)
+    //       <-[:member]-
+    //       (acl:ACL {
+    //         canReadType: true,
+    //         canEditType: true,
+    //         canReadBooks: true,
+    //         canEditBooks: true,
+    //         canReadMediums: true,
+    //         canEditMediums: true,
+    //         canReadPurposes: true,
+    //         canEditPurposes: true,
+    //         canReadApproach: true,
+    //         canEditApproach: true,
+    //         canReadMethodology: true,
+    //         canEditMethodology: true
+    //       })
+    //       -[:toNode]->(prod)
+    //     RETURN
+    //       prod.id as id,
+    //       prod.type as type,
+    //       prod.books as books,
+    //       prod.mediums as mediums,
+    //       prod.purposes as purposes,
+    //       prod.approach as approach,
+    //       prod.methodology as methodology,
+    //       acl.canReadType as canReadType,
+    //       acl.canEditType as canEditType,
+    //       acl.canReadBooks as canReadBooks,
+    //       acl.canEditBooks as canEditBooks,
+    //       acl.canReadMediums as canReadMediums,
+    //       acl.canEditMediums as canEditMediums,
+    //       acl.canReadPurposes as canReadPurposes,
+    //       acl.canEditPurposes as canEditPurposes,
+    //       acl.canReadApproach as canReadApproach,
+    //       acl.canEditApproach as canEditApproach,
+    //       acl.canReadMethodology as canReadMethodology,
+    //       acl.canEditMethodology as canEditMethodology
+
+    //     `,
+    //     {
+    //       token: session.token,
+    //       id: generate(),
+    //       type: input.type,
+    //       books: input.books,
+    //       mediums: input.mediums,
+    //       purposes: input.purposes,
+    //       approach: input.approach,
+    //       methodology: input.methodology,
+    //     },
+    //   )
+    //   .first();
+
+    //   if (!result) {
+    //     throw new Error('Could not create product');
+    //   }
+  
+    //   return {
+    //     id: result.id,
+    //     type: result.type,
+    //     books: result.books,
+    //     mediums: result.mediums,
+    //     purposes: result.purposes,
+    //     approach: result.approach,
+    //     methodology: result.methodology,
+    //     createdAt: result.createdAt,
+    //   };  
+    // } catch (e) {
+    //   console.log(e);
+    //   throw e;
+    // }
   }
-
-  // async create(input: CreateProduct, session: ISession): Promise<Product> {
-  //   const id = generate();
-  //   const acls = {
-  //     canReadType: true,
-  //     canEditType: true,
-  //   };
-
-  //   try {
-  //     await this.propertyUpdater.createNode({
-  //       session,
-  //       input: { id, ...input },
-  //       acls,
-  //       baseNodeLabel: 'Product',
-  //     });
-  //   } catch (e) {
-  //     console.log(e);
-  //     throw new Error('Could not create product');
-  //   }
-
-  //   return await this.readOne(id);
-
-  //   const response = new CreateProductOutputDto();
-  //   const session = this.db.driver.session();
-  //   const id = generate();
-  //   await session
-  //     .run(
-  //       `
-  //         MERGE
-  //           (product:Product {active: true, owningOrg: "seedcompany", id: $id})
-  //         ON CREATE SET
-  //           product.id = $id,
-  //           product.type  = $type,
-  //           product.books=$books,
-  //           product.mediums = $mediums,
-  //           product.purposes=$purposes,
-  //           product.approach=$approach,
-  //           product.methodology=$methodology,
-  //           product.timestamp = datetime()
-  //         RETURN
-  //           product.id as id,
-  //           product.type as type,
-  //           product.books as books,
-  //           product.mediums as mediums,
-  //           product.purposes as purposes,
-  //           product.approach as approach,
-  //           product.methodology as methodology
-  //         `,
-  //       {
-  //         id,
-  //         type: input.type,
-  //         books: input.books,
-  //         mediums: input.mediums,
-  //         purposes: input.purposes,
-  //         approach: input.approach,
-  //         methodology: input.methodology,
-  //       },
-  //     )
-  //     .then(result => {
-  //       response.product.id = result.records[0].get('id');
-  //       response.product.type = result.records[0].get('type');
-  //       response.product.books = result.records[0].get('books');
-  //       response.product.mediums = result.records[0].get('mediums');
-  //       response.product.purposes = result.records[0].get('purposes');
-  //       response.product.approach = result.records[0].get('approach');
-  //       response.product.methodology = result.records[0].get('methodology');
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     })
-  //     .then(() => session.close());
-
-  //   return response;
-  // }
 
   async readOne(id: string): Promise<Product> {
     const result = await this.db
@@ -231,6 +192,10 @@ export class ProductService {
         },
       )
       .first();
+
+      if (!result) {
+        throw new NotFoundException('Could not find product');
+      }
 
       return {
         id: result.id,
@@ -285,7 +250,7 @@ export class ProductService {
     const object = await this.readOne(id);
 
     if (!object) {
-      throw new NotFoundException('Could not find language');
+      throw new NotFoundException('Could not find product');
     }
 
     try {
