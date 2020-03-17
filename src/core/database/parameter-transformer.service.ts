884230b5f728bad2f96b37af80fd03b55386620a
Carson Full <carson_full@tsco.org>
2020-03-17 11:25:49 -0500

    Implement logic for ParameterTransformer

--- src/core/database/parameter-transformer.service.ts
+++ src/core/database/parameter-transformer.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { mapValues } from 'lodash';
+import { DateTime } from 'luxon';
 
 /**
  * Transforms parameters going into the database
@@ -6,6 +8,43 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ParameterTransformer {
   transform(parameters: Record<string, any>): Record<string, any> {
-    return parameters;
+    return this.transformValue(parameters);
+  }
+
+  transformValue(value: any): any {
+    if (this.isPlainValue(value)) {
+      return value;
+    }
+
+    // Ensure consistency across codebase
+    if (value instanceof Date) {
+      throw new Error('Use Luxon DateTime instead');
+    }
+
+    if (value instanceof DateTime) {
+      return value.toNeo4JDateTime();
+    }
+
+    // TODO Neo4J Date
+
+    if (Array.isArray(value)) {
+      return value.map(v => this.transformValue(v));
+    }
+
+    if (typeof value === 'object') {
+      return mapValues(value, v => this.transformValue(v));
+    }
+
+    throw new Error(`Could not determine how to transform value: ${value}`);
+  }
+
+  private isPlainValue(value: any) {
+    const type = typeof value;
+    return (
+      value == null ||
+      type === 'string' ||
+      type === 'boolean' ||
+      type === 'number'
+    );
   }
 }
