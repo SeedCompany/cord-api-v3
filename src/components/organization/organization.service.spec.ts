e4193cb090ec8e412b6f402f52e3ae8e9ca06136
ibohalsinghkoijam <koijam@olivetech.net>
2020-03-23 21:38:14 +0530

    unit test for organization service

--- src/components/organization/organization.service.spec.ts
+++ src/components/organization/organization.service.spec.ts
@@ -1,7 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoreModule, LoggerModule } from '../../core';
+import * as faker from 'faker';
 import { OrganizationService } from './organization.service';
 import { ISession } from '../auth';
+import {
+  CreateOrganization,
+  UpdateOrganization,
+  OrganizationListInput,
+} from './dto';
 
 describe('OrganizationService', () => {
   let module: TestingModule;
@@ -11,15 +17,21 @@ describe('OrganizationService', () => {
       imports: [LoggerModule.forRoot(), CoreModule],
       providers: [OrganizationService],
     }).compile();
+
+    // const session : ISession = { 
+    //   token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODQ5NTMxOTc0Mzl9.GqoNZAGzPpPhp1hs0Toi5bp8I2UUYHqR0FUrOxxLWFI", 
+    //   userId: "C3DDouWkM",
+    //   owningOrgId: "Seed Company",
+    // };
   });
 
   afterEach(async () => {
     await module.close();
   });
 
-  // READ ORG
-  it('read organization by id', async () => {
-    const orgId = "GQiiEX_jZ";
+  // CREATE ORG
+  it('create an organization', async () => {
+    const input = {name : faker.company.companyName()};
     const session = { 
       token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODQ5NTMxOTc0Mzl9.GqoNZAGzPpPhp1hs0Toi5bp8I2UUYHqR0FUrOxxLWFI", 
       userId: "C3DDouWkM",
@@ -27,9 +39,7 @@ describe('OrganizationService', () => {
     };
 
     try {
-      //jest.spyOn(OrganizationService, 'readOne').mockImplementation((orgId, session as ISession) => result);
-      const result = await module.get(OrganizationService).readOne(orgId, session as ISession);
-    
+      const result = await module.get(OrganizationService).create(input as CreateOrganization, session as ISession);
     } catch (e) {
       console.log(e);
       throw e;
@@ -38,7 +48,7 @@ describe('OrganizationService', () => {
 
   // READ ORG
   it('read organization by id', async () => {
-    const orgId = "GQiiEX_jZ";
+    const input = {name : faker.company.companyName()};
     const session = { 
       token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODQ5NTMxOTc0Mzl9.GqoNZAGzPpPhp1hs0Toi5bp8I2UUYHqR0FUrOxxLWFI", 
       userId: "C3DDouWkM",
@@ -46,9 +56,29 @@ describe('OrganizationService', () => {
     };
 
     try {
-      //jest.spyOn(OrganizationService, 'readOne').mockImplementation((orgId, session as ISession) => result);
-      const result = await module.get(OrganizationService).readOne(orgId, session as ISession);
-    
+      const org1 = await module.get(OrganizationService).create(input as CreateOrganization, session as ISession);
+      const org2 = await module.get(OrganizationService).readOne(org1.id, session as ISession);
+      expect(org2.name.value).toEqual(org1.name.value);
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
+  });
+
+  // UPDATE ORG
+  it('update organization', async () => {
+    const input = {name : faker.company.companyName()};
+    const session = { 
+      token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODQ5NTMxOTc0Mzl9.GqoNZAGzPpPhp1hs0Toi5bp8I2UUYHqR0FUrOxxLWFI", 
+      userId: "C3DDouWkM",
+      owningOrgId: "Seed Company",
+    };
+
+    try {
+      const org1 = await module.get(OrganizationService).create(input as CreateOrganization, session as ISession);
+      const inputNew = {id: org1.id, name : faker.company.companyName()};
+      const org2 = await module.get(OrganizationService).update(inputNew as UpdateOrganization, session as ISession);
+      expect(org2.name.value).toBe(inputNew.name);
     } catch (e) {
       console.log(e);
       throw e;
@@ -56,14 +86,36 @@ describe('OrganizationService', () => {
   });
 
   // DELETE ORG
-  it('delete user', async () => {
-    const orgId = 'GQiiEX_jZ';
-    
-    const session = {
-      token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODQ5NTMxOTc0Mzl9.GqoNZAGzPpPhp1hs0Toi5bp8I2UUYHqR0FUrOxxLWFI",
+  it('delete organization', async () => {
+    const input = {name : faker.company.companyName()};
+    const session = { 
+      token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODQ5NTMxOTc0Mzl9.GqoNZAGzPpPhp1hs0Toi5bp8I2UUYHqR0FUrOxxLWFI", 
       userId: "C3DDouWkM",
       owningOrgId: "Seed Company",
     };
-    const result = await module.get(OrganizationService).delete(orgId, session as ISession);
+    try{
+      const org1 = await module.get(OrganizationService).create(input as CreateOrganization, session as ISession);
+      await module.get(OrganizationService).delete(org1.id, session as ISession);
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
   });
+
+  // LIST ORGS
+  it('list view of organizations', async () => {
+    const input = { page : 1, count : 5, sort : "name", order : "DESC"};
+    const session = { 
+      token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODQ5NTMxOTc0Mzl9.GqoNZAGzPpPhp1hs0Toi5bp8I2UUYHqR0FUrOxxLWFI", 
+      userId: "C3DDouWkM",
+      owningOrgId: "Seed Company",
+    };
+    try{
+      module.get(OrganizationService).list(input as OrganizationListInput, session as ISession);
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
+  });
+
 });
