6ded0d30252d7ad64ee7a326a67550b58ba47112
ibohalsinghkoijam <koijam@olivetech.net>
2020-03-23 15:05:54 +0530

    WIP - verify the result

--- src/components/organization/organization.service.spec.ts
+++ src/components/organization/organization.service.spec.ts
@@ -19,15 +19,25 @@ describe('OrganizationService', () => {
 
   // READ ORG
   it('read organization by id', async () => {
-    const orgId = '1234';
+    const orgId = 'GQiiEX_jZ';
+    const session = { 
+      token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODQ2OTY0MjM1ODl9.IzqfPHG9HsAH5hAei-IxmDRcPggtTDZJQjnd2JASM4E", 
+      owningOrgId: "Seed Company"
+    };
+
+    try {
+      //jest.spyOn(OrganizationService, 'readOne').mockImplementation((orgId, session as ISession) => result);
+      const result = await module.get(OrganizationService).readOne(orgId, session as ISession);
     
-    const session = {token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODQ2OTY0MjM1ODl9.IzqfPHG9HsAH5hAei-IxmDRcPggtTDZJQjnd2JASM4E"};
-    module.get(OrganizationService).readOne(orgId, session as ISession);
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
   });
 
   // DELETE ORG
   it('delete user', async () => {
-    const orgId = '1234';
+    const orgId = 'GQiiEX_jZ';
     
     const session = {token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODQ2OTY0MjM1ODl9.IzqfPHG9HsAH5hAei-IxmDRcPggtTDZJQjnd2JASM4E"};
     module.get(OrganizationService).delete(orgId, session as ISession);
