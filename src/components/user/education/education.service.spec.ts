4ea749ddf32fb2bf012bd07cfbf7f8d1ad6901c2
ibohalsinghkoijam <koijam@olivetech.net>
2020-03-27 10:33:11 +0530

    WIP

---
+++ src/components/user/education/education.service.spec.ts
@@ -0,0 +1,100 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { DateTime } from 'luxon';
+import { generate } from 'shortid';
+import { CoreModule, DatabaseService, LoggerModule } from '../../../core';
+import { Degree, Education } from './dto';
+import { EducationService } from './education.service';
+
+describe('EducationService', () => {
+  let educationService: EducationService;
+  const id = generate();
+
+  const createTestEducation: Partial<Education> = {
+    id,
+    degree: {
+      value: Degree.Associates,
+      canRead: true,
+      canEdit: true,
+    },
+    major: {
+      value: 'Electronic',
+      canRead: true,
+      canEdit: true,
+    },
+    institution: {
+      value: 'Cambridge',
+      canRead: true,
+      canEdit: true,
+    },
+  };
+
+  // const updateTestEducation: Partial<Education> = {
+  //   id,
+  //   degree: {
+  //     value: 'MTECH',
+  //     canRead: true,
+  //     canEdit: true,
+  //   },
+  //   major: {
+  //     value: 'Electronic',
+  //     canRead: true,
+  //     canEdit: true,
+  //   },
+  //   institution: {
+  //     value: 'Cambridge',
+  //     canRead: true,
+  //     canEdit: true,
+  //   },
+  // };
+
+  const mockDbService = {
+    createNode: () => createTestEducation,
+    //updateProperties: () => updateTestEducation,
+    deleteNode: () => ({}),
+    query: () => ({
+      raw: () => ({
+        run: () => ({}),
+      }),
+    }),
+    readProperties: () => createTestEducation,
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [LoggerModule.forRoot(), CoreModule],
+      providers: [
+        EducationService,
+        {
+          provide: DatabaseService,
+          useValue: mockDbService,
+        },
+      ],
+    }).compile();
+
+    educationService = module.get<EducationService>(EducationService);
+  });
+
+  it('should be defined', () => {
+    expect(EducationService).toBeDefined();
+  });
+
+  it('should create education node', async () => {
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    educationService.readOne = jest.fn().mockReturnValue(createTestEducation);
+    const education = await educationService.create(
+      {
+        userId: '12345',
+        degree: Degree.Associates,
+        major: 'Electronic',
+        institution: 'Cambridge',
+      },
+      {
+        token:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODUxNjY0MTM3OTF9.xStLc8cYmOVT3ABW1b6GLuSpeoFNxrYE2o2CBmJR8-U',
+        userId: '12345',
+        issuedAt: DateTime.local(),
+      }
+    );
+    expect(education.institution).toEqual(createTestEducation.institution);
+  });
+});
