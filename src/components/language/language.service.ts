d2b4653dd3006fb334126a96ae3383e82490de1c
ibohalsinghkoijam <koijam@olivetech.net>
2020-04-03 15:18:29 +0530

    WIP - Add DB constraints to language service

--- src/components/language/language.service.ts
+++ src/components/language/language.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { generate } from 'shortid';
 import { ISession, Sensitivity } from '../../common';
-import { DatabaseService, ILogger, Logger } from '../../core';
+import { DatabaseService, ILogger, Logger, OnIndex } from '../../core';
 import {
   CreateLanguage,
   Language,
@@ -16,6 +16,52 @@ export class LanguageService {
     private readonly db: DatabaseService,
     @Logger('language:service') private readonly logger: ILogger
   ) {}
+
+  @OnIndex()
+  async createIndexes() {
+    const constraints = [
+      // LANGUAGE NODE
+      'CREATE CONSTRAINT ON (n:Language) ASSERT EXISTS(n.id)',
+      'CREATE CONSTRAINT ON (n:Language) ASSERT n.id IS UNIQUE',
+      'CREATE CONSTRAINT ON (n:Language) ASSERT EXISTS(n.active)',
+      'CREATE CONSTRAINT ON (n:Language) ASSERT EXISTS(n.createdAt)',
+      'CREATE CONSTRAINT ON (n:Language) ASSERT EXISTS(n.owningOrgId)',
+
+      // NAME REL
+      'CREATE CONSTRAINT ON ()-[r:name]-() ASSERT EXISTS(r.active)',
+      'CREATE CONSTRAINT ON ()-[r:name]-() ASSERT EXISTS(r.createdAt)',
+
+      // NAME NODE
+      'CREATE CONSTRAINT ON (n:LanguageName) ASSERT EXISTS(n.value)',
+      'CREATE CONSTRAINT ON (n:LanguageName) ASSERT n.value IS UNIQUE',
+
+      // DISPLAYNAME REL
+      'CREATE CONSTRAINT ON ()-[r:displayName]-() ASSERT EXISTS(r.active)',
+      'CREATE CONSTRAINT ON ()-[r:displayName]-() ASSERT EXISTS(r.createdAt)',
+
+      // DISPLAYNAME NODE
+      'CREATE CONSTRAINT ON (n:LanguageDisplayName) ASSERT EXISTS(n.value)',
+      'CREATE CONSTRAINT ON (n:LanguageDisplayName) ASSERT n.value IS UNIQUE',
+
+      // RODNUMBER REL
+      'CREATE CONSTRAINT ON ()-[r:rodNumber]-() ASSERT EXISTS(r.active)',
+      'CREATE CONSTRAINT ON ()-[r:rodNumber]-() ASSERT EXISTS(r.createdAt)',
+
+      // RODNUMBER NODE
+      'CREATE CONSTRAINT ON (n:LanguageRodNumber) ASSERT EXISTS(n.value)',
+      'CREATE CONSTRAINT ON (n:LanguageRodNumber) ASSERT n.value IS UNIQUE',
+
+      // PROPERTY NODE
+      'CREATE CONSTRAINT ON (n:Property) ASSERT EXISTS(n.value)',
+      'CREATE CONSTRAINT ON (n:Property) ASSERT EXISTS(n.active)',
+    ];
+    for (const query of constraints) {
+      await this.db
+        .query()
+        .raw(query)
+        .run();
+    }
+  }
 
   async create(input: CreateLanguage, session: ISession): Promise<Language> {
     this.logger.info(
@@ -57,6 +103,148 @@ export class LanguageService {
       throw new Error('Could not create language');
     }
   }
+
+  // async create(input: CreateLanguage, session: ISession): Promise<Language> {
+  //   this.logger.info(
+  //     `Mutation create Language: ${input.name} by ${session.userId}`
+  //   );
+
+  //   const id = generate();
+  //   //const pash = await argon2.hash(input.password);
+  //   const createdAt = DateTime.local();
+
+  //   // helper method for defining properties
+  //   const property = (prop: string, value: any) => {
+  //     if (!value) {
+  //       return [];
+  //     }
+
+  //     return [
+  //       [
+  //         node('language'),
+  //         relation('out', '', prop, {
+  //           active: true,
+  //           createdAt,
+  //         }),
+  //         node(prop, 'Property', {
+  //           active: true,
+  //           value,
+  //         }),
+  //       ],
+  //     ];
+  //   };
+
+  //   await this.db
+  //     .query()
+  //     .matchNode('token', 'Token', { active: true, value: session.token })
+  //     .create([
+  //       [
+  //         node('language', 'Language', {
+  //           id,
+  //           active: true,
+  //           createdAt,
+  //           createdByUserId: 'system',
+  //           canCreateBudget: true,
+  //           canReadBudgets: true,
+  //           canCreateBudgetRecord: true,
+  //           canReadBudgetRecords: true,
+  //           canCreateFileNode: true,
+  //           canReadFileNodes: true,
+  //           canCreateFileVersion: true,
+  //           canReadFileVersions: true,
+  //           canCreateDirectory: true,
+  //           canReadDirectorys: true,
+  //           canCreateOrg: true,
+  //           canReadOrgs: true,
+  //           canReadUsers: true,
+  //           canCreateLang: true,
+  //           canReadLangs: true,
+  //           canCreateEducation: true,
+  //           canReadEducationList: true,
+  //           canCreateUnavailability: true,
+  //           canReadUnavailabilityList: true,
+  //           canCreatePartnership: true,
+  //           canReadPartnerships: true,
+  //           canCreateProduct: true,
+  //           canReadProducts: true,
+  //           canCreateProject: true,
+  //           canReadProjects: true,
+  //           canCreateZone: true,
+  //           canReadZone: true,
+  //           canCreateRegion: true,
+  //           canReadRegion: true,
+  //           canCreateCountry: true,
+  //           canReadCountry: true,
+  //           canCreateCeremony: true,
+  //           canReadCeremonies: true,
+  //           canCreateProjectMember: true,
+  //           canReadProjectMembers: true,
+  //           canDeleteOwnUser: true,
+  //           canDeleteLocation: true,
+  //           owningOrgId: 'Seed Company',
+  //           isAdmin: true,
+  //         }),
+  //         relation('out', '', 'name', {
+  //           active: true,
+  //           createdAt,
+  //         }),
+  //         node('name', 'LanguageName:Property', {
+  //           active: true,
+  //           value: input.name,
+  //           createdAt,
+  //         }),
+  //       ],
+  //       [
+  //         node('language'),
+  //         relation('out', '', 'token', {
+  //           active: true,
+  //           createdAt,
+  //         }),
+  //         node('token'),
+  //       ],
+  //       ...property('name', input.name),
+  //       ...property('displayName', input.displayName),
+  //       ...property('beginFiscalYear', input.beginFiscalYear),
+  //       ...property('ethnologueName', input.ethnologueName),
+  //       ...property('ethnologuePopulation', input.ethnologuePopulation),
+  //       ...property('organizationPopulation', input.organizationPopulation),
+  //       ...property('rodNumber', input.rodNumber),
+  //       [
+  //         node('language'),
+  //         relation('in', '', 'member'),
+  //         node('acl', 'ACL', {
+  //           canReadName: true,
+  //           canEditName: true,
+  //           canReadDisplayName: true,
+  //           canEditDisplayName: true,
+  //           canReadBeginFiscalYear: true,
+  //           canEditBeginFiscalYear: true,
+  //           canReadEthnologueName: true,
+  //           canEditEthnologueName: true,
+  //           canReadEthnologuePopulation: true,
+  //           canEditEthnologuePopulation: true,
+  //           canReadOrganizationPopulation: true,
+  //           canEditOrganizationPopulation: true,
+  //           canReadRodNumber: true,
+  //           canEditRodNumber: true,
+  //         }),
+  //         relation('out', '', 'toNode'),
+  //         node('language'),
+  //       ],
+  //     ])
+  //     .return({
+  //       user: [{ id: 'id' }],
+  //     })
+  //     .run();
+
+  //   const result = await this.readOne(id, session);
+
+  //     return result;
+  //   } catch (e) {
+  //     this.logger.error(`Could not create`, { ...input, exception: e });
+  //     throw new Error('Could not create language');
+  //   }
+  // }
 
   async readOne(langId: string, session: ISession): Promise<Language> {
     this.logger.info(
