ead892b50e1243caeb86b9ff5eabbaa9eafe3a66
ibohalsinghkoijam <koijam@olivetech.net>
2020-04-30 20:56:45 +0530

    Added unit test for location service

--- src/components/location/location.service.spec.ts
+++ src/components/location/location.service.spec.ts
@@ -1,5 +1,6 @@
-import { Test } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
 import { DateTime } from 'luxon';
+import { generate } from 'shortid';
 import { Order } from '../../common';
 import { CoreModule, DatabaseService, LoggerModule } from '../../core';
 import { AuthenticationModule, AuthenticationService } from '../authentication';
@@ -10,23 +11,55 @@ import {
   UserModule,
   UserService,
 } from '../user';
-import { LocationListOutput } from './dto';
-import { LocationModule } from './location.module';
-import { LocationService } from './location.service';
+import { Country, LocationListOutput } from './dto';
+import { LocationModule, LocationService } from './index';
 
 describe('LocationService', () => {
   let locationService: LocationService;
+  const id = generate();
+  const regionId = '3456';
+
+  const createTestLocation: Partial<Country> = {
+    id,
+    name: {
+      value: 'seed-location',
+      canRead: true,
+      canEdit: true,
+    },
+  };
+
+  const updateTestLocation: Partial<Country> = {
+    id,
+    name: {
+      value: 'seed-new-location',
+      canRead: true,
+      canEdit: true,
+    },
+  };
+
+  const mockDbService = {
+    createNode: () => createTestLocation,
+    updateProperties: () => updateTestLocation,
+    deleteNode: () => ({}),
+    query: () => ({
+      raw: () => ({
+        run: () => ({}),
+        first: () => ({}),
+      }),
+    }),
+    readProperties: () => createTestLocation,
+  };
 
   const mockSession = {
     token:
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODUxNjY0MTM3OTF9.xStLc8cYmOVT3ABW1b6GLuSpeoFNxrYE2o2CBmJR8-U',
     userId: '12345',
     issuedAt: DateTime.local(),
-    owningOrgId: '',
+    owningOrgId: 'Seed Company',
   };
 
   beforeAll(async () => {
-    const module = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       imports: [
         LoggerModule.forRoot(),
         CoreModule,
@@ -35,13 +68,17 @@ describe('LocationService', () => {
         UserModule,
       ],
       providers: [
-        LocationService,
         AuthenticationService,
         UserService,
         OrganizationService,
         UnavailabilityService,
         DatabaseService,
         EducationService,
+        LocationService,
+        {
+          provide: DatabaseService,
+          useValue: mockDbService,
+        },
       ],
     }).compile();
 
@@ -50,6 +87,47 @@ describe('LocationService', () => {
 
   it('should be defined', () => {
     expect(locationService).toBeDefined();
+  });
+
+  it('should create location country node', async () => {
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    locationService.readOneCountry = jest
+      .fn()
+      .mockReturnValue(createTestLocation);
+    const country = await locationService.createCountry(
+      {
+        regionId,
+        name: 'seed-location',
+      },
+      mockSession
+    );
+    expect(country.id).toEqual(createTestLocation.id);
+    expect(country.name).toEqual(createTestLocation.name);
+  });
+
+  it('should read location country node', async () => {
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    locationService.readOneCountry = jest
+      .fn()
+      .mockReturnValue(createTestLocation);
+    const country = await locationService.readOneCountry(id, mockSession);
+    expect(country.id).toEqual(createTestLocation.id);
+    expect(country.name).toEqual(createTestLocation.name);
+  });
+
+  it('should update location country node', async () => {
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    locationService.readOneCountry = jest
+      .fn()
+      .mockReturnValue(updateTestLocation);
+    const country = await locationService.updateCountry(
+      {
+        id,
+        name: 'seed-new-location',
+      },
+      mockSession
+    );
+    expect(country.name).toEqual(updateTestLocation.name);
   });
 
   const listOutput: Partial<LocationListOutput> = {
@@ -82,104 +160,21 @@ describe('LocationService', () => {
     expect(listInput.items.length).toEqual(listOutput.items?.length);
   });
 
-  /*
-  const createTestZone: Partial<Zone> = {
-    name: 'seed-organization',
-    type: 'type',
-  };
-
-  it('should create zone node', async () => {
+  it('should delete location country node', async () => {
     // eslint-disable-next-line @typescript-eslint/unbound-method
-    locationService.readOne = jest.fn().mockReturnValue(createTestZone);
-    const zone = await locationService.createZone(
-      {
-        name: 'seed-organization',
-        directorId: generate(),
-      },
-      mockSession
-    );
-    expect(zone.name).toEqual(createTestZone.name);
-  });
-
-  it('should update zone node', async () => {
-    // eslint-disable-next-line @typescript-eslint/unbound-method
-    locationService.readOne = jest.fn().mockReturnValue(createZone);
-    const zone = await locationService.updateZone(
-      {
-        id: '',
-        name: 'seed-organization',
-        directorId: 'tmpUiLso',
-      },
-      mockSession
-    );
-    expect(zone.name).toEqual(createZone);
-  });
-
-  const createTestRegion: Partial<Region> = {};
-
-  it('should create region node', async () => {
-    // eslint-disable-next-line @typescript-eslint/unbound-method
-    locationService.readOne = jest.fn().mockReturnValue(createTestRegion);
-    const region = await locationService.createRegion(
-      {
-        name: 'seed-organization',
-        zoneId: '8O5BB3pv-',
-        directorId: 'tmpUiLso',
-      },
-      mockSession
-    );
-    expect(region.name).toEqual(createTestRegion);
-  });
-
-  it('should update region node', async () => {
-    // eslint-disable-next-line @typescript-eslint/unbound-method
-    locationService.readOne = jest.fn().mockReturnValue(createTestRegion);
-    const region = await locationService.updateRegion(
-      {
-        id: '8O5BB3pv',
-        name: 'seed-organization',
-        zoneId: '8O5BB3pv-',
-        directorId: 'tmpUiLso',
-      },
-      mockSession
-    );
-    expect(region.name).toEqual(createTestRegion);
-  });
-
-  it('should read region node', async () => {
-    // eslint-disable-next-line @typescript-eslint/unbound-method
-    locationService.readOne = jest
+    locationService.readOneCountry = jest
       .fn()
-      .mockReturnValue(createTestRegion);
-    const region = await locationService.readOne(id, mockSession);
-    expect(region.name).toEqual(createTestRegion.name);
-  });
-
-  const createTestCountry: Partial<country> = 'country';
-  it('should create country node', async () => {
-    // eslint-disable-next-line @typescript-eslint/unbound-method
-    locationService.readOne = jest.fn().mockReturnValue(createTestCountry);
+      .mockReturnValue(createTestLocation);
     const country = await locationService.createCountry(
       {
-        name: 'seed-organization',
-        regionId: 'tmpUiLso',
+        regionId,
+        name: 'seed-location',
       },
       mockSession
     );
-    expect(country.name).toEqual(createTestCountry);
+    await locationService.delete(id, mockSession);
+    // since delete is making the graph node inactive, we just test for the nodes existance now
+    expect(country.id).toEqual(createTestLocation.id);
+    expect(country.name).toEqual(createTestLocation.name);
   });
-
-  it('should update country node', async () => {
-    // eslint-disable-next-line @typescript-eslint/unbound-method
-    locationService.readOne = jest.fn().mockReturnValue(createTestCountry);
-    const country = await locationService.updateCountry(
-      {
-        id: 'tmpUiLso',
-        name: 'seed-organization',
-      },
-      mockSession
-    );
-    expect(country.name).toEqual(createTestCountry);
-  });
-  */
 });
