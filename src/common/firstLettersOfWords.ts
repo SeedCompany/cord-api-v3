197d9e038c98cdb0a083d68f610791c6aef3744b
Carson Full <carson_full@tsco.org>
2020-05-08 12:49:34 -0500

    Port firstLettersOfWords from v2
    Node +10 has native unicode property escapes so we don't need the XRegExp library
    Node +12 has matchAll() so we don't need to provide that either

---
+++ src/common/firstLettersOfWords.ts
@@ -0,0 +1,40 @@
+const hasUppercaseLettersPattern = /\p{Lu}/gu;
+
+// https://regex101.com/r/63P0Hw (make sure you are viewing the latest version)
+const uppercasePattern = RegExp(
+  // Match word-like boundaries
+  // \b fails in some cases, so also match start of string, and space before letter
+  '(?:^|\\b|\\s)' +
+    // Capture uppercase unicode letter or number
+    '([\\p{Lu}\\p{N}])',
+  'ug'
+);
+
+// https://regex101.com/r/t2xf5O (make sure you are viewing the latest version)
+const lowercasePattern = RegExp(
+  // Match word-like boundaries
+  // \b can't be used here has it will match lowercase char after a unicode lowercase char
+  // So match some punctuation, start of string, and spaces instead
+  `(?:^|'|\\(|-|\\s)` +
+    // Capture lower case unicode letter or number
+    '([\\p{Ll}\\p{N}])',
+  'ug'
+);
+
+export function firstLettersOfWords(
+  words: string,
+  limit: number | null = 3
+): string {
+  // If the string has uppercase characters we use the uppercase pattern which
+  // will ignore lowercase characters after word-like boundaries.
+  // If the string doesn't have any uppercase characters we fallback to the lowercase pattern
+  // which is less ideal but gives something instead of an empty string.
+  // See tests for differences in the two patterns.
+  const pattern = words.match(hasUppercaseLettersPattern)
+    ? uppercasePattern
+    : lowercasePattern;
+  const letters = Array.from(words.matchAll(pattern), (m) => m[1]).join('');
+  return limit === Infinity || limit == null
+    ? letters
+    : letters.substr(0, limit);
+}
