adc9738ca4e95446504754d09cf150df33eaca2c
ibohalsinghkoijam <koijam@olivetech.net>
2020-06-18 18:25:31 +0530

    Verifying test case adding condition - !result.type

--- src/components/project/project.service.ts
+++ src/components/project/project.service.ts
@@ -283,8 +283,7 @@ export class ProjectService {
       return await Promise.reject(e);
     }
 
-    if (!result) {
-      // || !result.type
+    if (!result || !result.type) {
       throw new NotFoundException(
         `Could not find project DEBUG: requestingUser ${session.userId} target ProjectId ${id}`
       );
