e9681d837d167cdb269da8a771f8738a7452fa6a
ibohalsinghkoijam <koijam@olivetech.net>
2020-06-24 19:22:30 +0530

    Breaking cypher query line

--- src/components/partnership/partnership.service.ts
+++ src/components/partnership/partnership.service.ts
@@ -497,17 +497,21 @@ export class PartnershipService {
 
       const query = `
         MATCH 
-        (token:Token {active: true, value: $token})
-        <-[:token {active: true}]-
-        (requestingUser:User {
-            active: true,
-            id: $requestingUserId
-        }),
-        (project:Project {id: $projectId, active: true, owningOrgId: $owningOrgId})
+          (token:Token {active: true, value: $token})
+          <-[:token {active: true}]-
+          (requestingUser:User {
+              active: true,
+              id: $requestingUserId
+          }),
+          (project:Project {id: $projectId, active: true, owningOrgId: $owningOrgId})
         -[:partnership]->(partnership:Partnership {active:true})
         WITH COUNT(partnership) as total, project, partnership
-        OPTIONAL MATCH (requestingUser)<-[:member { active: true }]-(sg:SecurityGroup { active: true })-[:permission { active: true }]->(canEditAgreementStatus:Permission { property: 'agreementStatus', active: true, edit: true })-[:baseNode { active: true }]->(partnership)-[:agreementStatus { active: true }]->(agreementStatus:Property { active: true })
-        OPTIONAL MATCH (requestingUser)<-[:member { active: true }]-(sg:SecurityGroup { active: true })-[:permission { active: true }]->(canReadAgreementStatus:Permission { property: 'agreementStatus', active: true, read: true })-[:baseNode { active: true }]->(partnership)-[:agreementStatus { active: true }]->(agreementStatus:Property { active: true })
+        OPTIONAL MATCH (requestingUser)<-[:member { active: true }]-(sg:SecurityGroup { active: true })-[:permission { active: true }]
+        ->(canEditAgreementStatus:Permission { property: 'agreementStatus', active: true, edit: true })
+        -[:baseNode { active: true }]->(partnership)-[:agreementStatus { active: true }]->(agreementStatus:Property { active: true })
+        OPTIONAL MATCH (requestingUser)<-[:member { active: true }]-(sg:SecurityGroup { active: true })-[:permission { active: true }]
+        ->(canReadAgreementStatus:Permission { property: 'agreementStatus', active: true, read: true })
+        -[:baseNode { active: true }]->(partnership)-[:agreementStatus { active: true }]->(agreementStatus:Property { active: true })
         RETURN total, partnership.id as id, agreementStatus.value as agreementStatus, partnership.createdAt as createdAt
         ORDER BY ${sort} ${order}
         SKIP $skip LIMIT $count
