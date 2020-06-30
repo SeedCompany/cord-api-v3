e27ae69be6b0dd949b80eb5404bf8fd8cd4780fd
Daniel Chen <daniel@olivetech.com>
2020-06-30 12:41:11 -0500

    unskip test

--- test/engagement.e2e-spec.ts
+++ test/engagement.e2e-spec.ts
@@ -206,7 +206,7 @@ describe('Engagement e2e', () => {
     expect(updated.paraTextRegistryId.value).toBe(updateParaTextRegistryId);
   });
 
-  it.skip('update internship engagement', async () => {
+  it('update internship engagement', async () => {
     const internshipEngagement = await createInternshipEngagement(app, {
       projectId: project.id,
       internId: intern.id,
