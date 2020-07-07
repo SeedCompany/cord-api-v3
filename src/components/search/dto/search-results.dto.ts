b006838440bef2836e2d954929feb1b772de83c4
Carson Full <carson_full@tsco.org>
2020-07-07 17:46:41 -0500

    Add more search helper types

--- src/components/search/dto/search-results.dto.ts
+++ src/components/search/dto/search-results.dto.ts
@@ -33,18 +33,22 @@ const searchableAbstracts = {
  * Everything below is based on objects above and should not need to be modified
  ******************************************************************************/
 
+export type SearchableMap = {
+  [K in keyof typeof searchable]: InstanceType<typeof searchable[K]>;
+};
+
+export const SearchResultTypes = Object.keys(searchable);
+
 // __typename is a GQL thing to identify type at runtime
 // It makes since to match this to not conflict with actual properties and
 // to match what GQL does on the consuming side.
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export type SearchItem<T, S> = T & { __typename: S };
 
-export type SearchResult = {
-  [K in keyof typeof searchable]: SearchItem<
-    InstanceType<typeof searchable[K]>,
-    K
-  >;
-}[keyof typeof searchable];
+export type SearchResultMap = {
+  [K in keyof SearchableMap]: SearchItem<SearchableMap[K], K>;
+};
+export type SearchResult = SearchResultMap[keyof SearchableMap];
 
 export const SearchResult = createUnionType({
   name: 'SearchResult',
