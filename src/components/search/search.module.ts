7c7d950cd43e6a16cf68b3a424a5d093d01a4f88
Carson Full <carson_full@tsco.org>
2020-07-07 17:51:27 -0500

    Idea for search implementation. Separates hydration from matching.

--- src/components/search/search.module.ts
+++ src/components/search/search.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { OrganizationModule } from '../organization/organization.module';
 import { SearchResolver } from './search.resolver';
 import { SearchService } from './search.service';
 
 @Module({
-  imports: [],
+  imports: [OrganizationModule],
   providers: [SearchResolver, SearchService],
   exports: [SearchService],
 })
