ba9c58a6e2e1a5a8dfe17517eb657027de79c2a2
ibohalsinghkoijam <koijam@olivetech.net>
2020-07-23 17:06:47 +0530

    removed the duplicate code

--- src/components/language/language.module.ts
+++ src/components/language/language.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { LocationModule } from '../location/location.module';
+import { ProjectModule } from '../project/project.module';
 import { EthnologueLanguageService } from './ethnologue-language';
 import { LanguageResolver } from './language.resolver';
 import { LanguageService } from './language.service';
 
 @Module({
-  imports: [LocationModule],
+  imports: [LocationModule, forwardRef(() => ProjectModule)],
   providers: [LanguageResolver, LanguageService, EthnologueLanguageService],
   exports: [LanguageService],
 })
