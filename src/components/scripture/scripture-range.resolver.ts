319d57a5428ce58a7c32a32586a9081ec5ce9b17
Carson Full <carson_full@tsco.org>
2020-07-24 15:32:02 -0500

    Stub ScriptureRange.label, totalVerses helper properties

---
+++ src/components/scripture/scripture-range.resolver.ts
@@ -0,0 +1,33 @@
+import { Int, Parent, ResolveField, Resolver } from '@nestjs/graphql';
+import { stripIndent } from 'common-tags';
+import { ScriptureRange } from './dto';
+
+@Resolver(ScriptureRange)
+export class ScriptureRangeResolver {
+  @ResolveField(() => String, {
+    description: stripIndent`
+      A human readable label for this range.
+
+      Examples:
+        - Matthew
+        - Matthew 1
+        - Matthew 1-4
+        - Matthew 1:4-20
+        - Matthew 1:4-3:21
+        - Matthew 1-John 2
+        - Matthew 1:3-John 2:4
+    `,
+  })
+  label(@Parent() _range: ScriptureRange): string {
+    // TODO
+    return ``;
+  }
+
+  @ResolveField(() => Int, {
+    description: 'The total number of verses in this scripture range',
+  })
+  totalVerses(@Parent() _range: ScriptureRange): number {
+    // TODO
+    return 0;
+  }
+}
