04fdd21e9192c4f401459e51b62b614547d4375d
Thomas Wright <>
2020-07-27 17:21:51 -0500

    W.I.P. fixed linting issues

--- src/components/budget/handlers/sync-budget-records-to-funding-partners.handler.ts
+++ src/components/budget/handlers/sync-budget-records-to-funding-partners.handler.ts
@@ -1,3 +1,5 @@
+import { InternalServerErrorException as ServerException } from '@nestjs/common';
+import { ISession } from '../../../common';
 import {
   DatabaseService,
   EventsHandler,
@@ -5,7 +7,7 @@ import {
   ILogger,
   Logger,
 } from '../../../core';
-import { PartnershipType, Partnership } from '../../partnership/dto';
+import { Partnership, PartnershipType } from '../../partnership/dto';
 import {
   PartnershipCreatedEvent,
   PartnershipDeletedEvent,
@@ -14,10 +16,6 @@ import {
 import { ProjectUpdatedEvent } from '../../project/events';
 import { BudgetService } from '../budget.service';
 import { BudgetRecord } from '../dto';
-import { ISession } from '../../../common';
-import {
-  InternalServerErrorException as ServerException,
-} from '@nestjs/common';
 
 type SubscribedEvent =
   | ProjectUpdatedEvent
@@ -37,80 +35,109 @@ export class SyncBudgetRecordsToFundingPartners
     private readonly db: DatabaseService,
     private readonly budgets: BudgetService,
     @Logger('budget:sync-partnerships') private readonly logger: ILogger
-  ) { }
+  ) {}
 
   // TODO: refactor into budget service or partnership service
   private async getBudgetIdForPartnership(partnership: Partnership) {
-    let budgets = await this.db.query()
-      .raw('MATCH (:Partnership { id: $partnershipId })-[:partnership]-(el)-[:budget]-(budget:Budget)', { partnershipId: partnership.id })
+    const budgets = await this.db
+      .query()
+      .raw(
+        'MATCH (:Partnership { id: $partnershipId })-[:partnership]-(el)-[:budget]-(budget:Budget)',
+        { partnershipId: partnership.id }
+      )
       .return('budget.id')
       .run();
 
     // TODO: determine if the business logic should allow for more then one budget per partnership
-    return budgets[0]["budget.id"]
+    return budgets[0]['budget.id'];
   }
 
-  private async getExistingBudgetRecordYearsForPartnership(partnership: Partnership, session: ISession) {
-    let budgetId = await this.getBudgetIdForPartnership(partnership);
-    let budget = await this.budgets.readOne(budgetId, session);
+  private async getExistingBudgetRecordYearsForPartnership(
+    partnership: Partnership,
+    session: ISession
+  ) {
+    const budgetId = await this.getBudgetIdForPartnership(partnership);
+    const budget = await this.budgets.readOne(budgetId, session);
 
-    return budget.records
+    return budget.records;
   }
 
   private calculateExpectedBudgetRecordYears(partnership: Partnership) {
-    let expectedBudgetRecordYears: number[]
-    if (!partnership.mouStart?.value ||
+    let expectedBudgetRecordYears: number[];
+    if (
+      !partnership.mouStart?.value ||
       !partnership.mouEnd?.value ||
       // TODO: decide whether to move the following funding and type checks to an earlier step.
       !partnership.types.value.includes(PartnershipType.Funding) || // Partnership is not funding, so do nothing
       !partnership.types?.value // Partnership Type is not provided, so do nothing.
     ) {
-
       expectedBudgetRecordYears = [];
     } else {
       // TODO: mous starting at midnight on Jan 1st might cause issues with UTC vs local issues here.
       // double check how year should be calculated
-      let mouStartYear = partnership.mouStart.value!.year;
-      let mouEndYear = partnership.mouEnd.value!.year;
+      const mouStartYear = partnership.mouStart.value.year;
+      const mouEndYear = partnership.mouEnd.value.year;
 
-      expectedBudgetRecordYears = Array.from({ length: (mouEndYear - mouStartYear) }, (v, k) => k + mouStartYear);
+      expectedBudgetRecordYears = Array.from(
+        { length: mouEndYear - mouStartYear },
+        (_v, k) => k + mouStartYear
+      );
     }
-    return expectedBudgetRecordYears
+    return expectedBudgetRecordYears;
   }
 
   private async getOrganizationIdByPartnership(partnership: Partnership) {
     // TODO: refactor so that partnership.organization.id returns a value instead of undefined.
     // this is currently a workaround because of the partnership.organization.id returning undefined.
-    let organization = await this.db.query()
-      .raw('MATCH (:Partnership { id: $partnershipId })-[:organization]-(organization:Organization)', { partnershipId: partnership.id })
+    const organization = await this.db
+      .query()
+      .raw(
+        'MATCH (:Partnership { id: $partnershipId })-[:organization]-(organization:Organization)',
+        { partnershipId: partnership.id }
+      )
       .return('organization.id')
       .run();
 
     // TODO: determine if the business logic should allow for more then one budget per partnership
-    return organization[0]["organization.id"]
+    return organization[0]['organization.id'];
   }
 
-  private async createMissingBudgetRecords(partnership: Partnership, session: ISession, existing: readonly BudgetRecord[], expected: number[]) {
-    let existingYears = existing.map(budgetRecord => budgetRecord.fiscalYear.value)
-    let budgetRecordsToBeCreated = expected.filter(year => !existingYears.includes(year))
-    let budgetId = await this.getBudgetIdForPartnership(partnership);
-    let organizationId = await this.getOrganizationIdByPartnership(partnership);
+  private async createMissingBudgetRecords(
+    partnership: Partnership,
+    session: ISession,
+    existing: readonly BudgetRecord[],
+    expected: number[]
+  ) {
+    const existingYears = existing.map(
+      (budgetRecord) => budgetRecord.fiscalYear.value
+    );
+    const budgetRecordsToBeCreated = expected.filter(
+      (year) => !existingYears.includes(year)
+    );
+    const budgetId = await this.getBudgetIdForPartnership(partnership);
+    const organizationId = await this.getOrganizationIdByPartnership(
+      partnership
+    );
 
-    const budgetRecords = budgetRecordsToBeCreated.map(year => ({
+    const budgetRecords = budgetRecordsToBeCreated.map((year) => ({
       budgetId: budgetId,
       organizationId: organizationId,
-      fiscalYear: year
+      fiscalYear: year,
     }));
     await Promise.all(
-      budgetRecords.map((record) =>
-        this.budgets.createRecord(record, session)
-      )
+      budgetRecords.map((record) => this.budgets.createRecord(record, session))
     );
   }
 
-  private async deactivateOldBudgetRecords(partnership: Partnership, session: ISession, existing: readonly BudgetRecord[], expected: number[]) {
+  private async deactivateOldBudgetRecords(
+    session: ISession,
+    existing: readonly BudgetRecord[],
+    expected: number[]
+  ) {
     // TODO: validate that record.fiscalYear.value cannot be undefined
-    let budgetRecoredsToBeDeactivated = existing.filter(record => !expected.includes(record.fiscalYear.value!))
+    const budgetRecoredsToBeDeactivated = existing.filter(
+      (record) => !expected.includes(record.fiscalYear.value!)
+    );
 
     await Promise.all(
       budgetRecoredsToBeDeactivated.map((record) =>
@@ -127,11 +154,25 @@ export class SyncBudgetRecordsToFundingPartners
 
     if (event instanceof (PartnershipUpdatedEvent || PartnershipCreatedEvent)) {
       try {
-        let expectedBudgetRecordYears = this.calculateExpectedBudgetRecordYears(event.partnership)
+        const expectedBudgetRecordYears = this.calculateExpectedBudgetRecordYears(
+          event.partnership
+        );
         // in cases where the expected budget years array is empty you could optimize by skipping some queries
-        let existingBudgetRecordYears = await this.getExistingBudgetRecordYearsForPartnership(event.partnership, event.session);
-        this.createMissingBudgetRecords(event.partnership, event.session, existingBudgetRecordYears, expectedBudgetRecordYears);
-        this.deactivateOldBudgetRecords(event.partnership, event.session, existingBudgetRecordYears, expectedBudgetRecordYears);
+        const existingBudgetRecordYears = await this.getExistingBudgetRecordYearsForPartnership(
+          event.partnership,
+          event.session
+        );
+        await this.createMissingBudgetRecords(
+          event.partnership,
+          event.session,
+          existingBudgetRecordYears,
+          expectedBudgetRecordYears
+        );
+        await this.deactivateOldBudgetRecords(
+          event.session,
+          existingBudgetRecordYears,
+          expectedBudgetRecordYears
+        );
       } catch (exception) {
         this.logger.error(`Could not synchronize budget records`, {
           userId: event.session.userId,
@@ -143,14 +184,17 @@ export class SyncBudgetRecordsToFundingPartners
 
     if (event instanceof PartnershipDeletedEvent) {
       try {
-        let expectedBudgetRecordYears: number[] = []
-        let existingBudgetRecordYears = await this.getExistingBudgetRecordYearsForPartnership(event.partnership, event.session);
-        this.deactivateOldBudgetRecords(event.partnership, event.session, existingBudgetRecordYears, expectedBudgetRecordYears);
+        const expectedBudgetRecordYears: number[] = [];
+        const existingBudgetRecordYears = await this.getExistingBudgetRecordYearsForPartnership(
+          event.partnership,
+          event.session
+        );
+        await this.deactivateOldBudgetRecords(
+          event.session,
+          existingBudgetRecordYears,
+          expectedBudgetRecordYears
+        );
       } catch (exception) {
-        this.logger.error(`Could not synchronize budget records`, {
-          userId: event.session.userId,
-          exception,
-        });
         throw new ServerException('Could not synchronize budget records');
       }
     }
