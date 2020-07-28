64c811b34fd54f2177279dff52856fac95f9d02a
Thomas Wright <>
2020-07-27 20:47:39 -0500

    added handler for projectUpdate events

--- src/components/budget/handlers/sync-budget-records-to-funding-partners.handler.ts
+++ src/components/budget/handlers/sync-budget-records-to-funding-partners.handler.ts
@@ -7,12 +7,14 @@ import {
   ILogger,
   Logger,
 } from '../../../core';
+import { PartnershipService } from '../../partnership';
 import { Partnership, PartnershipType } from '../../partnership/dto';
 import {
   PartnershipCreatedEvent,
   PartnershipDeletedEvent,
   PartnershipUpdatedEvent,
 } from '../../partnership/events';
+import { Project } from '../../project/dto';
 import { ProjectUpdatedEvent } from '../../project/events';
 import { BudgetService } from '../budget.service';
 import { BudgetRecord } from '../dto';
@@ -34,6 +36,7 @@ export class SyncBudgetRecordsToFundingPartners
   constructor(
     private readonly db: DatabaseService,
     private readonly budgets: BudgetService,
+    private readonly partnershipService: PartnershipService,
     @Logger('budget:sync-partnerships') private readonly logger: ILogger
   ) {}
 
@@ -52,6 +55,29 @@ export class SyncBudgetRecordsToFundingPartners
     return budgets[0]['budget.id'];
   }
 
+  // TODO: refactor into projectsService
+  private async getPartnershipsForProject(project: Project, session: ISession) {
+    const partnershipIds = await this.db
+      .query()
+      .raw(
+        'MATCH (:Project { id: $projectId })-[:partnership]-(partnership:Partnership)',
+        { projectId: project.id }
+      )
+      .return('partnership.id')
+      .run();
+
+    const partnerships = await Promise.all(
+      partnershipIds.map((partnershipId) =>
+        this.partnershipService.readOne(
+          partnershipId['partnership.id'],
+          session
+        )
+      )
+    );
+    return partnerships;
+  }
+
+  // TODO: refactor into partnershipsService
   private async getExistingBudgetRecordYearsForPartnership(
     partnership: Partnership,
     session: ISession
@@ -86,6 +112,7 @@ export class SyncBudgetRecordsToFundingPartners
     return expectedBudgetRecordYears;
   }
 
+  // TODO: refactor into partnershipsService
   private async getOrganizationIdByPartnership(partnership: Partnership) {
     // TODO: refactor so that partnership.organization.id returns a value instead of undefined.
     // this is currently a workaround because of the partnership.organization.id returning undefined.
@@ -146,44 +173,48 @@ export class SyncBudgetRecordsToFundingPartners
     );
   }
 
+  private async synchronizePartnershipBudgetRecords(
+    partnership: Partnership,
+    session: ISession
+  ) {
+    const expectedBudgetRecordYears = this.calculateExpectedBudgetRecordYears(
+      partnership
+    );
+    // in cases where the expected budget years array is empty you could optimize by skipping some queries
+    const existingBudgetRecordYears = await this.getExistingBudgetRecordYearsForPartnership(
+      partnership,
+      session
+    );
+    await this.createMissingBudgetRecords(
+      partnership,
+      session,
+      existingBudgetRecordYears,
+      expectedBudgetRecordYears
+    );
+    await this.deactivateOldBudgetRecords(
+      session,
+      existingBudgetRecordYears,
+      expectedBudgetRecordYears
+    );
+  }
+
   async handle(event: SubscribedEvent) {
     this.logger.debug('Partnership/Project mutation, syncing budget records', {
       ...event,
       event: event.constructor.name,
     });
 
-    if (event instanceof (PartnershipUpdatedEvent || PartnershipCreatedEvent)) {
-      try {
-        const expectedBudgetRecordYears = this.calculateExpectedBudgetRecordYears(
-          event.partnership
-        );
-        // in cases where the expected budget years array is empty you could optimize by skipping some queries
-        const existingBudgetRecordYears = await this.getExistingBudgetRecordYearsForPartnership(
+    try {
+      if (
+        event instanceof (PartnershipUpdatedEvent || PartnershipCreatedEvent)
+      ) {
+        await this.synchronizePartnershipBudgetRecords(
           event.partnership,
           event.session
         );
-        await this.createMissingBudgetRecords(
-          event.partnership,
-          event.session,
-          existingBudgetRecordYears,
-          expectedBudgetRecordYears
-        );
-        await this.deactivateOldBudgetRecords(
-          event.session,
-          existingBudgetRecordYears,
-          expectedBudgetRecordYears
-        );
-      } catch (exception) {
-        this.logger.error(`Could not synchronize budget records`, {
-          userId: event.session.userId,
-          exception,
-        });
-        throw new ServerException('Could not synchronize budget records');
       }
-    }
 
-    if (event instanceof PartnershipDeletedEvent) {
-      try {
+      if (event instanceof PartnershipDeletedEvent) {
         const expectedBudgetRecordYears: number[] = [];
         const existingBudgetRecordYears = await this.getExistingBudgetRecordYearsForPartnership(
           event.partnership,
@@ -194,14 +225,26 @@ export class SyncBudgetRecordsToFundingPartners
           existingBudgetRecordYears,
           expectedBudgetRecordYears
         );
-      } catch (exception) {
-        throw new ServerException('Could not synchronize budget records');
       }
-    }
 
-    if (event instanceof ProjectUpdatedEvent) {
-      // TODO: implement on project update
-      // updates will effect every partnership
+      if (event instanceof ProjectUpdatedEvent) {
+        const partnerships = await this.getPartnershipsForProject(
+          event.project,
+          event.session
+        );
+
+        await Promise.all(
+          partnerships.map((partnership) =>
+            this.synchronizePartnershipBudgetRecords(partnership, event.session)
+          )
+        );
+      }
+    } catch (exception) {
+      this.logger.error(`Could not synchronize budget records`, {
+        userId: event.session.userId,
+        exception,
+      });
+      throw new ServerException('Could not synchronize budget records');
     }
 
     // TODO: only continue if budget is pending
