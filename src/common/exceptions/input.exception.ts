b283a9a8bfa855a0d062c4cabe4d0e5ecc715170
Carson Full <carson_full@tsco.org>
2020-08-06 18:39:58 -0500

    Update InputException to be more flexible with it's creation

--- src/common/exceptions/input.exception.ts
+++ src/common/exceptions/input.exception.ts
@@ -1,10 +1,51 @@
 import { ClientException } from './exception';
 
+export type InputExceptionArgs =
+  | [Error?]
+  | [string, Error?]
+  | [string, string?, Error?];
+
 /**
- * Useful in services where complex logic is needed
- * to confirm user input/request is valid.
+ * Indicate the request cannot be completed because of requester has done
+ * something wrong. This is comparable to a BadRequestException.
  */
 export class InputException extends ClientException {
+  /**
+   * The field name in a.b.c nested notation from the Input DTO.
+   */
+  readonly field: string | undefined;
+
+  /**
+   * Create using a default message if you don't want to be more specific
+   *
+   * @example
+   * throw new InputException();
+   *
+   * @example
+   * catch (e) {
+   *   throw new InputException(e);
+   * }
+   *
+   * @param previous A previous error if any
+   */
+  constructor(previous?: Error);
+
+  /**
+   * Create with a custom message
+   *
+   * @example
+   * throw new InputException(`User's bio must be written in MLA format`);
+   *
+   * @example
+   * catch (e) {
+   *   throw new InputException(`User's bio must be written in MLA format`, e);
+   * }
+   *
+   * @param message A human (dev) readable message
+   * @param previous A previous error if any
+   */
+  constructor(message: string, previous?: Error);
+
   /**
    * @example
    * throw new InputException(
@@ -12,11 +53,50 @@ export class InputException extends ClientException {
    *   'user.bio'
    * );
    *
+   * @example
+   * catch (e) {
+   *   throw new InputException(
+   *     `User's bio must be written in MLA format`,
+   *     'user.bio',
+   *     e
+   *   );
+   * }
+   *
    * @param message A human (dev) readable message
    * @param field The field name in a.b.c nested notation from the Input DTO.
    * @param previous A previous error if any
    */
-  constructor(message: string, readonly field?: string, previous?: Error) {
+  constructor(message: string, field?: string, previous?: Error);
+
+  constructor(...args: InputExceptionArgs) {
+    const [message, field, previous] = InputException.parseArgs(
+      'Invalid request',
+      args
+    );
     super(message, previous);
+    this.field = field;
+  }
+
+  static parseArgs(
+    defaultMessage: string,
+    [one, two, three]: InputExceptionArgs
+  ) {
+    let message = defaultMessage;
+    let field;
+    let previous;
+    if (one instanceof Error) {
+      previous = one;
+    } else if (!one) {
+      // no args, do nothing
+    } else {
+      message = one;
+      if (two instanceof Error) {
+        previous = two;
+      } else {
+        field = two;
+        previous = three;
+      }
+    }
+    return [message, field, previous] as const;
   }
 }
--- src/common/exceptions/unauthorized.exception.ts
+++ src/common/exceptions/unauthorized.exception.ts
@@ -1,13 +1,69 @@
 import { HttpStatus } from '@nestjs/common';
-import { InputException } from './input.exception';
+import { Mutable } from 'type-fest';
+import { InputException, InputExceptionArgs } from './input.exception';
 
 /**
  * The requester has insufficient permission to do this operation.
  */
 export class UnauthorizedException extends InputException {
-  readonly status = HttpStatus.FORBIDDEN;
+  /**
+   * Use default message if you don't want to be more specific
+   *
+   * @example
+   * throw new UnauthorizedException();
+   *
+   * @example
+   * catch (e) {
+   *   throw new UnauthorizedException(e);
+   * }
+   *
+   * @param previous A previous error if any
+   */
+  constructor(previous?: Error);
 
-  constructor(message?: string, field?: string, previous?: Error) {
-    super(message ?? `Insufficient permission`, field, previous);
+  /**
+   * Create with a custom message
+   *
+   * @example
+   * throw new UnauthorizedException('You cannot do that');
+   *
+   * @example
+   * catch (e) {
+   *   throw new UnauthorizedException('You cannot do that', e);
+   * }
+   *
+   * @param message A human (dev) readable message
+   * @param previous A previous error if any
+   */
+  constructor(message: string, previous?: Error);
+
+  /**
+   * Indicate the requester does not have permission to take an action on a
+   * specific field.
+   *
+   * @example
+   * throw new UnauthorizedException(
+   *   `You cannot change the project's name`,
+   *   'project.name'
+   * );
+   *
+   * @example
+   * catch (e) {
+   *   throw new UnauthorizedException(
+   *     `You cannot change the project's name`,
+   *     'project.name',
+   *     e
+   *   );
+   * }
+   *
+   * @param message A human (dev) readable message
+   * @param field The field name in a.b.c nested notation from the Input DTO.
+   * @param previous A previous error if any
+   */
+  constructor(message: string, field: string, previous?: Error);
+
+  constructor(...args: InputExceptionArgs) {
+    super(...InputException.parseArgs(`Insufficient permission`, args));
+    (this as Mutable<this>).status = HttpStatus.FORBIDDEN;
   }
 }
