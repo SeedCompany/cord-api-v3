90ad0cbd4ae31145a49681060cf2a008f48448ff
Carson Full <carson_full@tsco.org>
2020-08-11 14:00:56 -0500

    Fix Transform decorator type
    Seems like these interfaces aren't available anymore

--- src/common/transform.decorator.ts
+++ src/common/transform.decorator.ts
@@ -1,12 +1,5 @@
 import { Transform as T } from 'class-transformer';
-import { TransformOptions } from 'class-transformer/metadata/ExposeExcludeOptions';
-import { TransformationType } from 'class-transformer/TransformOperationExecutor';
 
 export const Transform = T as (
-  transformFn: (
-    value: any,
-    obj: any,
-    transformationType: TransformationType
-  ) => any,
-  options?: TransformOptions
+  ...args: Parameters<typeof T>
 ) => PropertyDecorator;
