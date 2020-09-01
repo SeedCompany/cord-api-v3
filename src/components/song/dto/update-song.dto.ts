fbda0bc3e1a9d2c27eb8c77396a99f3fd5207525
zkhin <arandomname@gmail.com>
2020-09-02 00:28:48 +1000

    Validation error updating song with invalid id

--- src/components/song/dto/update-song.dto.ts
+++ src/components/song/dto/update-song.dto.ts
@@ -1,13 +1,13 @@
-import { Field, ID, InputType, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { Type } from 'class-transformer';
 import { ValidateNested } from 'class-validator';
-import { NameField } from '../../../common';
+import { IdField, NameField } from '../../../common';
 import { ScriptureRangeInput } from '../../scripture';
 import { Song } from './song.dto';
 
 @InputType()
 export abstract class UpdateSong {
-  @Field(() => ID)
+  @IdField()
   readonly id: string;
 
   @NameField({ nullable: true })
