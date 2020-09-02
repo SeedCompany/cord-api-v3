fec89ec90a4668e5258f8c2e4ea84c2503986b96
Carson Full <carson_full@tsco.org>
2020-09-02 14:12:41 -0500

    Remove user list filters

--- src/components/user/dto/list-users.dto.ts
+++ src/components/user/dto/list-users.dto.ts
@@ -1,36 +1,12 @@
-import { Field, InputType, ObjectType } from '@nestjs/graphql';
-import { Type } from 'class-transformer';
-import { ValidateNested } from 'class-validator';
+import { InputType, ObjectType } from '@nestjs/graphql';
 import { PaginatedList, SortablePaginationInput } from '../../../common';
 import { User } from './user.dto';
-
-@InputType()
-export abstract class UserFilters {
-  @Field({
-    description: 'Only users matching this first name',
-    nullable: true,
-  })
-  readonly displayFirstName?: string;
-
-  @Field({
-    description: 'Only users matching this last name',
-    nullable: true,
-  })
-  readonly displayLastName?: string;
-}
-
-const defaultFilters = {};
 
 @InputType()
 export class UserListInput extends SortablePaginationInput<keyof User>({
   defaultSort: 'id', // TODO How to sort on name?
 }) {
   static defaultVal = new UserListInput();
-
-  @Field({ nullable: true })
-  @Type(() => UserFilters)
-  @ValidateNested()
-  readonly filter: UserFilters = defaultFilters;
 }
 
 @ObjectType()
