df3048c13a1e3a000c62a02bac571705073661dd
Carson Full <carson_full@tsco.org>
2020-09-03 11:06:50 -0500

    Re-enable validation pipe in e2e tests

--- test/utility/create-app.ts
+++ test/utility/create-app.ts
@@ -1,12 +1,9 @@
 import { INestApplication } from '@nestjs/common';
-import { ApplicationConfig } from '@nestjs/core';
 import { GRAPHQL_MODULE_OPTIONS } from '@nestjs/graphql/dist/graphql.constants';
 import { Test } from '@nestjs/testing';
-import { remove } from 'lodash';
 import { AppModule } from '../../src/app.module';
 import { LogLevel } from '../../src/core/logger';
 import { LevelMatcher } from '../../src/core/logger/level-matcher';
-import { ValidationPipe } from '../../src/core/validation.pipe';
 import {
   createGraphqlClient,
   getGraphQLOptions,
@@ -26,11 +23,6 @@ export const createTestApp = async () => {
     .overrideProvider(LevelMatcher)
     .useValue(new LevelMatcher({}, LogLevel.ERROR))
     .compile();
-
-  // Remove ValidationPipe for tests because of failures
-  // @ts-expect-error ignore the fact that it's private
-  const appConfig: ApplicationConfig = moduleFixture.applicationConfig;
-  remove(appConfig.getGlobalPipes(), (p) => p instanceof ValidationPipe);
 
   const app = moduleFixture.createNestApplication<TestApp>();
   await app.init();
