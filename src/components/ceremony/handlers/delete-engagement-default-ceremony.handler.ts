43aa960b3cf622155e20b883d2210dbbe772bf5e
Carson Full <carson_full@tsco.org>
2020-09-12 13:53:21 -0500

    Fix query to set engagement-ceremony relationship to inactive on delete

--- src/components/ceremony/handlers/delete-engagement-default-ceremony.handler.ts
+++ src/components/ceremony/handlers/delete-engagement-default-ceremony.handler.ts
@@ -13,25 +13,29 @@ export class DetachEngagementRootDirectoryHandler
 
   async handle({ engagement, session }: EngagementDeletedEvent) {
     const ceremonyId = engagement?.ceremony?.value;
-
-    if (ceremonyId) {
-      await this.ceremonies.delete(ceremonyId, session);
-
-      this.db
-        .query()
-        .matchNode('engagement', 'Engagement', {
-          id: engagement.id,
-          active: true,
-        })
-        .matchNode('ceremony', 'Ceremony', { id: ceremonyId, active: true })
-        .match([
-          node('ceremony'),
-          relation('in', 'ceremonyRel', 'ceremony', {
-            active: true,
-          }),
-          node('engagement'),
-        ])
-        .delete('ceremonyRel');
+    if (!ceremonyId) {
+      return;
     }
+
+    await this.ceremonies.delete(ceremonyId, session);
+
+    await this.db
+      .query()
+      .matchNode('engagement', 'Engagement', {
+        id: engagement.id,
+        active: true,
+      })
+      .matchNode('ceremony', 'Ceremony', { id: ceremonyId, active: true })
+      .match([
+        node('ceremony'),
+        relation('in', 'ceremonyRel', 'ceremony', {
+          active: true,
+        }),
+        node('engagement'),
+      ])
+      .setValues({
+        'ceremonyRel.active': false,
+      })
+      .run();
   }
 }
