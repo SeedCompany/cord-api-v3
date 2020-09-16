4e0d59e44ac11519dfde8686475cb28aa3388141
Carson Full <carson_full@tsco.org>
2020-09-16 15:35:55 -0500

    Strip out undefined values since argon2 doesn't like them

--- src/components/authentication/authentication.service.ts
+++ src/components/authentication/authentication.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import * as argon2 from 'argon2';
 import { node, relation } from 'cypher-query-builder';
 import { sign, verify } from 'jsonwebtoken';
+import { pickBy } from 'lodash';
 import { DateTime } from 'luxon';
 import { Except } from 'type-fest';
 import {
@@ -420,11 +421,13 @@ export class AuthenticationService {
     }
   }
 
-  private get argon2Options(): Except<argon2.Options, 'raw'> {
-    return {
+  private get argon2Options() {
+    const options: Except<argon2.Options, 'raw'> = {
       secret: this.config.passwordSecret
         ? Buffer.from(this.config.passwordSecret, 'utf-8')
         : undefined,
     };
+    // argon doesn't like undefined values even though the types allow them
+    return pickBy(options, (v) => v !== undefined);
   }
 }
