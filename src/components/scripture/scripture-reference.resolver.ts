198f11d9a8e24d7a6e84b6c0044bb63933574d0d
zkhin <arandomname@gmail.com>
2020-09-23 04:02:12 +1000

    Drop book code

--- src/components/scripture/scripture-reference.resolver.ts
+++ src/components/scripture/scripture-reference.resolver.ts
@@ -1,14 +1,9 @@
 import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
-import { Book, Verse } from './books';
+import { Verse } from './books';
 import { ScriptureReference } from './dto';
 
 @Resolver(ScriptureReference)
 export class ScriptureReferenceResolver {
-  @ResolveField(() => String)
-  bookName(@Parent() ref: ScriptureReference): string {
-    return Book.fromRef(ref).name;
-  }
-
   @ResolveField(() => String)
   label(@Parent() ref: ScriptureReference): string {
     return Verse.fromRef(ref).label;
