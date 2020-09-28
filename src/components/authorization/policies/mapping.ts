fb2969464f37d857e31f51b87fcbeeaa3a0e78e5
Michael Marshall <michaelmarshall220@gmail.com>
2020-09-28 09:11:37 -0500

    policies loaded. permissions aren't attaching.

--- src/components/authorization/policies/mapping.ts
+++ src/components/authorization/policies/mapping.ts
@@ -56,47 +56,208 @@ type SecuredKeys<Dto extends Record<string, any>> = ConditionalKeys<
 >;
 
 export interface TypeToSecuredProps {
-  Budget: SecuredKeys<Budget> | 'status' | 'records';
-  BudgetRecord: SecuredKeys<BudgetRecord>;
-  Ceremony: SecuredKeys<Ceremony> | 'type';
-  Country: SecuredKeys<Country>;
+  Budget:
+    | SecuredKeys<Budget>
+    | 'status'
+    | 'budget'
+    | 'records'
+    | 'universalTemplateFile'
+    | 'organization';
+  BudgetRecord:
+    | SecuredKeys<BudgetRecord>
+    | 'fiscalYear'
+    | 'amount'
+    | 'record'
+    | 'partnership'
+    | 'organization';
+  Ceremony:
+    | SecuredKeys<Ceremony>
+    | 'planned'
+    | 'actualDate'
+    | 'estimatedDate'
+    | 'type'
+    | 'ceremony';
+  Country: SecuredKeys<Country> | 'name';
   Directory:
     | SecuredKeys<Directory>
     | 'name'
-    | 'type'
     | 'createdBy'
     | 'parent'
-    | 'description';
-  Education: SecuredKeys<Education>;
-  EthnologueLanguage: SecuredKeys<EthnologueLanguage>;
-  File: SecuredKeys<File>;
-  FileVersion: SecuredKeys<FileVersion>;
-  Film: SecuredKeys<Film>;
-  FundingAccount: SecuredKeys<FundingAccount>;
-  Language: SecuredKeys<Language>;
-  LanguageEngagement: SecuredKeys<LanguageEngagement>;
-  LiteracyMaterial: SecuredKeys<LiteracyMaterial>;
-  InternshipEngagement: SecuredKeys<InternshipEngagement>;
-  Organization: SecuredKeys<Organization>;
-  Partner: SecuredKeys<Partner>;
-  Partnership: SecuredKeys<Partnership>;
-  Product: SecuredKeys<Product>;
-
+    | 'rootDirectory';
+  Education:
+    | SecuredKeys<Education>
+    | 'degree'
+    | 'major'
+    | 'institution'
+    | 'education';
+  EthnologueLanguage:
+    | SecuredKeys<EthnologueLanguage>
+    | 'code'
+    | 'provisionalCode'
+    | 'name'
+    | 'population';
+  File: SecuredKeys<File> | 'name' | 'createdBy' | 'parent' | 'mimeType';
+  FileVersion:
+    | SecuredKeys<FileVersion>
+    | 'name'
+    | 'category'
+    | 'size'
+    | 'mimeType'
+    | 'createdBy'
+    | 'parent';
+  Film: SecuredKeys<Film> | 'name' | 'scriptureReferences' | 'produces';
+  FundingAccount: SecuredKeys<FundingAccount> | 'name';
+  Language:
+    | SecuredKeys<Language>
+    | 'name'
+    | 'displayName'
+    | 'sensitivity'
+    | 'isDialect'
+    | 'populationOverride'
+    | 'registryOfDialectsCode'
+    | 'leastOfThese'
+    | 'leastOfTheseReason'
+    | 'displayNamePronunciation'
+    | 'isSignLanguage'
+    | 'signLanguageCode'
+    | 'sponsorEstimatedEndDate'
+    | 'ethnologue';
+  LanguageEngagement:
+    | SecuredKeys<LanguageEngagement>
+    | 'status'
+    | 'completeDate'
+    | 'disbursementCompleteDate'
+    | 'communicationsCompleteDate'
+    | 'initialEndDate'
+    | 'startDate'
+    | 'endDate'
+    | 'startDateOverride'
+    | 'endDateOverride'
+    | 'updatedAt'
+    | 'lastReactivatedAt'
+    | 'lastSuspendedAt'
+    | 'modifiedAt'
+    | 'product'
+    | 'ceremony'
+    | 'language'
+    | 'paraTextRegistryId'
+    | 'projectEngagementTag'
+    | 'ceremonyPlanned'
+    | 'sentPrinting'
+    | 'lukePartnership'
+    | 'firstScripture';
+  LiteracyMaterial:
+    | SecuredKeys<LiteracyMaterial>
+    | 'name'
+    | 'scriptureReferences'
+    | 'produces';
+  InternshipEngagement:
+    | SecuredKeys<InternshipEngagement>
+    | 'status'
+    | 'completeDate'
+    | 'disbursementCompleteDate'
+    | 'communicationsCompleteDate'
+    | 'initialEndDate'
+    | 'startDate'
+    | 'endDate'
+    | 'startDateOverride'
+    | 'endDateOverride'
+    | 'updatedAt'
+    | 'lastReactivatedAt'
+    | 'lastSuspendedAt'
+    | 'modifiedAt'
+    | 'position'
+    | 'methodologies'
+    | 'intern'
+    | 'mentor'
+    | 'ceremony'
+    | 'countryOfOrigin'
+    | 'language'
+    | 'growthPlan';
+  Organization:
+    | SecuredKeys<Organization>
+    | 'name'
+    | 'organization'
+    | 'organizations';
+  Partner:
+    | SecuredKeys<Partner>
+    | 'pointOfContact'
+    | 'organization'
+    | 'type'
+    | 'financialReportingType';
+  Partnership:
+    | SecuredKeys<Partnership>
+    | 'mou'
+    | 'agreement'
+    | 'agreementStatus'
+    | 'mouStatus'
+    | 'mouStart'
+    | 'mouEnd'
+    | 'mouStartOverride'
+    | 'mouEndOverride'
+    | 'types'
+    | 'comment'
+    | 'partnership'
+    | 'organization'
+    | 'financialReportingType';
+  Product:
+    | SecuredKeys<Product>
+    | 'scriptureReferences'
+    | 'scriptureReferencesOverride'
+    | 'mediums'
+    | 'purposes'
+    | 'methodology'
+    | 'produces'
+    | 'engagement'
+    | 'isOverriding';
   Project:
     | SecuredKeys<Project>
+    | 'estimatedSubmission'
+    | 'step'
+    | 'name'
     | 'status'
-    | 'modifiedAt'
+    | 'deptId'
+    | 'mouStart'
+    | 'mouEnd'
     | 'rootDirectory'
     | 'member'
     | 'locations'
+    | 'engagement'
     | 'partnership'
-    | 'budget';
-  ProjectMember: SecuredKeys<ProjectMember> | 'modifiedAt';
-  Region: SecuredKeys<Region>;
-  Song: SecuredKeys<Song>;
-  Story: SecuredKeys<Story>;
-  Unavailability: SecuredKeys<Unavailability>;
-  User: SecuredKeys<User>;
-  Zone: SecuredKeys<Zone>;
+    | 'budget'
+    | 'modifiedAt'
+    | 'organization';
+  ProjectMember:
+    | SecuredKeys<ProjectMember>
+    | 'roles'
+    | 'member'
+    | 'user'
+    | 'modifiedAt';
+  Region: SecuredKeys<Region> | 'name' | 'director' | 'zone' | 'region';
+  Song: SecuredKeys<Song> | 'name' | 'scriptureReferences' | 'produces';
+  Story: SecuredKeys<Story> | 'name' | 'scriptureReferences' | 'produces';
+  Unavailability:
+    | SecuredKeys<Unavailability>
+    | 'description'
+    | 'start'
+    | 'end'
+    | 'unavailability';
+  User:
+    | SecuredKeys<User>
+    | 'realFirstName'
+    | 'realLastName'
+    | 'displayFirstName'
+    | 'displayLastName'
+    | 'email'
+    | 'education'
+    | 'organization'
+    | 'unavailablity'
+    | 'phone'
+    | 'timezone'
+    | 'bio'
+    | 'status'
+    | 'roles'
+    | 'title';
+  Zone: SecuredKeys<Zone> | 'name' | 'director' | 'zone';
   // Add more here as needed
 }
--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -37,7 +37,7 @@ describe('User e2e', () => {
     await app.close();
   });
 
-  it('read one user by id', async () => {
+  it.only('read one user by id', async () => {
     const fakeUser = generateRegisterInput();
 
     const user = await createUser(app, fakeUser);
