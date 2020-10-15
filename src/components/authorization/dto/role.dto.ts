1ac6f7faeb9e7753caad23dc118a54a3eb23008e
Michael Marshall <michaelmarshall220@gmail.com>
2020-10-14 23:07:24 -0500

    removed internal role enum

--- src/components/authorization/dto/role.dto.ts
+++ src/components/authorization/dto/role.dto.ts
@@ -36,7 +36,3 @@ registerEnumType(Role, { name: 'Role' });
   description: SecuredEnumList.descriptionFor('roles'),
 })
 export abstract class SecuredRoles extends SecuredEnumList(Role) {}
-
-export enum InternalRole {
-  ReadAll = 'ReadAll',
-}
--- src/components/authorization/model/role.model.db.ts
+++ src/components/authorization/model/role.model.db.ts
@@ -1,18 +1,18 @@
-import { InternalRole, Role } from '../dto';
+import { Role } from '../dto';
 import { Powers } from '../dto/powers';
 import { AnyBaseNode } from './any-base-node.type';
 import { DbBaseNodeGrant } from './base-node-grant.model.db';
 import { DbPermission } from './permission.model.db';
 
 export class DbRole {
-  name: Role | InternalRole;
+  name: Role;
   powers: Powers[];
   grants: Array<DbBaseNodeGrant<AnyBaseNode>>;
 
   constructor({
     ...rest
   }: {
-    name: string;
+    name: Role;
     powers: Powers[];
     grants: Array<DbBaseNodeGrant<AnyBaseNode>>;
   }) {
