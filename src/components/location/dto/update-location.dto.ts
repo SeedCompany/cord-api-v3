3d712951b967c0673878fe2a211e90405578a3b5
Carson Full <carson_full@tsco.org>
2020-10-20 15:16:15 -0500

    Update location.isoAlpha3 input handling
    - nulls are allowed now (and allowed through transformation)
    - codes are validated to actual list, so random 3 letter codes fail
    - changed field description to describe what it is instead of the validation failure message

--- src/components/location/dto/update-location.dto.ts
+++ src/components/location/dto/update-location.dto.ts
@@ -1,8 +1,12 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { Type } from 'class-transformer';
-import { IsAlpha, Length, ValidateNested } from 'class-validator';
-import { toUpper } from 'lodash';
-import { IdField, NameField, Sensitivity } from '../../../common';
+import { ValidateNested } from 'class-validator';
+import {
+  IdField,
+  ISO31661Alpha3,
+  NameField,
+  Sensitivity,
+} from '../../../common';
 import { Transform } from '../../../common/transform.decorator';
 import { LocationType } from './location-type.enum';
 import { Location } from './location.dto';
@@ -21,11 +25,13 @@ export abstract class UpdateLocation {
   @Field(() => Sensitivity, { nullable: true })
   readonly sensitivity?: Sensitivity;
 
-  @Field({ nullable: true, description: 'Must be 3 alpha characters' })
-  @IsAlpha()
-  @Length(3, 3)
-  @Transform(toUpper)
-  readonly isoAlpha3?: string;
+  @Field(() => String, {
+    nullable: true,
+    description: 'An ISO 3166-1 alpha-3 country code',
+  })
+  @ISO31661Alpha3()
+  @Transform((str) => (str ? str.toUpperCase() : null))
+  readonly isoAlpha3?: string | null;
 
   @IdField({ nullable: true })
   readonly fundingAccountId?: string;
