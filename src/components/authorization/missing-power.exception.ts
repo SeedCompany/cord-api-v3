7cb8b9533e3def7ce7ca91f9051d29020b3b5121
Carson Full <carson_full@tsco.org>
2020-10-21 09:52:48 -0500

    Created specific exception for missing power so we can handle it programmatically

---
+++ src/components/authorization/missing-power.exception.ts
@@ -0,0 +1,12 @@
+import { UnauthorizedException } from '../../common/exceptions';
+import { Powers } from './dto/powers';
+
+export class MissingPowerException extends UnauthorizedException {
+  constructor(
+    readonly power: Powers,
+    message = `Missing required power: ${power}`,
+    previous?: Error
+  ) {
+    super(message, previous);
+  }
+}
