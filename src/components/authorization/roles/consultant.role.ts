6e08f919f909daf048c6e7c202d27edb206a4740
Frank Ma <yurenfrankma@gmail.com>
2020-10-22 17:18:49 -0700

    feat(consultant.role.ts): add powers

--- src/components/authorization/roles/consultant.role.ts
+++ src/components/authorization/roles/consultant.role.ts
@@ -33,7 +33,19 @@ const write = true;
 
 export const Consultant = new DbRole({
   name: Role.Consultant,
-  powers: [...(Object.keys(Powers) as Powers[])],
+  powers: [
+    Powers.CreateDirectory,
+    Powers.CreateEducation,
+    Powers.CreateFile,
+    Powers.CreateFileVersion,
+    Powers.CreateFilm,
+    Powers.CreateLiteracyMaterial,
+    Powers.CreateProduct,
+    Powers.CreateSong,
+    Powers.CreateStory,
+    Powers.CreateUnavailability,
+    Powers.CreateUser,
+  ],
   grants: [
     new DbBaseNodeGrant<DbBudget>({
       __className: 'DbBudget',
