0373be79d0705eec371d8e65bf89cc0fb5d2e70f
Carson Full <carson_full@tsco.org>
2020-11-04 11:59:40 -0600

    Buffer log entries until winston can be setup

--- src/core/logger/logger.module.ts
+++ src/core/logger/logger.module.ts
@@ -6,6 +6,7 @@ import {
   Provider,
 } from '@nestjs/common';
 import { format, transports } from 'winston';
+import { BufferLoggerService } from './buffer-logger.service';
 import {
   colorize,
   exceptionInfo,
@@ -22,21 +23,29 @@ import { ILogger } from './logger.interface';
 import { NamedLoggerService } from './named-logger.service';
 import { NestLoggerAdapterService } from './nest-logger-adapter.service';
 import { NullLoggerService } from './null-logger.service';
+import { ProxyLoggerService } from './proxy-logger.service';
 import { LoggerOptions, WinstonLoggerService } from './winston-logger.service';
 
 @Global()
 @Module({
   providers: [
     LevelMatcherProvider,
+    NamedLoggerService,
+    BufferLoggerService,
+    ProxyLoggerService,
+    WinstonLoggerService,
     {
       provide: ILogger,
-      useClass: WinstonLoggerService,
+      useFactory: (proxy: ProxyLoggerService, buffer: BufferLoggerService) => {
+        proxy.setLogger(buffer);
+        return proxy;
+      },
+      inject: [ProxyLoggerService, BufferLoggerService],
     },
     {
       provide: NestLogger,
       useClass: NestLoggerAdapterService,
     },
-    NamedLoggerService,
   ],
 })
 export class LoggerModule {
@@ -80,6 +89,15 @@ export class LoggerModule {
       ],
       exports: namedLoggerProviders,
     };
+  }
+
+  constructor(
+    proxy: ProxyLoggerService,
+    winston: WinstonLoggerService,
+    buffer: BufferLoggerService
+  ) {
+    proxy.setLogger(winston);
+    buffer.flushTo(winston);
   }
 }
 
---
+++ src/core/logger/proxy-logger.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { AbstractLogger } from './abstract-logger';
+import { ILogger, LogEntry } from './logger.interface';
+
+/**
+ * Implementation of our Logger that forwards to another logger.
+ */
+@Injectable()
+export class ProxyLoggerService extends AbstractLogger {
+  private logger?: ILogger;
+
+  logEntry(entry: LogEntry): void {
+    if (this.logger) {
+      this.logger.log(entry);
+    }
+  }
+
+  setLogger(logger: ILogger) {
+    const prev = this.logger;
+    this.logger = logger;
+    return prev;
+  }
+}
