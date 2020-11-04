3587a9c63b569b3a027315a097a79daaed1dd090
Michael Marshall <michaelmarshall220@gmail.com>
2020-11-04 13:49:32 -0600

    don't give all roles to admin user. it should be necessary

--- src/components/admin/admin.service.ts
+++ src/components/admin/admin.service.ts
@@ -112,13 +112,15 @@ export class AdminService implements OnApplicationBootstrap {
   }
 
   async onApplicationBootstrap(): Promise<void> {
-    const finishing = this.db.runOnceUntilCompleteAfterConnecting(() => this.setupRootObjects());
+    const finishing = this.db.runOnceUntilCompleteAfterConnecting(() =>
+      this.setupRootObjects()
+    );
     // Wait for root object setup when running tests, else just let it run in
     // background and allow webserver to start.
     if (this.config.jest) {
       await finishing;
     } else {
-      finishing.catch(exception => {
+      finishing.catch((exception) => {
         this.logger.error('Failed to setup root objects', {
           exception,
         });
@@ -268,7 +270,7 @@ export class AdminService implements OnApplicationBootstrap {
         realLastName: 'root',
         phone: 'root',
         about: 'root',
-        roles: Object.values(Role),
+        roles: [Role.Administrator], // do not give root all the roles
       });
 
       // update config with new root admin id
