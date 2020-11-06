c36a509612cac36dabdede1ea77edd9ab97a1aad
Frank Ma <yurenfrankma@gmail.com>
2020-11-06 13:07:19 -0800

    feat(query.helpers.ts): refactor languageListFilter to use 1 where clause

--- src/components/language/query.helpers.ts
+++ src/components/language/query.helpers.ts
@@ -1,33 +1,32 @@
-import { inArray, Query } from 'cypher-query-builder';
+import { inArray, node, Query } from 'cypher-query-builder';
 import { propMatch } from '../project';
 import { LanguageFilters } from './dto';
 
 export function languageListFilter(query: Query, filter: LanguageFilters) {
-  query
-    .call((q) =>
-      filter.sensitivity
-        ? q
-            .match(propMatch('sensitivity'))
-            .where({ sensitivity: { value: inArray(filter.sensitivity) } })
-        : q
-    )
-    .call((q) =>
-      filter.leastOfThese
-        ? q
-            .match(propMatch('leastOfThese'))
-            .where({ leastOfThese: { value: true } })
-        : q
-    )
-    .call((q) =>
-      filter.isSignLanguage
-        ? q
-            .match(propMatch('isSignLanguage'))
-            .where({ isSignLanguage: { value: true } })
-        : q
-    )
-    .call((q) =>
-      filter.isDialect
-        ? q.match(propMatch('isDialect')).where({ isDialect: { value: true } })
-        : q
-    );
+  query.call((q) =>
+    filter.sensitivity ||
+    filter.leastOfThese ||
+    filter.isSignLanguage ||
+    filter.isDialect
+      ? q
+          .match(filter.sensitivity ? propMatch('sensitivity') : [node('node')])
+          .match(
+            filter.leastOfThese ? propMatch('leastOfThese') : [node('node')]
+          )
+          .match(
+            filter.isSignLanguage ? propMatch('isSignLanguage') : [node('node')]
+          )
+          .match(filter.isDialect ? propMatch('isDialect') : [node('node')])
+          .where({
+            ...(filter.sensitivity
+              ? { sensitivity: { value: inArray(filter.sensitivity) } }
+              : {}),
+            ...(filter.leastOfThese ? { leastOfThese: { value: true } } : {}),
+            ...(filter.isSignLanguage
+              ? { isSignLanguage: { value: true } }
+              : {}),
+            ...(filter.isDialect ? { isDialect: { value: true } } : {}),
+          })
+      : q
+  );
 }
