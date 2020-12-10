9f1da1c1775c5a2397fb6577226c574d9abf7563
Blake Krammes <blake_krammes@tsco.org>
2020-12-10 14:29:03 -0600

    Add parsing logic for P&P extraction

--- src/components/engagement/pnp-extractor.service.ts
+++ src/components/engagement/pnp-extractor.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import got from 'got';
-import { read } from 'xlsx';
+import { read, utils } from 'xlsx';
 import { Session } from '../../common';
 import { CreateDefinedFileVersionInput, FileService } from '../file';
 import { PnpData } from './dto';
@@ -15,11 +15,42 @@ export class PnpExtractor {
   ): Promise<PnpData> {
     const workbook = await this.downloadWorkbook(input, session);
 
-    // TODO
+    const pnp = read(workbook, { type: 'buffer' });
+    const progressSheet = utils.sheet_to_json(pnp.Sheets.Progress, {
+      header: 'A',
+      raw: false,
+    });
+    let progressPlanned = '';
+    let progressActual = '';
+    let variance = '';
+    progressSheet.forEach((row: any) => {
+      // new version (2020)
+      if (row.AL === 'Summary Info ====>') {
+        progressPlanned = row.AN;
+        progressActual = row.AO;
+        variance = row.AP;
+      }
+      // row.CK is current year. if current year is greater than 2019 grab data
+      else if (row.CK && parseInt(row.CK) >= 2019) {
+        progressPlanned = row.CT;
+        progressActual = row.CU;
+        variance = row.CV;
+        // 09 version
+        // BX is current year
+      } else if (row.BX && parseInt(row.BX) >= 2019) {
+        progressPlanned = row.BZ;
+        progressActual = row.CA;
+        variance = row.CB;
+      }
+    });
     return {
-      progressPlanned: 0.0,
-      progressActual: 0.0,
-      variance: 0.0,
+      progressPlanned: progressPlanned
+        ? parseFloat(progressPlanned.replace('%', ''))
+        : 0.0,
+      progressActual: progressActual
+        ? parseFloat(progressActual.replace('%', ''))
+        : 0.0,
+      variance: variance ? parseFloat(variance.replace('%', '')) : 0.0,
     };
   }
 
