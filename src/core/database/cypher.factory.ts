2db580b0230ae453f408d63809ea125677ea6e47
Carson Full <carson_full@tsco.org>
2021-01-14 17:03:28 -0600

    Refactor db transactions
    Previously we were wrapping the neo4j transaction object
    so it could work nicely with cypher query builder. However,
    this required that the transaction object would be explicitly
    passed around everywhere. This didn't fit well with the way we
    were abstracting methods, since the db was never passed in but
    rather injected into the class.
    
    The bigger problem is that we were not using the builtin retry
    logic. neo4j driver will retry logic if it encounters a transient
    error, like connection problems & constraint changes.
    It only uses this if the execution is inside of the
    `session.readTransaction/writeTransaction` callbacks.
    
    So solution. We are now exclusively using those to read/write transaction
    wrappers. We are storing the transaction object in a native `AsyncLocalStorage`.
    This allows us to fetch the current transaction regardless of which async
    context we are in (there can be multiple transactions at once for multiple requests).
    This allows us to patch the session runner to use the current transaction runner instead.
    This means that all the current db code will "just work" on the transaction when executed
    inside of a `runInTransaction` call.
    This also means that `runInTransaction` calls can be nested without creating multiple transactions.
    Only caveat is it requires Node 12.17+ or 14+, which shouldn't be a problem.

--- src/core/database/cypher.factory.ts
+++ src/core/database/cypher.factory.ts
@@ -1,7 +1,10 @@
 import { FactoryProvider } from '@nestjs/common/interfaces';
+import { AsyncLocalStorage } from 'async_hooks';
 import { stripIndent } from 'common-tags';
 import { Connection } from 'cypher-query-builder';
-import { Session } from 'neo4j-driver';
+import { Session, Transaction } from 'neo4j-driver';
+import QueryRunner from 'neo4j-driver/types/query-runner';
+import { Merge } from 'type-fest';
 import { ConfigService } from '..';
 import { jestSkipFileInExceptionSource } from '../jest-skip-source-file';
 import { ILogger, LoggerToken, LogLevel } from '../logger';
@@ -10,6 +13,15 @@ import { ParameterTransformer } from './parameter-transformer.service';
 import { MyTransformer } from './transformer';
 import './transaction'; // import our transaction augmentation
 import './query.overrides'; // import our query augmentation
+
+export type PatchedConnection = Merge<
+  Connection,
+  {
+    transactionStorage: AsyncLocalStorage<Transaction>;
+    logger: ILogger;
+    transformer: MyTransformer;
+  }
+>;
 
 export const CypherFactory: FactoryProvider<Connection> = {
   provide: Connection,
@@ -20,7 +32,8 @@ export const CypherFactory: FactoryProvider<Connection> = {
     driverLogger: ILogger
   ) => {
     const { url, username, password, driverConfig } = config.neo4j;
-    const conn = new Connection(
+    // @ts-expect-error yes we are patching the connection object
+    const conn: PatchedConnection = new Connection(
       url,
       { username, password },
       {
@@ -38,60 +51,56 @@ export const CypherFactory: FactoryProvider<Connection> = {
       }
     );
 
-    // wrap session.run calls to add logging
-    /* eslint-disable @typescript-eslint/unbound-method */
-    const origSession = conn.session;
-    conn.session = function (this: never) {
-      const session: Session | null = origSession.call(conn);
-      if (session) {
-        const origRun = session.run;
-        session.run = function (this: never, origStatement, parameters, conf) {
-          const statement = stripIndent(origStatement.slice(0, -1)) + ';';
-          logger.log(
-            (parameters?.logIt as LogLevel | undefined) ?? LogLevel.DEBUG,
-            'Executing query',
-            {
-              statement,
-              ...parameters,
-            }
-          );
+    // Holder for the current transaction using native async storage context.
+    conn.transactionStorage = new AsyncLocalStorage();
 
-          const params = parameters
-            ? parameterTransformer.transform(parameters)
-            : undefined;
-          const result = origRun.call(session, statement, params, conf);
-
-          const origSubscribe = result.subscribe;
-          result.subscribe = function (this: never, observer) {
-            if (observer.onError) {
-              const onError = observer.onError;
-              observer.onError = (e) => {
-                const patched = jestSkipFileInExceptionSource(e, __filename);
-                const mapped = createBetterError(patched);
-                if (isNeo4jError(mapped) && mapped.logProps) {
-                  logger.log(mapped.logProps);
-                }
-                onError(mapped);
-              };
-            }
-            origSubscribe.call(result, observer);
-          };
-
-          return result;
+    // Wrap session call to apply:
+    // - transparent transaction handling
+    // - query logging
+    // - parameter transformation
+    // - error transformation
+    const origSession = conn.session.bind(conn);
+    conn.session = function (this: PatchedConnection) {
+      const currentTransaction = this.transactionStorage.getStore();
+      if (currentTransaction) {
+        // Fake a "session", which is really only used as a QueryRunner,
+        // in order to forward methods to the current transaction.
+        // @ts-expect-error yes we are only supporting these two methods
+        const txSession: Session = {
+          run: wrapQueryRun(currentTransaction, logger, parameterTransformer),
+          close: async () => {
+            // No need to close anything when finishing the query inside of the
+            // transaction. The close will happen when the transaction work finishes.
+          },
         };
+        return txSession;
       }
-      /* eslint-enable @typescript-eslint/unbound-method */
+
+      const session: Session | null = origSession();
+      if (!session) {
+        return null;
+      }
+
+      session.run = wrapQueryRun(session, logger, parameterTransformer);
 
       return session;
     };
 
+    // Also tear down transaction storage on close.
+    const origClose = conn.close.bind(conn);
+    conn.close = async () => {
+      await origClose();
+      conn.transactionStorage.disable();
+    };
+
     // inject logger so transactions can use it
-    (conn as any).logger = logger;
+    conn.logger = logger;
 
     // Replace transformer with our own
-    (conn as any).transformer = new MyTransformer();
+    conn.transformer = new MyTransformer();
 
-    return conn;
+    // @ts-expect-error yes we are patching it back
+    return conn as Connection;
   },
   inject: [
     ConfigService,
@@ -99,4 +108,44 @@ export const CypherFactory: FactoryProvider<Connection> = {
     LoggerToken('database:query'),
     LoggerToken('database:driver'),
   ],
+};
+
+const wrapQueryRun = (
+  runner: QueryRunner,
+  logger: ILogger,
+  parameterTransformer: ParameterTransformer
+): QueryRunner['run'] => {
+  const origRun = runner.run.bind(runner);
+  return (origStatement, parameters) => {
+    const statement = stripIndent(origStatement.slice(0, -1)) + ';';
+    logger.log(
+      (parameters?.logIt as LogLevel | undefined) ?? LogLevel.DEBUG,
+      'Executing query',
+      {
+        statement,
+        ...parameters,
+      }
+    );
+
+    const params = parameters
+      ? parameterTransformer.transform(parameters)
+      : undefined;
+    const result = origRun(statement, params);
+
+    const origSubscribe = result.subscribe.bind(result);
+    result.subscribe = function (this: never, observer) {
+      const onError = observer.onError?.bind(observer);
+      observer.onError = (e) => {
+        const patched = jestSkipFileInExceptionSource(e, __filename);
+        const mapped = createBetterError(patched);
+        if (isNeo4jError(mapped) && mapped.logProps) {
+          logger.log(mapped.logProps);
+        }
+        onError?.(mapped);
+      };
+      origSubscribe(observer);
+    };
+
+    return result;
+  };
 };
--- src/core/database/transaction.ts
+++ src/core/database/transaction.ts
@@ -1,145 +1,54 @@
-/* eslint-disable @typescript-eslint/unbound-method */
+import { Connection } from 'cypher-query-builder';
+import { TransactionConfig } from 'neo4j-driver/types/session';
+import { PatchedConnection } from './cypher.factory';
 
-import { stripIndent } from 'common-tags';
-import { Connection, Query, Transformer } from 'cypher-query-builder';
-import { Dictionary } from 'lodash';
-import { Transaction as NeoTransaction, Session } from 'neo4j-driver';
-import { Observable } from 'rxjs';
-import { ILogger } from '../logger';
+export interface TransactionOptions extends TransactionConfig {
+  mode?: 'read' | 'write';
+}
 
 declare module 'cypher-query-builder/dist/typings/connection' {
   interface Connection {
-    /**
-     * Returns a new transaction.
-     */
-    transaction: () => Transaction;
-
     /**
      * This will create a transaction and call the given function with it.
      * The result of the function is returned.
      * Afterwards it will commit the transaction.
      * On any error the transaction will be rolled back.
+     *
+     * Normal db query methods inside of this function call will be applied
+     * to the transaction.
      */
-    withTransaction: <R>(inTx: (tx: Transaction) => Promise<R>) => Promise<R>;
+    runInTransaction: <R>(
+      inTx: (this: void) => Promise<R>,
+      options?: TransactionOptions
+    ) => Promise<R>;
   }
 }
 
-Connection.prototype.withTransaction = async function withTransaction<R>(
-  this: Connection,
-  inner: (tx: Transaction) => Promise<R>
+Connection.prototype.runInTransaction = async function withTransaction<R>(
+  this: PatchedConnection,
+  inner: (this: void) => Promise<R>,
+  options?: TransactionOptions
 ): Promise<R> {
-  const tx = this.transaction();
-  let res: R;
+  const outer = this.transactionStorage.getStore();
+  if (outer) {
+    return await inner();
+  }
+  const session = this.session();
+  if (!session) {
+    throw new Error('Cannot run query because connection is not open.');
+  }
+
+  const runTransaction =
+    options?.mode === 'read'
+      ? session.readTransaction.bind(session)
+      : session.writeTransaction.bind(session);
+
   try {
-    res = await inner(tx);
-  } catch (e) {
-    await tx.rollback();
-    throw e;
+    return await runTransaction(
+      (tx) => this.transactionStorage.run(tx, inner),
+      options
+    );
+  } finally {
+    await session.close();
   }
-  await tx.commit();
-  return res;
 };
-
-Connection.prototype.transaction = function transaction(this: Connection) {
-  return new Transaction(this, this.transformer, (this as any).logger);
-};
-
-/** A type matching what Query actually uses from its connection parameter */
-type QueryConnection = Pick<Connection, 'run' | 'stream'>;
-
-export class Transaction implements QueryConnection {
-  private session: Session | null;
-  private wrapped: NeoTransaction;
-
-  constructor(
-    private readonly connection: Connection,
-    private readonly transformer: Transformer,
-    private readonly logger: ILogger
-  ) {}
-
-  query(): Query {
-    // Query only calls `run` and `stream` from connection
-    return new Query((this as any) as Connection);
-  }
-
-  /** Commit the transaction */
-  async commit(): Promise<void> {
-    if (!this.wrapped) {
-      return;
-    }
-    try {
-      await this.wrapped.commit();
-    } finally {
-      await this.close();
-    }
-  }
-
-  /** Rollback the transaction */
-  async rollback(): Promise<void> {
-    if (!this.wrapped) {
-      return;
-    }
-    try {
-      await this.wrapped.rollback();
-    } finally {
-      await this.close();
-    }
-  }
-
-  /**
-   * Close this transaction's session.
-   * This will be automatically called on commit/rollback.
-   */
-  async close() {
-    if (this.session) {
-      await this.session?.close();
-      this.session = null;
-    }
-  }
-
-  private begin(): void {
-    if (this.wrapped) {
-      // Keep previous transaction
-      // trying to run a query on a finalized transaction should throw an error.
-      return;
-    }
-    const session = this.connection.session();
-    if (!session) {
-      throw new Error('Could not open session: connection is not open.');
-    }
-    this.session = session;
-    this.wrapped = this.session.beginTransaction();
-  }
-
-  /**
-   * Runs the provided query on this transaction, regardless of which connection
-   * the query was created from (if any).
-   *
-   * @see {Connection.run} for more details
-   */
-  async run<R = any>(query: Query): Promise<Array<Dictionary<R>>> {
-    if (query.getClauses().length === 0) {
-      throw new Error('Cannot run query: no clauses attached to the query.');
-    }
-
-    this.begin();
-
-    const { query: q, params } = query.buildQueryObject();
-    const statement = stripIndent(q);
-    this.logger.debug('\n' + statement, params);
-
-    const result = await this.wrapped.run(statement, params);
-
-    return this.transformer.transformRecords(result.records);
-  }
-
-  /**
-   * Streaming is not supported on transactions
-   * This observable will immediately emit an error.
-   */
-  stream<R = any>(_query: Query): Observable<Dictionary<R>> {
-    return new Observable((subscriber: { error: (e: Error) => void }) => {
-      subscriber.error(new Error('Transactions cannot be streamed.'));
-    });
-  }
-}
