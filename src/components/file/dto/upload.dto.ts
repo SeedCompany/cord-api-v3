9004df3e08d1fd5623c1e1f4ebf5486cd9e76c38
Carson Full <carson_full@tsco.org>
2021-02-02 10:27:19 -0600

    Allow mime type to be overridden on defined files as well

--- src/components/file/dto/upload.dto.ts
+++ src/components/file/dto/upload.dto.ts
@@ -32,7 +32,7 @@ export abstract class CreateFileVersionInput {
 
   @Field({
     description:
-      'Override the mime type of the file. Default pulls mime typed defined on uploaded file',
+      'Override the mime type of the file. Default pulls mime type defined on uploaded file',
     nullable: true,
   })
   readonly mimeType?: string;
@@ -49,6 +49,13 @@ export abstract class CreateDefinedFileVersionInput {
     description: 'An optional name. Defaults to file name.',
   })
   readonly name?: string;
+
+  @Field({
+    description:
+      'Override the mime type of the file version. Default pulls mime type defined on uploaded file',
+    nullable: true,
+  })
+  readonly mimeType?: string;
 }
 
 @InputType()
