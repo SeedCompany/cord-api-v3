32089f047bee0063dc94f471d29c21cc7baa3724
Carson Full <carson_full@tsco.org>
2021-02-09 18:00:17 -0600

    Separate time data out of workbook parsing

--- src/components/engagement/pnp-extractor.service.ts
+++ src/components/engagement/pnp-extractor.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { DateTime } from 'luxon';
-import { read, utils } from 'xlsx';
+import { read, utils, WorkBook } from 'xlsx';
 import { Session } from '../../common';
 import { ILogger, Logger } from '../../core';
 import {
@@ -17,7 +17,89 @@ export class PnpExtractor {
     @Logger('pnp:extractor') private readonly logger: ILogger
   ) {}
 
-  private getYearAndQuarter(fileName: string) {
+  async extract(
+    input: CreateDefinedFileVersionInput,
+    session: Session
+  ): Promise<PnpData | null> {
+    const file = await this.files.getFileVersion(input.uploadId, session);
+    const pnp = await this.downloadWorkbook(file);
+    const workbookData = this.parseWorkbook(pnp, file);
+    if (!workbookData) {
+      return null;
+    }
+    const timeData = this.parseYearAndQuarter(file.name);
+    return { ...workbookData, ...timeData };
+  }
+
+  private parseWorkbook(pnp: WorkBook, file: FileVersion) {
+    // new standard 2020 version has new sheet "Harvest" which isolates relevant progress data
+    const sheet = pnp.Sheets.Harvest ?? pnp.Sheets.Progress;
+    if (!sheet) {
+      this.logger.warning('Unable to parse pnp file', {
+        name: file.name,
+        id: file.id,
+      });
+      return null;
+    }
+
+    const rows = utils.sheet_to_json<any>(sheet, {
+      header: 'A',
+      raw: false,
+    });
+    try {
+      for (const row of rows) {
+        // new standard 11/09/2020
+        if (pnp.Sheets.Harvest && /\d/.test(row?.AC)) {
+          return this.parseRawData(row.AC, row?.AD, row?.AE);
+        }
+        // other 2020 version
+        else if (!pnp.Sheets.Harvest && row?.AL === 'Summary Info ====>') {
+          return this.parseRawData(row?.AN, row?.AO, row?.AP);
+        }
+        // row.CK is current year. if current year is greater than 2019 grab data
+        else if (!pnp.Sheets.Harvest && parseInt(row?.CK) >= 2019) {
+          return this.parseRawData(row?.CT, row?.CU, row?.CV);
+          // 09 version
+          // BX is current year
+        } else if (!pnp.Sheets.Harvest && parseInt(row?.BX) >= 2019) {
+          return this.parseRawData(row?.BZ, row?.CA, row?.CB);
+        }
+      }
+    } catch (e) {
+      this.logger.warning('Unable to parse summary data in pnp file', {
+        name: file.name,
+        id: file.id,
+        exception: e,
+      });
+      return null;
+    }
+
+    this.logger.warning('Unable to find summary data in pnp file', {
+      name: file.name,
+      id: file.id,
+    });
+    return null;
+  }
+
+  private async downloadWorkbook(file: FileVersion) {
+    const buffer = await this.files.downloadFileVersion(file.id);
+    return read(buffer, { type: 'buffer' });
+  }
+
+  private parseRawData(
+    progressPlanned: string,
+    progressActual: string,
+    variance: string
+  ) {
+    if (!progressPlanned || !progressActual || !variance) return null;
+    return {
+      progressPlanned: parsePercent(progressPlanned),
+      progressActual: parsePercent(progressActual),
+      variance: parsePercent(variance),
+    };
+  }
+
+  private parseYearAndQuarter(fileName: string) {
     // I don't want to mess with this since there are some files names with a range
     // i.e. fy18-20. I think this naming has become outmoded anyway.
     const fyReg = /fy19|fy20|fy21/i;
@@ -47,103 +129,6 @@ export class PnpExtractor {
       : 0;
 
     return { year, quarter };
-  }
-
-  async extract(
-    input: CreateDefinedFileVersionInput,
-    session: Session
-  ): Promise<PnpData | null> {
-    const file = await this.files.getFileVersion(input.uploadId, session);
-    const pnp = await this.downloadWorkbook(file);
-    const { year, quarter } = this.getYearAndQuarter(file.name);
-
-    // new standard 2020 version has new sheet "Harvest" which isolates relevant progress data
-    const sheet = pnp.Sheets.Harvest ?? pnp.Sheets.Progress;
-    if (!sheet) {
-      this.logger.warning('Unable to parse pnp file', {
-        name: file.name,
-        id: file.id,
-      });
-      return null;
-    }
-
-    const rows = utils.sheet_to_json<any>(sheet, {
-      header: 'A',
-      raw: false,
-    });
-    try {
-      for (const row of rows) {
-        // new standard 11/09/2020
-        if (pnp.Sheets.Harvest && /\d/.test(row?.AC)) {
-          const parsedData = this.parseRawData(row.AC, row?.AD, row?.AE);
-          return parsedData
-            ? {
-                ...parsedData,
-                year,
-                quarter,
-              }
-            : null;
-        }
-        // other 2020 version
-        else if (!pnp.Sheets.Harvest && row?.AL === 'Summary Info ====>') {
-          const parsedData = this.parseRawData(row?.AN, row?.AO, row?.AP);
-          return parsedData ? { ...parsedData, year, quarter } : null;
-        }
-        // row.CK is current year. if current year is greater than 2019 grab data
-        else if (!pnp.Sheets.Harvest && parseInt(row?.CK) >= 2019) {
-          const parsedData = this.parseRawData(row?.CT, row?.CU, row?.CV);
-          return parsedData
-            ? {
-                ...parsedData,
-                year,
-                quarter,
-              }
-            : null;
-          // 09 version
-          // BX is current year
-        } else if (!pnp.Sheets.Harvest && parseInt(row?.BX) >= 2019) {
-          const parsedData = this.parseRawData(row?.BZ, row?.CA, row?.CB);
-          return parsedData
-            ? {
-                ...parsedData,
-                year,
-                quarter,
-              }
-            : null;
-        }
-      }
-    } catch (e) {
-      this.logger.warning('Unable to parse summary data in pnp file', {
-        name: file.name,
-        id: file.id,
-        exception: e,
-      });
-      return null;
-    }
-
-    this.logger.warning('Unable to find summary data in pnp file', {
-      name: file.name,
-      id: file.id,
-    });
-    return null;
-  }
-
-  private async downloadWorkbook(file: FileVersion) {
-    const buffer = await this.files.downloadFileVersion(file.id);
-    return read(buffer, { type: 'buffer' });
-  }
-
-  private parseRawData(
-    progressPlanned: string,
-    progressActual: string,
-    variance: string
-  ): Omit<PnpData, 'year' | 'quarter'> | null {
-    if (!progressPlanned || !progressActual || !variance) return null;
-    return {
-      progressPlanned: parsePercent(progressPlanned),
-      progressActual: parsePercent(progressActual),
-      variance: parsePercent(variance),
-    };
   }
 }
 
