65e7b535527b81c0b5b68ddeb79ac18b1cb59afd
E10001204 <sorrelle04@gmail.com>
2021-02-10 17:04:48 -0600

    fix error

--- src/common/validators/index.ts
+++ src/common/validators/index.ts
@@ -2,4 +2,3 @@ export * from './email.validator';
 export * from './iana-timezone.validator';
 export * from './iso-3166-1-alpha-3.validator';
 export * from './short-id.validator';
-export * from './each-item-is-not-blank.validator';
