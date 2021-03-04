e4d3b9a4a5cf64525f72e8ac49350a9e17d0ff46
Carson Full <carson_full@tsco.org>
2021-03-03 18:34:01 -0600

    Remove addRolesToBetaTesters

--- src/components/admin/admin.module.ts
+++ src/components/admin/admin.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthorizationModule } from '../authorization/authorization.module';
-import { OrganizationModule } from '../organization/organization.module';
-import { AdminResolver } from './admin.resolver';
 import { AdminService } from './admin.service';
 
 @Module({
-  imports: [OrganizationModule, AuthorizationModule],
-  providers: [AdminService, AdminResolver],
+  imports: [AuthorizationModule],
+  providers: [AdminService],
   exports: [AdminService],
 })
 export class AdminModule {}
