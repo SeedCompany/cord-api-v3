55888652edae5f0d87372e063c884423bb7a5f7a
Carson Full <carson_full@tsco.org>
2021-03-10 12:43:31 -0600

    Refactor the after transaction logic to a separate method

--- src/components/authorization/authorization.service.ts
+++ src/components/authorization/authorization.service.ts
@@ -50,26 +50,33 @@ export class AuthorizationService {
     baseNodeId: string,
     creatorUserId: string
   ) {
-    const label = baseNodeObj.__className.substring(2);
+    await this.afterTransaction(async () => {
+      await this.db
+        .query()
+        .raw(
+          `CALL cord.processNewBaseNode($baseNodeId, $label, $creatorUserId)`,
+          {
+            baseNodeId,
+            label: baseNodeObj.__className.substring(2),
+            creatorUserId,
+          }
+        )
+        .run();
+    });
+  }
+
+  /**
+   * Run code after current transaction finishes, if there is one.
+   * This is a hack to allow our procedure and apoc.periodic.iterate to work
+   * without dead-locking. They use separate transactions so they need the
+   * resource being modified to be unlocked (which happens after the
+   * transaction commits/finishes).
+   */
+  private async afterTransaction(fn: () => Promise<void>) {
     const process = async () => {
-      await retry(
-        async () => {
-          await this.db
-            .query()
-            .raw(
-              `CALL cord.processNewBaseNode($baseNodeId, $label, $creatorUserId)`,
-              {
-                baseNodeId,
-                label,
-                creatorUserId,
-              }
-            )
-            .run();
-        },
-        {
-          retries: 3,
-        }
-      );
+      await retry(fn, {
+        retries: 3,
+      });
     };
 
     const tx = this.dbConn.currentTransaction;
