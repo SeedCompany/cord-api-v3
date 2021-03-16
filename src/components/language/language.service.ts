15c3a2afcb50c06b16f32ee5071f0da835ab8a16
Carson Full <carson_full@tsco.org>
2021-03-16 09:52:40 -0500

    Fix language sorting by id & createdAt

--- src/components/language/language.service.ts
+++ src/components/language/language.service.ts
@@ -500,17 +500,19 @@ export class LanguageService {
         input,
         this.securedProperties,
         (q, sort, order) =>
-          q
-            .match([
-              node('node'),
-              relation('out', '', sort, { active: true }),
-              node('prop', 'Property'),
-            ])
-            .with([
-              '*',
-              ...(input.sort === 'sensitivity' ? [sensitivityCase] : []),
-            ])
-            .orderBy(sortBy, order)
+          ['id', 'createdAt'].includes(sort)
+            ? q.with('*').orderBy(`node.${sort}`, order)
+            : q
+                .match([
+                  node('node'),
+                  relation('out', '', sort, { active: true }),
+                  node('prop', 'Property'),
+                ])
+                .with([
+                  '*',
+                  ...(input.sort === 'sensitivity' ? [sensitivityCase] : []),
+                ])
+                .orderBy(sortBy, order)
       );
 
     return await runListQuery(query, input, (id) => this.readOne(id, session));
