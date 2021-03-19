98594b9939f4c346085bb4ff29563713a8e9bde8
Carson Full <carson_full@tsco.org>
2021-03-19 18:30:15 -0500

    Add ID to Pinnable interface
    This allows the toggle mutation to work without needing concrete types

--- src/components/pin/dto/pinnable.dto.ts
+++ src/components/pin/dto/pinnable.dto.ts
@@ -1,9 +1,13 @@
 import { Field, InterfaceType } from '@nestjs/graphql';
+import { IdField } from '../../../common';
 
 @InterfaceType({
   description: 'An item that can be pinned',
 })
 export class Pinnable {
+  @IdField()
+  readonly id: string;
+
   @Field({
     description: 'Does the requesting user have this pinned?',
   })
