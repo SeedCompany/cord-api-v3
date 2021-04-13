44722bdef7d1e3c34df2c4a820b2ee469f0ada6c
Carson Full <carson_full@tsco.org>
2021-04-12 23:07:12 -0500

    Move deleteBaseNode logic out of query execution so that other queries can use that logic

--- src/core/database/database.service.ts
+++ src/core/database/database.service.ts
@@ -31,6 +31,7 @@ import { ILogger, Logger, ServiceUnavailableError, UniquenessError } from '..';
 import { AbortError, retry, RetryOptions } from '../../common/retry';
 import { ConfigService } from '../config/config.service';
 import { DbChanges, getChanges } from './changes';
+import { deleteBaseNode } from './query';
 import { determineSortValue } from './query.helpers';
 import { hasMore } from './results';
 import { Transactional } from './transactional.decorator';
@@ -557,56 +558,10 @@ export class DatabaseService {
   }: {
     object: TObject;
   }) {
-    const deletedAt = DateTime.local();
     const query = this.db
       .query()
-      .match([
-        node('baseNode', { id: object.id }),
-        /**
-         in this case we want to set Deleted_ labels for all properties
-         including active = false
-         deleteProperties does this, but deletes from before that was changed only prefixed
-         unique property labels
-        */
-        relation('out', ''),
-        node('propertyNode', 'Property'),
-      ])
-      // Mark any parent base node relationships (pointing to the base node) as active = false.
-      .optionalMatch([
-        node('baseNode'),
-        relation('in', 'baseNodeRel'),
-        node('', 'BaseNode'),
-      ])
-      .setValues({
-        'baseNode.deletedAt': deletedAt,
-        'baseNodeRel.active': false,
-      })
-      /**
-       if we set anything on property nodes or property relationships in the query above (as was done previously)
-       we need to distinct propertyNode to avoid collecting and labeling each propertyNode more than once
-      */
-      .with('[baseNode] + collect(propertyNode) as nodeList')
-      /**
-       check if labels already have the "Deleted_" prefix to avoid "Deleted_Deleted_"
-       yielding a node from the label procedures is necessary I believe
-       they're not used in rest of the query and are aliased to avoid colliding with the unwound "node" alias
-      */
-      .raw(
-        `
-        unwind nodeList as node
-        with node,
-        reduce(
-          deletedLabels = [], label in labels(node) |
-            case
-              when label starts with "Deleted_" then deletedLabels + label
-              else deletedLabels + ("Deleted_" + label)
-            end
-        ) as deletedLabels
-        call apoc.create.removeLabels(node, labels(node)) yield node as nodeRemoved
-        with node, deletedLabels
-        call apoc.create.addLabels(node, deletedLabels) yield node as nodeAdded
-      `
-      )
+      .matchNode('baseNode', { id: object.id })
+      .call(deleteBaseNode)
       .return('*');
     await query.run();
   }
