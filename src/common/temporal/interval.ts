ab6bdcf06d79838ac38487a0c5392c8307652d1b
Carson Full <carson_full@tsco.org>
2021-04-19 17:37:52 -0500

    Add Interval.compare

--- src/common/temporal/interval.ts
+++ src/common/temporal/interval.ts
@@ -1,4 +1,5 @@
 import { DateTime, DurationUnit, Interval } from 'luxon';
+import { Mutable } from 'type-fest';
 import { inspect } from 'util';
 
 /* eslint-disable @typescript-eslint/method-signature-style */
@@ -10,6 +11,13 @@ declare module 'luxon/src/interval' {
      * Expand this interval to the full duration unit given
      */
     expandToFull(unit: DurationUnit): Interval;
+  }
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Interval {
+    export const compare: (
+      previous: Interval | null,
+      updated: Interval | null
+    ) => Record<'additions' | 'removals', Interval[]>;
   }
 }
 /* eslint-enable @typescript-eslint/method-signature-style */
@@ -25,4 +33,13 @@ Interval.prototype.expandToFull = function (
   unit: DurationUnit
 ) {
   return Interval.fromDateTimes(this.start.startOf(unit), this.end.endOf(unit));
+};
+
+(Interval as Mutable<typeof Interval>).compare = (
+  prev: Interval | null,
+  now: Interval | null
+) => {
+  const removals = !prev ? [] : !now ? [prev] : prev.difference(now);
+  const additions = !now ? [] : !prev ? [now] : now.difference(prev);
+  return { removals, additions };
 };
