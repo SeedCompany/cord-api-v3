5ecd027360d14d271e6802912ee15c0c66ca13d7
questreal <vivekvarma15516@gmail.com>
2021-05-12 20:27:04 +0530

    refactor literacy-material.service

--- src/components/literacy-material/literacy-material.module.ts
+++ src/components/literacy-material/literacy-material.module.ts
@@ -1,12 +1,17 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { AuthorizationModule } from '../authorization/authorization.module';
 import { ScriptureModule } from '../scripture/scripture.module';
+import { LiteracyMaterialRepository } from './literacy-material.repository';
 import { LiteracyMaterialResolver } from './literacy-material.resolver';
 import { LiteracyMaterialService } from './literacy-material.service';
 
 @Module({
   imports: [forwardRef(() => AuthorizationModule), ScriptureModule],
-  providers: [LiteracyMaterialResolver, LiteracyMaterialService],
-  exports: [LiteracyMaterialService],
+  providers: [
+    LiteracyMaterialResolver,
+    LiteracyMaterialService,
+    LiteracyMaterialRepository,
+  ],
+  exports: [LiteracyMaterialService, LiteracyMaterialRepository],
 })
 export class LiteracyMaterialModule {}
