59ec3ddb159472d8483303fc9f00b8431a678f4b
questreal <vivekvarma15516@gmail.com>
2021-05-14 04:58:18 +0530

    refactor post.service

--- src/components/post/post.module.ts
+++ src/components/post/post.module.ts
@@ -3,13 +3,14 @@ import { AuthorizationModule } from '../authorization/authorization.module';
 import { UserModule } from '../user/user.module';
 import { PostResolver } from './post.resolver';
 import { PostService } from './post.service';
+import { PostRepository } from './post.repository';
 
 @Module({
   imports: [
     forwardRef(() => UserModule),
     forwardRef(() => AuthorizationModule),
   ],
-  providers: [PostResolver, PostService],
-  exports: [PostService],
+  providers: [PostResolver, PostService, PostRepository],
+  exports: [PostService, PostRepository],
 })
 export class PostModule {}
