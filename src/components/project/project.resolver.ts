63e3958c9363290b8f81d8e4b37d03adf286eb5b
Carson Full <carson_full@tsco.org>
2021-06-14 11:14:46 -0500

    Rename Project.changesets to planChanges as it's a list of concrete PlanChanges

--- src/components/project/project.resolver.ts
+++ src/components/project/project.resolver.ts
@@ -108,7 +108,7 @@ export class ProjectResolver {
   }
 
   @ResolveField(() => SecuredChangesetList)
-  async changesets(
+  async planChanges(
     @AnonSession() session: Session,
     @Parent() project: Project,
     @Args({
