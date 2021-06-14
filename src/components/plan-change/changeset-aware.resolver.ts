e2330eb91ea8c2f5544a01433303779d96a0b213
Carson Full <carson_full@tsco.org>
2021-06-14 11:04:53 -0500

    Add Changeset interface and updated ChangesetAware interface to have changeset property pointing to it

--- src/components/plan-change/changeset-aware.resolver.ts
+++ src/components/plan-change/changeset-aware.resolver.ts
@@ -1,20 +1,20 @@
 import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
 import { LoggedInSession, Session } from '../../common';
-import { ChangesetAware, PlanChange } from './dto';
+import { Changeset, ChangesetAware } from './dto';
 import { PlanChangeService } from './plan-change.service';
 
 @Resolver(ChangesetAware)
 export class ChangesetAwareResolver {
   constructor(private readonly service: PlanChangeService) {}
 
-  @ResolveField(() => PlanChange, {
-    description: 'The current change plan/object that this object is for.',
+  @ResolveField(() => Changeset, {
+    description: 'The current changeset that this object is for.',
     nullable: true,
   })
-  async change(
+  async changeset(
     @Parent() object: ChangesetAware,
     @LoggedInSession() session: Session
-  ): Promise<PlanChange | null> {
+  ): Promise<Changeset | null> {
     return object.changeset
       ? await this.service.readOne(object.changeset, session)
       : null;
--- src/components/plan-change/dto/plan-change.dto.ts
+++ src/components/plan-change/dto/plan-change.dto.ts
@@ -1,13 +1,18 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, InterfaceType, ObjectType } from '@nestjs/graphql';
 import { keys as keysOf } from 'ts-transformer-keys';
 import { Resource, SecuredProps, SecuredString } from '../../../common';
 import { SecuredPlanChangeStatus } from './plan-change-status.enum';
 import { SecuredPlanChangeTypes } from './plan-change-type.enum';
 
-@ObjectType({
+@InterfaceType({
   implements: [Resource],
 })
-export abstract class PlanChange extends Resource {
+export class Changeset extends Resource {}
+
+@ObjectType({
+  implements: [Changeset, Resource],
+})
+export abstract class PlanChange extends Resource implements Changeset {
   static readonly Props = keysOf<PlanChange>();
   static readonly SecuredProps = keysOf<SecuredProps<PlanChange>>();
 
