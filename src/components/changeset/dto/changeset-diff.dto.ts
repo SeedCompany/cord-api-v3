c60d728058c2cad9217f66dd3b469ebfde5b03c0
Carson Full <carson_full@tsco.org>
2021-06-18 17:00:44 -0500

    Fix any usage on changeset difference

--- src/components/changeset/dto/changeset-diff.dto.ts
+++ src/components/changeset/dto/changeset-diff.dto.ts
@@ -1,5 +1,9 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { ValueOf } from 'type-fest';
 import { Resource } from '../../../common';
+import { ResourceMap } from '../../authorization/model/resource-map';
+
+type SomeResource = ValueOf<ResourceMap>;
 
 @ObjectType({
   description:
@@ -9,17 +13,17 @@ export class ChangesetDiff {
   @Field(() => [Resource], {
     description: 'The list of resources that have been added in this changeset',
   })
-  readonly added: readonly Resource[];
+  readonly added: readonly SomeResource[];
 
   @Field(() => [Resource], {
     description:
       'The list of resources that have been removed in this changeset',
   })
-  readonly removed: readonly Resource[];
+  readonly removed: readonly SomeResource[];
 
   @Field(() => [Resource], {
     description:
       'The list of resources that have been changed in this changeset',
   })
-  readonly changed: readonly Resource[];
+  readonly changed: readonly SomeResource[];
 }
