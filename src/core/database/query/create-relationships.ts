9d959e0e50527e57f35afad2dadc59d1a53a3df9
Carson Full <carson_full@tsco.org>
2021-06-20 00:32:43 -0500

    Update createRelationships to support a list of IDs to set under the relationship label

--- src/core/database/query/create-relationships.ts
+++ src/core/database/query/create-relationships.ts
@@ -2,7 +2,7 @@ import { node, Query, relation } from 'cypher-query-builder';
 import { RelationDirection } from 'cypher-query-builder/dist/typings/clauses/relation-pattern';
 import { Maybe as Nullable } from 'graphql/jsutils/Maybe';
 import { DateTime } from 'luxon';
-import { ID } from '../../../common';
+import { ID, many } from '../../../common';
 import { ResourceMap } from '../../../components/authorization/model/resource-map';
 
 /**
@@ -28,18 +28,20 @@ export const createRelationships =
   (
     direction: RelationDirection,
     labelsToRelationships: Partial<
-      Record<keyof ResourceMap, Record<string, Nullable<ID>>>
+      Record<keyof ResourceMap, Record<string, Nullable<ID> | readonly ID[]>>
     >
   ) =>
   (query: Query) => {
     const flattened = Object.entries(labelsToRelationships).flatMap(
       ([nodeLabel, relationships]) =>
-        Object.entries(relationships ?? {}).flatMap(([prop, id]) => ({
-          nodeLabel,
-          id,
-          relLabel: prop,
-          variable: prop,
-        }))
+        Object.entries(relationships ?? {}).flatMap(([prop, ids]) =>
+          many(ids ?? []).map((id, i) => ({
+            nodeLabel,
+            id,
+            relLabel: prop,
+            variable: Array.isArray(ids) ? `${prop}${i}` : prop,
+          }))
+        )
     );
 
     const createdAt = DateTime.local();
