6ff9e46e3fb7a5b900d2db5fc4285f68fb3268ac
Carson Full <carson_full@tsco.org>
2021-06-22 18:52:00 -0500

    Replace changeset readOne, which didn't lookup changeset correctly with cheat to ResourceResolver assuming the concrete type

--- src/components/changeset/changeset-aware.resolver.ts
+++ src/components/changeset/changeset-aware.resolver.ts
@@ -1,17 +1,29 @@
 import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
 import { LoggedInSession, Session } from '../../common';
-import { ChangesetRepository } from './changeset.repository';
+import { ResourceResolver } from '../../core';
+import { ProjectChangeRequest } from '../project-change-request/dto';
 import { Changeset, ChangesetAware } from './dto';
 
 @Resolver(ChangesetAware)
 export class ChangesetAwareResolver {
-  constructor(private readonly repo: ChangesetRepository) {}
+  constructor(private readonly resources: ResourceResolver) {}
 
   @ResolveField()
   async changeset(
     @Parent() object: ChangesetAware,
-    @LoggedInSession() _session: Session
+    @LoggedInSession() session: Session
   ): Promise<Changeset | null> {
-    return object.changeset ? await this.repo.readOne(object.changeset) : null;
+    if (!object.changeset) {
+      return null;
+    }
+
+    // Cheat for now and assume concrete type since it is the only one.
+    // May need BaseNode lookup in future.
+    // eslint-disable-next-line @typescript-eslint/return-await -- false positive
+    return await this.resources.lookup(
+      ProjectChangeRequest,
+      object.changeset,
+      session
+    );
   }
 }
--- src/components/changeset/changeset.repository.ts
+++ src/components/changeset/changeset.repository.ts
@@ -2,40 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { node, relation } from 'cypher-query-builder';
 import { ID, NotFoundException, Session } from '../../common';
 import { DtoRepository } from '../../core';
-import { matchProps } from '../../core/database/query';
-import { BaseNode, DbPropsOfDto } from '../../core/database/results';
+import { BaseNode } from '../../core/database/results';
 import { Changeset, ChangesetDiff } from './dto';
 
 @Injectable()
 export class ChangesetRepository extends DtoRepository(Changeset) {
-  async readOne(id: ID): Promise<Changeset> {
-    const query = this.db
-      .query()
-      .match([
-        node('node', 'Changeset', { id }),
-        relation('in', '', 'changeset', { active: true }),
-        node('parent', 'BaseNode'),
-      ])
-      .apply(matchProps())
-      .return([
-        'props',
-        `[l in labels(node) where not l in ['Changeset', 'BaseNode']][0] as type`,
-      ])
-      .asResult<{
-        props: DbPropsOfDto<Changeset, true>;
-        type: string;
-      }>();
-    const result = await query.first();
-    if (!result) {
-      throw new NotFoundException('Changeset not found');
-    }
-    return {
-      ...result.props,
-      __typename: result.type,
-      canDelete: false,
-    };
-  }
-
   async difference(id: ID, _session: Session) {
     const result = await this.db
       .query()
