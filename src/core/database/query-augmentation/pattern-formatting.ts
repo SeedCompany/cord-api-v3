7675f46eb889682fe43dcfc8e39a408955631cd7
Carson Full <carson_full@tsco.org>
2021-06-24 14:35:22 -0500

    Fix with & return clauses from aliasing as same name when using object notation
    e.g. .with({ node: 'node' })
    // => WITH node as node
    Causing: Neo4jError: key not found: SymbolUse(node@821)

--- src/core/database/query-augmentation/pattern-formatting.ts
+++ src/core/database/query-augmentation/pattern-formatting.ts
@@ -1,4 +1,6 @@
-import { Create, Match, Merge } from 'cypher-query-builder';
+import { Create, Match, Merge, With } from 'cypher-query-builder';
+import type { TermListClause as TSTermListClause } from 'cypher-query-builder/dist/typings/clauses/term-list-clause';
+import { Class } from 'type-fest';
 
 // Add line breaks for each pattern when there's multiple per statement
 for (const Cls of [Match, Create, Merge]) {
@@ -8,3 +10,23 @@ for (const Cls of [Match, Create, Merge]) {
     return str.split(', ').join(',\n    ');
   };
 }
+
+// This class is not exported so grab it a hacky way
+const TermListClause = Object.getPrototypeOf(With) as Class<TSTermListClause>;
+
+// Change With & Return clauses to not alias as same variable since cypher as
+// problems with it some times.
+// e.g. .with({ node: 'node' })
+// WITH node as node
+// Neo4jError: key not found: SymbolUse(node@821)
+const origStringifyProperty = TermListClause.prototype.stringifyProperty;
+TermListClause.prototype.stringifyProperty = function stringifyProperty(
+  prop: string,
+  alias?: string,
+  node?: string
+) {
+  if (prop === alias && !node) {
+    return prop;
+  }
+  return origStringifyProperty(prop, alias, node);
+};
