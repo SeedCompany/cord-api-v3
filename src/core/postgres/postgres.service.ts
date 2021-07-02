a3020d4c38fa56b79a0a32d0d806e09d3169f7ee
Vivek Varma <vivekvarma15516@gmail.com>
2021-07-02 14:41:47 +0530

    database init function working

--- src/core/postgres/postgres.service.ts
+++ src/core/postgres/postgres.service.ts
@@ -10,16 +10,31 @@ import * as path from 'path';
 @Injectable()
 export class PostgresService {
   constructor(private readonly config: ConfigService) {}
-  // pools/clients will use environment variables
-  // for connection information
+ 
   pool = new Pool();
   client = new Client(this.config.postgres);
-  
+
+  async executeSQLFiles(dirPath:string,connectedClient:Client):Promise<number>{
+    fs.readdirSync(dirPath).forEach(async (name)=>{
+      const fileOrDirPath = path.join(dirPath,name);
+      if(fs.existsSync(fileOrDirPath) && fs.lstatSync(fileOrDirPath).isDirectory()){
+        console.log('dir: ',fileOrDirPath);
+        this.executeSQLFiles(fileOrDirPath, connectedClient);
+      }
+      else{
+        // load script into db
+        console.log('file: ',fileOrDirPath);
+        const sql = fs.readFileSync(fileOrDirPath).toString();
+        await this.client.query(sql);
+      }
+    });
+    return 0;
+  }
   
   async db_init():Promise<number>{
-    const connectedClient = await this.client.connect();
-    console.log(__dirname);
-    fs.readdirSync(path.join(__dirname,'..','..', '..', 'src/core/postgres/sql/db_init')).forEach(name=>console.log(name));
+    await this.client.connect();
+    const dbInitPath = path.join(__dirname,'..','..', '..', 'src/core/postgres/sql/db_init')
+    const fileExecutionStatus = await this.executeSQLFiles(dbInitPath,this.client)
     return 0;
   }
 
