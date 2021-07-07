371132a23ac156b1e16a8775816a932c34fa4d72
Carson Full <carson_full@tsco.org>
2021-07-07 01:57:01 -0500

    Update cypher expression to accept literal maps, lists, numbers, booleans, nulls
    This will let us format the complex cypher expressions in plain TS/JS, which in
    turns makes it a lot harder to mess up syntax.

--- src/core/database/query/cypher-expression.ts
+++ src/core/database/query/cypher-expression.ts
@@ -1,4 +1,9 @@
-import { ServerException } from '../../../common';
+import { Many, ServerException } from '../../../common';
+
+export type ExpressionInput =
+  | Many<string | boolean | number | null | undefined>
+  | readonly ExpressionInput[]
+  | { [prop: string]: ExpressionInput };
 
 export type CypherExpression = string & {
   /**
@@ -7,22 +12,70 @@ export type CypherExpression = string & {
   as: (output: string) => string;
 };
 
-export const exp = (exp: string): CypherExpression =>
-  new Proxy<any>(
+export const exp = (exp: ExpressionInput): CypherExpression => {
+  const expression = buildExp(exp);
+  return new Proxy<any>(
     {},
     {
       get(target: never, p: PropertyKey): any {
         if (p === 'as') {
-          return (output: string) => `${exp} as ${output}`;
+          return (output: string) => `${expression} as ${output}`;
+        }
+        if (p === CypherExp) {
+          return true;
         }
         if (
           p === Symbol.toPrimitive ||
           p === Symbol.toStringTag ||
           p === 'toString'
         ) {
-          return () => exp;
+          return () => expression;
         }
         throw new ServerException('Something went wrong');
       },
     }
   );
+};
+
+export const isExp = (value: unknown): value is CypherExpression =>
+  // @ts-expect-error I know indexes and symbols. how hard is it.
+  value && typeof value === 'object' && value[CypherExp] === true;
+
+const CypherExp = Symbol('CypherExpression');
+
+const isArray = (arg: unknown): arg is readonly unknown[] => Array.isArray(arg);
+const buildExp = (exp: ExpressionInput): string => {
+  if (exp == null) {
+    return 'null';
+  }
+  if (typeof exp === 'number' || typeof exp === 'boolean') {
+    return exp.toString();
+  }
+  if (typeof exp === 'string') {
+    return exp; // string literals must be quoted explicitly
+  }
+  if (isExp(exp)) {
+    return exp.toString();
+  }
+
+  if (isArray(exp)) {
+    const list = exp.filter((e) => e !== undefined).map(buildExp);
+    return shouldMultiline(list)
+      ? `[${makeMultiline(list)}]`
+      : `[${list.join(', ')}]`;
+  }
+
+  const pairs = Object.entries(exp).flatMap(([key, value]) =>
+    value !== undefined ? `${key}: ${buildExp(value)}` : []
+  );
+  return shouldMultiline(pairs)
+    ? `{${makeMultiline(pairs)}}`
+    : `{ ${pairs.join(', ')} }`;
+};
+
+const shouldMultiline = (list: string[]) => {
+  const joined = list.join(', ');
+  return joined.includes('\n') || joined.length > 80;
+};
+const makeMultiline = (list: string[]) =>
+  `\n  ${list.map((item) => item.replace(/\n/g, `\n  `)).join(',\n  ')}\n`;
