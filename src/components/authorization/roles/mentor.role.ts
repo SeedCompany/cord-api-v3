703d225a4c9a7707cc06d68c6660f2ee07abd1fd
tmpacifitech <webmaster@motordirex.com>
2021-07-08 07:22:26 -0700

    Update users permission for periodic reports edit

--- src/components/authorization/roles/mentor.role.ts
+++ src/components/authorization/roles/mentor.role.ts
@@ -353,10 +353,10 @@ export const Mentor = new DbRole({
     new DbBaseNodeGrant<DbPeriodicReport>({
       __className: 'DbPeriodicReport',
       properties: [
-        { propertyName: 'type', permission: { read, write, }, },
-        { propertyName: 'start', permission: { read, write, }, },
-        { propertyName: 'end', permission: { read, write, }, },
-        { propertyName: 'reportFile', permission: { read, write, }, },
+        { propertyName: 'type', permission: { read, }, },
+        { propertyName: 'start', permission: { read, }, },
+        { propertyName: 'end', permission: { read, }, },
+        { propertyName: 'reportFile', permission: { read, }, },
       ],
       canDelete: true,
     }),
