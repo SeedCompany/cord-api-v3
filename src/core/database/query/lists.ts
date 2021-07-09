f782846ecae5681d809edfc9c62becf0863543a6
Carson Full <carson_full@tsco.org>
2021-07-09 00:01:59 -0500

    New pagination abstraction that allows hydrating items inside a single query O(n+1) -> O(1)

--- src/core/database/query/lists.ts
+++ src/core/database/query/lists.ts
@@ -2,11 +2,64 @@ import { node, Query, relation } from 'cypher-query-builder';
 import {
   ID,
   Order,
+  PaginatedListType,
+  PaginationInput,
   Resource,
   ResourceShape,
   SortablePaginationInput,
 } from '../../../common';
+import { collect } from './cypher-functions';
 
+/**
+ * Adds pagination to a query based on input.
+ *
+ * Expects a `node` query variable for all available items.
+ * Expects filtering and sorting to already be applied.
+ *
+ * Optionally hydrate each item of the page requested with the hydrate() function.
+ * It expects a `dto` output variable.
+ * If it's omitted, then by default only the IDs are returned.
+ * Note that only `node` is available in this function.
+ */
+export const paginate =
+  <R = ID>(
+    { count, page }: PaginationInput,
+    hydrate?: (query: Query) => Query<{ dto: R }>
+  ) =>
+  (query: Query) => {
+    let list = 'list';
+    const params: Record<string, any> = {
+      limit: count,
+    };
+    if (page !== 1) {
+      params.offset = ((page ?? 1) - 1) * count;
+      list = `list[$offset..]`;
+    }
+
+    return query.comment`paginate()`
+      .with('collect(distinct node) as list')
+      .raw(`WITH list, ${list}[..$limit] as page`, params)
+      .subQuery('page', (sub) =>
+        sub
+          .raw('UNWIND page as node')
+          .comment('Hydrating node')
+          .apply(hydrate ?? ((q) => q.with('node.id as dto')))
+          // Using collect in sub query ensures that unwinding an empty list
+          // and then collecting in the return will maintain the one row outside
+          // this sub query so that total and other pagination info is returned.
+          .return(collect('dto').as('hydratedPage'))
+      )
+      .return<PaginatedListType<R>>([
+        'hydratedPage as items',
+        'size(list) as total',
+        // We use list & page here as comparison needs to be done with neo4j nodes
+        'size(page) > 0 and page[-1] <> list[-1] as hasMore',
+      ]);
+  };
+
+/**
+ * @deprecated Use {@link paginate} instead.
+ */
 export const calculateTotalAndPaginateList =
   <TResourceStatic extends ResourceShape<any>>(
     resource: TResourceStatic,
