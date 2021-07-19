63f96e55996fcd3a48bb2545508ec87251c87f4b
Vivek Varma <vivekvarma15516@gmail.com>
2021-07-19 19:28:02 +0530

    add retool queries for projects

---
+++ src/core/postgres/sql/useful_scripts/retool_queries/projects.sql
@@ -0,0 +1,37 @@
+-- MODIFICATIONS: 
+update public.projects_data set primary_location  = 1;
+refresh materialized view public.projects_materialized_view;
+-- BUDGETS
+insert into sc.directories_data(id, name) values(0,'dir0');
+insert into sc.files_data(id,directory,name ) values(0,0,'file0');
+insert into sc.file_versions_data(id,category,name,mime_type, file,file_url) values(0,'budgets','file_ver0', 'A', 0, 'home/file0');
+insert into sc.budgets_data(base64,project, universal_template) values('lalala', 0 ,0);
+insert into sc.partnerships_data(id,base64,project,partner,agreement) values(0,'defaultPartnership',0,'defaultOrg',0);
+insert into sc.budget_records_data(base64,budget,active,fiscal_year,partnership,amount ) values('defaultBudgetRecord',1,true, 2021,'defaultPartnership', 2500);
+-- FILES    
+-- FINANCIAL REPORTS
+-- NARRATIVE REPORTS
+-- TEAM MEMBERS
+-- PARTNERSHIPS
+-- CHANGE REQS
+-- LANGUAGE ENGAGEMENTS
+
+
+
+
+-- RETOOL QUERIES
+-- PROJECT PARENT TABLE - REFACTOR TO USE CTEs
+select pmv.name,pmv.id,smv.department,pmv.sensitivity,smv.active, smv.change_to_plan,lmv.name, 
+(select count(*) from sc.language_engagements_materialized_view where project = pmv.id) as 
+language_engagements from public.projects_materialized_view pmv inner join 
+sc.projects_materialized_view smv on pmv.id = smv.project inner join (select __person_id,id,name from 
+public.locations_materialized_view) as lmv on lmv.id = pmv.primary_location
+where pmv.__person_id = 1 and smv.__person_id = 1 and lmv.__person_id = 1;
+
+-- BUDGET
+select sum(amount) from records_materialized_view brmv where brmv.budget in (select id from 
+sc.budgets_materialized_view bmv where bmv.project = 0) and brmv.__person_id = 1;
+select partnership, fiscal_year,amount  from sc.budget_records_materialized_view brmv where brmv.budget in (select id from 
+sc.budgets_materialized_view bmv where bmv.project = 0) and brmv.__person_id = 1;
+
+-- FILES
