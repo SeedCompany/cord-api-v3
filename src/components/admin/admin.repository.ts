d7aea9ddca0d58ac4ba506627a6b1e2d941b7023
Vivek Varma <vivekvarma15516@gmail.com>
2021-07-21 19:47:37 +0530

    retool projects - change request queries

--- src/components/admin/admin.repository.ts
+++ src/components/admin/admin.repository.ts
@@ -249,10 +249,11 @@ export class AdminRepository {
     await client.query(`insert into sc.organizations_data(id,base64, internal) values(0,'defaultOrg', 'internal_defaultOrg');
     `);
     await client.query(
-      `insert into sc.change_to_plans_data(id, created_by) values(0,0);`
+      `insert into sc.change_to_plans_data(id, created_by, status, summary,type) values(0,0, 'a', 'what can i change?', 'a' );`
     );
     await client.query(`insert into sc.projects_data(id,project,base64,active,department) 
     values(0,0,'proj0', true,  'dept0');`);
+    
 
     // inserting a lot of data
     for (let i = 1; i < 10; i++) {
