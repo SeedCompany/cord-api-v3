980c2da5cb59ea7de7a05eddb72dcd24c80daaa1
Carson Full <carson_full@tsco.org>
2021-07-20 21:58:07 -0500

    Remove old, now unused prop parsing code

--- src/core/database/results/parse-secured.ts
+++ src/core/database/results/parse-secured.ts
@@ -4,7 +4,6 @@ import {
   permissionDefaults,
   PermissionsOf,
 } from '../../../components/authorization/authorization.service';
-import { parsePropList, PropListDbResult } from './parse-props';
 
 /**
  * Parses secured properties from a DB result's propList & permList.
@@ -12,8 +11,7 @@ import { parsePropList, PropListDbResult } from './parse-props';
  * The propKeys are passed in to determine the keys of the resulting object.
  * This allows default values to be given when the DB result doesn't include them.
  *
- * @param propList This can straight from the DB result, or an object already
- *                 ran through the `parsePropList` function.
+ * @param props    The unsecured props to secure.
  * @param perms    The permissions available for this user/object
  * @param propKeysObjectOrList A list of keys that define the result object or
  *                 an object that defines the keys of the result object.
@@ -22,12 +20,10 @@ export const parseSecuredProperties = <
   DbProps extends Record<string, any>,
   PickedKeys extends keyof DbProps
 >(
-  propList: PropListDbResult<DbProps> | DbProps,
+  props: DbProps,
   perms: PermissionsOf<DbProps>,
   propKeysObjectOrList: Record<PickedKeys, boolean> | readonly PickedKeys[]
 ) => {
-  const props = Array.isArray(propList) ? parsePropList(propList) : propList;
-
   const mapKey = (key: PickedKeys) => {
     const res = {
       value: undefined,
