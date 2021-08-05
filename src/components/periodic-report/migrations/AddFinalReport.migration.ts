cc389c2a8e121d84fd439b8478885290b26e8603
Blake Krammes <blake_krammes@tsco.org>
2021-08-05 16:26:56 -0500

    Add migrations
    - 87Six comments-->Cord V3 posts
    - Generating final periodic reports

--- src/components/periodic-report/migrations/AddFinalReport.migration.ts
+++ src/components/periodic-report/migrations/AddFinalReport.migration.ts
@@ -1,8 +1,74 @@
+import { node, relation } from 'cypher-query-builder';
 import { BaseMigration, Migration } from '../../../core';
+import { ReportType } from '../dto';
 
 @Migration('2021-08-03T15:30:05')
 export class AddFinalReportMigration extends BaseMigration {
   async up() {
-    // TODO implement
+    await this.migrate(ReportType.Financial);
+    await this.migrate(ReportType.Narrative);
+    await this.migrate(ReportType.Progress);
+  }
+
+  // the finalReportMigration property is present on all the migrated :PeriodicReport nodes if there is a need to revert
+  private async migrate(type: ReportType) {
+    const before = await this.getBaseNodeCount(type, false);
+    this.logger.info(
+      `${before ?? 0} ${
+        type !== ReportType.Progress ? 'projects' : 'engagements'
+      } with ${type} reports before migration`
+    );
+    await this.db
+      .query()
+      .raw(
+        `
+          match(u:User)-[:email { active: true }]->(:Property { value: "devops@tsco.org" })
+          match(b:BaseNode)-[:report]->(:${type}Report)
+          with u, collect(distinct b) as baseNodes
+          unwind baseNodes as baseNode
+          call {
+            with baseNode
+            match(baseNode)-[:report]->(:${type}Report)-[:end]->(en:Property)
+            with en
+            order by en.value desc limit 1
+            return en.value as lastEnd
+          }
+          create
+            (baseNode)-[:report { active: true, createdAt: datetime() }]->(pr:${type}Report:PeriodicReport:BaseNode{ finalReportMigration: true, createdAt: datetime(), id: apoc.create.uuid() }),
+              (pr)-[:type { active: true, createdAt: datetime() }]->(:Property { createdAt: datetime(), value: "${type}" }),
+              (pr)-[:start { active: true, createdAt: datetime() }]->(:Property { createdAt: datetime(), value: date(lastEnd) }),
+              (pr)-[:end { active: true, createdAt: datetime() }]->(:Property { createdAt: datetime(), value: date(lastEnd) }),
+              (pr)-[:receivedDate { active: true, createdAt: datetime() }]->(:Property { createdAt: datetime(), value: null }),
+              (pr)-[:reportFile { active: true, createdAt: datetime() }]->(reportFile:Property { createdAt: datetime(),  value: apoc.create.uuid() }),
+              (pr)-[:reportFileNode { active: true } ]->(rfn:BaseFile:BaseNode:File:FileNode{ createdAt: datetime(), id: reportFile.value }),
+                (rfn)-[:createdBy { active: true, createdAt: datetime() }]->(u),
+                (rfn)-[:name { active: true, createdAt: datetime() }]->(:Property { value: toString(lastEnd) }),
+                (rfn)-[:canDelete { active: true, createdAt: datetime() }]->(:Property { value: true })
+        `
+      )
+      .run();
+    const after = await this.getBaseNodeCount(type, true);
+    this.logger.info(
+      `${after ?? 0} ${
+        type !== ReportType.Progress ? 'projects' : 'engagements'
+      } with new final report after ${type} report migration`
+    );
+  }
+
+  private async getBaseNodeCount(type: ReportType, after: boolean) {
+    const res = await this.db
+      .query()
+      .match([
+        node('b', 'BaseNode'),
+        relation('out', '', 'report'),
+        node(
+          'rn',
+          `${type}Report`,
+          after ? { finalReportMigration: true } : {}
+        ),
+      ])
+      .return<{ count: number }>('count(distinct b) as count')
+      .first();
+    return res?.count;
   }
 }
---
+++ src/components/periodic-report/migrations/Post.migration.ts
@@ -0,0 +1,72 @@
+import { BaseMigration, Migration } from '../../../core';
+
+@Migration('2021-08-03T15:30:05')
+export class PostMigration extends BaseMigration {
+  // :Post nodes have postMigration property in case of reversion
+  // using project.createdAt for post.modifiedAt –– it's an approximation, but not sure if we want to do that
+  async up() {
+    const before = await this.getPostCount(true);
+    this.logger.info(`${before ?? 0} 87Six comments before migration`);
+    await this.db
+      .query()
+      .raw(
+        ` 
+          match(u:User)-[:email { active: true }]->(:Property { value: "devops@tsco.org" })
+          match(p:Project)
+          with u.id as rootId, p,
+          [
+            { type: 'Note', shareability: 'Internal', body: p.commentDescription }, 
+            { type: 'Prayer', shareability: 'AskToShareExternally', body: p.commentPrayerNeeds }, 
+            { type: 'Note', shareability: 'Internal', body: p.commentProposalComments }
+          ] as posts
+          unwind posts as post
+          with rootId, p, post
+          where size(post.body) > 0
+          create 
+            (p)-[:post { active: true, createdAt: datetime() }]->(po:BaseNode:Post{ postMigration: true, createdAt: datetime(), id: apoc.create.uuid() }),
+              (po)-[:shareability { active: true, createdAt: datetime() }]->(:Property { createdAt: datetime(), value: post.shareability, sortValue: post.shareability }),
+              (po)-[:type { active: true, createdAt: datetime() }]->(:Property { createdAt: datetime(), value: post.type, sortValue: post.type }),
+              (po)-[:body { active: true, createdAt: datetime() }]->(:Property { createdAt: datetime(), value: post.body, sortValue: post.body }),
+              (po)-[:modifiedAt { active: true, createdAt: datetime() }]->(:Property { createdAt: datetime(), value: p.createdAt, sortValue: p.createdAt }),
+              (po)-[:creator { active: true, createdAt: datetime() }]->(:Property { createdAt: datetime(), value: rootId, sortValue: rootId })
+        `
+      )
+      .run();
+    const after = await this.getPostCount(false);
+    this.logger.info(`${after ?? 0} Cord V3 posts after migration`);
+
+    // this can be omitted if we want to keep data here in case of reversion
+    // if (before === after) {
+    //   await this.db
+    //     .query()
+    //     .raw(
+    //       `
+    //       match(p:Project)
+    //       remove p.commentDescription, p.commentPrayerNeeds, p.commentProposalComments
+    //     `
+    //     )
+    //     .run();
+    // }
+  }
+
+  private async getPostCount(before: boolean) {
+    const res = await this.db
+      .query()
+      .raw(
+        before
+          ? `
+              match(p:Project)
+              with [ p.commentDescription, p.commentPrayerNeeds, p.commentProposalComments ] as protoPosts
+              unwind protoPosts as po
+              with po
+              where size(po) > 0
+            `
+          : `
+              match(po:Post { postMigration: true })
+            `
+      )
+      .return<{ count: number }>('count(po) as count')
+      .first();
+    return res?.count;
+  }
+}
