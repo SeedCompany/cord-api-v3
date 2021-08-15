f986dd54fb3824b9f6088a1a7fd1d13b291542e9
Rhuan Souza <rhuan@superia.global>
2021-08-15 13:05:10 -0300

    remove session

--- src/components/flag-approved-inventory/flag.service.ts
+++ src/components/flag-approved-inventory/flag.service.ts
@@ -1,25 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { ID, Session } from '../../common';
+import { ID } from '../../common';
 import { FlagRepository } from './flag.repository';
 
 @Injectable()
 export class FlagService {
   constructor(private readonly repo: FlagRepository) {}
 
-  async isPinned(id: ID, session: Session): Promise<boolean> {
-    return await this.repo.isFlagged(id, session);
+  async isPinned(id: ID): Promise<boolean> {
+    return await this.repo.isFlagged(id);
   }
 
-  async toggleFlagged(
-    id: ID,
-    session: Session,
-    flagged?: boolean
-  ): Promise<boolean> {
-    flagged ??= !(await this.repo.isFlagged(id, session));
+  async toggleFlagged(id: ID, flagged?: boolean): Promise<boolean> {
+    flagged ??= !(await this.repo.isFlagged(id));
     if (!flagged) {
-      await this.repo.add(id, session);
+      await this.repo.add(id);
     } else {
-      await this.repo.remove(id, session);
+      await this.repo.remove(id);
     }
     return flagged;
   }
