3785aa1631028207db1eec87481642fa2c3ffb6b
Daniel Leo <danielld707@gmail.com>
2021-09-03 11:52:01 -0400

    refactor product query.helpers

--- src/components/product/query.helpers.ts
+++ src/components/product/query.helpers.ts
@@ -1,19 +1,29 @@
 import { inArray, Query } from 'cypher-query-builder';
+import { AndConditions } from 'cypher-query-builder/src/clauses/where-utils';
+import { intersection } from 'lodash';
 import { propMatch } from '../project';
 import { ApproachToMethodologies, ProductFilters } from './dto';
 
 export const productListFilter = (filter: ProductFilters) => (query: Query) => {
-  if (filter.methodology) {
-    query
-      .match(propMatch('methodology'))
-      .where({ methodology: { value: filter.methodology } });
+  const conditions: AndConditions = {};
+
+  if (filter.approach || filter.methodology) {
+    query.match(propMatch('methodology'));
+    conditions.methodology =
+      filter.methodology && filter.approach
+        ? {
+            value: inArray(
+              intersection(ApproachToMethodologies[filter.approach], [
+                filter.methodology,
+              ])
+            ),
+          }
+        : filter.methodology
+        ? { value: filter.methodology }
+        : { value: inArray(ApproachToMethodologies[filter.approach!]) };
   }
 
-  if (filter.approach) {
-    query.match(propMatch('methodology')).where({
-      methodology: {
-        value: inArray(ApproachToMethodologies[filter.approach]),
-      },
-    });
+  if (conditions.methodology) {
+    query.where(conditions);
   }
 };
