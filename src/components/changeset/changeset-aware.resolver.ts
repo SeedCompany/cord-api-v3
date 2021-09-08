70f01797bd4e628319ba135c8c8cee20234073bc
Carson Full <carson_full@tsco.org>
2021-09-08 08:47:08 -0500

    Implement ChangesetLoader (no batching)

--- src/components/changeset/changeset-aware.resolver.ts
+++ src/components/changeset/changeset-aware.resolver.ts
@@ -1,29 +1,14 @@
 import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
-import { LoggedInSession, Session } from '../../common';
-import { ResourceResolver } from '../../core';
-import { ProjectChangeRequest } from '../project-change-request/dto';
+import { DataLoader, Loader } from '../../core';
 import { Changeset, ChangesetAware } from './dto';
 
 @Resolver(ChangesetAware)
 export class ChangesetAwareResolver {
-  constructor(private readonly resources: ResourceResolver) {}
-
   @ResolveField()
   async changeset(
     @Parent() object: ChangesetAware,
-    @LoggedInSession() session: Session
+    @Loader(Changeset) changesets: DataLoader<Changeset>
   ): Promise<Changeset | null> {
-    if (!object.changeset) {
-      return null;
-    }
-
-    // Cheat for now and assume concrete type since it is the only one.
-    // May need BaseNode lookup in future.
-    // eslint-disable-next-line @typescript-eslint/return-await -- false positive
-    return await this.resources.lookup(
-      ProjectChangeRequest,
-      object.changeset,
-      session
-    );
+    return object.changeset ? await changesets.load(object.changeset) : null;
   }
 }
