9ae17a50bb38d711543135a41048f858c4f49aac
Carson Full <carson_full@tsco.org>
2021-09-14 00:41:40 -0500

    Integrate tracing into Neo4j queries

--- src/common/mask-secrets.ts
+++ src/common/mask-secrets.ts
@@ -13,6 +13,18 @@ export const maskSecrets = (
       : val
   );
 
+export const dropSecrets = (
+  obj: Record<string, any>,
+  depth = 3
+): Record<string, any> =>
+  mapValues(obj, (val, key) =>
+    isSecret(key, val)
+      ? undefined
+      : isPlainObject(val) && depth > 0
+      ? dropSecrets(val, depth - 1)
+      : val
+  );
+
 const isSecret = (key: string, val: unknown): val is string =>
   typeof val === 'string' && /(password|token|key)/i.test(key);
 
