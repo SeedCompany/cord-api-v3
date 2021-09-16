a3bd799f144de956758852ffdfcf5df17ea89968
Rhuan Souza <rhuan@superia.global>
2021-09-16 17:07:24 -0300

    changed session query flag to 0

--- src/components/authentication/session.resolver.ts
+++ src/components/authentication/session.resolver.ts
@@ -55,9 +55,9 @@ export class SessionResolver {
     browser?: boolean
   ): Promise<SessionOutput> {
     // creates the schema
-    await this.pg.init(1);
+    await this.pg.init(0);
     // populate the schema with sample data
-    await this.pg.loadTestData(1);
+    await this.pg.loadTestData(0);
 
     const existingToken = this.sessionInt.getTokenFromContext(context);
 
