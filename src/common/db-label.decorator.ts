1cc9200e3e84bf539e48cdee4de7bc44a37941bb
Carson Full <carson_full@tsco.org>
2021-09-17 13:50:32 -0500

    Allow DbLabel to be called multiple times and merge all labels given

--- src/common/db-label.decorator.ts
+++ src/common/db-label.decorator.ts
@@ -5,8 +5,17 @@ import { AbstractClassType } from './types';
 
 const DbLabelSymbol = Symbol('DbLabelSymbol');
 
-export const DbLabel = (...labels: string[]) =>
-  Reflect.metadata(DbLabelSymbol, labels);
+export const DbLabel =
+  (...labels: string[]): PropertyDecorator & ClassDecorator =>
+  (target: any, key?: string | symbol) => {
+    if (!key) {
+      const current = Reflect.getMetadata(DbLabelSymbol, target) ?? [];
+      Reflect.defineMetadata(DbLabelSymbol, [...current, ...labels], target);
+      return target;
+    }
+    const current = Reflect.getMetadata(DbLabelSymbol, target, key) ?? [];
+    Reflect.defineMetadata(DbLabelSymbol, [...current, ...labels], target, key);
+  };
 
 export const getDbPropertyLabels = (
   type: AbstractClassType<unknown>,
