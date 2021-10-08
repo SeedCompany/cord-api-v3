b11a869768cb205caef033add329f3f6df6db9a9
Carson Full <carson_full@tsco.org>
2021-10-07 20:08:53 -0500

    Extract product info from pnp, on "planning" file upload, and use it to create products
    #1951

--- src/common/fiscal-year.ts
+++ src/common/fiscal-year.ts
@@ -1,5 +1,6 @@
 import { range } from 'lodash';
 import { DateTime } from 'luxon';
+import { CalendarDate, DateInterval } from './temporal';
 
 export const fiscalYear = (dt: DateTime) =>
   dt.month >= 10 ? dt.year + 1 : dt.year;
@@ -9,3 +10,9 @@ export const fiscalYears = (start?: DateTime, end?: DateTime) =>
 
 export const fiscalQuarter = (dt: DateTime) =>
   dt.quarter === 4 ? 1 : dt.quarter + 1;
+
+export const fullFiscalYear = (fiscalYear: number) =>
+  DateInterval.fromDateTimes(
+    CalendarDate.local(fiscalYear, 10, 1),
+    CalendarDate.local(fiscalYear + 1, 9, 30)
+  );
--- src/components/product/product.module.ts
+++ src/components/product/product.module.ts
@@ -1,19 +1,24 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { AuthorizationModule } from '../authorization/authorization.module';
+import { FileModule } from '../file/file.module';
 import { ScriptureModule } from '../scripture/scripture.module';
+import * as handlers from './handlers';
 import * as migrations from './migrations';
+import { ProductExtractor } from './product-extractor.service';
 import { ProductLoader } from './product.loader';
 import { ProductRepository } from './product.repository';
 import { ProductResolver } from './product.resolver';
 import { ProductService } from './product.service';
 
 @Module({
-  imports: [forwardRef(() => AuthorizationModule), ScriptureModule],
+  imports: [forwardRef(() => AuthorizationModule), ScriptureModule, FileModule],
   providers: [
     ProductResolver,
     ProductService,
     ProductRepository,
     ProductLoader,
+    ProductExtractor,
+    ...Object.values(handlers),
     ...Object.values(migrations),
   ],
   exports: [ProductService],
