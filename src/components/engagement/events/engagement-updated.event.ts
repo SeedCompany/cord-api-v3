c7608ff98bbb5c11e872bafadd84139481e07c98
Carson Full <carson_full@tsco.org>
2021-10-07 20:02:04 -0500

    Fix EngagementUpdatedEvent to reference a union of the concrete engagement types

--- src/components/engagement/events/engagement-updated.event.ts
+++ src/components/engagement/events/engagement-updated.event.ts
@@ -1,11 +1,15 @@
 import { Session } from '../../../common';
-import { Engagement, UpdateEngagement } from '../dto';
+import {
+  Engagement,
+  UpdateInternshipEngagement,
+  UpdateLanguageEngagement,
+} from '../dto';
 
 export class EngagementUpdatedEvent {
   constructor(
     public updated: Engagement,
     readonly previous: Engagement,
-    readonly updates: UpdateEngagement,
+    readonly updates: UpdateLanguageEngagement | UpdateInternshipEngagement,
     readonly session: Session
   ) {}
 }
