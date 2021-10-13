0cb7f340c35756996f8e2c09d1ee3921d3b7f914
Carson Full <carson_full@tsco.org>
2021-10-12 23:04:30 -0500

    Add fakeAdminSession to database migration service
    This should work for the majority of cases where a session is needed in the
    context of database migrations
    
    (cherry picked from commit e9c453223dd6818bc15307f57b437ef4bdd69c46)

--- src/core/database/migration/base-migration.service.ts
+++ src/core/database/migration/base-migration.service.ts
@@ -1,4 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { DateTime } from 'luxon';
+import { ID, Session } from '../../../common';
 import { ILogger, Logger } from '../../logger';
 import { DatabaseService } from '../database.service';
 
@@ -11,4 +13,19 @@ export abstract class BaseMigration {
   protected logger: ILogger;
 
   abstract up(): Promise<void>;
+
+  /**
+   * Sometimes a session is "needed" to read data from the app.
+   * Usually it's required just to provide user authorization.
+   * This is not guaranteed to work, especially for usage with writes.
+   */
+  get fakeAdminSession(): Session {
+    return {
+      token: '',
+      issuedAt: DateTime.now(),
+      roles: ['global:Administrator'],
+      userId: 'fake admin :(' as ID,
+      anonymous: false,
+    };
+  }
 }
