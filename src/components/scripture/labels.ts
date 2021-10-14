b3181abfa22716acfd00c3dd8c5137b5d3c232ac
Carson Full <carson_full@tsco.org>
2021-10-14 14:09:24 -0500

    Merge scripture ranges into a equivalent minimal set before labeling

--- src/components/scripture/labels.ts
+++ src/components/scripture/labels.ts
@@ -1,4 +1,5 @@
 import { uniq } from 'lodash';
+import { Range } from '../../common';
 import { Verse } from './books';
 import { mapRange, ScriptureRange, ScriptureReference } from './dto';
 
@@ -79,6 +80,11 @@ export const labelOfScriptureRanges = (refs: readonly ScriptureRange[]) => {
   if (refs.length === 1) {
     return labelOfScriptureRange(refs[0]);
   }
+  refs = mergeScriptureRanges(refs);
+  if (refs.length === 1) {
+    return labelOfScriptureRange(refs[0]);
+  }
+
   const hasSame = (key: keyof ScriptureReference) =>
     uniq(refs.flatMap((ref) => [ref.start[key], ref.end[key]])).length === 1;
   const same = hasSame('book')
@@ -98,4 +104,45 @@ export const labelOfScriptureRanges = (refs: readonly ScriptureRange[]) => {
     prefix += ' ';
   }
   return `${prefix}${labels}`;
+};
+
+/**
+ * Merges ScriptureRanges into a equivalent minimal set of ScriptureRanges.
+ * Combines overlapping and adjacent ScriptureRanges.
+ * Also sorts them.
+ */
+export const mergeScriptureRanges = (
+  ranges: readonly ScriptureRange[]
+): readonly ScriptureRange[] => {
+  // Adapted from Luxon's Interval.merge logic
+  const [found, final] = ranges
+    .map(ScriptureRange.fromReferences)
+    .sort((a, b) => a.start - b.start)
+    .reduce(
+      ([sofar, current], item) => {
+        if (!current) {
+          return [sofar, item];
+        }
+        // if current overlaps item or current's end is adjacent to item's start
+        if (
+          (current.end > item.start && current.start < item.end) ||
+          current.end === item.start - 1
+        ) {
+          return [
+            sofar,
+            // Merge current & item
+            {
+              start: current.start < item.start ? current.start : item.start,
+              end: current.end > item.end ? current.end : item.end,
+            },
+          ];
+        }
+        return [[...sofar, current], item];
+      },
+      [[], null] as [Array<Range<number>>, Range<number> | null]
+    );
+  if (final) {
+    found.push(final);
+  }
+  return found.map(ScriptureRange.fromIds);
 };
