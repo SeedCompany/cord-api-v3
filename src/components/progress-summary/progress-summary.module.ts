31afbf672e7363faef3868118bbfc55281c79e67
Carson Full <carson_full@tsco.org>
2021-10-19 11:20:48 -0500

    Add migration to re-extract ProgressSummary from PnPs for all periods

--- src/components/progress-summary/progress-summary.module.ts
+++ src/components/progress-summary/progress-summary.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { FileModule } from '../file/file.module';
 import { PeriodicReportModule } from '../periodic-report/periodic-report.module';
 import * as handlers from './handlers';
+import * as migrations from './migrations';
 import { ProgressExtractor } from './progress-extractor.service';
 import { ProgressReportConnectionResolver } from './progress-report-connection.resolver';
 import { ProgressSummaryEngagementConnectionResolver } from './progress-summary-engagement-connection.resolver';
@@ -17,6 +18,7 @@ import { ProgressSummaryResolver } from './progress-summary.resolver';
     ProgressSummaryRepository,
     ProgressExtractor,
     ...Object.values(handlers),
+    ...Object.values(migrations),
   ],
 })
 export class ProgressSummaryModule {}
