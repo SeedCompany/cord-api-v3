b14a16a0e617bc02475f6be61c68d4f1c39a9f7c
Carson Full <carson_full@tsco.org>
2021-10-19 11:22:05 -0500

    Remove BC condition for summary period missing now that all nodes have it

--- src/components/progress-summary/progress-summary.repository.ts
+++ src/components/progress-summary/progress-summary.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { isNull, Node, node, relation } from 'cypher-query-builder';
+import { Node, node, relation } from 'cypher-query-builder';
 import { ID } from '../../common';
 import { DtoRepository } from '../../core';
 import { ACTIVE } from '../../core/database/query';
@@ -17,17 +17,8 @@ export class ProgressSummaryRepository extends DtoRepository(ProgressSummary) {
       .match([
         node('', 'ProgressReport', { id: reportId }),
         relation('out', '', 'summary', ACTIVE),
-        node('ps', 'ProgressSummary'),
+        node('ps', 'ProgressSummary', { period }),
       ])
-      .where({
-        ps: [
-          { period },
-          // Handle BC with previous schema that only had cumulative without period specification
-          ...(period === SummaryPeriod.Cumulative
-            ? [{ period: isNull() }]
-            : []),
-        ],
-      })
       .return<{ summary: Node<ProgressSummary> }>('ps as summary')
       .first();
     return result?.summary.properties;
