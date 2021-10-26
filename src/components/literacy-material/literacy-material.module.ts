945034ac6dd09d4d5b2543791e2e5ac617ddc538
Daniel Leo <danielld707@gmail.com>
2021-10-26 11:19:41 -0500

    Deprecate Song and LiteracyMaterial in favor of EthnoArts
    co-authored-by: Carson Full <carson_full@tsco.org>

--- src/components/literacy-material/literacy-material.module.ts
+++ src/components/literacy-material/literacy-material.module.ts
@@ -1,13 +1,13 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { AuthorizationModule } from '../authorization/authorization.module';
-import { ScriptureModule } from '../scripture/scripture.module';
+import { EthnoArtModule } from '../ethno-art/ethno-art.module';
 import { LiteracyMaterialLoader } from './literacy-material.loader';
 import { LiteracyMaterialRepository } from './literacy-material.repository';
 import { LiteracyMaterialResolver } from './literacy-material.resolver';
 import { LiteracyMaterialService } from './literacy-material.service';
 
 @Module({
-  imports: [forwardRef(() => AuthorizationModule), ScriptureModule],
+  imports: [forwardRef(() => AuthorizationModule), EthnoArtModule],
   providers: [
     LiteracyMaterialResolver,
     LiteracyMaterialService,
--- src/components/song/song.module.ts
+++ src/components/song/song.module.ts
@@ -1,13 +1,13 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { AuthorizationModule } from '../authorization/authorization.module';
-import { ScriptureModule } from '../scripture/scripture.module';
+import { EthnoArtModule } from '../ethno-art/ethno-art.module';
 import { SongLoader } from './song.loader';
 import { SongRepository } from './song.repository';
 import { SongResolver } from './song.resolver';
 import { SongService } from './song.service';
 
 @Module({
-  imports: [forwardRef(() => AuthorizationModule), ScriptureModule],
+  imports: [forwardRef(() => AuthorizationModule), EthnoArtModule],
   providers: [SongResolver, SongLoader, SongService, SongRepository],
   exports: [SongService],
 })
