SeedCompany/cord-api-v3

commit 45aa73e9f36e88926e73177560ae4881206b0059

Don't create products with no steps matched

src/components/product/product-extractor.service.ts
@@ -33,7 +33,9 @@ export class ProductExtractor {
       return [];
     }
 
-    return findProductRows(sheet).map(parseProductRow(sheet, interval));
+    return findProductRows(sheet)
+      .map(parseProductRow(sheet, interval))
+      .filter((row) => row.steps.length > 1);
   }
 }
 

commit 6eed449824410f6df3b4dd9379fd0c21ac9e3447

Fix logic in empty steps filter on product extraction
Mistake introduced in 45aa73e9f36e88926e73177560ae4881206b0059
Related to #2233

src/components/product/product-extractor.service.ts
@@ -40,7 +40,7 @@ export class ProductExtractor {
 
     return findProductRows(sheet)
       .map(parseProductRow(sheet, expandToFullFiscalYears(interval)))
-      .filter((row) => row.steps.length > 1);
+      .filter((row) => row.steps.length > 0);
   }
 }
 