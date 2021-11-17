ff53b6ba46aa7b9b0aab3d1d30a9ade7e55fd393
Carson Full <carson_full@tsco.org>
2021-11-16 19:13:30 -0600

    Fix merging of Unknown Song & LM into EthnoArt

--- src/components/ethno-art/migrations/RenameSongAndLiteracyMaterialToEthnoArts.migration.ts
+++ src/components/ethno-art/migrations/RenameSongAndLiteracyMaterialToEthnoArts.migration.ts
@@ -1,37 +1,57 @@
-import { node, relation } from 'cypher-query-builder';
+import { hasLabel, node, relation } from 'cypher-query-builder';
 import { BaseMigration, Migration } from '../../../core';
 import { ACTIVE } from '../../../core/database/query';
 
 @Migration('2021-10-20T16:28:16')
 export class RenameSongAndLiteracyMaterialToEthnoArts extends BaseMigration {
   async up() {
+    // Replace "Unknown" Literacy Material with "Unknown" Song and delete the former
+    // This should fix the only duplicate below when merging.
+    await this.db
+      .query()
+      .match([
+        [
+          node('product', 'Product'),
+          relation('out', 'produces', 'produces', ACTIVE),
+          node('unknownLM', 'LiteracyMaterial'),
+          relation('out', '', 'name', ACTIVE),
+          node('name', 'Property', { value: 'Unknown' }),
+        ],
+        [
+          node('unknownSong', 'Song'),
+          relation('out', '', 'name', ACTIVE),
+          node('', 'Property', { value: 'Unknown' }),
+        ],
+      ])
+      .detachDelete(['produces', 'unknownLM', 'name'])
+      .create([
+        node('product'),
+        relation('out', 'produces', ACTIVE),
+        node('unknownSong'),
+      ])
+      .return('*')
+      .run();
+
     const res = await this.db
       .query()
       .match([
-        node('literacyMaterial', 'LiteracyMaterial'),
+        node('producible', 'Producible'),
         relation('out', '', 'name', ACTIVE),
-        node('literacyName', 'LiteracyName'),
+        node('name', 'Property'),
       ])
-      .match([
-        node('song', 'Song'),
-        relation('out', '', 'name', ACTIVE),
-        node('songName', 'SongName'),
-      ])
+      .where({
+        producible: [hasLabel('LiteracyMaterial'), hasLabel('Song')],
+      })
       .removeLabels({
-        literacyMaterial: 'LiteracyMaterial',
-        literacyName: 'LiteracyName',
-        song: 'Song',
-        songName: 'SongName',
+        producible: ['LiteracyMaterial', 'Song'],
+        name: ['LiteracyName', 'SongName'],
       })
       .setLabels({
-        literacyMaterial: 'EthnoArt',
-        literacyName: 'EthnoArtName',
-        song: 'EthnoArt',
-        songName: 'EthnoArtName',
+        producible: 'EthnoArt',
+        name: 'EthnoArtName',
       })
-
       .return<{ numNodesRelabeled: number }>(
-        'count(distinct song) + count(distinct literacyMaterial) as numNodesRelabeled'
+        'count(producible) as numNodesRelabeled'
       )
       .first();
     this.logger.info(`${res?.numNodesRelabeled ?? 0} Relabeled producibles`);
