0d146fe90cb506e9aca162d123b9af9a7233d4ae
Carson Full <carson_full@tsco.org>
2021-12-01 18:14:44 -0600

    Add more format options for StepProgress completed values
    This could be done in the UI without much difficulty, but
    it felt not simple enough to not provide the option in the API

--- src/components/product-progress/step-progress.resolver.ts
+++ src/components/product-progress/step-progress.resolver.ts
@@ -1,9 +1,54 @@
-import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
-import { SecuredFloatNullable } from '../../common';
-import { StepProgress } from './dto';
+import { Args, Parent, ResolveField, Resolver } from '@nestjs/graphql';
+import { stripIndent } from 'common-tags';
+import {
+  ID,
+  mapSecuredValue,
+  SecuredFloatNullable,
+  simpleSwitch,
+} from '../../common';
+import { Loader, LoaderOf } from '../../core';
+import { ProductLoader } from '../product';
+import { ProgressFormat, StepProgress } from './dto';
 
 @Resolver(StepProgress)
 export class StepProgressResolver {
+  @ResolveField(() => SecuredFloatNullable)
+  async completed(
+    @Parent() sp: StepProgress & { productId: ID },
+    @Loader(() => ProductLoader) products: LoaderOf<ProductLoader>,
+    @Args('format', {
+      type: () => ProgressFormat,
+      defaultValue: ProgressFormat.Numerator,
+      description: stripIndent`
+        The format the progress value will be returned in
+
+        Some formats are not supported for all products, like verses in other products,
+        in these cases a null \`value\` is returned.
+      `,
+    })
+    format: ProgressFormat
+  ): Promise<SecuredFloatNullable> {
+    const product = await products.load(sp.productId);
+    if (
+      !product.progressTarget.canRead ||
+      // progress target should always be >0 so this is just a sanity check
+      !product.progressTarget.value
+    ) {
+      return { canRead: false, canEdit: false };
+    }
+    const denominator = product.progressTarget.value;
+    const factor = simpleSwitch(format, {
+      [ProgressFormat.Numerator]: denominator,
+      [ProgressFormat.Decimal]: 1,
+      [ProgressFormat.Percent]: 100,
+      [ProgressFormat.Verses]: product.totalVerses,
+      [ProgressFormat.VerseEquivalents]: product.totalVerseEquivalents,
+    });
+    return await mapSecuredValue(sp.completed, async (val) =>
+      factor != null ? (val / denominator) * factor : null
+    );
+  }
+
   @ResolveField(() => SecuredFloatNullable, {
     deprecationReason: 'Use `StepProgress.completed` instead.',
   })
