0a7dd155970534c343a124b36207b758366dc27c
Daniel Leo <danielld707@gmail.com>
2021-12-14 22:57:33 -0400

    Add composite field migration

---
+++ src/components/product/migrations/AddComposite.migration.ts
@@ -0,0 +1,37 @@
+import { node, not, relation } from 'cypher-query-builder';
+import { DerivativeScriptureProduct } from '..';
+import { BaseMigration, Migration } from '../../../core';
+import { ACTIVE, createProperty, path } from '../../../core/database/query';
+
+@Migration('2021-12-14-T00:00:00')
+export class AddCompositeMigration extends BaseMigration {
+  async up() {
+    const res = await this.db
+      .query()
+      .matchNode('node', 'DerivativeScriptureProduct')
+      .where(
+        not(
+          path([
+            node('node'),
+            relation('out', '', 'composite', ACTIVE),
+            node('', 'Property'),
+          ])
+        )
+      )
+      .apply(
+        createProperty({
+          resource: DerivativeScriptureProduct,
+          key: 'composite',
+          value: false,
+          numCreatedVar: 'numCompositeCreated',
+        })
+      )
+      .return<{ numCompositeCreated: number }>(
+        'sum(numCompositeCreated) as numCompositeCreated'
+      )
+      .first();
+    this.logger.info(
+      `Composite prop added to ${res?.numCompositeCreated ?? 0} products`
+    );
+  }
+}
