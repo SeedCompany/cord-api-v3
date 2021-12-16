030d66855f6f2a37586c4f2a0a6cb28e21d14680
Carson Full <carson_full@tsco.org>
2021-12-16 16:35:23 -0600

    Remove deprecated DatabaseService.list

--- src/core/database/results/lists.ts
+++ src/core/database/results/lists.ts
@@ -1,4 +1,4 @@
-import { PaginatedListType, PaginationInput } from '../../../common';
+import { PaginatedListType } from '../../../common';
 
 export const mapListResults = async <T, R>(
   results: PaginatedListType<T>,
@@ -7,7 +7,3 @@ export const mapListResults = async <T, R>(
   ...results,
   items: await Promise.all(results.items.map(mapper)),
 });
-
-export const hasMore = (input: PaginationInput, total: number) =>
-  // if skip + count is less than total, there is more
-  (input.page - 1) * input.count + input.count < total;
