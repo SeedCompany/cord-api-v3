6b116d2859a870553d4d03163632731a3f0d9528
Carson Full <carson_full@tsco.org>
2022-02-11 08:59:47 -0600

    Change Duration.from to forward to builtin fromDurationLike

--- src/common/temporal/duration.ts
+++ src/common/temporal/duration.ts
@@ -1,27 +1,14 @@
-import { isNumber } from 'lodash';
 import { Duration, DurationLike } from 'luxon';
 import { Mutable } from 'type-fest';
 
 declare module 'luxon/src/duration' {
   // eslint-disable-next-line @typescript-eslint/no-namespace -- augmenting static class method
   namespace Duration {
-    export const from: (durationish: DurationLike) => Duration;
+    export const from: (duration: DurationLike) => Duration;
   }
 }
-(Duration as Mutable<typeof Duration>).from = function (
-  durationish: DurationLike
-) {
-  if (isNumber(durationish)) {
-    return Duration.fromMillis(durationish);
-  } else if (Duration.isDuration(durationish)) {
-    return durationish;
-  } else if (typeof durationish === 'object') {
-    return Duration.fromObject(durationish);
-  } else {
-    const _: never = durationish;
-    throw new Error();
-  }
-};
+const D = Duration as Mutable<typeof Duration>;
+D.from = Duration.fromDurationLike;
 
 // @ts-expect-error Adding here, which will be called by pg client
 Duration.prototype.toPostgres = function (this: Duration) {
