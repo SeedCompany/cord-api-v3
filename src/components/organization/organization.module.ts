b86cd46c92bc4e46229c2db55c12e616d3623c2e
Daniel Leo <danielld707@gmail.com>
2022-02-28 17:46:13 -0400

    Add PgOrganizationRepository

--- src/components/organization/organization.module.ts
+++ src/components/organization/organization.module.ts
@@ -1,8 +1,12 @@
 import { forwardRef, Module } from '@nestjs/common';
+import { splitDb } from '../../core';
 import { AuthorizationModule } from '../authorization/authorization.module';
 import { LocationModule } from '../location/location.module';
 import { OrganizationLoader } from './organization.loader';
-import { OrganizationRepository } from './organization.repository';
+import {
+  OrganizationRepository,
+  PgOrganizationRepository,
+} from './organization.repository';
 import { OrganizationResolver } from './organization.resolver';
 import { OrganizationService } from './organization.service';
 
@@ -13,6 +17,8 @@ import { OrganizationService } from './organization.service';
     OrganizationService,
     OrganizationRepository,
     OrganizationLoader,
+    splitDb(OrganizationRepository, PgOrganizationRepository),
+    PgOrganizationRepository,
   ],
   exports: [OrganizationService],
 })
--- src/components/organization/organization.repository.ts
+++ src/components/organization/organization.repository.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { node, Query, relation } from 'cypher-query-builder';
-import { ID, Session, UnsecuredDto } from '../../common';
-import { DtoRepository } from '../../core';
+import { compact } from 'lodash';
+import {
+  ID,
+  MaybeUnsecuredInstance,
+  PaginatedListType,
+  PublicOf,
+  ResourceShape,
+  Session,
+  UnsecuredDto,
+} from '../../common';
+import { DtoRepository, Pg } from '../../core';
+import { ChangesOf, DbChanges } from '../../core/database/changes';
 import {
   ACTIVE,
   createNode,
@@ -17,7 +27,14 @@ import {
   sorting,
 } from '../../core/database/query';
 import { AuthSensitivityMapping } from '../authorization/authorization.service';
-import { CreateOrganization, Organization, OrganizationListInput } from './dto';
+import { BaseNode } from '../../core/database/results';
+import { PgTransaction } from '../../core/postgres/transaction.decorator';
+import {
+  CreateOrganization,
+  Organization,
+  OrganizationListInput,
+  UpdateOrganization,
+} from './dto';
 
 @Injectable()
 export class OrganizationRepository extends DtoRepository<
@@ -118,5 +135,191 @@ export class OrganizationRepository extends DtoRepository<
       .apply(paginate(input, this.hydrate(session)))
       .logIt();
     return await result.first()!; // result from paginate() will always have 1 row.
+  }
+}
+
+@Injectable()
+export class PgOrganizationRepository
+  implements PublicOf<OrganizationRepository>
+{
+  constructor(private readonly pg: Pg) {}
+
+  @PgTransaction()
+  async create(input: CreateOrganization, _session: Session) {
+    // TODO: Add primary_location
+    const orgId = await this.pg.query<{ id: ID; sensitivity: string }>(
+      `
+      INSERT INTO common.organizations(
+          name, 
+          created_by,
+          modified_by, 
+          owning_person, 
+          owning_group)
+      VALUES (
+          $1,
+          (SELECT person FROM admin.tokens WHERE token = 'public'), 
+          (SELECT person FROM admin.tokens WHERE token = 'public'), 
+          (SELECT person FROM admin.tokens WHERE token = 'public'), 
+          (SELECT id FROM admin.groups WHERE  name = 'Administrators'))
+      RETURNING id, sensitivity;
+      `,
+      [input.name]
+    );
+
+    const rows = await this.pg.query<{ id: ID }>(
+      `
+      INSERT INTO sc.organizations(
+          id,
+          address,
+          sensitivity,
+          created_by, 
+          modified_by, 
+          owning_person, 
+          owning_group)
+      VALUES (
+          $1,
+          $2,
+          $3,
+          (SELECT person FROM admin.tokens WHERE token = 'public'), 
+          (SELECT person FROM admin.tokens WHERE token = 'public'), 
+          (SELECT person FROM admin.tokens WHERE token = 'public'), 
+          (SELECT id FROM admin.groups WHERE  name = 'Administrators'))
+      RETURNING id;
+    `,
+      [orgId[0].id, input.address, orgId[0].sensitivity]
+    );
+
+    return rows[0];
+  }
+
+  async readOne(
+    id: ID,
+    _session: Session
+  ): Promise<UnsecuredDto<Organization>> {
+    const rows = await this.pg.query<UnsecuredDto<Organization>>(
+      `
+      SELECT c.id, c.name, s.address, c.sensitivity, c.created_at as "createdAt" 
+      FROM common.organizations c, sc.organizations s
+      WHERE c.id = s.id AND c.id = $1;
+      `,
+      [id]
+    );
+    return rows[0];
+  }
+
+  async readMany(
+    ids: readonly ID[],
+    _session: Session
+  ): Promise<ReadonlyArray<UnsecuredDto<Organization>>> {
+    const rows = await this.pg.query<UnsecuredDto<Organization>>(
+      `
+      SELECT c.id, c.name, s.address, c.sensitivity, c.created_at as "createdAt"
+      FROM common.organizations c, sc.organizations s
+      WHERE c.id = s.id AND c.id = ANY($1::text[])
+      `,
+      [ids]
+    );
+    return rows;
+  }
+
+  async list(
+    input: OrganizationListInput,
+    _session: Session
+  ): Promise<PaginatedListType<UnsecuredDto<Organization>>> {
+    const limit = input.count;
+    const offset = (input.page - 1) * input.count;
+
+    const [{ count }] = await this.pg.query<{ count: string }>(
+      `
+      SELECT count(*)
+      FROM common.organizations c, sc.organizations s
+      WHERE c.id = s.id;
+      `
+    );
+
+    const rows = await this.pg.query<UnsecuredDto<Organization>>(
+      `
+      SELECT c.id, c.name, s.address, c.sensitivity, c.created_at as "createdAt"
+      FROM common.organizations c, sc.organizations s
+      WHERE c.id = s.id
+      ORDER BY ${input.sort} ${input.order} 
+      LIMIT ${limit ?? 25} OFFSET ${offset ?? 10};
+      `
+    );
+
+    const organizationList: PaginatedListType<UnsecuredDto<Organization>> = {
+      items: rows,
+      total: +count,
+      hasMore: rows.length < +count,
+    };
+
+    return organizationList;
+  }
+
+  async update(input: UpdateOrganization) {
+    const { id, ...changes } = input;
+    compact(Object.keys(changes)).map(async (key) => {
+      await this.pg.query(
+        `UPDATE ${key === 'name' ? 'common' : 'sc'}.organizations 
+         SET ${key} = $1 WHERE id = $2;`,
+        [changes[key as keyof Omit<UpdateOrganization, 'id'>], id]
+      );
+    });
+  }
+
+  @PgTransaction()
+  async delete(id: ID) {
+    await this.pg.query('DELETE FROM sc.organizations WHERE id = $1;', [id]);
+    await this.pg.query('DELETE FROM common.organizations WHERE id = $1;', [
+      id,
+    ]);
+  }
+
+  async isUnique(orgName: string): Promise<boolean> {
+    const rows = await this.pg.query(
+      'SELECT name FROM common.organizations WHERE name = $1',
+      [orgName]
+    );
+    return !!rows[0];
+  }
+
+  getActualChanges: <
+    TResource extends MaybeUnsecuredInstance<typeof Organization>,
+    Changes extends ChangesOf<TResource>
+  >(
+    existingObject: TResource,
+    changes: Changes & Record<any, any>
+  ) => Partial<any>;
+
+  getBaseNode(
+    _id: ID,
+    _label?: string | ResourceShape<any>
+  ): Promise<BaseNode | undefined> {
+    throw new Error('Method not implemented.');
+  }
+  updateProperties<
+    TObject extends Partial<MaybeUnsecuredInstance<typeof Organization>> & {
+      id: ID;
+    }
+  >(
+    _object: TObject,
+    _changes: DbChanges<Organization>,
+    _changeset?: ID
+  ): Promise<TObject> {
+    throw new Error('Method not implemented.');
+  }
+  updateRelation(
+    _relationName: string,
+    _otherLabel: string,
+    _id: ID,
+    _otherId: ID | null
+  ): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  checkDeletePermission(_id: ID, _session: Session | ID): Promise<boolean> {
+    throw new Error('Method not implemented.');
+  }
+  deleteNode(_objectOrId: ID | { id: ID }, _changeset?: ID): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
