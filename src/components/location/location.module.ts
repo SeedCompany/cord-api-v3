c66bab0d921b511932b5e396ae4ec6615ad08b82
Daniel Leo <danielld707@gmail.com>
2022-03-02 19:09:37 -0400

    Add PgLocationRepository

--- src/components/location/location.module.ts
+++ src/components/location/location.module.ts
@@ -1,9 +1,13 @@
 import { forwardRef, Module } from '@nestjs/common';
+import { splitDb } from '../../core';
 import { AuthorizationModule } from '../authorization/authorization.module';
 import { FieldRegionModule } from '../field-region/field-region.module';
 import { FundingAccountModule } from '../funding-account/funding-account.module';
 import { LocationLoader } from './location.loader';
-import { LocationRepository } from './location.repository';
+import {
+  LocationRepository,
+  PgLocationRepository,
+} from './location.repository';
 import { LocationResolver } from './location.resolver';
 import { LocationService } from './location.service';
 
@@ -18,6 +22,8 @@ import { LocationService } from './location.service';
     LocationService,
     LocationRepository,
     LocationLoader,
+    splitDb(LocationRepository, PgLocationRepository),
+    PgLocationRepository,
   ],
   exports: [LocationService],
 })
