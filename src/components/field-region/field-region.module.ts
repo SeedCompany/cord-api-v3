54e94cc83a43806c3a2705384255c9cf61d2434e
Daniel Leo <danielld707@gmail.com>
2022-03-04 18:25:44 -0400

    Add PgFieldRegionRepository

--- src/components/field-region/field-region.module.ts
+++ src/components/field-region/field-region.module.ts
@@ -1,9 +1,13 @@
 import { forwardRef, Module } from '@nestjs/common';
+import { splitDb } from '../../core';
 import { AuthorizationModule } from '../authorization/authorization.module';
 import { FieldZoneModule } from '../field-zone/field-zone.module';
 import { UserModule } from '../user/user.module';
 import { FieldRegionLoader } from './field-region.loader';
-import { FieldRegionRepository } from './field-region.repository';
+import {
+  FieldRegionRepository,
+  PgFieldRegionRepository,
+} from './field-region.repository';
 import { FieldRegionResolver } from './field-region.resolver';
 import { FieldRegionService } from './field-region.service';
 
@@ -18,6 +22,8 @@ import { FieldRegionService } from './field-region.service';
     FieldRegionService,
     FieldRegionRepository,
     FieldRegionLoader,
+    splitDb(FieldRegionRepository, PgFieldRegionRepository),
+    PgFieldRegionRepository,
   ],
   exports: [FieldRegionService],
 })
