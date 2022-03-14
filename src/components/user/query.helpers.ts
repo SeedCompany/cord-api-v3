54347762c240734f262d632ae9f3515dcf005bfb
Carson Full <carson_full@tsco.org>
2022-03-14 18:48:09 -0500

    Fix user list query with pinned filter

--- src/components/user/query.helpers.ts
+++ src/components/user/query.helpers.ts
@@ -1,21 +1,24 @@
 import { node, not, Query, relation } from 'cypher-query-builder';
 import { AndConditions } from 'cypher-query-builder/src/clauses/where-utils';
-import { path } from '../../core/database/query';
+import { Session } from '../../common';
+import { matchRequestingUser, path } from '../../core/database/query';
 import { UserFilters } from './dto';
 
-export const userListFilter = (filter: UserFilters) => (query: Query) => {
-  const conditions: AndConditions = {};
+export const userListFilter =
+  (filter: UserFilters, session: Session) => (query: Query) => {
+    const conditions: AndConditions = {};
 
-  if (filter.pinned != null) {
-    const pinned = path([
-      node('requestingUser'),
-      relation('out', '', 'pinned'),
-      node('node'),
-    ]);
-    conditions.pinned = filter.pinned ? pinned : not(pinned);
-  }
+    if (filter.pinned != null) {
+      matchRequestingUser(session)(query);
+      const pinned = path([
+        node('requestingUser'),
+        relation('out', '', 'pinned'),
+        node('node'),
+      ]);
+      conditions.pinned = filter.pinned ? pinned : not(pinned);
+    }
 
-  if (Object.keys(conditions).length > 0) {
-    query.where(conditions);
-  }
-};
+    if (Object.keys(conditions).length > 0) {
+      query.where(conditions);
+    }
+  };
