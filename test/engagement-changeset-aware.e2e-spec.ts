27b02596ae54a7c43d67760e7c156642092cc6f9
E10001204 <sorrelle04@gmail.com>
2022-03-29 11:55:23 -0500

    oops forgot to uncomment some tests

--- test/engagement-changeset-aware.e2e-spec.ts
+++ test/engagement-changeset-aware.e2e-spec.ts
@@ -1,9 +1,11 @@
 import { gql } from 'apollo-server-core';
 import { CalendarDate, ID } from '../src/common';
 import { Role } from '../src/components/authorization';
+import { EngagementStatus } from '../src/components/engagement';
 import { Language } from '../src/components/language';
 import { ProjectStep } from '../src/components/project';
 import {
+  approveProjectChangeRequest,
   createFundingAccount,
   createLanguage,
   createLanguageEngagement,
@@ -42,6 +44,41 @@ const readEngagements = (app: TestApp, id: string, changeset?: string) =>
         }
       }
       ${fragments.project}
+    `,
+    {
+      id,
+      changeset,
+    }
+  );
+
+const readLanguageEngagement = (app: TestApp, id: string, changeset?: string) =>
+  app.graphql.query(
+    gql`
+      query engagement($id: ID!, $changeset: ID) {
+        engagement(id: $id, changeset: $changeset) {
+          ...languageEngagement
+          changeset {
+            id
+            difference {
+              added {
+                id
+              }
+              removed {
+                id
+              }
+              changed {
+                previous {
+                  id
+                }
+                updated {
+                  id
+                }
+              }
+            }
+          }
+        }
+      }
+      ${fragments.languageEngagement}
     `,
     {
       id,
@@ -130,177 +167,177 @@ describe('Engagement Changeset Aware e2e', () => {
     await app.close();
   });
 
-  // it('Create', async () => {
-  //   const project = await createProject(app);
-  //   await createLanguageEngagement(app, {
-  //     projectId: project.id,
-  //   });
-  //   await activeProject(app, project.id);
-  //   const changeset = await createProjectChangeRequest(app, {
-  //     projectId: project.id,
-  //   });
+  it('Create', async () => {
+    const project = await createProject(app);
+    await createLanguageEngagement(app, {
+      projectId: project.id,
+    });
+    await activeProject(app, project.id);
+    const changeset = await createProjectChangeRequest(app, {
+      projectId: project.id,
+    });
 
-  //   // Create new engagement with changeset
-  //   const changesetEngagement = await app.graphql.mutate(
-  //     gql`
-  //       mutation createLanguageEngagement(
-  //         $input: CreateLanguageEngagementInput!
-  //       ) {
-  //         createLanguageEngagement(input: $input) {
-  //           engagement {
-  //             ...languageEngagement
-  //           }
-  //         }
-  //       }
-  //       ${fragments.languageEngagement}
-  //     `,
-  //     {
-  //       input: {
-  //         engagement: {
-  //           languageId: language.id,
-  //           projectId: project.id,
-  //           status: EngagementStatus.InDevelopment,
-  //         },
-  //         changeset: changeset.id,
-  //       },
-  //     }
-  //   );
-  //   // list engagements without changeset
-  //   let result = await readEngagements(app, project.id);
-  //   expect(result.project.engagements.items.length).toBe(1);
-  //   // list engagements with changeset
-  //   result = await readEngagements(app, project.id, changeset.id);
-  //   expect(result.project.engagements.items.length).toBe(2);
-  //   expect(result.project.engagements.items[1].id).toBe(
-  //     changesetEngagement.createLanguageEngagement.engagement.id
-  //   );
-  //   await approveProjectChangeRequest(app, changeset.id);
-  //   result = await readEngagements(app, project.id);
-  //   expect(result.project.engagements.items.length).toBe(2);
-  // });
+    // Create new engagement with changeset
+    const changesetEngagement = await app.graphql.mutate(
+      gql`
+        mutation createLanguageEngagement(
+          $input: CreateLanguageEngagementInput!
+        ) {
+          createLanguageEngagement(input: $input) {
+            engagement {
+              ...languageEngagement
+            }
+          }
+        }
+        ${fragments.languageEngagement}
+      `,
+      {
+        input: {
+          engagement: {
+            languageId: language.id,
+            projectId: project.id,
+            status: EngagementStatus.InDevelopment,
+          },
+          changeset: changeset.id,
+        },
+      }
+    );
+    // list engagements without changeset
+    let result = await readEngagements(app, project.id);
+    expect(result.project.engagements.items.length).toBe(1);
+    // list engagements with changeset
+    result = await readEngagements(app, project.id, changeset.id);
+    expect(result.project.engagements.items.length).toBe(2);
+    expect(result.project.engagements.items[1].id).toBe(
+      changesetEngagement.createLanguageEngagement.engagement.id
+    );
+    await approveProjectChangeRequest(app, changeset.id);
+    result = await readEngagements(app, project.id);
+    expect(result.project.engagements.items.length).toBe(2);
+  });
 
-  // it('Update', async () => {
-  //   const project = await createProject(app);
-  //   const languageEngagement = await createLanguageEngagement(app, {
-  //     languageId: language.id,
-  //     projectId: project.id,
-  //     status: EngagementStatus.InDevelopment,
-  //   });
-  //   await activeProject(app, project.id);
-  //   const changeset = await createProjectChangeRequest(app, {
-  //     projectId: project.id,
-  //   });
-  //   // Update engagement prop with changeset
-  //   await app.graphql.mutate(
-  //     gql`
-  //       mutation updateLanguageEngagement(
-  //         $input: UpdateLanguageEngagementInput!
-  //       ) {
-  //         updateLanguageEngagement(input: $input) {
-  //           engagement {
-  //             ...languageEngagement
-  //           }
-  //         }
-  //       }
-  //       ${fragments.languageEngagement}
-  //     `,
-  //     {
-  //       input: {
-  //         engagement: {
-  //           id: languageEngagement.id,
-  //           completeDate: CalendarDate.fromISO('2100-08-22'),
-  //         },
-  //         changeset: changeset.id,
-  //       },
-  //     }
-  //   );
+  it('Update', async () => {
+    const project = await createProject(app);
+    const languageEngagement = await createLanguageEngagement(app, {
+      languageId: language.id,
+      projectId: project.id,
+      status: EngagementStatus.InDevelopment,
+    });
+    await activeProject(app, project.id);
+    const changeset = await createProjectChangeRequest(app, {
+      projectId: project.id,
+    });
+    // Update engagement prop with changeset
+    await app.graphql.mutate(
+      gql`
+        mutation updateLanguageEngagement(
+          $input: UpdateLanguageEngagementInput!
+        ) {
+          updateLanguageEngagement(input: $input) {
+            engagement {
+              ...languageEngagement
+            }
+          }
+        }
+        ${fragments.languageEngagement}
+      `,
+      {
+        input: {
+          engagement: {
+            id: languageEngagement.id,
+            completeDate: CalendarDate.fromISO('2100-08-22'),
+          },
+          changeset: changeset.id,
+        },
+      }
+    );
 
-  //   // read engagement without changeset
-  //   let result = await readLanguageEngagement(app, languageEngagement.id);
-  //   expect(result.engagement.completeDate.value !== '2100-08-22').toBeTruthy();
-  //   // read engagement with changeset
-  //   result = await readLanguageEngagement(
-  //     app,
-  //     languageEngagement.id,
-  //     changeset.id
-  //   );
-  //   expect(result.engagement.completeDate.value).toBe('2100-08-22');
-  //   await approveProjectChangeRequest(app, changeset.id);
-  //   result = await readLanguageEngagement(app, languageEngagement.id);
-  //   expect(result.engagement.completeDate.value).toBe('2100-08-22');
-  // });
+    // read engagement without changeset
+    let result = await readLanguageEngagement(app, languageEngagement.id);
+    expect(result.engagement.completeDate.value !== '2100-08-22').toBeTruthy();
+    // read engagement with changeset
+    result = await readLanguageEngagement(
+      app,
+      languageEngagement.id,
+      changeset.id
+    );
+    expect(result.engagement.completeDate.value).toBe('2100-08-22');
+    await approveProjectChangeRequest(app, changeset.id);
+    result = await readLanguageEngagement(app, languageEngagement.id);
+    expect(result.engagement.completeDate.value).toBe('2100-08-22');
+  });
 
-  // it('Update - created in changeset', async () => {
-  //   const project = await createProject(app);
-  //   await activeProject(app, project.id);
-  //   const changeset = await createProjectChangeRequest(app, {
-  //     projectId: project.id,
-  //   });
+  it('Update - created in changeset', async () => {
+    const project = await createProject(app);
+    await activeProject(app, project.id);
+    const changeset = await createProjectChangeRequest(app, {
+      projectId: project.id,
+    });
 
-  //   // Create new engagement with changeset
-  //   const changesetEngagement = await app.graphql.mutate(
-  //     gql`
-  //       mutation createLanguageEngagement(
-  //         $input: CreateLanguageEngagementInput!
-  //       ) {
-  //         createLanguageEngagement(input: $input) {
-  //           engagement {
-  //             ...languageEngagement
-  //           }
-  //         }
-  //       }
-  //       ${fragments.languageEngagement}
-  //     `,
-  //     {
-  //       input: {
-  //         engagement: {
-  //           languageId: language.id,
-  //           projectId: project.id,
-  //           completeDate: CalendarDate.fromISO('2021-09-22'),
-  //         },
-  //         changeset: changeset.id,
-  //       },
-  //     }
-  //   );
-  //   expect(
-  //     changesetEngagement.createLanguageEngagement.engagement.ceremony
-  //   ).toBeDefined();
+    // Create new engagement with changeset
+    const changesetEngagement = await app.graphql.mutate(
+      gql`
+        mutation createLanguageEngagement(
+          $input: CreateLanguageEngagementInput!
+        ) {
+          createLanguageEngagement(input: $input) {
+            engagement {
+              ...languageEngagement
+            }
+          }
+        }
+        ${fragments.languageEngagement}
+      `,
+      {
+        input: {
+          engagement: {
+            languageId: language.id,
+            projectId: project.id,
+            completeDate: CalendarDate.fromISO('2021-09-22'),
+          },
+          changeset: changeset.id,
+        },
+      }
+    );
+    expect(
+      changesetEngagement.createLanguageEngagement.engagement.ceremony
+    ).toBeDefined();
 
-  //   const engagementId =
-  //     changesetEngagement.createLanguageEngagement.engagement.id;
+    const engagementId =
+      changesetEngagement.createLanguageEngagement.engagement.id;
 
-  //   await app.graphql.mutate(
-  //     gql`
-  //       mutation updateLanguageEngagement(
-  //         $input: UpdateLanguageEngagementInput!
-  //       ) {
-  //         updateLanguageEngagement(input: $input) {
-  //           engagement {
-  //             ...languageEngagement
-  //           }
-  //         }
-  //       }
-  //       ${fragments.languageEngagement}
-  //     `,
-  //     {
-  //       input: {
-  //         engagement: {
-  //           id: engagementId,
-  //           completeDate: CalendarDate.fromISO('2100-08-22'),
-  //         },
-  //         changeset: changeset.id,
-  //       },
-  //     }
-  //   );
+    await app.graphql.mutate(
+      gql`
+        mutation updateLanguageEngagement(
+          $input: UpdateLanguageEngagementInput!
+        ) {
+          updateLanguageEngagement(input: $input) {
+            engagement {
+              ...languageEngagement
+            }
+          }
+        }
+        ${fragments.languageEngagement}
+      `,
+      {
+        input: {
+          engagement: {
+            id: engagementId,
+            completeDate: CalendarDate.fromISO('2100-08-22'),
+          },
+          changeset: changeset.id,
+        },
+      }
+    );
 
-  //   // read engagement with changeset
-  //   let result = await readLanguageEngagement(app, engagementId, changeset.id);
-  //   expect(result.engagement.completeDate.value).toBe('2100-08-22');
-  //   // approve changeset
-  //   await approveProjectChangeRequest(app, changeset.id);
-  //   result = await readLanguageEngagement(app, engagementId);
-  //   expect(result.engagement.completeDate.value).toBe('2100-08-22');
-  // });
+    // read engagement with changeset
+    let result = await readLanguageEngagement(app, engagementId, changeset.id);
+    expect(result.engagement.completeDate.value).toBe('2100-08-22');
+    // approve changeset
+    await approveProjectChangeRequest(app, changeset.id);
+    result = await readLanguageEngagement(app, engagementId);
+    expect(result.engagement.completeDate.value).toBe('2100-08-22');
+  });
 
   it('Delete', async () => {
     const project = await createProject(app);
