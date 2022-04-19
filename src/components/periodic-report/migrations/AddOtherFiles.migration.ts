98353678f77fe2b2fbdcaf6c20083bb777ac9a56
Elisabeth Shakespeare <shakespearee04@gmail.com>
2022-04-19 11:58:39 -0500

    fix naming of node variables

--- src/components/periodic-report/migrations/AddOtherFiles.migration.ts
+++ src/components/periodic-report/migrations/AddOtherFiles.migration.ts
@@ -1,12 +1,13 @@
 import { node, not, relation } from 'cypher-query-builder';
 import { initial } from 'lodash';
 import { DateTime } from 'luxon';
+import { generateId } from '../../../common';
 import { BaseMigration, DatabaseService, Migration } from '../../../core';
 import {
   ACTIVE,
   path,
 } from '../../../core/database/query';
-import { Directory } from '../../file';
+import { Directory, FileService } from '../../file';
 import { IPeriodicReport, PeriodicReport } from '../dto';
 
 @Migration('2022-05-19T15:43:26')
@@ -25,19 +26,31 @@ export class AddOtherFiles extends BaseMigration {
           ])
         )
       )
-      .subQuery('collect(report) as reports', (sub) => sub.with('reports')
+      .with('collect(report) as reports')
+      .subQuery('reports', (sub) => sub.with('reports')
         .raw('UNWIND reports as report')
-        .create(node(['Directory'], { initialProps: { name: 'Other Directory' } }))
-          .match(node('user', 'RootUser'))
-          .create([node('node'), relation('out', '', 'createdBy', {
-            createdAt: DateTime.local(), active: true,
-          }), node('user')])
-          .create([node('report'), relation('out', '', 'otherFiles', ACTIVE), node('node')])
-          .return('node as directory')
+        .raw(`
+        // createNode(Directory)
+        CALL {
+            CREATE (node:Directory:FileNode:BaseNode { createdAt: datetime(), id: apoc.create.uuid() }),
+                (node)-[:name { active: true, createdAt: datetime() }]->(:Property { createdAt: datetime(), value: 'Other Directory' }),
+                (node)-[:canDelete { active: true, createdAt: datetime() }]->(:Property { createdAt: datetime(), value: true })
+            RETURN node
+          }
+          // createRelationships(Directory)
+        CALL {
+          WITH node
+          MATCH (createdBy:RootUser)
+          CREATE (node)-[:createdBy { active: true, createdAt: datetime() }]->(createdBy)
+          RETURN createdBy
+        } 
+        `)
+        .create([node('report'), relation('out', '', 'otherFiles', ACTIVE), node('node')])
+        .return('node as directory, report')
         )
       .return<{report: IPeriodicReport, directory: Directory}>(
         'report, directory'
-      ).run();
+      ).logIt().run();
     this.logger.info(
       `Created ${res}`
     );
