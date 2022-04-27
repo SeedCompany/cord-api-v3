27535e02bf77222078eda46ebaf6411e74bf4a5c
Elisabeth Shakespeare <shakespearee04@gmail.com>
2022-04-27 14:57:14 -0500

    try to match the relationships in each subquery. for some reason, just doing it once doesn't work with the rename

--- src/components/file/migrations/ChangeFileToFileRels.migration.ts
+++ src/components/file/migrations/ChangeFileToFileRels.migration.ts
@@ -1,40 +1,45 @@
 import { node, relation } from 'cypher-query-builder';
 import { BaseMigration, Migration } from '../../../core';
 
-@Migration('2022-04-25T14:46:26')
+@Migration('2022-04-27T14:46:26')
 export class ChangeFileToFileRels extends BaseMigration {
   async up() {
     // first, invert the relationship from incoming to outgoing
     const res = await this.db
       .query()
-      .match([
-        node('parent', 'FileNode'),
-        relation('in', 'rel', 'parent'),
-        node('child'),
-      ])
-      .subQuery('rel', (q) =>
+      .subQuery((q) =>
         q
+          .match([
+            node('parent', 'FileNode'),
+            relation('in', 'rel', 'parent'),
+            node('child'),
+          ])
           .raw(`CALL apoc.refactor.invert(rel) yield input, output`)
           .return('input as inversionInput')
       )
-      .subQuery('rel', (q) =>
+      .subQuery((q) =>
         q
+          .match([
+            node('parent', 'FileNode'),
+            relation('out', 'rel', 'parent'),
+            node('child'),
+          ])
           .raw(
             `
           with collect(rel) AS rels
           CALL apoc.refactor.rename.type("parent", "child", rels )
           yield committedOperations`
           )
-          .return('committedOpertions as relRenames')
+          .return('committedOperations as relRenames')
       )
-      .return<{ inversionInput: number; committedOperations: number }>(
-        'committedOperations, inversionInput'
+      .return<{ inversionInput: number; relRenames: number }>(
+        'relRenames, inversionInput'
       )
       .first();
     this.logger.info(
       `Relationships inverted: ${
         res?.inversionInput ?? 0
-      } \nRelationship Names Changed: ${res?.committedOperations ?? 0}`
+      } \nRelationship Names Changed: ${res?.relRenames ?? 0}`
     );
   }
 }
