ed00c6fc491d57d1ffae2ca6fd08135686a65eea
Elisabeth Shakespeare <shakespearee04@gmail.com>
2022-04-27 15:06:29 -0500

    have a separate match for each subquery, and put outside of subqueries because there's multiple relationships being created instead of rename/reverted

--- src/components/file/migrations/ChangeFileToFileRels.migration.ts
+++ src/components/file/migrations/ChangeFileToFileRels.migration.ts
@@ -1,32 +1,32 @@
 import { node, relation } from 'cypher-query-builder';
 import { BaseMigration, Migration } from '../../../core';
 
-@Migration('2022-04-27T14:46:26')
+@Migration('2022-04-28T14:46:26')
 export class ChangeFileToFileRels extends BaseMigration {
   async up() {
     // first, invert the relationship from incoming to outgoing
     const res = await this.db
       .query()
-      .subQuery((q) =>
+      .match([
+        node('parent', 'FileNode'),
+        relation('in', 'rel', 'parent'),
+        node('child'),
+      ])
+      .subQuery('rel', (q) =>
         q
-          .match([
-            node('parent', 'FileNode'),
-            relation('in', 'rel', 'parent'),
-            node('child'),
-          ])
           .raw(`CALL apoc.refactor.invert(rel) yield input, output`)
           .return('input as inversionInput')
       )
-      .subQuery((q) =>
+      .match([
+        node('parent2', 'FileNode'),
+        relation('out', 'rel2', 'parent2'),
+        node('child2'),
+      ])
+      .subQuery('rel2', (q) =>
         q
-          .match([
-            node('parent', 'FileNode'),
-            relation('out', 'rel', 'parent'),
-            node('child'),
-          ])
           .raw(
             `
-          with collect(rel) AS rels
+          with collect(rel2) AS rels
           CALL apoc.refactor.rename.type("parent", "child", rels )
           yield committedOperations`
           )
