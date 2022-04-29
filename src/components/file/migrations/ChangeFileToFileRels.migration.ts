0d44ceedd4deffb2774a778b8fdae8966eb1d2a3
Elisabeth Shakespeare <shakespearee04@gmail.com>
2022-04-29 11:48:59 -0500

    get some more logging statements and fix stuff

--- src/components/file/migrations/ChangeFileToFileRels.migration.ts
+++ src/components/file/migrations/ChangeFileToFileRels.migration.ts
@@ -1,32 +1,51 @@
 import { node, relation } from 'cypher-query-builder';
 import { BaseMigration, Migration } from '../../../core';
 
-@Migration('2022-04-29T17:46:26')
+type ChildOrParentLabel = 'child' | 'parent';
+
+@Migration('2022-04-29T19:46:26')
 export class ChangeFileToFileRels extends BaseMigration {
   async up() {
+    const parentBeforeRefactor = await this.getTotalFileNodeRelsByLabel(
+      'parent'
+    );
+    this.logger.info(
+      `Total :parent rels before refactor: ${parentBeforeRefactor}`
+    );
     // first, invert the relationship from incoming to outgoing
-    const res = await this.db
-      .query()
-      .match([
-        node('parent', 'FileNode'),
-        relation('in', 'rel', 'parent'),
-        node('child'),
-      ])
-      .raw(
+    const stats = await this.db
+      .query<Record<string, any>>(
         `
         CALL apoc.periodic.iterate(
-            'CALL apoc.refactor.setType(rel, 'child') YIELD output as setTypeOutput',
-            'CALL apoc.refactor.invert(setTypeOutput) YIELD output as invertOutput',
+            'MATCH (parent:FileNode)<-[rel:parent]-(child) RETURN rel',
+            'CALL apoc.refactor.setType(rel, "child") YIELD output as setTypeOutput
+             CALL apoc.refactor.invert(setTypeOutput) YIELD output as invertOutput
+             RETURN *',
             { batchSize: 5 }
         )`
       )
-      .return<{ totalInverted: number; totalRenamed: number }>(
-        'size(collect(invertOutput)) as totalInverted, size(collect(setTypeOutput)) as totalRenamed'
-      )
       .first();
-    this.logger.info(`${res?.totalInverted ?? 0} relationships inverted`);
-    this.logger.info(
-      `${res?.totalRenamed ?? 0} relationships renamed from 'parent' to 'child'`
+    this.logger.info('Stats', stats);
+    const parentAfterRefactor = await this.getTotalFileNodeRelsByLabel(
+      'parent'
     );
+    const childAfterRefactor = await this.getTotalFileNodeRelsByLabel('child');
+    this.logger.info(
+      `Total :parent rels before refactor: ${parentAfterRefactor}`
+    );
+    this.logger.info(`Total :child rels after refactor: ${childAfterRefactor}`);
+  }
+  async getTotalFileNodeRelsByLabel(relLabel: ChildOrParentLabel) {
+    const direction = relLabel === 'child' ? 'out' : 'in';
+    const result = await this.db
+      .query()
+      .match([
+        node('parent', 'FileNode'),
+        relation(direction, 'rel', relLabel),
+        node('child'),
+      ])
+      .return<{ totalRels: number }>('count(rel) as totalRels')
+      .first();
+    return result?.totalRels ?? 0;
   }
 }
