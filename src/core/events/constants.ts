1efd0179a5dde710ef5a684ee45cc6dc0b0d7564
Carson Full <carson_full@tsco.org>
2022-05-02 12:31:42 -0500

    Fully reimplement EventBus without @nestjs/cqrs
    We already deviated much from their OOTB experience.
    I think it's over-engineered. We are able to do the same thing with almost the same amount of code.

---
+++ src/core/events/constants.ts
@@ -0,0 +1,6 @@
+import { ID } from '../../common';
+
+export const EVENT_METADATA = '__event__';
+export const EVENTS_HANDLER_METADATA = '__eventsHandler__';
+
+export type EventHandlerMetadata = ID[];
--- src/core/events/event-handler.decorator.ts
+++ src/core/events/event-handler.decorator.ts
@@ -1,9 +1,29 @@
-/* eslint-disable no-restricted-imports */
-import { IEvent } from '@nestjs/cqrs';
+import { Type } from '@nestjs/common';
+import { nanoid } from 'nanoid';
+import { ID } from '../../common';
+import {
+  EVENT_METADATA,
+  EventHandlerMetadata,
+  EVENTS_HANDLER_METADATA,
+} from './constants';
 
-export { EventsHandler } from '@nestjs/cqrs';
+export const EventsHandler =
+  (...events: Type[]): ClassDecorator =>
+  (target) => {
+    const metadata: EventHandlerMetadata = events.map(getOrDefineEventId);
+    Reflect.defineMetadata(EVENTS_HANDLER_METADATA, metadata, target);
+  };
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-export interface IEventHandler<T extends IEvent = any> {
+export interface IEventHandler<T> {
   handle: (event: T) => Promise<void>;
 }
+
+const getOrDefineEventId = (event: Type): ID => {
+  if (!Reflect.hasMetadata(EVENT_METADATA, event)) {
+    const id = nanoid() as ID;
+    Reflect.defineMetadata(EVENT_METADATA, { id }, event);
+    return id;
+  }
+  return Reflect.getMetadata(EVENT_METADATA, event).id;
+};
