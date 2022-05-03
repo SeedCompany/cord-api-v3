cd609709792bb0aec7987655bc8d5cfcbbe47614
Carson Full <carson_full@tsco.org>
2022-05-03 10:24:45 -0500

    Automatically run all gql mutations in neo4j transactions

---
+++ src/core/database/transactional-mutations.interceptor.ts
@@ -0,0 +1,36 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
+import { Connection } from 'cypher-query-builder';
+import { GraphQLResolveInfo } from 'graphql';
+import { lastValueFrom, of } from 'rxjs';
+
+/**
+ * Run all mutations in a neo4j transaction.
+ * This allows automatic rollbacks on error.
+ */
+@Injectable()
+export class TransactionalMutationsInterceptor implements NestInterceptor {
+  constructor(private readonly db: Connection) {}
+
+  async intercept(context: ExecutionContext, next: CallHandler) {
+    if (context.getType<GqlContextType>() !== 'graphql') {
+      return next.handle();
+    }
+
+    const ctx = GqlExecutionContext.create(context);
+    const info = ctx.getInfo<GraphQLResolveInfo>();
+    if (info.operation.operation !== 'mutation') {
+      return next.handle();
+    }
+
+    const res = await this.db.runInTransaction(
+      async () => await lastValueFrom(next.handle())
+    );
+    return of(res);
+  }
+}
