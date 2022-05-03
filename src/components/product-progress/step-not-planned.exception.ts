9ea8f0834fcb7a356a72aa0d78b49d00f414d342
Carson Full <carson_full@tsco.org>
2022-05-03 13:11:23 -0500

    Custom error for step not planned to help consumption

---
+++ src/components/product-progress/step-not-planned.exception.ts
@@ -0,0 +1,12 @@
+import { ID, InputException } from '../../common';
+import { ProductStep } from '../product';
+
+export class StepNotPlannedException extends InputException {
+  constructor(
+    readonly productId: ID,
+    readonly step: ProductStep,
+    readonly index: number
+  ) {
+    super('Step is not planned', `steps.${index}.step`);
+  }
+}
