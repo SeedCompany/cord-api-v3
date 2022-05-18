bab949b0e8204f133d1f0f9a0a5389c30ce0f00d
Bryan Nelson <bryan_nelson@tsco.org>
2022-05-18 13:00:51 -0400

    Added unit test shell.

---
+++ src/components/pnp/isGoalStepPlannedInsideProject.spec.ts
@@ -0,0 +1,13 @@
+describe('stepCompleteDate', () => {
+  it('should return null when fiscal quarter is nullish', () => {
+    //TODO
+  });
+
+  it('should return null when fiscal year is nullish', () => {
+    //TODO
+  });
+
+  it('should return end of full fiscal quarter when valid cell is passed in', () => {
+    //TODO
+  });
+});
