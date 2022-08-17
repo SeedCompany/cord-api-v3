f5cfd4d1d59efee470277004fc360349daa6b637
Carson Full <carson_full@tsco.org>
2022-08-17 14:56:03 -0500

    Implement ResourceLoader
    This sits on top of the existing DataLoaders.
    Each DataLoader is picked based on the type(s) given.

---
+++ src/core/resources/resource.loader.ts
@@ -0,0 +1,90 @@
+import { Inject, Injectable, Scope } from '@nestjs/common';
+import { CONTEXT } from '@nestjs/graphql';
+import { ValueOf } from 'type-fest';
+import {
+  GqlContextType,
+  ID,
+  Many,
+  ObjectView,
+  ServerException,
+} from '../../common';
+import { ResourceMap } from '../../components/authorization/model/resource-map';
+import { DataLoader, LoaderContextType } from '../data-loader';
+import { NEST_LOADER_CONTEXT_KEY } from '../data-loader/constants';
+import { BaseNode } from '../database/results';
+import { ResourceLoaderRegistry } from './loader.registry';
+import { ResourceResolver } from './resource-resolver.service';
+
+type SomeResourceType = ValueOf<ResourceMap>;
+
+interface ObjectRef<Key extends keyof ResourceMap> {
+  __typename: Key;
+  id: ID;
+}
+
+@Injectable({ scope: Scope.REQUEST })
+export class ResourceLoader {
+  constructor(
+    private readonly loaderRegistry: ResourceLoaderRegistry,
+    @Inject(CONTEXT)
+    private readonly context: GqlContextType & {
+      [NEST_LOADER_CONTEXT_KEY]: LoaderContextType;
+    },
+    private readonly resourceResolver: ResourceResolver
+  ) {}
+
+  async loadByBaseNode(node: BaseNode, view?: ObjectView) {
+    // @ts-expect-error we are expecting at least one of the labels to be one
+    // of our registered resources. If this turns out to be false a runtime
+    // error is thrown.
+    // I like this better than allowing string on load argument.
+    const types: Array<keyof ResourceMap> = node.labels;
+    return await this.load(types, node.properties.id, view);
+  }
+
+  async loadByRef<Key extends keyof ResourceMap>(
+    obj: ObjectRef<Key>,
+    view?: ObjectView
+  ) {
+    return await this.load(obj.__typename, obj.id, view);
+  }
+
+  async load<TResource extends SomeResourceType>(
+    type: TResource,
+    id: ID,
+    view?: ObjectView
+  ): Promise<TResource['prototype']>;
+  async load<TResourceName extends keyof ResourceMap>(
+    type: TResourceName,
+    id: ID,
+    view?: ObjectView
+  ): Promise<ResourceMap[TResourceName]['prototype']>;
+  async load(
+    type: Many<keyof ResourceMap | SomeResourceType>,
+    id: ID,
+    view?: ObjectView
+  ): Promise<SomeResourceType['prototype']>;
+  async load(
+    type: Many<keyof ResourceMap | SomeResourceType>,
+    id: ID,
+    view?: ObjectView
+  ): Promise<SomeResourceType['prototype']> {
+    const resolvedType = this.resourceResolver.resolveType(type);
+    if (!this.loaderRegistry.loaders.has(resolvedType)) {
+      throw new ServerException(
+        `Could not find loader for type: ${resolvedType}`
+      );
+    }
+    const { factory, objectViewAware } =
+      this.loaderRegistry.loaders.get(resolvedType)!;
+    const loader: DataLoader<any, any> = await this.context[
+      NEST_LOADER_CONTEXT_KEY
+    ].getLoader(factory);
+    const key = objectViewAware ? { id, view: view ?? { active: true } } : id;
+    const result = await loader.load(key);
+    return {
+      __typename: resolvedType, // Add typename so that Resource.resolveType can work.
+      ...result,
+    };
+  }
+}
--- src/core/resources/resource.module.ts
+++ src/core/resources/resource.module.ts
@@ -2,11 +2,12 @@ import { Global, Module } from '@nestjs/common';
 import { GraphqlModule } from '../graphql';
 import { ResourceLoaderRegistry } from './loader.registry';
 import { ResourceResolver } from './resource-resolver.service';
+import { ResourceLoader } from './resource.loader';
 
 @Global()
 @Module({
   imports: [GraphqlModule],
-  providers: [ResourceResolver, ResourceLoaderRegistry],
-  exports: [ResourceResolver],
+  providers: [ResourceResolver, ResourceLoaderRegistry, ResourceLoader],
+  exports: [ResourceResolver, ResourceLoader],
 })
 export class ResourceModule {}
