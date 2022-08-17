82a1060099eea1d371a76cd7874d33f3a58703f2
Carson Full <carson_full@tsco.org>
2022-08-17 10:22:01 -0500

    Create registry to discover and hold resource loaders
    FYI nestjs-discovery doesn't work with request scoped services

--- src/core/resources/resource.module.ts
+++ src/core/resources/resource.module.ts
@@ -1,11 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { GraphqlModule } from '../graphql';
+import { ResourceLoaderRegistry } from './loader.registry';
 import { ResourceResolver } from './resource-resolver.service';
 
 @Global()
 @Module({
   imports: [GraphqlModule],
-  providers: [ResourceResolver],
+  providers: [ResourceResolver, ResourceLoaderRegistry],
   exports: [ResourceResolver],
 })
 export class ResourceModule {}
