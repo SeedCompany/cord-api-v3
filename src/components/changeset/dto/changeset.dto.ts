4fb778ec2763daaea9f24bf935dce96e3fb8232a
Carson Full <carson_full@tsco.org>
2022-08-18 10:22:30 -0500

    Stub two fields for Changeset: finalized & applied
    These communicate a low level status that I believe is safe to have for any implementation.

--- src/components/changeset/dto/changeset.dto.ts
+++ src/components/changeset/dto/changeset.dto.ts
@@ -1,4 +1,5 @@
-import { InterfaceType } from '@nestjs/graphql';
+import { Field, InterfaceType } from '@nestjs/graphql';
+import { stripIndent } from 'common-tags';
 import { keys as keysOf } from 'ts-transformer-keys';
 import { Resource, SecuredProps } from '../../../common';
 
@@ -10,4 +11,17 @@ export class Changeset extends Resource {
   static readonly Props: string[] = keysOf<Changeset>();
   static readonly SecuredProps: string[] = keysOf<SecuredProps<Changeset>>();
   __typename: string;
+
+  @Field({
+    description: 'Whether this changeset is editable or finalized',
+  })
+  finalized: boolean;
+
+  @Field({
+    description: stripIndent`
+      Whether the changes have been applied to live data.
+      This probably assumes \`finalized\` is true as well.
+    `,
+  })
+  applied: boolean;
 }
