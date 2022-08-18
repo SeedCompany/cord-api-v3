0cbf14c33f47042eec193c881ccb0d7e5cc55d8f
Daniel Leo <danielld707@gmail.com>
2022-08-18 13:31:30 -0400

    Update Comment/CommentThread loaders

--- src/components/comments/comment-thread.loader.ts
+++ src/components/comments/comment-thread.loader.ts
@@ -1,10 +1,9 @@
-import { Injectable, Scope } from '@nestjs/common';
 import { ID } from '../../common';
-import { OrderedNestDataLoader } from '../../core';
+import { LoaderFactory, OrderedNestDataLoader } from '../../core';
 import { CommentService } from './comment.service';
 import { CommentThread } from './dto';
 
-@Injectable({ scope: Scope.REQUEST })
+@LoaderFactory()
 export class CommentThreadLoader extends OrderedNestDataLoader<CommentThread> {
   constructor(private readonly commentThreads: CommentService) {
     super();
--- src/components/comments/comment.loader.ts
+++ src/components/comments/comment.loader.ts
@@ -1,10 +1,10 @@
-import { Injectable, Scope } from '@nestjs/common';
 import { ID } from '../../common';
 import { OrderedNestDataLoader } from '../../core';
+import { LoaderFactory } from '../../core/resources/loader.registry';
 import { CommentService } from './comment.service';
 import { Comment } from './dto';
 
-@Injectable({ scope: Scope.REQUEST })
+@LoaderFactory()
 export class CommentLoader extends OrderedNestDataLoader<Comment> {
   constructor(private readonly comments: CommentService) {
     super();
