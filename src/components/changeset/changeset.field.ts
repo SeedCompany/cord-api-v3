278065973f4296a99b559dbe172f3cf91c9f38dd
Carson Full <carson_full@tsco.org>
2022-08-19 16:28:00 -0500

    Define custom changeset input field decorator & mark property as needing editability validation

---
+++ src/components/changeset/changeset.field.ts
@@ -0,0 +1,20 @@
+import { applyDecorators } from '@nestjs/common';
+import { Field, FieldOptions, ID as IDType } from '@nestjs/graphql';
+import { stripIndent } from 'common-tags';
+import { ValidateEditability } from './validate-editability.decorator';
+
+/**
+ * A changeset input field
+ */
+export const ChangesetIdField = (options?: FieldOptions): PropertyDecorator =>
+  applyDecorators(
+    Field(() => IDType, {
+      nullable: true,
+      description: stripIndent`
+        A changeset ID to reference as the version to load
+        or associate these changes with.
+      `,
+      ...options,
+    }),
+    ValidateEditability()
+  );
