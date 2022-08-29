6b81102e3499a09b7ff11dc07d9a54aec55e3180
Carson Full <carson_full@tsco.org>
2022-08-29 12:09:41 -0500

    Refactor e2e gql client to fully decouple from src code
    Now we make a POST request to the graphql endpoint,
    which is exactly how any consumer would do it.
    This saves us lots of business logic that we were doing before.
    
    Previously we were jumping halfway into the server execution skipping some src code.
    We in turn had to reimplement that code here (tracing, etc) and fake requests.
    
    Also, because of this error handling was unique and confusing.
    Now we to assert errors from the JSON response, which is better for e2e tests.

--- test/utility/create-app.ts
+++ test/utility/create-app.ts
@@ -1,13 +1,11 @@
 import { faker } from '@faker-js/faker';
 import { INestApplication } from '@nestjs/common';
-import { GRAPHQL_MODULE_OPTIONS } from '@nestjs/graphql/dist/graphql.constants';
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../../src/app.module';
 import { LogLevel } from '../../src/core/logger';
 import { LevelMatcher } from '../../src/core/logger/level-matcher';
 import {
   createGraphqlClient,
-  getGraphQLOptions,
   GraphQLTestClient,
 } from './create-graphql-client';
 
@@ -24,8 +22,6 @@ export const createTestApp = async () => {
   const moduleFixture = await Test.createTestingModule({
     imports: [AppModule],
   })
-    .overrideProvider(GRAPHQL_MODULE_OPTIONS)
-    .useValue(getGraphQLOptions())
     .overrideProvider(LevelMatcher)
     .useValue(new LevelMatcher({}, LogLevel.ERROR))
     .compile();
