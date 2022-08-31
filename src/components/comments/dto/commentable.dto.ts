2c25ea05b57a58ea62c95bf5ac329ea342bcf624
Carson Full <carson_full@tsco.org>
2022-08-31 09:26:28 -0500

    Enforce that Commentable objects also implement Resource

--- src/components/comments/dto/commentable.dto.ts
+++ src/components/comments/dto/commentable.dto.ts
@@ -1,23 +1,18 @@
 import { InterfaceType } from '@nestjs/graphql';
-import { stripIndent } from 'common-tags';
 import { keys as keysOf } from 'ts-transformer-keys';
-import { ID, IdField, SecuredProps } from '~/common';
+import { Resource, SecuredProps } from '~/common';
 import { CommentThread } from './comment-thread.dto';
 
 @InterfaceType({
-  description: stripIndent`
-    An object that can be used to enable comment threads on a Node.
-  `,
+  description: 'A resource that can be commented on',
+  implements: [Resource],
 })
-export abstract class Commentable {
+export abstract class Commentable extends Resource {
   static readonly Props: string[] = keysOf<Commentable>();
   static readonly SecuredProps: string[] = keysOf<SecuredProps<Commentable>>();
   static readonly Relations = {
     commentThreads: [CommentThread],
   };
 
-  @IdField({
-    description: "The object's ID",
-  })
-  readonly id: ID;
+  __typename: string;
 }
