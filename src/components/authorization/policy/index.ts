6010346236100082407c4c505dbf82dd7b23d5bd
Carson Full <carson_full@tsco.org>
2022-09-09 16:50:21 -0500

    Add `Role.assignable` helper to convey which roles are assignable in policies in a more readable way

--- src/components/authorization/policy/index.ts
+++ src/components/authorization/policy/index.ts
@@ -1,3 +1,4 @@
 export { Policy } from './builder/policy.decorator';
+export { ResourcesGranter } from './builder/resource-granter';
 export * from './executor/privileges';
 export * from './executor/resource-privileges';
