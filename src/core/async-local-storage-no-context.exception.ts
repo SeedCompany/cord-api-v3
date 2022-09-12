89b241da979f12c3a51e6de68b51ce6579ed5726
Carson Full <carson_full@tsco.org>
2022-09-12 09:09:59 -0500

    Tweak exception message to reference 18.9 fix

--- src/core/async-local-storage-no-context.exception.ts
+++ src/core/async-local-storage-no-context.exception.ts
@@ -4,10 +4,8 @@ export class AsyncLocalStorageNoContextException extends Error {
     if (major === 18) {
       message += `
 
-There's a bug with this process when using Node 18 with debugger ATTACHED (just listening is fine).
-Please downgrade to Node 16 for now if you want to debug and are encountering this.
-
-$ brew unlink node && brew install node@16 && brew link --overwrite node@16
+There's a bug with this process when using Node 18.8 or lower with debugger ATTACHED (just listening is fine).
+Please upgrade to Node 18.9+ if you want to debug and are encountering this.
 `;
     }
 
