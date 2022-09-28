00f4567fe7ac12f3175c544b5fc748d73e0dc7e4
Carson Full <carson_full@tsco.org>
2022-09-28 09:36:14 -0500

    Implement owner condition (still only User) in cypher

--- src/components/authorization/policies/conditions/owner.condition.ts
+++ src/components/authorization/policies/conditions/owner.condition.ts
@@ -2,7 +2,13 @@ import { Query } from 'cypher-query-builder';
 import { inspect, InspectOptionsStylized } from 'util';
 import { ResourceShape } from '~/common';
 import { User } from '../../../user/dto';
-import { Condition, IsAllowedParams } from '../../policy/conditions';
+import {
+  AsCypherParams,
+  Condition,
+  IsAllowedParams,
+} from '../../policy/conditions';
+
+const CQL_VAR = 'requestingUser';
 
 class OwnerCondition<TResourceStatic extends ResourceShape<any> & typeof User>
   implements Condition<TResourceStatic>
@@ -15,8 +21,24 @@ class OwnerCondition<TResourceStatic extends ResourceShape<any> & typeof User>
     return resource.is(User) && object.id === session.userId;
   }
 
-  asCypherCondition(_query: Query) {
-    return `false`; // stubbed
+  setupCypherContext(
+    query: Query,
+    prevApplied: Set<any>,
+    other: AsCypherParams<TResourceStatic>
+  ) {
+    if (prevApplied.has('owner')) {
+      return query;
+    }
+    prevApplied.add('owner');
+
+    const param = query.params.addParam(other.session.userId, CQL_VAR);
+    Reflect.set(other, CQL_VAR, param);
+
+    return query;
+  }
+
+  asCypherCondition(_query: Query, other: AsCypherParams<TResourceStatic>) {
+    return `node:User AND node.id = ${String(Reflect.get(other, CQL_VAR))}`;
   }
 
   [inspect.custom](_depth: number, _options: InspectOptionsStylized) {
