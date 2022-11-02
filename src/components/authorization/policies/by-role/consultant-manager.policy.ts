e810408bbb63741a9579c4140cd4e234ff0c4d2d
Bryan Nelson <bryan_nelson@tsco.org>
2022-11-02 17:14:13 -0400

    Code cleanup.

--- src/components/authorization/policies/by-role/consultant-manager.policy.ts
+++ src/components/authorization/policies/by-role/consultant-manager.policy.ts
@@ -24,22 +24,22 @@ import { member, Policy, Role, sensMediumOrLower, sensOnlyLow } from '../util';
     .specifically((p) => p.pointOfContact.none)
     .children((r) => r.posts.edit),
   r.Partnership.read.specifically((p) => [
-    p.partner.whenAny(member, sensOnlyLow).read,
-    p.organization.whenAny(member, sensOnlyLow).read,
-    // clean up above
+    p.many('partner', 'organization').whenAny(member, sensOnlyLow).read,
   ]),
   r.Project.read
     .specifically((p) => [
-      p.step.edit,
-      p.stepChangedAt.edit,
+      p.many('step', 'stepChangedAt').edit,
+      p
+        .many(
+          'financialReportPeriod',
+          'financialReportReceivedAt',
+          'otherLocations',
+          'primaryLocation',
+          'sensitivity'
+        )
+        .whenAny(member, sensMediumOrLower).read,
       p.rootDirectory.whenAny(member, sensMediumOrLower).edit,
-      p.primaryLocation.whenAny(member, sensMediumOrLower).read,
-      p.financialReportReceivedAt.whenAny(member, sensMediumOrLower).read,
-      p.financialReportPeriod.whenAny(member, sensMediumOrLower).read,
-      p.sensitivity.whenAny(member, sensMediumOrLower).read,
-      p.otherLocations.whenAny(member, sensMediumOrLower).read,
     ])
-    // clean up above
     .children((c) => [c.posts.edit]),
   r.ProjectMember.edit,
 ])
