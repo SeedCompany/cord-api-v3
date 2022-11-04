d9bd7999cdb11ad866e665aec83cbbaa176f14ee
Carson Full <carson_full@tsco.org>
2022-11-04 12:47:17 -0500

    Add method to grab variant by key from list or throw

--- src/components/prompts/dto/variant.dto.ts
+++ src/components/prompts/dto/variant.dto.ts
@@ -1,6 +1,6 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { stripIndent } from 'common-tags';
-import { entries, IdField, ResourceShape } from '~/common';
+import { entries, IdField, InputException, ResourceShape } from '~/common';
 import { Role } from '../../authorization';
 
 @ObjectType()
@@ -28,10 +28,28 @@ export class Variant<Key extends string = string> {
 
   static createList<VKey extends string>(
     map: Record<VKey, Omit<Variant, 'key'>>
-  ): ReadonlyArray<Variant<VKey>> {
-    return entries(map).map(([key, val]) => ({ key, ...val }));
+  ): VariantList<VKey> {
+    const list = entries(map).map(([key, val]) => ({ key, ...val }));
+    Object.defineProperty(list, 'byKey', {
+      enumerable: false,
+      value: (key: VKey) => {
+        const found = list.find((v) => v.key === key);
+        if (!found) {
+          throw new InputException(
+            `Variant with key "${key}" was not found`,
+            'variant'
+          );
+        }
+        return found;
+      },
+    });
+    return list as any;
   }
 }
+
+export type VariantList<VKey extends string> = ReadonlyArray<Variant<VKey>> & {
+  byKey: (key: VKey) => Variant<VKey>;
+};
 
 export type VariantKeyOf<T> = T extends Variant<infer K> ? K : never;
 
