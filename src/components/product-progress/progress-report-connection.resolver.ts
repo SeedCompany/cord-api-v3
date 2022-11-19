2594cd2e0ea32f18076214f54a099f203b6adf28
Carson Full <carson_full@tsco.org>
2022-11-18 18:32:11 -0600

    Add progressForAllVariants field
    I don't really like this multi-level array, but
    it seems the easiest right now.

--- src/components/product-progress/progress-report-connection.resolver.ts
+++ src/components/product-progress/progress-report-connection.resolver.ts
@@ -1,7 +1,12 @@
 import { Args, Parent, ResolveField, Resolver } from '@nestjs/graphql';
-import { Loader, LoaderOf } from '../../core';
+import { ClientException } from '~/common';
+import { Loader, LoaderOf } from '~/core';
 import { ProgressReport } from '../progress-report/dto';
-import { ProductProgress, VariantProgressArg } from './dto';
+import {
+  ProductProgress,
+  ProgressReportVariantProgress as Progress,
+  VariantProgressArg,
+} from './dto';
 import { ProductProgressByReportLoader } from './product-progress-by-report.loader';
 
 @Resolver(() => ProgressReport)
@@ -16,5 +21,25 @@ export class ProgressReportConnectionResolver {
     loader: LoaderOf<ProductProgressByReportLoader>
   ): Promise<readonly ProductProgress[]> {
     return (await loader.load({ report, variant })).details;
+  }
+
+  @ResolveField(() => [[ProductProgress]])
+  async progressForAllVariants(
+    @Parent() report: ProgressReport,
+    @Loader(ProductProgressByReportLoader)
+    loader: LoaderOf<ProductProgressByReportLoader>
+  ): Promise<ReadonlyArray<readonly ProductProgress[]>> {
+    const detailsOrErrors = await loader.loadMany(
+      Progress.Variants.map((variant) => ({ report, variant }))
+    );
+    return detailsOrErrors.flatMap((entry) => {
+      if (entry instanceof Error) {
+        if (entry instanceof ClientException) {
+          return [];
+        }
+        throw entry;
+      }
+      return [entry.details];
+    });
   }
 }
