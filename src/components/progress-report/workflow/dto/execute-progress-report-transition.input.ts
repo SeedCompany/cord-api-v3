8c00131f98b4d8337b5aa7d84922e782d92981bf
Carson Full <carson_full@tsco.org>
2022-11-21 11:45:21 -0600

    Add some more docs to progress report transition input

--- src/components/progress-report/workflow/dto/execute-progress-report-transition.input.ts
+++ src/components/progress-report/workflow/dto/execute-progress-report-transition.input.ts
@@ -1,28 +1,35 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { stripIndent } from 'common-tags';
 import { ID, IdField, RichTextDocument, RichTextField } from '~/common';
 import { ProgressReportStatus } from '../../dto';
 
 @InputType()
 export abstract class ExecuteProgressReportTransitionInput {
   @IdField({
-    name: 'report',
+    description: 'The progress report ID to transition',
   })
   readonly report: ID;
 
   @IdField({
-    name: 'transition',
-    description: 'Execute this transition',
+    description: stripIndent`
+      Execute this transition.
+      This is required unless specifying bypassing the workflow with a \`status\` input.
+    `,
     nullable: true,
   })
   readonly transition?: ID;
 
   @Field(() => ProgressReportStatus, {
-    description: 'Bypass the workflow, and go straight to this status.',
+    description: stripIndent`
+      Bypass the workflow, and go straight to this status.
+      \`transition\` is not required and ignored when using this.
+    `,
     nullable: true,
   })
   readonly status?: ProgressReportStatus;
 
   @RichTextField({
+    description: 'Any additional user notes related to this transition',
     nullable: true,
   })
   readonly notes?: RichTextDocument;
