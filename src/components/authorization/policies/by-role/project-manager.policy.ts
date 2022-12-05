8cb509640d1abefb999ce2cfea853b677d773c9c
Carson Full <carson_full@tsco.org>
2022-12-05 16:05:45 -0600

    DRY FPM Highlights & Stories

--- src/components/authorization/policies/by-role/project-manager.policy.ts
+++ src/components/authorization/policies/by-role/project-manager.policy.ts
@@ -54,24 +54,17 @@ import {
     r.Producible.edit.create,
     r.Product.read.when(member).edit.create.delete,
     r.ProgressReport.when(member).edit,
-    [r.ProgressReportCommunityStory].flatMap((it) => [
-      it.read,
-      it.when(member).create,
-      it.specifically((p) => [
-        p.responses.whenAll(sensOnlyLow, variant('fpm', 'published')).read,
-        p.responses.whenAll(member, variant('translated')).read,
-        p.responses.whenAll(member, variant('fpm')).edit,
-      ]),
-    ]),
-    [r.ProgressReportHighlight].flatMap((it) => [
-      it.read,
-      it.when(member).create,
-      it.specifically((p) => [
-        p.responses.whenAll(sensOnlyLow, variant('fpm', 'published')).read,
-        p.responses.whenAll(member, variant('translated')).read,
-        p.responses.whenAll(member, variant('fpm')).edit,
-      ]),
-    ]),
+    [r.ProgressReportCommunityStory, r.ProgressReportHighlight].flatMap(
+      (it) => [
+        it.read,
+        it.when(member).create,
+        it.specifically((p) => [
+          p.responses.whenAll(sensOnlyLow, variant('fpm', 'published')).read,
+          p.responses.whenAll(member, variant('translated')).read,
+          p.responses.whenAll(member, variant('fpm')).edit,
+        ]),
+      ]
+    ),
     r.Project.read.create
       .when(member)
       .edit.specifically((p) => [
