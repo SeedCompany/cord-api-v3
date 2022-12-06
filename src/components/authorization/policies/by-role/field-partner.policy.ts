9649ab24152b1495d4657f629c84daeb884d6d23
Bryan Nelson <bryan_nelson@tsco.org>
2022-12-06 16:30:54 -0500

    Fleshed out field partner policy per business direction.

--- src/components/authorization/policies/by-role/field-partner.policy.ts
+++ src/components/authorization/policies/by-role/field-partner.policy.ts
@@ -1,6 +1,17 @@
 import { member, Policy, Role, variant } from '../util';
 
 @Policy(Role.FieldPartner, (r) => [
+  r.Ceremony.read,
+  r.Engagement.when(member).read,
+  r.EthnologueLanguage.when(member).read,
+  r.FieldRegion.read,
+  r.FieldZone.read,
+  r.Language.when(member).read,
+  r.Organization.when(member).read.specifically((p) => p.address.none),
+  r.Partner.when(member).read.specifically((p) => p.pointOfContact.none),
+  r.Partnership.when(member).read,
+  r.PeriodicReport.read,
+  r.Product.read,
   r.ProgressReport.when(member).read.specifically((p) => p.reportFile.none),
   [r.ProgressReportCommunityStory, r.ProgressReportHighlight].flatMap((it) => [
     it.when(member).create.read,
@@ -9,6 +20,10 @@ import { member, Policy, Role, variant } from '../util';
       p.responses.whenAll(member, variant('draft')).edit,
     ]),
   ]),
+  r.Project.when(member).read.specifically((p) => p.rootDirectory.none),
+  r.ProjectMember.when(member).read,
   r.StepProgress.whenAll(member, variant('partner')).edit,
+  r.Unavailability.read,
+  r.User.read,
 ])
 export class FieldPartnerPolicy {}
