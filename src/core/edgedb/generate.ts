0372d82a4006235b7ded13bc139a72df3450ddbb
Carson Full <carson_full@tsco.org>
2023-06-21 18:23:41 -0500

    Generate .edgeql.ts files from .edgeql files

--- src/core/edgedb/generate.ts
+++ src/core/edgedb/generate.ts
@@ -1,7 +1,12 @@
 /* eslint-disable no-console */
 import { generateQueryBuilder } from '@edgedb/generate/dist/edgeql-js.js';
+import { headerComment } from '@edgedb/generate/dist/genutil.js';
 import { runInterfacesGenerator as generateTsSchema } from '@edgedb/generate/dist/interfaces.js';
-import { Client, createClient } from 'edgedb';
+import {
+  generateFiles,
+  stringifyImports,
+} from '@edgedb/generate/dist/queries.js';
+import { $, adapter, Client, createClient } from 'edgedb';
 
 (async () => {
   const client = createClient({
@@ -45,4 +50,45 @@ async function generateAll({ client }: { client: Client }) {
     client,
     root,
   });
+
+  await generateQueryFiles({ client, root });
+}
+
+async function generateQueryFiles({
+  client,
+  root,
+}: {
+  client: Client;
+  root: string;
+}) {
+  const srcDir = adapter.path.join(root, 'src');
+  const files = await adapter.walk(srcDir, {
+    match: [/[^/]\.edgeql$/],
+  });
+  async function generateFilesForQuery(path: string) {
+    try {
+      const query = await adapter.readFileUtf8(path);
+      if (!query) return;
+      const types = await $.analyzeQuery(client, query);
+      const [{ imports, contents }] = generateFiles({
+        target: 'ts',
+        path,
+        types,
+      });
+      const prettyPath = './' + adapter.path.posix.relative(srcDir, path);
+      console.log(`   ${prettyPath}`);
+      await adapter.fs.writeFile(
+        path + '.ts',
+        headerComment + `${stringifyImports(imports)}\n\n${contents}`,
+      );
+    } catch (err) {
+      console.log(
+        `Error in file './${adapter.path.posix.relative(root, path)}': ${String(
+          err,
+        )}`,
+      );
+    }
+  }
+  console.log(`Generating files for following queries:`);
+  await Promise.all(files.map(generateFilesForQuery));
 }
