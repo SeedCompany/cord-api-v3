d1d4b1ce0790fc3e6f50e6645e8f410584f94ae4
Carson Full <carson_full@tsco.org>
2023-06-22 19:34:00 -0500

    Add authentication queries/repository

--- src/core/edgedb/index.ts
+++ src/core/edgedb/index.ts
@@ -1,4 +1,5 @@
 export * from './reexports';
 export { edgeql, EdgeQLArgsOf, EdgeQLReturnOf } from './edgeql';
 export * from './edgedb.service';
-export { default as e } from './generated-client';
+export { default as e, $infer } from './generated-client';
+export * from './withScope';
