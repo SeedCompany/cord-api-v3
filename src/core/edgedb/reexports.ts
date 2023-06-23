ad6d909d4d75e7a261e49e9d7c52411512e1b950
Carson Full <carson_full@tsco.org>
2023-06-23 17:13:58 -0500

    Adjust EdgeDb injection symbol so it works with REPL

--- src/core/edgedb/reexports.ts
+++ src/core/edgedb/reexports.ts
@@ -1,6 +1,12 @@
+import { Transaction } from 'edgedb/dist/transaction';
+
 // Only use this if you need the extra methods.
 // Otherwise, for querying, use EdgeDb from below.
 export { Client } from 'edgedb/dist/baseClient';
+
 // Using this as it's a runtime symbol for the Executor TS shape
 // which makes it perfect for injection.
-export { Transaction as EdgeDb } from 'edgedb/dist/transaction';
+// @ts-expect-error private constructor; doesn't matter that's not how we are using it.
+// We could just export the Transaction as an aliased named, but this also
+// allows REPL to reference it with the correct name.
+export abstract class EdgeDb extends Transaction {}
