e9f07e99c9fe737b358c60bb84c3370a09f97d7a
Andre Turner <79720641+atGit2021@users.noreply.github.com>
2023-07-13 10:52:38 -0500

    Added hybrid financial reporting type (#2814)

--- src/components/partnership/dto/financial-reporting-type.ts
+++ src/components/partnership/dto/financial-reporting-type.ts
@@ -3,6 +3,7 @@ import { registerEnumType } from '@nestjs/graphql';
 export enum FinancialReportingType {
   Funded = 'Funded',
   FieldEngaged = 'FieldEngaged',
+  Hybrid = 'Hybrid',
 }
 
 registerEnumType(FinancialReportingType, { name: 'FinancialReportingType' });
