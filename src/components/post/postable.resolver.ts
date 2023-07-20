5e239684d53a63e5abfd369fac57c01125b7cb04
Brent <brentkulwicki@gmail.com>
2023-07-20 13:16:59 -0400

    fix linting

--- src/components/post/postable.resolver.ts
+++ src/components/post/postable.resolver.ts
@@ -1,12 +1,6 @@
 import { Info, Parent, ResolveField, Resolver } from '@nestjs/graphql';
 import { GraphQLResolveInfo } from 'graphql';
-import {
-  EnhancedResource,
-  ListArg,
-  LoggedInSession,
-  Resource,
-  Session,
-} from '~/common';
+import { ListArg, LoggedInSession, Resource, Session } from '~/common';
 import { Loader, LoaderOf, ResourcesHost } from '~/core';
 import { Postable } from './dto';
 import { PostListInput, SecuredPostList } from './dto/list-posts.dto';
