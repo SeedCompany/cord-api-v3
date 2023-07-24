29fbcf95d92472b8822d9614d552ed97102b7472
Carson Full <carson_full@tsco.org>
2023-07-24 10:25:42 -0500

    Fix email lower case transform overruling explicit null input
    null -> "" with lodash function, which was then an invalid email

--- src/common/email-field.ts
+++ src/common/email-field.ts
@@ -1,12 +1,11 @@
 import { applyDecorators } from '@nestjs/common';
 import { Field, FieldOptions } from '@nestjs/graphql';
-import { toLower } from 'lodash';
 import { Transform } from './transform.decorator';
 import { IsEmail } from './validators';
 
 export const EmailField = (options: FieldOptions = {}) =>
   applyDecorators(
     Field(() => String, options),
-    Transform(({ value }) => toLower(value)),
+    Transform(({ value }) => value?.toLowerCase()),
     IsEmail(),
   );
