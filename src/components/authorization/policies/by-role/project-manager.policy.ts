c679172df826c8ca54399962380dc47b15ef2d42
Brent <brentkulwicki@gmail.com>
2023-07-24 17:26:48 -0400

    Revert "updates posts with Carson's changes"
    This reverts commit d5cadda954cd5332cf553c057be7d6e38e15f1f9.

--- src/components/authorization/policies/by-role/project-manager.policy.ts
+++ src/components/authorization/policies/by-role/project-manager.policy.ts
@@ -87,7 +87,6 @@ import {
           .whenAny(member, sensMediumOrLower).read,
       ])
       .children((c) => c.posts.read.create),
-    r.Post.read,
     r.ProjectMember.read.when(member).edit.create.delete,
     [r.StepProgress].flatMap((it) => [
       it.whenAll(member, variant('partner')).read,
