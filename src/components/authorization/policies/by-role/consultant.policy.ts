e36087b54257780d931c1254bb48881b5c4376ee
Carson Full <carson_full@tsco.org>
2023-08-02 08:47:55 -0500

    Consultant policy - group up membership read only perms

--- src/components/authorization/policies/by-role/consultant.policy.ts
+++ src/components/authorization/policies/by-role/consultant.policy.ts
@@ -1,31 +1,35 @@
-import { inherit, member, Policy, Role } from '../util';
+import { member, Policy, Role } from '../util';
 
 // NOTE: There could be other permissions for this role from other policies
 @Policy([Role.Consultant, Role.ConsultantManager], (r) => [
   r.Ceremony.read,
-  inherit(
-    r.Engagement.read,
-    r.InternshipEngagement.when(member).edit.specifically(
-      (p) => p.ceremony.none,
-    ),
-    r.LanguageEngagement.when(member).read.specifically(
-      (p) => p.many('pnp', 'paratextRegistryId').edit,
-    ),
-  ),
-  r.EthnologueLanguage.when(member).read,
+  r.Engagement.read,
+  [
+    r.EthnologueLanguage,
+    r.Language,
+    r.Organization,
+    r.Partner,
+    r.Partnership,
+    r.Project,
+    r.ProjectMember,
+  ].map((it) => it.when(member).read),
+
+  r.InternshipEngagement.when(member).edit.specifically((p) => [
+    p.ceremony.none,
+  ]),
+  r.LanguageEngagement.when(member).read.specifically((p) => [
+    p.many('pnp', 'paratextRegistryId').edit,
+  ]),
   r.FieldRegion.read,
   r.FieldZone.read,
-  r.Language.when(member).read,
   r.NarrativeReport.when(member).edit.create,
-  r.Organization.when(member).read.specifically((p) => p.address.none),
-  r.Partner.when(member).read.specifically((p) => p.pointOfContact.none),
-  r.Partnership.when(member).read,
+  r.Organization.specifically((p) => p.address.none),
+  r.Partner.specifically((p) => p.pointOfContact.none),
   r.PeriodicReport.read,
   r.Product.read,
-  r.Project.when(member).read.specifically(
-    (p) => p.many('step', 'stepChangedAt', 'rootDirectory').edit,
-  ),
-  r.ProjectMember.when(member).read,
+  r.Project.when(member).specifically((p) => [
+    p.many('step', 'stepChangedAt', 'rootDirectory').edit,
+  ]),
   r.StepProgress.read,
   r.Unavailability.read,
   r.User.read.create,
