f304327848f38844d6cd0c4254140a26439fc15d
Carson Full <carson_full@tsco.org>
2023-08-03 17:26:59 -0500

    Adjust policy CLI table to allow multiple roles & multiple resources to be displayed

--- src/components/authorization/policy/executor/policy-dumper.ts
+++ src/components/authorization/policy/executor/policy-dumper.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { Nil } from '@seedcompany/common';
+import { cleanJoin, many, Many, Nil } from '@seedcompany/common';
 import Chalk, { Chalk as ChalkInstance } from 'chalk';
 import Table from 'cli-table3';
 import { sortBy, startCase } from 'lodash';
 import { DateTime } from 'luxon';
 import { Command, Console } from 'nestjs-console';
 import { keys as keysOf } from 'ts-transformer-keys';
+import { LiteralUnion } from 'type-fest';
 import { inspect } from 'util';
 import xlsx from 'xlsx';
 import {
+  csv,
   EnhancedResource,
   firstOr,
   ID,
@@ -43,7 +45,9 @@ export class PolicyDumper {
     const chalk = new Chalk.Instance({ level: 0 });
     const resources = await this.selectResources();
     for (const role of Role.all) {
-      const dumped = resources.flatMap((res) => this.dumpRes(role, res));
+      const dumped = resources.flatMap((res) =>
+        this.dumpRes(role, res, { props: true }),
+      );
       const data = dumped.map((row) => ({
         Resource: startCase(row.resource.name),
         'Property/Relationship': startCase(row.edge),
@@ -62,38 +66,58 @@ export class PolicyDumper {
   @Command({
     command: 'policy:dump <role> <resource>',
   })
-  async dump(role: Role, resource: ResourceLike) {
-    role = firstOr(searchCamelCase(Role.all, role), notFound('role', role));
-    resource =
-      typeof resource === 'string'
-        ? firstOr(
-            searchCamelCase(await this.resources.getNames(), resource),
-            notFound('resource', resource),
-          )
-        : resource;
+  async dump(
+    rolesIn: Many<LiteralUnion<Role, string>>,
+    resourcesIn: Many<ResourceLike & string>,
+  ) {
+    const roles = search(rolesIn, [...Role.all], 'role').sort((a, b) =>
+      a.localeCompare(b),
+    );
+    const resBank = await this.resources.getNames();
+    const resources = await Promise.all(
+      search(resourcesIn, resBank, 'resource')
+        .sort((a, b) => a.localeCompare(b))
+        .map((res) => this.resources.enhance(res)),
+    );
 
-    const res = await this.resources.enhance(resource);
-    const data = this.dumpRes(role, res);
+    const data = roles.flatMap((role) =>
+      resources.flatMap((res) =>
+        this.dumpRes(role, res, {
+          props: roles.length === 1 && resources.length === 1,
+        }),
+      ),
+    );
 
     const table = new Table({
       style: { compact: true },
     });
     const chalk = new Chalk.Instance();
 
+    const showRoleCol = roles.length > 1;
+    const showResCol = resources.length > 1 || !showRoleCol;
+
     // Table title
     table.push([
       {
-        content: chalk.magentaBright`Permissions for ${chalk.italic(
-          startCase(role),
-        )} for ${chalk.italic(startCase(res.name))}`,
-        colSpan: Object.keys(data[0]).slice(1).length,
+        content: chalk.magentaBright(
+          cleanJoin(' ', [
+            'Permissions',
+            roles.length === 1 && `for ${chalk.italic(startCase(roles[0]))}`,
+            resources.length === 1 &&
+              `for ${chalk.italic(startCase(resources[0].name))}`,
+          ]),
+        ),
+        colSpan: 4 + (showRoleCol ? 1 : 0) + (showResCol ? 1 : 0),
         hAlign: 'center',
       },
     ]);
 
     // Table header row
     table.push([
-      undefined,
+      ...(showRoleCol ? [chalk.magentaBright('Role')] : []),
+      ...(showResCol
+        ? [resources.length === 1 ? undefined : chalk.magentaBright('Resource')]
+        : []),
       ...['Read', 'Edit', 'Create', 'Delete'].map((k) =>
         chalk.magentaBright(k),
       ),
@@ -102,7 +126,14 @@ export class PolicyDumper {
     // Table data rows
     table.push(
       ...data.map((row) => [
-        chalk.cyan(row.edge ? '.' + row.edge : row.resource.name),
+        ...(showRoleCol ? [chalk.cyan(startCase(row.role))] : []),
+        ...(showResCol
+          ? [
+              chalk.cyan(
+                row.edge ? ' .' + row.edge : startCase(row.resource.name),
+              ),
+            ]
+          : []),
         ...[row.read, row.edit, row.create, row.delete].map((perm) =>
           this.humanizePerm(perm, chalk),
         ),
@@ -136,7 +167,11 @@ export class PolicyDumper {
     return chalk.yellow(inspect(perm));
   }
 
-  private dumpRes(role: Role, resource: AnyResource): DumpedRow[] {
+  private dumpRes(
+    role: Role,
+    resource: AnyResource,
+    options: { props: boolean },
+  ): DumpedRow[] {
     const session: Session = {
       token: 'system',
       issuedAt: DateTime.now(),
@@ -154,6 +189,7 @@ export class PolicyDumper {
       });
     return [
       {
+        role,
         resource,
         edge: undefined,
         ...mapFromList(keysOf<Record<ResourceAction, boolean>>(), (action) => [
@@ -161,14 +197,16 @@ export class PolicyDumper {
           resolve(action),
         ]),
       },
-      ...(
-        [
-          [resource.securedPropsPlusExtra, keysOf<Record<PropAction, ''>>()],
-          [resource.childSingleKeys, keysOf<Record<ChildSingleAction, ''>>()],
-          [resource.childListKeys, keysOf<Record<ChildListAction, ''>>()],
-        ] as const
+      ...(options.props
+        ? ([
+            [resource.securedPropsPlusExtra, keysOf<Record<PropAction, ''>>()],
+            [resource.childSingleKeys, keysOf<Record<ChildSingleAction, ''>>()],
+            [resource.childListKeys, keysOf<Record<ChildListAction, ''>>()],
+          ] as const)
+        : []
       ).flatMap(([set, actions]) =>
         [...set].map((prop) => ({
+          role,
           resource,
           edge: prop,
           ...mapFromList(actions, (action) => [action, resolve(action, prop)]),
@@ -179,6 +217,7 @@ export class PolicyDumper {
 }
 
 interface DumpedRow {
+  role: Role;
   resource: AnyResource;
   edge?: string;
   read: Permission;
@@ -187,5 +226,21 @@ interface DumpedRow {
   delete?: Permission;
 }
 
-const notFound = (thing: string, input: string) => () =>
-  new Error(`Could not find ${thing} from "${input}"`);
+const search = <T extends string>(
+  input: Many<string>,
+  bank: T[],
+  thing: string,
+) => {
+  const values = many(input);
+  if (values.some((v) => v === '*' || v === '_')) {
+    return bank;
+  }
+  return values
+    .flatMap(csv)
+    .map((r) =>
+      firstOr(
+        searchCamelCase(bank, r),
+        () => new Error(`Could not find ${thing} from "${r}"`),
+      ),
+    );
+};
