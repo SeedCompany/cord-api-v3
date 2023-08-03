271f053364bfbc0f5ebbd6157a7d590fb07fa8b5
Carson Full <carson_full@tsco.org>
2023-08-03 13:38:46 -0500

    Migrate to our new scripture library (#2855)

--- src/components/scripture/dto/book.transformer.ts
+++ src/components/scripture/dto/book.transformer.ts
@@ -1,10 +1,10 @@
-import { Transform } from '../../../common/transform.decorator';
-import { Book } from '../books';
+import { Book } from '@seedcompany/scripture';
+import { Transform } from '~/common/transform.decorator';
 
 export const NormalizeBook = () =>
   Transform(({ value: book }) => {
     try {
-      return Book.find(book).label;
+      return Book.named(book).label;
     } catch (e) {
       // Let validator throw error
       return book;
