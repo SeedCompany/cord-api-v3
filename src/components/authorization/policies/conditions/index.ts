88c52df66c2d6d2bb6d50561ab6b5f904c1d0cbe
Carson Full <carson_full@tsco.org>
2023-08-14 19:12:26 -0500

    Add status policy condition

--- src/components/authorization/policies/conditions/index.ts
+++ src/components/authorization/policies/conditions/index.ts
@@ -1,4 +1,5 @@
 export * from './member.condition';
 export * from './owner.condition';
 export * from './sensitivity.condition';
+export * from './status.condition';
 export * from './variant.condition';
