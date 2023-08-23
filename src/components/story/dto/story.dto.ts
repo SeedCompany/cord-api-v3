fa00483d2d83498f9ab4989e43bbdf6c3311de5b
Carson Full <carson_full@tsco.org>
2023-08-23 18:25:32 -0500

    Change ProducibleType enum to use new enum abstraction

--- src/components/story/dto/story.dto.ts
+++ src/components/story/dto/story.dto.ts
@@ -8,15 +8,13 @@ import {
   SecuredProps,
   SecuredString,
 } from '../../../common';
-import { Producible, ProducibleType } from '../../product/dto/producible.dto';
+import { Producible } from '../../product/dto/producible.dto';
 
 declare module '../../product/dto/producible.dto' {
-  enum ProducibleType {
-    Story = 'Story',
+  interface ProducibleTypeEntries {
+    Story: true;
   }
 }
-
-Object.assign(ProducibleType, { Story: 'Story' });
 
 @RegisterResource()
 @ObjectType({
