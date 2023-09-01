2b9cd9bdb66841c51bd0b29eb9e25f41660931a4
Carson Full <carson_full@tsco.org>
2023-09-01 10:58:10 -0500

    Add helper to get the attachedType resource
    Since it's likely that listeners will need to base logic off this,
    it makes sense to provide this for them and only do it once.

--- src/components/file/media/events/can-update-event.ts
+++ src/components/file/media/events/can-update-event.ts
@@ -1,5 +1,7 @@
-import { Injectable, Optional, Scope } from '@nestjs/common';
+import { Inject, Injectable, Optional, Scope } from '@nestjs/common';
+import { CachedByArg as Once } from '@seedcompany/common';
 import { PollVoter } from '~/common';
+import { ResourceResolver, ResourcesHost } from '~/core';
 import { AnyMedia, MediaUserMetadata } from '../media.dto';
 
 /**
@@ -8,9 +10,20 @@ import { AnyMedia, MediaUserMetadata } from '../media.dto';
  */
 @Injectable({ scope: Scope.TRANSIENT })
 export class CanUpdateMediaUserMetadataEvent {
+  @Inject() private readonly resourceHost: ResourcesHost;
+  @Inject() private readonly resourceResolver: ResourceResolver;
+
   constructor(
     @Optional() readonly media: AnyMedia,
     @Optional() readonly input: MediaUserMetadata,
     @Optional() readonly allowUpdate: PollVoter<boolean>,
   ) {}
+
+  @Once() async getAttachedResource() {
+    const attachedResName = this.resourceResolver.resolveTypeByBaseNode(
+      this.media.attachedTo[0],
+    );
+    const attachedResource = await this.resourceHost.getByName(attachedResName);
+    return attachedResource;
+  }
 }
