0f6c558fabe0f500b075f76a9b10d48ea99364e6
Carson Full <carson_full@tsco.org>
2023-09-01 15:23:33 -0500

    Add the root node and resource attached to root node to hydrated FileNode results

--- src/components/file/file.repository.ts
+++ src/components/file/file.repository.ts
@@ -11,7 +11,7 @@ import {
 } from 'cypher-query-builder';
 import { Direction } from 'cypher-query-builder/dist/typings/clauses/order-by';
 import { AnyConditions } from 'cypher-query-builder/dist/typings/clauses/where-utils';
-import { isEmpty } from 'lodash';
+import { isEmpty, keyBy } from 'lodash';
 import { DateTime } from 'luxon';
 import {
   ID,
@@ -174,7 +174,27 @@ export class FileRepository extends CommonRepository {
             .where({ node: hasLabel(FileNodeType.Directory) })
             .apply(this.hydrateDirectory()),
         )
-        .return<{ dto: FileNode }>('dto');
+        .subQuery('node', (sub) =>
+          sub
+            .raw('MATCH p=(node)-[:parent*0..]->(root:FileNode)')
+            .return('root')
+            .orderBy('length(p)', 'DESC')
+            .raw('LIMIT 1'),
+        )
+        .subQuery('root', (sub) =>
+          sub
+            .raw('MATCH (resource:BaseNode)-[rel]->(root)')
+            // Need to filter out FileNodes which are children of this dir
+            // (the schema was mistakenly pointing these relationships in the wrong direction)
+            // Also filter to ACTIVE, if applicable.
+            .raw(
+              'WHERE NOT resource:FileNode AND coalesce(rel.active, true) <> false',
+            )
+            .return('[resource, type(rel)] as rootAttachedTo'),
+        )
+        .return<{ dto: FileNode }>(
+          merge('dto', keyBy(['root', 'rootAttachedTo'])).as('dto'),
+        );
   }
 
   private hydrateFile() {
