93aafd0d9e0907846125443aa9d7af35d89d3c9a
Carson Full <carson_full@tsco.org>
2023-09-20 17:46:59 -0500

    Abstract away rollback management (#2889)

--- src/core/database/transactional-mutations.interceptor.ts
+++ src/core/database/transactional-mutations.interceptor.ts
@@ -8,6 +8,7 @@ import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
 import { Connection } from 'cypher-query-builder';
 import { GraphQLResolveInfo } from 'graphql';
 import { from, lastValueFrom } from 'rxjs';
+import { RollbackManager } from './rollback-manager';
 
 /**
  * Run all mutations in a neo4j transaction.
@@ -15,7 +16,10 @@ import { from, lastValueFrom } from 'rxjs';
  */
 @Injectable()
 export class TransactionalMutationsInterceptor implements NestInterceptor {
-  constructor(private readonly db: Connection) {}
+  constructor(
+    private readonly db: Connection,
+    private readonly rollbacks: RollbackManager,
+  ) {}
 
   async intercept(context: ExecutionContext, next: CallHandler) {
     if (context.getType<GqlContextType>() !== 'graphql') {
@@ -29,7 +33,14 @@ export class TransactionalMutationsInterceptor implements NestInterceptor {
     }
 
     return from(
-      this.db.runInTransaction(async () => await lastValueFrom(next.handle())),
+      this.db.runInTransaction(async () => {
+        try {
+          return await lastValueFrom(next.handle());
+        } catch (e) {
+          await this.rollbacks.runAndClear();
+          throw e;
+        }
+      }),
     );
   }
 }
