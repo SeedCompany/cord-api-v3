f1eb2fc0c5b23ed02fcfda5d71f930abd916ffbe
Andre Turner <andre_turner@tsco.org>
2023-10-06 12:09:04 -0500

    Added Short Label to prompts and removed prompts for  progress report highlights

---
+++ src/components/progress-report/community-stories/community-story-prompts.ts
@@ -0,0 +1,26 @@
+import { Prompt } from '../../prompts/dto';
+
+export const prompts = [
+  Prompt.create({
+    id: 'bnrqZXOtEeW',
+    text: 'What are some responses you have seen in the community? Think of community outreach, youth camps, and other community events related to this project. (Ex: increased unity, freedom from false religions, less violence, etc.)',
+    shortLabel: 'What are some responses you have seen in the community?',
+  }),
+  Prompt.create({
+    id: 'Bn3zwnzb3JN',
+    text: 'How was the translated Scripture used among the churches, local ministries, or other groups in the last quarter?',
+    shortLabel: 'How was the translated Scripture used?',
+  }),
+  Prompt.create({
+    id: 'MsdfnoZkYda',
+    text: 'Share a specific example of how translated Scripture changed or challenged the way one person viewed themselves, others, and/or God. (Ex: in marriage, parenting, family life, personal growth, friendships, relationship with God, etc.)',
+    shortLabel:
+      'Share a specific example of how translated Scripture changed a person?',
+  }),
+  Prompt.create({
+    id: 'P4FO95xsZ7A',
+    text: 'Tell how someone responded to the Scripture in a checking session. Was there a passage that was meaningful to this specific person? If yes, what did they discover?',
+    shortLabel:
+      'Tell how someone responded to the Scripture in a checking session.',
+  }),
+];
