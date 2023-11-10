7e43b76483de7ef940c514b5d35048c4c15fead3
Carson Full <carson_full@tsco.org>
2023-11-10 15:57:34 -0600

    Fix stripping excessive documentation on updated connection timeout error message

--- src/core/database/errors.ts
+++ src/core/database/errors.ts
@@ -52,7 +52,7 @@ export class ServiceUnavailableError extends Neo4jError {
       )
       // Strip excessive documentation.
       .replace(
-        'Please ensure that your database is listening on the correct host and port and that you have compatible encryption settings both on Neo4j server and driver. Note that the default encryption setting has changed in Neo4j 4.0. ',
+        ' Please ensure that your database is listening on the correct host and port and that you have compatible encryption settings both on Neo4j server and driver. Note that the default encryption setting has changed in Neo4j 4.0.',
         '',
       );
     const ex = new this(message);
