6e04a995b4e91e75c11b6ba0577a6bc9d071c5a1
Carson Full <carson_full@tsco.org>
2023-11-13 13:52:57 -0600

    Switch generated query builder's target `mts` -> `ts` to fix usage with jest
    Originally switched to support ESM.
    Jest struggles with the imports though, and I could not find a configuration that worked.
    The generated files produced with the `ts` target are fine for our use,
    aside from the import/export from 'edgedb/*'.
    These need the file extension to work with ESM.
    Example:
    - import { Cardinality } from "edgedb/dist/reflection/index";
    + import { Cardinality } from "edgedb/dist/reflection/index.js";

--- src/core/edgedb/index.ts
+++ src/core/edgedb/index.ts
@@ -1,4 +1,4 @@
 export * from './reexports';
 export { edgeql, EdgeQLArgsOf, EdgeQLReturnOf } from './edgeql';
 export * from './edgedb.service';
-export { default as e } from './generated-client/index.mjs';
+export { default as e } from './generated-client';
