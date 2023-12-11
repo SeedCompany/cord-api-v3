b2ec0d73cfa39ef8d156f9cc20c7db64c01ab9a0
Carson Full <carson_full@tsco.org>
2023-12-11 11:17:01 -0600

    Create implementation of DtoRepo for EdgeDB

--- src/core/edgedb/index.ts
+++ src/core/edgedb/index.ts
@@ -4,3 +4,4 @@ export * from './edgedb.service';
 export * from './withScope';
 export * from './exclusivity-violation.error';
 export * from './common.repository';
+export * from './dto.repository';
