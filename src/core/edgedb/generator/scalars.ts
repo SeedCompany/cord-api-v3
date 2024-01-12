caacaf23106a33e79e9235787d6ec327426815fc
Carson Full <carson_full@tsco.org>
2024-01-12 17:23:12 -0600

    Register custom scalar codecs for type generation
    This means we can gen correct types, not statically known,
    for query files & inline queries.
    These two processes (via `$.analyzeQuery`) use
    the `tsType` property from the codec.
    
    Since we set the codecs in the client's codec registry now,
    we don't need to modify the SCALAR_CODECS global.

--- src/core/edgedb/generator/scalars.ts
+++ src/core/edgedb/generator/scalars.ts
@@ -1,7 +1,5 @@
 import { scalarToLiteralMapping } from '@edgedb/generate/dist/genutil.js';
 import { mapEntries } from '@seedcompany/common';
-import { SCALAR_CODECS } from 'edgedb/dist/codecs/codecs.js';
-import { KNOWN_TYPENAMES } from 'edgedb/dist/codecs/consts.js';
 import { codecs } from '../codecs';
 
 export const customScalars = mapEntries(codecs, (codec) => [
@@ -9,15 +7,10 @@ export const customScalars = mapEntries(codecs, (codec) => [
   codec.info,
 ]).asMap;
 
-export function changeScalarCodecsToOurCustomTypes() {
-  for (const codec of codecs) {
-    const fqName = `${codec.info.module}::${codec.info.type}`;
-
-    // codecs are used for edgeql files & inline queries ($.analyzeQuery)
-    const id = KNOWN_TYPENAMES.get(fqName);
-    id && SCALAR_CODECS.set(id, new codec(id));
-
-    // this is used for schema interfaces
-    scalarToLiteralMapping[fqName].type = codec.info.ts;
+export function setTsTypesFromOurScalarCodecs() {
+  // this is used for schema interfaces
+  for (const { info } of codecs) {
+    const fqName = `${info.module}::${info.type}`;
+    scalarToLiteralMapping[fqName].type = info.ts;
   }
 }
