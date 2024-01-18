a73d9ead81baf74516a89bd7599ce3acddddbe69
Carson Full <carson_full@tsco.org>
2024-01-18 12:28:41 -0600

    Refactor FieldZone service to move neo4j needs into neo4j repo

--- src/components/field-zone/field-zone.service.ts
+++ src/components/field-zone/field-zone.service.ts
@@ -1,15 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import {
-  DuplicateException,
   ID,
   ObjectView,
-  SecuredList,
   ServerException,
   Session,
   UnsecuredDto,
 } from '../../common';
 import { HandleIdLookup, ILogger, Logger } from '../../core';
-import { mapListResults } from '../../core/database/results';
 import { Privileges } from '../authorization';
 import {
   CreateFieldZone,
@@ -30,20 +27,8 @@ export class FieldZoneService {
 
   async create(input: CreateFieldZone, session: Session): Promise<FieldZone> {
     this.privileges.for(session, FieldZone).verifyCan('create');
-    if (!(await this.repo.isUnique(input.name))) {
-      throw new DuplicateException(
-        'fieldZone.name',
-        'FieldZone with this name already exists.',
-      );
-    }
-    const result = await this.repo.create(input, session);
-
-    if (!result) {
-      throw new ServerException('failed to create field zone');
-    }
-
-    this.logger.debug(`field zone created`, { id: result.id });
-    return await this.readOne(result.id, session);
+    const dto = await this.repo.create(input, session);
+    return this.secure(dto, session);
   }
 
   @HandleIdLookup(FieldZone)
@@ -58,32 +43,26 @@ export class FieldZoneService {
     });
 
     const result = await this.repo.readOne(id);
-    return await this.secure(result, session);
+    return this.secure(result, session);
   }
 
   async readMany(ids: readonly ID[], session: Session) {
     const fieldZones = await this.repo.readMany(ids);
-    return await Promise.all(
-      fieldZones.map((dto) => this.secure(dto, session)),
-    );
+    return fieldZones.map((dto) => this.secure(dto, session));
   }
 
-  private async secure(
-    dto: UnsecuredDto<FieldZone>,
-    session: Session,
-  ): Promise<FieldZone> {
+  private secure(dto: UnsecuredDto<FieldZone>, session: Session) {
     return this.privileges.for(session, FieldZone).secure(dto);
   }
 
   async update(input: UpdateFieldZone, session: Session): Promise<FieldZone> {
-    const fieldZone = await this.readOne(input.id, session);
+    const fieldZone = await this.repo.readOne(input.id);
 
     const changes = this.repo.getActualChanges(fieldZone, input);
     this.privileges.for(session, FieldZone, fieldZone).verifyChanges(changes);
 
-    await this.repo.update(fieldZone, changes);
-
-    return await this.readOne(input.id, session);
+    const updated = await this.repo.update(fieldZone, changes);
+    return this.secure(updated, session);
   }
 
   async delete(id: ID, session: Session): Promise<void> {
@@ -103,11 +82,10 @@ export class FieldZoneService {
     input: FieldZoneListInput,
     session: Session,
   ): Promise<FieldZoneListOutput> {
-    if (this.privileges.for(session, FieldZone).can('read')) {
-      const results = await this.repo.list(input, session);
-      return await mapListResults(results, (dto) => this.secure(dto, session));
-    } else {
-      return SecuredList.Redacted;
-    }
+    const results = await this.repo.list(input, session);
+    return {
+      ...results,
+      items: results.items.map((dto) => this.secure(dto, session)),
+    };
   }
 }
