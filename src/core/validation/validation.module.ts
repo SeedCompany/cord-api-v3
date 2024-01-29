419e27049b9cf62f20d2fbf027c5a0fa09ff5ff5
Carson Full <carson_full@tsco.org>
2024-01-29 12:07:45 -0600

    Connect nestjs container to validator so constraints can be injectable

--- src/core/validation/validation.module.ts
+++ src/core/validation/validation.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
+import { Validator } from 'class-validator';
 import { ValidationPipe } from './validation.pipe';
 
 @Module({
   providers: [
+    Validator,
     ValidationPipe,
     { provide: APP_PIPE, useExisting: ValidationPipe },
   ],
--- src/core/validation/validation.pipe.ts
+++ src/core/validation/validation.pipe.ts
@@ -2,15 +2,34 @@ import {
   ValidationPipe as BaseValidationPipe,
   Injectable,
 } from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
+import { useContainer, ValidatorOptions } from 'class-validator';
 import { ValidationException } from './validation.exception';
 
 @Injectable()
 export class ValidationPipe extends BaseValidationPipe {
-  constructor() {
+  constructor(private readonly moduleRef: ModuleRef) {
     super({
       transform: true,
       skipMissingProperties: true,
       exceptionFactory: (es) => new ValidationException(es),
     });
+  }
+  private readonly containerForLib = {
+    get: (type: any) => {
+      if (type.name === 'CustomConstraint') {
+        // Prototype-less constraints. Null to fall back to default, which just calls constructor once.
+        return null;
+      }
+      return this.moduleRef.get(type);
+    },
+  };
+
+  protected async validate(
+    object: object,
+    validatorOptions?: ValidatorOptions,
+  ) {
+    useContainer(this.containerForLib, { fallback: true });
+    return await super.validate(object, validatorOptions);
   }
 }
