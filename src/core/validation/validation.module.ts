9b2acf9eb5d5e02b52e9f6c5ded96c8ccdf87128
Carson Full <carson_full@tsco.org>
2024-01-29 15:01:22 -0600

    Automatically convert IDs that are aliases to their target IDs

--- src/core/validation/validation.module.ts
+++ src/core/validation/validation.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { Validator } from 'class-validator';
 import {
-  IdResolver,
   ValidateIdPipe,
   ValidIdConstraint,
 } from '~/common/validators/short-id.validator';
@@ -15,8 +14,7 @@ import { ValidationPipe } from './validation.pipe';
     { provide: APP_PIPE, useExisting: ValidationPipe },
     ValidIdConstraint,
     ValidateIdPipe,
-    IdResolver,
   ],
-  exports: [ValidationPipe, ValidateIdPipe, IdResolver],
+  exports: [ValidationPipe, ValidateIdPipe],
 })
 export class ValidationModule {}
