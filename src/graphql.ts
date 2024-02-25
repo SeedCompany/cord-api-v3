4b6fcf7382cf4f021eeed1d988a66c8302eb368b
Carson Full <carson_full@tsco.org>
2024-02-25 11:26:01 -0600

    Setup gql.tada

---
+++ src/graphql.ts
@@ -0,0 +1,24 @@
+import { initGraphQLTada, type VariablesOf } from 'gql.tada';
+import { type ID } from '~/common';
+import type { introspection } from './graphql-env.generated';
+
+export const graphql = initGraphQLTada<{
+  introspection: introspection;
+  scalars: {
+    ID: ID;
+    Date: string;
+    DateTime: string;
+    InlineMarkdown: string;
+    Markdown: string;
+    RichText: object;
+    URL: string;
+  };
+  disableMasking: true;
+}>();
+
+export type { FragmentOf, ResultOf, VariablesOf } from 'gql.tada';
+export { readFragment } from 'gql.tada';
+
+export type InputOf<T> = VariablesOf<T> extends { input: infer Input }
+  ? Input
+  : never;
