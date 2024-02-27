ec0c7f2bf0a710da18cc9a074c9dcc8bc29c7561
Carson Full <carson_full@tsco.org>
2024-02-26 18:14:36 -0600

    Refactor status condition to work for any field name

--- src/components/authorization/policies/conditions/index.ts
+++ src/components/authorization/policies/conditions/index.ts
@@ -1,5 +1,5 @@
 export * from './member.condition';
 export * from './owner.condition';
 export * from './sensitivity.condition';
-export * from './status.condition';
+export * from './enum-field.condition';
 export * from './variant.condition';
