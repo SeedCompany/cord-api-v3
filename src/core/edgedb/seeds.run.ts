ca26f5bdb8a092f52a122190df2e9b7d133e71ee
Carson Full <carson_full@tsco.org>
2024-03-22 15:28:29 -0500

    Rewrite seed execution to a TS script

---
+++ src/core/edgedb/seeds.run.ts
@@ -0,0 +1,49 @@
+import { many } from '@seedcompany/common';
+import { Command, runExit } from 'clipanion';
+import { createClient } from 'edgedb';
+import { glob } from 'glob';
+import fs from 'node:fs/promises';
+import { inspect } from 'util';
+
+const db = createClient();
+
+class SeedCommand extends Command {
+  async execute() {
+    let code = 0;
+    const files = await glob('./dbschema/seeds/**/*.edgeql');
+    files.sort((a, b) => a.localeCompare(b));
+
+    const colors = this.context.colorDepth > 1;
+    const printResult = (result: unknown) => {
+      for (const row of many(result)) {
+        this.context.stdout.write(inspect(row, { colors }) + '\n');
+      }
+    };
+
+    const runQuery = async (query: string) => {
+      try {
+        const rows = await db.query(query);
+        printResult(rows);
+      } catch (e) {
+        this.context.stderr.write((e as Error).message + '\n');
+        code = 1;
+      }
+    };
+
+    for (const file of files) {
+      const query = await fs.readFile(file, 'utf-8');
+      await runQuery(query);
+    }
+
+    return code;
+  }
+
+  async validateAndExecute() {
+    try {
+      return await super.validateAndExecute();
+    } finally {
+      await db.close();
+    }
+  }
+}
+await runExit(SeedCommand);
