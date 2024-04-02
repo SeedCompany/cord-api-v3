a53261212b9c373181c339c0d57497935825a591
Carson Full <carson_full@tsco.org>
2024-04-02 17:28:15 -0500

    Ensure TX config bug errors loud

--- src/core/edgedb/transaction.context.ts
+++ src/core/edgedb/transaction.context.ts
@@ -1,7 +1,8 @@
 import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import { AsyncLocalStorage } from 'async_hooks';
-import { EdgeDBError, Executor } from 'edgedb';
-import { getPreviousList } from '~/common';
+import { EdgeDBError, Executor, Session } from 'edgedb';
+import { getPreviousList, ServerException } from '~/common';
+import { OptionsContext } from './options.context';
 import { Client } from './reexports';
 
 @Injectable()
@@ -9,17 +10,30 @@ export class TransactionContext
   extends AsyncLocalStorage<Executor>
   implements OnModuleDestroy
 {
-  constructor(private readonly client: Client) {
+  constructor(
+    private readonly client: Client,
+    private readonly optionsContext: OptionsContext,
+  ) {
     super();
   }
 
   async inTx<R>(fn: () => Promise<R>): Promise<R> {
     const errorMap = new WeakMap<Error, Error>();
 
+    const txSession = this.optionsContext.current.session;
+
     try {
       return await this.client.transaction(async (tx) => {
+        const txx = new Proxy(tx, {
+          get: (target: typeof tx, p: string, receiver: any) => {
+            const current = this.optionsContext.current.session;
+            ensureCompatibleSession(txSession, current);
+            Reflect.get(target, p, receiver);
+          },
+        });
+
         try {
-          return await this.run(tx, fn);
+          return await this.run(txx, fn);
         } catch (error) {
           // If the error "wraps" an EdgeDB error, then
           // throw that here and save the original.
@@ -47,5 +61,19 @@ export class TransactionContext
 
   onModuleDestroy() {
     this.disable();
+  }
+}
+
+function ensureCompatibleSession(txSession: Session, current: Session) {
+  // This should work, but is bugged right now.
+  // https://github.com/edgedb/edgedb/issues/7138
+  // It's hard to reason about, so I'm making sure it errors loud.
+  if (
+    txSession.config.apply_access_policies !==
+    current.config.apply_access_policies
+  ) {
+    throw new ServerException(
+      'Access policies cannot be toggled within a transaction',
+    );
   }
 }
