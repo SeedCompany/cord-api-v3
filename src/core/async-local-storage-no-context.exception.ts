929bf2f4c259e69edd869633653e0fa097c8f191
Carson Full <carson_full@tsco.org>
2024-04-08 15:04:23 -0500

    Drop node 18 support

--- src/core/async-local-storage-no-context.exception.ts
+++ src/core/async-local-storage-no-context.exception.ts
@@ -1,14 +1,1 @@
-export class AsyncLocalStorageNoContextException extends Error {
-  constructor(message: string) {
-    const [major] = process.versions.node.split('.').map(Number);
-    if (major === 18) {
-      message += `
-
-There's a bug with this process when using Node 18.8 or lower with debugger ATTACHED (just listening is fine).
-Please upgrade to Node 18.9+ if you want to debug and are encountering this.
-`;
-    }
-
-    super(message);
-  }
-}
+export class AsyncLocalStorageNoContextException extends Error {}
