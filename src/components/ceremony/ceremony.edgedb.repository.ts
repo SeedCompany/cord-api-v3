8be0ec2ca9b4d1d17719600c9ec38ce230edafd7
Brent Kulwicki <brentkulwicki@Brents-MacBook-Pro-2.local>
2024-04-26 16:55:40 -0400

    adds defaults into ceremony queries

--- src/components/ceremony/ceremony.edgedb.repository.ts
+++ src/components/ceremony/ceremony.edgedb.repository.ts
@@ -1,24 +1,45 @@
 import { Injectable } from '@nestjs/common';
-import { PublicOf } from '~/common';
-import { RepoFor } from '~/core/edgedb';
+import { PaginatedListType, PublicOf, UnsecuredDto } from '~/common';
+import { DbTypeOf } from '~/core';
+import { ChangesOf } from '~/core/database/changes';
+import { castToEnum, RepoFor } from '~/core/edgedb';
 import { CeremonyRepository } from './ceremony.repository';
-import { Ceremony, CreateCeremony } from './dto';
+import {
+  Ceremony,
+  CeremonyListInput,
+  CeremonyType,
+  CreateCeremony,
+  UpdateCeremony,
+} from './dto';
 
 @Injectable()
 export class CeremonyEdgeDBRepository
   extends RepoFor(Ceremony, {
-    hydrate: (ceremony) => ({ ...ceremony['*'], engagement: true }),
+    hydrate: (ceremony) => ({
+      ...ceremony['*'],
+      engagement: true,
+      type: castToEnum(ceremony.__type__.name.slice(12, -8), CeremonyType),
+    }),
   }).customize((cls, { defaults }) => {
     return class extends cls {
-      static omit = [
-        defaults.update,
-        defaults.list,
-        defaults.readMany,
-        defaults.readOne,
-      ];
+      static omit = [defaults.readOne, defaults.readMany];
       async create(input: CreateCeremony): Promise<any> {
         // EdgeDB creates these ceremonies in the process of creating the parent
         return;
+      }
+      async update(
+        existing: DbTypeOf<Ceremony>,
+        changes: ChangesOf<Ceremony, UpdateCeremony>,
+      ): Promise<DbTypeOf<Ceremony>> {
+        return await this.defaults.update({ existing, changes });
+      }
+      async list({
+        filter,
+        ...input
+      }: CeremonyListInput): Promise<
+        PaginatedListType<UnsecuredDto<Ceremony>>
+      > {
+        return await this.defaults.list({ filter, ...input });
       }
     };
   })
