cf457b4f3ae9ba9a6a6877dec91b90f6cab0f5d0
William Harris <will_harris@tsco.org>
2024-05-06 14:58:53 -0700

    Register story EdgeDB repository

--- src/components/story/story.module.ts
+++ src/components/story/story.module.ts
@@ -1,6 +1,8 @@
 import { forwardRef, Module } from '@nestjs/common';
+import { splitDb } from '~/core';
 import { AuthorizationModule } from '../authorization/authorization.module';
 import { ScriptureModule } from '../scripture';
+import { StoryEdgeDBRepository } from './story.edgedb.repository';
 import { StoryLoader } from './story.loader';
 import { StoryRepository } from './story.repository';
 import { StoryResolver } from './story.resolver';
@@ -8,7 +10,12 @@ import { StoryService } from './story.service';
 
 @Module({
   imports: [forwardRef(() => AuthorizationModule), ScriptureModule],
-  providers: [StoryResolver, StoryService, StoryRepository, StoryLoader],
+  providers: [
+    StoryResolver,
+    StoryService,
+    splitDb(StoryRepository, StoryEdgeDBRepository),
+    StoryLoader,
+  ],
   exports: [StoryService],
 })
 export class StoryModule {}
