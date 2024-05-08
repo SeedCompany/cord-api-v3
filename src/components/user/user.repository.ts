SeedCompany/cord-api-v3

commit 77b4bfc712f7d0d1795b153090c198eda74880ad

Stop matching session in queries when not directly used
This was an early strategy to confirm requester has been authenticated.
All of that is abstracted now and doesn't need to be confirmed by each repo query.

src/components/user/user.repository.ts
@@ -17,7 +17,6 @@ import {
   deactivateProperty,
   filter,
   matchProps,
-  matchRequestingUser,
   merge,
   paginate,
   property,
@@ -201,7 +200,6 @@ export class UserRepository extends DtoRepository<typeof User, [Session | ID]>(
     const result = await this.db
       .query()
       .matchNode('node', 'User')
-      .apply(matchRequestingUser(session))
       .apply(
         filter.builder(input.filter, {
           pinned: filter.isPinned,

src/components/budget/budget.repository.ts
@@ -19,7 +19,6 @@ import {
   createRelationships,
   matchChangesetAndChangedProps,
   matchPropsAndProjectSensAndScopedRoles,
-  matchRequestingUser,
   merge,
   oncePerProject,
   paginate,
@@ -59,7 +58,6 @@ export class BudgetRepository extends DtoRepository<
 
     const result = await this.db
       .query()
-      .apply(matchRequestingUser(session))
       .apply(await createNode(Budget, { initialProps }))
       .apply(
         createRelationships(Budget, 'in', {

src/components/ceremony/ceremony.repository.ts
@@ -7,7 +7,6 @@ import {
   ACTIVE,
   createNode,
   matchPropsAndProjectSensAndScopedRoles,
-  matchRequestingUser,
   oncePerProject,
   paginate,
   requestingUser,
@@ -35,7 +34,6 @@ export class CeremonyRepository extends DtoRepository<
     };
     return await this.db
       .query()
-      .apply(matchRequestingUser(session))
       .apply(await createNode(Ceremony, { initialProps }))
       .return<{ id: ID }>('node.id as id')
       .first();

src/components/field-region/field-region.repository.ts
@@ -14,7 +14,6 @@ import {
   createNode,
   createRelationships,
   matchProps,
-  matchRequestingUser,
   merge,
   paginate,
   requestingUser,
@@ -45,7 +44,6 @@ export class FieldRegionRepository extends DtoRepository(FieldRegion) {
     // create field region
     const query = this.db
       .query()
-      .apply(matchRequestingUser(session))
       .apply(await createNode(FieldRegion, { initialProps }))
       .apply(
         createRelationships(FieldRegion, 'out', {

src/components/field-zone/field-zone.repository.ts
@@ -15,7 +15,6 @@ import {
   createNode,
   createRelationships,
   matchProps,
-  matchRequestingUser,
   merge,
   paginate,
   requestingUser,
@@ -46,7 +45,6 @@ export class FieldZoneRepository extends DtoRepository(FieldZone) {
     // create field zone
     const query = this.db
       .query()
-      .apply(matchRequestingUser(session))
       .apply(await createNode(FieldZone, { initialProps }))
       .apply(
         createRelationships(FieldZone, 'out', {

src/components/file/file.repository.ts
@@ -21,7 +21,6 @@ import {
   createProperty,
   createRelationships,
   matchProps,
-  matchSession,
   merge,
   paginate,
   sorting,
@@ -536,7 +535,6 @@ export class FileRepository extends CommonRepository {
       await this.db
         .query()
         .match([
-          matchSession(session),
           [node('newParent', [], { id: newParentId })],
           [
             node('file', 'FileNode', { id }),

src/components/film/film.repository.ts
@@ -6,7 +6,6 @@ import { DbTypeOf, DtoRepository } from '../../core';
 import {
   createNode,
   matchProps,
-  matchRequestingUser,
   merge,
   paginate,
   requestingUser,
@@ -28,7 +27,6 @@ export class FilmRepository extends DtoRepository(Film) {
     };
     return await this.db
       .query()
-      .apply(matchRequestingUser(session))
       .apply(await createNode(Film, { initialProps }))
       .return<{ id: ID }>('node.id as id')
       .first();

src/components/funding-account/funding-account.repository.ts
@@ -4,7 +4,6 @@ import { ID, Session } from '../../common';
 import { DtoRepository } from '../../core';
 import {
   createNode,
-  matchRequestingUser,
   paginate,
   requestingUser,
   sorting,
@@ -26,7 +25,6 @@ export class FundingAccountRepository extends DtoRepository(FundingAccount) {
     };
     const query = this.db
       .query()
-      .apply(matchRequestingUser(session))
       .apply(await createNode(FundingAccount, { initialProps }))
       .return<{ id: ID }>('node.id as id');
 

commit ebb839fb61eae7f42c338d9de8aee3b5accb8da4

Fix user pin filter
77b4bfc712f7d0d1795b153090c198eda74880ad mistakenly removed this

src/components/user/user.repository.ts
@@ -200,6 +200,7 @@ export class UserRepository extends DtoRepository<typeof User, [Session | ID]>(
     const result = await this.db
       .query()
       .matchNode('node', 'User')
+      .match(requestingUser(session))
       .apply(
         filter.builder(input.filter, {
           pinned: filter.isPinned,