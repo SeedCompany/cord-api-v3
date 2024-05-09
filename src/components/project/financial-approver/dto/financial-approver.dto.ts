bffa83afecd110a260ff3d9aae715a8873c110a2
Carson Full <carson_full@tsco.org>
2024-05-09 15:29:54 -0500

    Financial Approvers (#3206)

---
+++ src/components/project/financial-approver/dto/financial-approver.dto.ts
@@ -0,0 +1,39 @@
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { keys as keysOf } from 'ts-transformer-keys';
+import { ID, IdField, SecuredProps, UnsecuredDto } from '~/common';
+import { LinkTo, RegisterResource } from '~/core';
+import { e } from '~/core/edgedb';
+import { User } from '../../../user';
+import { ProjectType } from '../../dto';
+
+@RegisterResource({
+  db: e.Project.FinancialApprover,
+})
+@ObjectType('ProjectTypeFinancialApprover')
+export class FinancialApprover {
+  static readonly Props = keysOf<FinancialApprover>();
+  static readonly SecuredProps = keysOf<SecuredProps<FinancialApprover>>();
+
+  readonly user: LinkTo<'User'> & Pick<UnsecuredDto<User>, 'email'>;
+
+  @Field(() => [ProjectType])
+  readonly projectTypes: readonly [ProjectType, ...ProjectType[]];
+}
+
+@InputType('ProjectTypeFinancialApproverInput')
+export abstract class FinancialApproverInput {
+  @IdField()
+  readonly user: ID<'User'>;
+
+  @Field(() => [ProjectType])
+  readonly projectTypes: readonly [ProjectType, ...ProjectType[]];
+}
+
+declare module '~/core/resources/map' {
+  interface ResourceMap {
+    FinancialApprover: typeof FinancialApprover;
+  }
+  interface ResourceDBMap {
+    FinancialApprover: typeof e.Project.FinancialApprover;
+  }
+}
---
+++ src/components/project/financial-approver/financial-approver.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { splitDb2 } from '~/core';
+import { FinancialApproverNeo4jRepository } from './financial-approver-neo4j.repository';
+import { FinancialApproverRepository } from './financial-approver.repository';
+import { FinancialApproverResolver } from './financial-approver.resolver';
+
+@Module({
+  providers: [
+    FinancialApproverResolver,
+    splitDb2(FinancialApproverRepository, {
+      edge: FinancialApproverRepository,
+      neo4j: FinancialApproverNeo4jRepository,
+    }),
+  ],
+  exports: [FinancialApproverRepository],
+})
+export class FinancialApproverModule {}
