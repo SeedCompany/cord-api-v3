SeedCompany/cord-api-v3

commit c2dd6e3a21f6f45900adf752bc1c0f2a2e8a2690

pass in roleSensitivity mapping if cannot list for engagemetn

src/components/engagement/engagement.repository.ts
@@ -26,6 +26,7 @@ import {
   createRelationships,
   INACTIVE,
   matchChangesetAndChangedProps,
+  matchProjectSensToLimitedScopeMap,
   matchPropsAndProjectSensAndScopedRoles,
   merge,
   paginate,
@@ -381,17 +382,23 @@ export class EngagementRepository extends CommonRepository {
         sub
           .match([
             ...(limitedScope
-              ? [node()]
+              ? [
+                  node('project', 'Project'),
+                  relation('out', '', 'engagement', ACTIVE),
+                ]
               : input.filter.projectId
               ? [
                   relation('in', '', 'engagement', ACTIVE),
                   node('project', 'Project', { id: input.filter.projectId }),
                 ]
               : []),
+            node('node', 'Engagement'),
           ])
-          .match(requestingUser(session))
           .apply(whereNotDeletedInChangeset(changeset))
-          .return('node')
+          .return([
+            'node',
+            input.filter.projectId || limitedScope ? 'project' : '',
+          ])
           .apply((q) =>
             changeset && input.filter.projectId
               ? q
@@ -407,6 +414,8 @@ export class EngagementRepository extends CommonRepository {
               : q
           )
       )
+      .match(requestingUser(session))
+      .apply(matchProjectSensToLimitedScopeMap(limitedScope))
       .apply(sorting(IEngagement, input))
       .apply(paginate(input, this.hydrate(session, viewOfChangeset(changeset))))
       .first();

src/components/engagement/engagement.service.ts
@@ -9,6 +9,7 @@ import {
   SecuredResource,
   ServerException,
   Session,
+  simpleSwitch,
   UnauthorizedException,
   UnsecuredDto,
   viewOfChangeset,
@@ -41,6 +42,7 @@ import {
   EngagementListInput,
   EngagementListOutput,
   EngagementStatus,
+  IEngagement,
   InternshipEngagement,
   LanguageEngagement,
   UpdateInternshipEngagement,
@@ -274,13 +276,13 @@ export class EngagementService {
       !session.roles.includes(`global:Administrator`)
         ? false
         : await this.repo.checkDeletePermission(dto.id, session);
-
     if (isLanguageEngagement) {
       // help TS understand that the secured props are for a LanguageEngagement
       const secured = securedProperties as SecuredResource<
         typeof LanguageEngagement,
         false
       >;
+
       return {
         ...(dto as UnsecuredDto<LanguageEngagement>),
         ...secured,
@@ -501,7 +503,28 @@ export class EngagementService {
     session: Session,
     view?: ObjectView
   ): Promise<EngagementListOutput> {
-    const results = await this.repo.list(input, session, view?.changeset);
+    const engagementType =
+      simpleSwitch(input.filter.type, {
+        language: LanguageEngagement,
+        internship: InternshipEngagement,
+      }) ?? IEngagement;
+
+    const limited = (await this.authorizationService.canList(
+      engagementType,
+      session
+    ))
+      ? undefined
+      : await this.authorizationService.getListRoleSensitivityMapping(
+          engagementType
+        );
+
+    const results = await this.repo.list(
+      input,
+      session,
+      view?.changeset,
+      limited
+    );
+
     return await mapListResults(results, (dto) => this.secure(dto, session));
   }
 

commit 9c8a59737dbea2919cb86c64f1477792e6abd320

Fix bug with engagement type filter
Regressed with c2dd6e3a21f6f45900adf752bc1c0f2a2e8a2690

src/components/engagement/engagement.repository.ts
@@ -339,7 +339,7 @@ export class EngagementRepository extends CommonRepository {
           .match([
             node('project', 'Project', pickBy({ id: input.filter.projectId })),
             relation('out', '', 'engagement', ACTIVE),
-            node('node', 'Engagement'),
+            node('node', label),
           ])
           .apply(whereNotDeletedInChangeset(changeset))
           .return(['node', 'project'])