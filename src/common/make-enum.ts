60bf359e5bbb7e9733c6d3db63cde4d1b9a461c8
Carson Full <carson_full@tsco.org>
2024-05-22 15:56:30 -0500

    Default all enum member (human) labels if not declared
    This heuristic matches what the UI does too.
    Not declaring these to GQL schema though, so only handwritten labels are provided there.

--- src/common/make-enum.ts
+++ src/common/make-enum.ts
@@ -1,5 +1,8 @@
 import { registerEnumType } from '@nestjs/graphql';
 import { cleanJoin, mapKeys, nonEnumerable } from '@seedcompany/common';
+import { lowerCase } from 'lodash';
+import { titleCase } from 'title-case';
+import { SetRequired } from 'type-fest';
 import { inspect, InspectOptionsStylized } from 'util';
 
 export type EnumType<Enum> = Enum extends MadeEnum<infer Values, any, any>
@@ -145,6 +148,11 @@ export const makeEnum = <
     registerEnumType(object, { name, description, valuesMap });
   }
 
+  for (const entry of entries) {
+    // @ts-expect-error ignoring immutable here.
+    entry.label ??= titleCase(lowerCase(entry.value)).replace(/ and /g, ' & ');
+  }
+
   return object as any;
 };
 
@@ -183,19 +191,21 @@ type ValuesOfDeclarations<ValueDeclaration extends EnumValueDeclarationShape> =
  * properties as optional.
  */
 type NormalizedValueDeclaration<Declaration extends EnumValueDeclarationShape> =
-  // For values that are objects, accept them as they are...
-  | (Extract<Declaration, EnumValueDeclarationObjectShape> &
-      // plus all the normal object keys
-      EnumValueDeclarationObjectShape<ValuesOfDeclarations<Declaration>>)
-  // For values that are strings, convert them to the standard shape...
-  | (EnumValueDeclarationObjectShape<Extract<Declaration, string>> &
-      // and include all the extra keys as optional
-      Partial<
-        Omit<
-          Extract<Declaration, EnumValueDeclarationObjectShape>,
-          keyof EnumValueDeclarationObjectShape
-        >
-      >);
+  SetRequired<Pick<EnumValueDeclarationObjectShape<any>, 'label'>, 'label'> &
+    // For values that are objects, accept them as they are...
+    (| (Extract<Declaration, EnumValueDeclarationObjectShape> &
+          // plus all the normal object keys
+          EnumValueDeclarationObjectShape<ValuesOfDeclarations<Declaration>>)
+      // For values that are strings, convert them to the standard shape...
+      | (EnumValueDeclarationObjectShape<Extract<Declaration, string>> &
+          // and include all the extra keys as optional
+          Partial<
+            Omit<
+              Extract<Declaration, EnumValueDeclarationObjectShape>,
+              keyof EnumValueDeclarationObjectShape
+            >
+          >)
+    );
 
 interface EnumHelpers<Values extends string, ValueDeclaration> {
   readonly values: ReadonlySet<Values>;
