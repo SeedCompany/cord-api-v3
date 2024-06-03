986c01f4dce1e26842d9055dcb0e98e9376e4cd1
Carson Full <carson_full@tsco.org>
2024-06-03 18:37:25 -0500

    Fix injection in notifiers

--- src/components/project/workflow/transitions/notifiers.ts
+++ src/components/project/workflow/transitions/notifiers.ts
@@ -28,7 +28,7 @@ export const TeamMembersWithRole = (...roles: Role[]): Notifier => ({
 export const FinancialApprovers: Notifier = {
   description: 'All financial approvers according to the project type',
   async resolve({ project, moduleRef }) {
-    const repo = moduleRef.get(FinancialApproverRepository);
+    const repo = moduleRef.get(FinancialApproverRepository, { strict: false });
     const approvers = await repo.read(project.type);
     return approvers.map((approver) => approver.user);
   },
@@ -37,7 +37,7 @@ export const FinancialApprovers: Notifier = {
 export const EmailDistros = (...emails: string[]): Notifier => ({
   description: `These email addresses: ${emails.join(', ')}`,
   resolve({ moduleRef }) {
-    const config = moduleRef.get(ConfigService);
+    const config = moduleRef.get(ConfigService, { strict: false });
     if (!config.email.notifyDistributionLists) {
       return [];
     }
