cc712d8ef1cd35c62f7445e455ce210d2c6698bb
Carson Full <carson_full@tsco.org>
2024-06-10 15:15:35 -0500

    [EdgeDB] Pin Queries (#3237)

--- src/components/pin/pin.module.ts
+++ src/components/pin/pin.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { splitDb } from '~/core';
+import { PinEdgeDBRepository } from './pin.edgedb.repository';
 import { PinRepository } from './pin.repository';
 import { PinResolver } from './pin.resolver';
 import { PinService } from './pin.service';
 
 @Module({
-  providers: [PinResolver, PinService, PinRepository],
+  providers: [
+    PinResolver,
+    PinService,
+    splitDb(PinRepository, PinEdgeDBRepository),
+  ],
   exports: [PinService],
 })
 export class PinModule {}
