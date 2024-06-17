1e22c08abe66d695045c2500bcd9e56ccca84716
Bryan Nelson <bryan_nelson@tsco.org>
2024-06-17 14:32:01 -0400

    Refactor `PeriodicReport` service/repo layers

--- src/components/periodic-report/periodic-report.service.ts
+++ src/components/periodic-report/periodic-report.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import {
   type CalendarDate,
-  CreationFailed,
   DateInterval,
   type ID,
-  NotFoundException,
   type ObjectView,
   type Range,
   type UnsecuredDto,
@@ -45,20 +43,15 @@ export class PeriodicReportService {
     if (input.intervals.length === 0) {
       return;
     }
-    try {
-      const result = await this.repo.merge(input);
-      this.logger.info(`Merged ${input.type.toLowerCase()} reports`, {
-        existing: input.intervals.length - result.length,
-        new: result.length,
-        parent: input.parent,
-        newIntervals: result.map(({ interval }) =>
-          DateInterval.fromObject(interval).toISO(),
-        ),
-      });
-    } catch (exception) {
-      const Report = resolveReportType({ type: input.type });
-      throw new CreationFailed(Report);
-    }
+    const result = await this.repo.merge(input);
+    this.logger.info(`Merged ${input.type.toLowerCase()} reports`, {
+      existing: input.intervals.length - result.length,
+      new: result.length,
+      parent: input.parent,
+      newIntervals: result.map(({ interval }) =>
+        DateInterval.fromObject(interval).toISO(),
+      ),
+    });
   }
 
   async update(input: UpdatePeriodicReportInput) {
@@ -71,11 +64,22 @@ export class PeriodicReportService {
 
     const { reportFile, ...simpleChanges } = changes;
 
-    const updated = await this.repo.update(current, simpleChanges);
+    const updated = this.secure(
+      await this.repo.update(
+        {
+          id: current.id,
+          start: current.start,
+          end: current.end,
+          ...simpleChanges,
+        },
+        session,
+      ),
+      session,
+    );
 
     if (reportFile) {
       const file = await this.files.updateDefinedFile(
-        current.reportFile,
+        this.secure(current, session).reportFile,
         'file',
         reportFile,
       );
@@ -205,10 +209,7 @@ export class PeriodicReportService {
         // no change
         return;
       }
-      await this.repo.update(report, {
-        start: at,
-        end: at,
-      });
+      await this.repo.update({ id: report.id, start: at, end: at }, session);
     } else {
       await this.merge({
         intervals: [{ start: at, end: at }],
