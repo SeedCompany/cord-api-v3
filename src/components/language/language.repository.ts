21654f2b40337df0b0ce24a9803c607a8d8b1600
Carson Full <carson_full@tsco.org>
2024-06-18 08:39:18 -0500

    Extract language filters

--- src/components/language/language.repository.ts
+++ src/components/language/language.repository.ts
@@ -44,6 +44,7 @@ import {
   CreateLanguage,
   EthnologueLanguage,
   Language,
+  LanguageFilters,
   LanguageListInput,
   UpdateLanguage,
 } from './dto';
@@ -184,7 +185,7 @@ export class LanguageRepository extends DtoRepository<
           node('eth', 'EthnologueLanguage'),
         ])
         .apply(matchProps({ nodeName: 'eth', outputVar: 'ethProps' }))
-        .apply(this.isPresetInventory())
+        .apply(isPresetInventory)
         .optionalMatch([
           node('node'),
           relation('in', '', 'language', ACTIVE),
@@ -217,31 +218,7 @@ export class LanguageRepository extends DtoRepository<
       ])
       // match requesting user once (instead of once per row)
       .match(requestingUser(session))
-      .apply(
-        filter.builder(input.filter ?? {}, {
-          sensitivity: filter.stringListProp(),
-          leastOfThese: filter.propVal(),
-          isSignLanguage: filter.propVal(),
-          isDialect: filter.propVal(),
-          partnerId: filter.pathExists((id) => [
-            node('node'),
-            relation('in', '', 'language', ACTIVE),
-            node('', 'LanguageEngagement'),
-            relation('in', '', 'engagement', ACTIVE),
-            node('', 'Project'),
-            relation('out', '', 'partnership', ACTIVE),
-            node('', 'Partnership'),
-            relation('out', '', 'partner', ACTIVE),
-            node('', 'Partner', { id }),
-          ]),
-          presetInventory: ({ value, query }) => {
-            query.apply(this.isPresetInventory()).with('*');
-            const condition = equals('true', true);
-            return { presetInventory: value ? condition : not(condition) };
-          },
-          pinned: filter.isPinned,
-        }),
-      )
+      .apply(languageFilters(input.filter))
       .apply(
         this.privileges.forUser(session).filterToReadable({
           wrapContext: oncePerProject,
@@ -290,40 +267,62 @@ export class LanguageRepository extends DtoRepository<
       .first();
     return !!res;
   }
-
-  private isPresetInventory() {
-    return (query: Query) =>
-      query.subQuery('node', (sub) =>
-        sub
-          .optionalMatch([
-            node('node'),
-            relation('in', '', 'language', ACTIVE),
-            node('', 'LanguageEngagement'),
-            relation('in', '', 'engagement', ACTIVE),
-            node('project', 'Project'),
-            relation('out', '', 'status', ACTIVE),
-            node('status', 'ProjectStatus'),
-          ])
-          .where({
-            'status.value': inArray(
-              `['${ProjectStatus.InDevelopment}', '${ProjectStatus.Active}']`,
-              true,
-            ),
-          })
-          .return(
-            any(
-              'project',
-              collect('project'),
-              exp.path([
-                node('project'),
-                relation('out', '', 'presetInventory', ACTIVE),
-                node('', 'Property', { value: variable('true') }),
-              ]),
-            ).as('presetInventory'),
-          ),
-      );
-  }
 }
+
+export const languageFilters = filter.define(() => LanguageFilters, {
+  sensitivity: filter.stringListProp(),
+  leastOfThese: filter.propVal(),
+  isSignLanguage: filter.propVal(),
+  isDialect: filter.propVal(),
+  partnerId: filter.pathExists((id) => [
+    node('node'),
+    relation('in', '', 'language', ACTIVE),
+    node('', 'LanguageEngagement'),
+    relation('in', '', 'engagement', ACTIVE),
+    node('', 'Project'),
+    relation('out', '', 'partnership', ACTIVE),
+    node('', 'Partnership'),
+    relation('out', '', 'partner', ACTIVE),
+    node('', 'Partner', { id }),
+  ]),
+  presetInventory: ({ value, query }) => {
+    query.apply(isPresetInventory).with('*');
+    const condition = equals('true', true);
+    return { presetInventory: value ? condition : not(condition) };
+  },
+  pinned: filter.isPinned,
+});
+
+const isPresetInventory = (query: Query) =>
+  query.subQuery('node', (sub) =>
+    sub
+      .optionalMatch([
+        node('node'),
+        relation('in', '', 'language', ACTIVE),
+        node('', 'LanguageEngagement'),
+        relation('in', '', 'engagement', ACTIVE),
+        node('project', 'Project'),
+        relation('out', '', 'status', ACTIVE),
+        node('status', 'ProjectStatus'),
+      ])
+      .where({
+        'status.value': inArray(
+          `['${ProjectStatus.InDevelopment}', '${ProjectStatus.Active}']`,
+          true,
+        ),
+      })
+      .return(
+        any(
+          'project',
+          collect('project'),
+          exp.path([
+            node('project'),
+            relation('out', '', 'presetInventory', ACTIVE),
+            node('', 'Property', { value: variable('true') }),
+          ]),
+        ).as('presetInventory'),
+      ),
+  );
 
 export const languageSorters = defineSorters(Language, {
   // eslint-disable-next-line @typescript-eslint/naming-convention
