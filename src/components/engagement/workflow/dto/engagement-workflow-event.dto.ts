983adfb4ea261f6737f81388fba64a587def5c52
Andre Turner <andre_turner@tsco.org>
2024-06-26 17:15:37 -0500

    revised for edgedb

--- src/components/engagement/workflow/dto/engagement-workflow-event.dto.ts
+++ src/components/engagement/workflow/dto/engagement-workflow-event.dto.ts
@@ -1,13 +1,13 @@
 import { ObjectType } from '@nestjs/graphql';
 import { keys as keysOf } from 'ts-transformer-keys';
 import { SecuredProps } from '~/common';
-//import { e } from '~/core/edgedb';
-//import { RegisterResource } from '~/core/resources';
+import { e } from '~/core/edgedb';
+import { RegisterResource } from '~/core/resources';
 import { WorkflowEvent } from '../../../workflow/dto';
 import { EngagementStatus, IEngagement } from '../../dto';
 import { EngagementWorkflowTransition } from './engagement-workflow-transition.dto';
 
-//@RegisterResource({ db: e.Project.WorkflowEvent })
+@RegisterResource({ db: e.Engagement.WorkflowEvent })
 @ObjectType()
 export abstract class EngagementWorkflowEvent extends WorkflowEvent(
   EngagementStatus,
@@ -26,7 +26,7 @@ declare module '~/core/resources/map' {
   interface ResourceMap {
     EngagementWorkflowEvent: typeof EngagementWorkflowEvent;
   }
-  // interface ResourceDBMap {
-  //   ProjectWorkflowEvent: typeof e.Project.WorkflowEvent;
-  // }
+  interface ResourceDBMap {
+    EngagementWorkflowEvent: typeof e.Engagement.WorkflowEvent;
+  }
 }
