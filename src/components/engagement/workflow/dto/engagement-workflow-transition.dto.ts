ddc8fbd44434e92e9ace6a363997f0394b9b7658
Andre Turner <andre_turner@tsco.org>
2024-06-28 15:04:01 -0500

    revised object type name

--- src/components/engagement/workflow/dto/engagement-workflow-transition.dto.ts
+++ src/components/engagement/workflow/dto/engagement-workflow-transition.dto.ts
@@ -2,7 +2,7 @@ import { ObjectType } from '@nestjs/graphql';
 import { WorkflowTransition } from '../../../workflow/dto';
 import { EngagementStatus } from '../../dto';
 
-@ObjectType('EngagementStatusTransition', {
+@ObjectType('EngagementWorkflowTransition', {
   description: WorkflowTransition.descriptionFor('engagement'),
 })
 export abstract class EngagementWorkflowTransition extends WorkflowTransition(
