82956657ba43790b21f413e0b99331ee27027b4a
Rob Donigian <rob_donigian@tsco.org>
2024-07-30 09:58:48 -0400

    create rod to rolv migrations

--- src/components/language/migrations/rename-rod-to-rolv.migration.ts
+++ src/components/language/migrations/rename-rod-to-rolv.migration.ts
@@ -2,19 +2,19 @@ import { node, relation } from 'cypher-query-builder';
 import { BaseMigration, Migration } from '~/core/database';
 import { collect, variable } from '~/core/database/query';
 
-@Migration('2024-07-30T13:02:04')
+@Migration('2024-07-30T13:02:06')
 export class MigrateRegistryOfDialectToRegistryOfLanguageVarieties extends BaseMigration {
   async up() {
     await this.db
       .query()
       .match([
         node('', 'Language'),
-        relation('out', 'rel', 'registryOfDialectCode'),
+        relation('out', 'rel', 'registryOfDialectsCode'),
         node('', 'Property'),
       ])
       .with(collect('rel').as('rels'))
       .call('apoc.refactor.rename.type', [
-        variable('"registryOfDialectCode"'),
+        variable('"registryOfDialectsCode"'),
         variable('"registryOfLanguageVarietiesCode"'),
         variable('rels'),
       ])
