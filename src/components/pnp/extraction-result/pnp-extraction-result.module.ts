7420217c770f89f6ebf1843aed330795e3e80537
Carson Full <carson_full@tsco.org>
2024-08-27 21:41:09 -0500

    Run planning extraction on progress upload to hydrate problems in that process

--- src/components/pnp/extraction-result/pnp-extraction-result.module.ts
+++ src/components/pnp/extraction-result/pnp-extraction-result.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { splitDb2 } from '~/core';
+import { ProductModule } from '../../product/product.module';
 import { PnpExtractionResultProgressReportConnectionResolver } from './pnp-extraction-result-progress-report-connection.resolver';
 import { PnpExtractionResultRepository } from './pnp-extraction-result.edgedb.repository';
 import { PnpExtractionResultLoader } from './pnp-extraction-result.loader';
@@ -7,6 +8,7 @@ import { PnpExtractionResultNeo4jRepository } from './pnp-extraction-result.neo4
 import { SaveProgressExtractionResultHandler } from './save-progress-extraction-result.handler';
 
 @Module({
+  imports: [ProductModule],
   providers: [
     PnpExtractionResultProgressReportConnectionResolver,
     PnpExtractionResultLoader,
--- src/components/pnp/extraction-result/save-progress-extraction-result.handler.ts
+++ src/components/pnp/extraction-result/save-progress-extraction-result.handler.ts
@@ -1,15 +1,30 @@
 import { EventsHandler } from '~/core';
 import { PeriodicReportUploadedEvent } from '../../periodic-report/events';
+import { ProductStep } from '../../product/dto';
+import { PnpProductSyncService } from '../../product/pnp-product-sync.service';
 import { PnpExtractionResultRepository } from './pnp-extraction-result.edgedb.repository';
 
 @EventsHandler([PeriodicReportUploadedEvent, -1])
 export class SaveProgressExtractionResultHandler {
-  constructor(private readonly repo: PnpExtractionResultRepository) {}
+  constructor(
+    private readonly repo: PnpExtractionResultRepository,
+    private readonly productSyncer: PnpProductSyncService,
+  ) {}
 
   async handle(event: PeriodicReportUploadedEvent) {
     if (!event.pnpResultUsed) {
       return;
     }
+
+    // Parse product/goal sync to hydrate problems from that process.
+    await this.productSyncer.parse({
+      engagementId: event.report.parent.properties.id,
+      // Roll with all the steps to get something since we don't have the actual
+      // methodology from the user to filter with
+      availableSteps: [...ProductStep],
+      pnp: event.file,
+      result: event.pnpResult,
+    });
 
     await this.repo.save(event.file.id, event.pnpResult);
   }
