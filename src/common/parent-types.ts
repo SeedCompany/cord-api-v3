f78a73140302637726b83b4882f80ef7d5ec7a13
Carson Full <carson_full@tsco.org>
2024-09-19 14:52:24 -0500

    Adjust `getParentTypes` to work with `IntersectTypes()`
    This is possible now that we have our own that adds a static `members` array.

--- src/common/parent-types.ts
+++ src/common/parent-types.ts
@@ -1,4 +1,5 @@
-import { AbstractClassType } from './types';
+import { cmpBy } from '@seedcompany/common';
+import { AbstractClassType, IntersectTypes } from './types';
 
 /**
  * Returns a list of all the parent class types including the given one.
@@ -7,6 +8,20 @@ export function getParentTypes(
   type: AbstractClassType<unknown>,
   types: ReadonlyArray<AbstractClassType<unknown>> = [type],
 ): ReadonlyArray<AbstractClassType<unknown>> {
+  // Special handling of classes coming from IntersectTypes()
+  const members = Object.getOwnPropertyDescriptor(type, 'members')?.value as
+    | ReturnType<typeof IntersectTypes>['members']
+    | undefined;
+  if (members && Array.isArray(members)) {
+    const intersectedAncestors = members
+      .flatMap((member) => [...getParentTypes(member).entries()])
+      // Try to maintain order when merging ancestors
+      .sort(cmpBy(([idx]) => idx))
+      .map(([_, type]) => type);
+    const uniqueAncestors = [...new Set(intersectedAncestors)];
+    return [...types, ...uniqueAncestors];
+  }
+
   const parent = Object.getPrototypeOf(type);
   if (parent == null) {
     // if no parent we are at native Object. We are done but drop the last two
@@ -14,5 +29,6 @@ export function getParentTypes(
     // These are never explicitly declared in user-land code, and we don't care about them.
     return types.slice(0, -2);
   }
+
   return getParentTypes(parent, [...types, parent]);
 }
