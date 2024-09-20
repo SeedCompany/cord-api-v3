65f90fcf813d8677c61efcbaf1a753eea9cc9544
Carson Full <carson_full@tsco.org>
2024-09-20 09:58:44 -0500

    Expose progressReports GQL query

--- src/components/progress-report/dto/progress-report-list.dto.ts
+++ src/components/progress-report/dto/progress-report-list.dto.ts
@@ -1,6 +1,30 @@
-import { ObjectType } from '@nestjs/graphql';
-import { PaginatedList, SecuredList } from '~/common';
+import { InputType, ObjectType } from '@nestjs/graphql';
+import {
+  FilterField,
+  OmitType,
+  PaginatedList,
+  PickType,
+  SecuredList,
+} from '~/common';
+import { PeriodicReportListInput } from '../../periodic-report/dto';
 import { ProgressReport } from './progress-report.entity';
+
+@InputType()
+export abstract class ProgressReportFilters extends PickType(
+  PeriodicReportListInput,
+  ['start', 'end', 'parent'],
+) {}
+
+@InputType()
+export class ProgressReportListInput extends OmitType(PeriodicReportListInput, [
+  'type',
+  'start',
+  'end',
+  'parent',
+]) {
+  @FilterField(() => ProgressReportFilters)
+  readonly filter?: ProgressReportFilters;
+}
 
 @ObjectType({
   description: SecuredList.descriptionFor('progress reports'),
