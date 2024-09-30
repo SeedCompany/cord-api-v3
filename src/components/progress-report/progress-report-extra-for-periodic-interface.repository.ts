1ae5066a783453c7be8cbc5636890a6000d8e5ec
Carson Full <carson_full@tsco.org>
2024-09-30 09:20:39 -0500

    Give explicit type for sorter to prevent circular type definition

--- src/components/progress-report/progress-report-extra-for-periodic-interface.repository.ts
+++ src/components/progress-report/progress-report-extra-for-periodic-interface.repository.ts
@@ -2,9 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { node, relation } from 'cypher-query-builder';
 import {
   CreateNodeOptions,
+  DefinedSorters,
   defineSorters,
   exp,
   QueryFragment,
+  SortFieldOf,
   sortWith,
 } from '~/core/database/query';
 import { engagementSorters } from '../engagement/engagement.repository';
@@ -37,7 +39,9 @@ export class ProgressReportExtraForPeriodicInterfaceRepository {
   }
 }
 
-export const progressReportExtrasSorters = defineSorters(ProgressReport, {
+export const progressReportExtrasSorters: DefinedSorters<
+  SortFieldOf<typeof ProgressReport>
+> = defineSorters(ProgressReport, {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   'engagement.*': (query, input) =>
     query
