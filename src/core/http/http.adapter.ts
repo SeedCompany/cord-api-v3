681c65dcf9a945cdfcbac382b8b7ec5ea4d30c37
Carson Full <carson_full@tsco.org>
2024-09-30 10:25:06 -0500

    Configure fastify route injection ourselves
    This is prep for our own expansion

--- src/core/http/http.adapter.ts
+++ src/core/http/http.adapter.ts
@@ -1,14 +1,20 @@
 import compression from '@fastify/compress';
 import cookieParser from '@fastify/cookie';
 import cors from '@fastify/cors';
+import {
+  VERSION_NEUTRAL,
+  type VersionValue,
+} from '@nestjs/common/interfaces/version-options.interface.js';
 // eslint-disable-next-line @seedcompany/no-restricted-imports
 import { HttpAdapterHost as HttpAdapterHostImpl } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import type { FastifyInstance, HTTPMethods, RouteOptions } from 'fastify';
 import * as zlib from 'node:zlib';
 import { ConfigService } from '~/core/config/config.service';
+import { RouteConfig, RouteConstraints } from './decorators';
 import type { CookieOptions, CorsOptions, IResponse } from './types';
 
 export type NestHttpApplication = NestFastifyApplication & {
@@ -20,7 +26,18 @@ export type NestHttpApplication = NestFastifyApplication & {
 
 export class HttpAdapterHost extends HttpAdapterHostImpl<HttpAdapter> {}
 
-export class HttpAdapter extends FastifyAdapter {
+// @ts-expect-error Convert private methods to protected
+class PatchedFastifyAdapter extends FastifyAdapter {
+  protected injectRouteOptions(
+    routerMethodKey: Uppercase<HTTPMethods>,
+    ...args: any[]
+  ): FastifyInstance {
+    // @ts-expect-error work around being marked as private
+    return super.injectRouteOptions(routerMethodKey, ...args);
+  }
+}
+
+export class HttpAdapter extends PatchedFastifyAdapter {
   async configure(app: NestFastifyApplication, config: ConfigService) {
     await app.register(compression, {
       brotliOptions: {
@@ -40,6 +57,36 @@ export class HttpAdapter extends FastifyAdapter {
     app.setGlobalPrefix(config.hostUrl$.value.pathname.slice(1));
 
     config.applyTimeouts(app.getHttpServer(), config.httpTimeouts);
+  }
+
+  protected injectRouteOptions(
+    method: Uppercase<HTTPMethods>,
+    urlOrHandler: string | RouteOptions['handler'],
+    maybeHandler?: RouteOptions['handler'],
+  ) {
+    // I don't know why NestJS allows url/path parameter to be omitted.
+    const url = typeof urlOrHandler === 'function' ? '' : urlOrHandler;
+    const handler =
+      typeof urlOrHandler === 'function' ? urlOrHandler : maybeHandler!;
+
+    const config = RouteConfig.get(handler) ?? {};
+    const constraints = RouteConstraints.get(handler) ?? {};
+
+    let version: VersionValue | undefined = (handler as any).version;
+    version = version === VERSION_NEUTRAL ? undefined : version;
+    if (version) {
+      // @ts-expect-error this is what upstream does
+      constraints.version = version;
+    }
+
+    const route: RouteOptions = {
+      method,
+      url,
+      handler,
+      ...(Object.keys(constraints).length > 0 ? { constraints } : {}),
+      ...(Object.keys(config).length > 0 ? { config } : {}),
+    };
+    return this.instance.route(route);
   }
 
   setCookie(
