1f8a08649e904a8d88163e16606c7cbea8e30417
Carson Full <carson_full@tsco.org>
2024-09-30 10:28:18 -0500

    Allow injecting HttpAdapter directly via Proxy HttpAdapterHost

--- src/core/http/http.module.ts
+++ src/core/http/http.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 // eslint-disable-next-line @seedcompany/no-restricted-imports
 import { HttpAdapterHost as HttpAdapterHostImpl } from '@nestjs/core';
-import { HttpAdapterHost } from './http.adapter';
+import { setOf } from '@seedcompany/common';
+import { getParentTypes } from '~/common';
+import { HttpAdapter, HttpAdapterHost } from './http.adapter';
 
 @Module({
   providers: [
@@ -9,7 +11,37 @@ import { HttpAdapterHost } from './http.adapter';
       provide: HttpAdapterHost,
       useExisting: HttpAdapterHostImpl,
     },
+    {
+      provide: HttpAdapter,
+      inject: [HttpAdapterHost],
+      useFactory: async (host: HttpAdapterHost) => {
+        const availableKeys = setOf(
+          getParentTypes(HttpAdapter).flatMap((cls) => [
+            ...Object.getOwnPropertyNames(cls.prototype),
+            ...Object.getOwnPropertySymbols(cls.prototype),
+          ]),
+        );
+        return new Proxy(host, {
+          get(_, key, receiver) {
+            const { httpAdapter } = host;
+            if (key === 'httpAdapter') {
+              return httpAdapter;
+            }
+            if (key === 'constructor') {
+              return HttpAdapter.constructor;
+            }
+            if (!availableKeys.has(key)) {
+              return undefined;
+            }
+            if (!httpAdapter) {
+              throw new Error('HttpAdapter is not yet available');
+            }
+            return Reflect.get(httpAdapter, key, receiver);
+          },
+        });
+      },
+    },
   ],
-  exports: [HttpAdapterHost],
+  exports: [HttpAdapter, HttpAdapterHost],
 })
 export class HttpModule {}
