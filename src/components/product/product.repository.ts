SeedCompany/cord-api-v3

commit bacb4ef9137a7fafd54576e507f2584ce57eafb5

Abstract list filter query building

src/components/product/product.repository.ts
@@ -28,6 +28,7 @@ import {
   createRelationships,
   deactivateProperty,
   escapeLuceneSyntax,
+  filter,
   fullTextQuery,
   matchProps,
   matchPropsAndProjectSensAndScopedRoles,
@@ -43,6 +44,7 @@ import {
   UnspecifiedScripturePortionInput,
 } from '../scripture';
 import {
+  ApproachToMethodologies,
   CreateDerivativeScriptureProduct,
   CreateDirectScriptureProduct,
   CreateOtherProduct,
@@ -58,7 +60,6 @@ import {
   ProgressMeasurement,
   UpdateProduct,
 } from './dto';
-import { productListFilter } from './query.helpers';
 
 export type HydratedProductRow = Merge<
   Omit<
@@ -488,22 +489,36 @@ export class ProductRepository extends CommonRepository {
     });
   }
 
-  async list({ filter, ...input }: ProductListInput, session: Session) {
+  async list(input: ProductListInput, session: Session) {
     const result = await this.db
       .query()
       .matchNode('node', 'Product')
       .match([
-        ...(filter.engagementId
+        ...(input.filter.engagementId
           ? [
               node('node'),
               relation('in', '', 'product', ACTIVE),
               node('engagement', 'Engagement', {
-                id: filter.engagementId,
+                id: input.filter.engagementId,
               }),
             ]
           : []),
       ])
-      .apply(productListFilter(filter))
+      .apply((q) => {
+        const { approach, methodology, ...rest } = input.filter;
+        const merged = [
+          ...(approach ? ApproachToMethodologies[approach] : []),
+          ...(methodology ? [methodology] : []),
+        ];
+        filter.builder(
+          { ...rest, ...(merged.length ? { methodology: merged } : {}) },
+          {
+            engagementId: filter.skip,
+            placeholder: filter.isPropNotNull('placeholderDescription'),
+            methodology: filter.propVal(),
+          }
+        )(q);
+      })
       .apply(sorting(Product, input))
       .apply(paginate(input, this.hydrate(session)))
       .first();

src/components/language/language.repository.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { inArray, node, Query, relation } from 'cypher-query-builder';
+import {
+  equals,
+  inArray,
+  node,
+  not,
+  Query,
+  relation,
+} from 'cypher-query-builder';
 import {
   ID,
   labelForView,
@@ -15,6 +22,7 @@ import {
   createNode,
   createRelationships,
   exp,
+  filter,
   matchChangesetAndChangedProps,
   matchProjectScopedRoles,
   matchProjectSens,
@@ -31,7 +39,6 @@ import {
 import { AuthSensitivityMapping } from '../authorization/authorization.service';
 import { ProjectStatus } from '../project';
 import { CreateLanguage, Language, LanguageListInput } from './dto';
-import { languageListFilter } from './query.helpers';
 
 @Injectable()
 export class LanguageRepository extends DtoRepository<
@@ -161,7 +168,20 @@ export class LanguageRepository extends DtoRepository<
       ])
       // match requesting user once (instead of once per row)
       .match(requestingUser(session))
-      .apply(languageListFilter(input.filter, this))
+      .apply(
+        filter.builder(input.filter, {
+          sensitivity: filter.stringListProp(),
+          leastOfThese: filter.propVal(),
+          isSignLanguage: filter.propVal(),
+          isDialect: filter.propVal(),
+          presetInventory: ({ value, query }) => {
+            query.apply(this.isPresetInventory()).with('*');
+            const condition = equals('true', true);
+            return { presetInventory: value ? condition : not(condition) };
+          },
+          pinned: filter.isPinned,
+        })
+      )
       .apply(matchProjectSensToLimitedScopeMap(limitedScope))
       .apply(sorting(Language, input))
       .apply(paginate(input, this.hydrate(session)))

src/components/language/query.helpers.ts
@@ -1,59 +1,0 @@
-import {
-  equals,
-  inArray,
-  node,
-  not,
-  Query,
-  relation,
-} from 'cypher-query-builder';
-import { AndConditions } from 'cypher-query-builder/src/clauses/where-utils';
-import { identity } from 'rxjs';
-import { ACTIVE, path } from '../../core/database/query';
-import { propMatch } from '../project';
-import { LanguageFilters } from './dto';
-import { LanguageRepository } from './language.repository';
-
-export const languageListFilter =
-  (filter: LanguageFilters, repo: LanguageRepository) => (query: Query) => {
-    const conditions: AndConditions = {};
-
-    if (filter.sensitivity) {
-      query.match(propMatch('sensitivity'));
-      conditions.sensitivity = { value: inArray(filter.sensitivity) };
-    }
-
-    if (filter.leastOfThese != null) {
-      conditions.lot = boolProp('leastOfThese', filter.leastOfThese);
-    }
-    if (filter.isSignLanguage != null) {
-      conditions.sign = boolProp('isSignLanguage', filter.isSignLanguage);
-    }
-    if (filter.isDialect != null) {
-      conditions.dialect = boolProp('isDialect', filter.isDialect);
-    }
-    if (filter.presetInventory != null) {
-      query.apply(repo.isPresetInventory()).with('*');
-      conditions.presetInventory = (filter.presetInventory ? identity : not)(
-        equals('true', true)
-      );
-    }
-    if (filter.pinned != null) {
-      const pinned = path([
-        node('requestingUser'),
-        relation('out', '', 'pinned'),
-        node('node'),
-      ]);
-      conditions.pinned = filter.pinned ? pinned : not(pinned);
-    }
-
-    if (Object.keys(conditions).length > 0) {
-      query.where(conditions);
-    }
-  };
-
-const boolProp = (prop: string, val: boolean) =>
-  path([
-    node('node'),
-    relation('out', '', prop, ACTIVE),
-    node('', 'Property', { value: val }),
-  ]);

src/components/partner/dto/list-partner.dto.ts
@@ -11,7 +11,6 @@ import { Partner } from './partner.dto';
 
 @InputType()
 export abstract class PartnerFilters {
-  readonly organizationId?: ID;
   readonly userId?: ID;
 
   @Field({

src/components/partner/partner.repository.ts
@@ -7,6 +7,7 @@ import {
   ACTIVE,
   createNode,
   createRelationships,
+  filter as filters,
   matchProjectScopedRoles,
   matchProjectSens,
   matchProjectSensToLimitedScopeMap,
@@ -20,7 +21,6 @@ import {
 } from '../../core/database/query';
 import { AuthSensitivityMapping } from '../authorization/authorization.service';
 import { CreatePartner, Partner, PartnerListInput } from './dto';
-import { partnerListFilter } from './query.helpers';
 
 @Injectable()
 export class PartnerRepository extends DtoRepository<
@@ -185,7 +185,12 @@ export class PartnerRepository extends DtoRepository<
       )
       // match requesting user once (instead of once per row)
       .match(requestingUser(session))
-      .apply(partnerListFilter(filter))
+      .apply(
+        filters.builder(filter, {
+          pinned: filters.isPinned,
+          userId: filters.skip, // already applied above
+        })
+      )
       .apply(matchProjectSensToLimitedScopeMap(limitedScope))
       .apply(
         sorting(Partner, input, {

src/components/partner/query.helpers.ts
@@ -1,21 +1,0 @@
-import { node, not, Query, relation } from 'cypher-query-builder';
-import { AndConditions } from 'cypher-query-builder/src/clauses/where-utils';
-import { path } from '../../core/database/query';
-import { PartnerFilters } from './dto';
-
-export const partnerListFilter = (filter: PartnerFilters) => (query: Query) => {
-  const conditions: AndConditions = {};
-
-  if (filter.pinned != null) {
-    const pinned = path([
-      node('requestingUser'),
-      relation('out', '', 'pinned'),
-      node('node'),
-    ]);
-    conditions.pinned = filter.pinned ? pinned : not(pinned);
-  }
-
-  if (Object.keys(conditions).length > 0) {
-    query.where(conditions);
-  }
-};

src/components/product/query.helpers.ts
@@ -1,54 +1,0 @@
-import {
-  inArray,
-  isNull,
-  node,
-  not,
-  Query,
-  relation,
-} from 'cypher-query-builder';
-import { AndConditions } from 'cypher-query-builder/src/clauses/where-utils';
-import { identity, intersection } from 'lodash';
-import { ACTIVE, path } from '../../core/database/query';
-import { propMatch } from '../project';
-import { ApproachToMethodologies, ProductFilters } from './dto';
-
-export const productListFilter = (filter: ProductFilters) => (query: Query) => {
-  const conditions: AndConditions = {};
-
-  if (filter.engagementId) {
-    conditions.engagement = path([
-      node('node'),
-      relation('in', '', 'product', ACTIVE),
-      node('', 'Engagement', {
-        id: filter.engagementId,
-      }),
-    ]);
-  }
-
-  if (filter.approach || filter.methodology) {
-    query.match(propMatch('methodology'));
-    conditions.methodology =
-      filter.methodology && filter.approach
-        ? {
-            value: inArray(
-              intersection(ApproachToMethodologies[filter.approach], [
-                filter.methodology,
-              ])
-            ),
-          }
-        : filter.methodology
-        ? { value: filter.methodology }
-        : { value: inArray(ApproachToMethodologies[filter.approach!]) };
-  }
-
-  if (filter.placeholder != null) {
-    query.match(propMatch('placeholderDescription'));
-    conditions.placeholderDescription = {
-      value: (filter.placeholder ? not : identity)(isNull()),
-    };
-  }
-
-  if (Object.keys(conditions).length > 0) {
-    query.where(conditions);
-  }
-};

src/components/project/dto/list-projects.dto.ts
@@ -54,8 +54,6 @@ export abstract class ProjectFilters {
   })
   readonly pinned?: boolean;
 
-  readonly locationIds?: ID[];
-
   @Field({
     nullable: true,
     description: 'Only projects created within this time range',
@@ -71,9 +69,6 @@ export abstract class ProjectFilters {
   @Type(() => DateTimeFilter)
   @ValidateNested()
   readonly modifiedAt?: DateTimeFilter;
-
-  // User IDs ANY of which are team members
-  readonly userIds?: ID[];
 
   @Field({
     nullable: true,

commit 4239683a547f5c874f608a3282fa67b481458328

Fix ProductFilters.methodology/approach
Broken since bacb4ef refactor

src/components/product/product.repository.ts
@@ -587,7 +587,7 @@ export const productFilters = filter.define<
 >(() => undefined as any, {
   engagementId: filter.skip,
   placeholder: filter.isPropNotNull('placeholderDescription'),
-  methodology: filter.propVal(),
+  methodology: filter.stringListProp(),
 });
 
 const ProductCompletionDescriptionIndex = FullTextIndex({