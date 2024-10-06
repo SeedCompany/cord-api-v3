50165467a72890937ed6569f224da8332046b806
Carson Full <carson_full@tsco.org>
2024-10-06 12:10:18 -0500

    Convert ALS for gql context to yoga plugin

--- src/core/graphql/graphql.module.ts
+++ src/core/graphql/graphql.module.ts
@@ -1,7 +1,5 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { Module } from '@nestjs/common';
 import { GraphQLModule as NestGraphqlModule } from '@nestjs/graphql';
-import { HttpAdapterHost } from '~/core/http';
 import { TracingModule } from '../tracing';
 import { Driver } from './driver';
 import { GqlContextHost, GqlContextHostImpl } from './gql-context.host';
@@ -37,19 +35,7 @@ export class GraphqlOptionsModule {}
   providers: [
     GqlContextHostImpl,
     { provide: GqlContextHost, useExisting: GqlContextHostImpl },
-    { provide: APP_INTERCEPTOR, useExisting: GqlContextHostImpl },
   ],
   exports: [NestGraphqlModule, GqlContextHost],
 })
-export class GraphqlModule implements NestModule {
-  constructor(
-    private readonly middleware: GqlContextHostImpl,
-    private readonly app: HttpAdapterHost,
-  ) {}
-
-  configure(consumer: MiddlewareConsumer) {
-    // Always attach our GQL Context middleware.
-    // It has its own logic to handle non-gql requests.
-    consumer.apply(this.middleware.use).forRoutes('*');
-  }
-}
+export class GraphqlModule {}
