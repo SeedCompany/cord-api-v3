12e92dc39f988b3dd24583165450ffcca1181878
Carson Full <carson_full@tsco.org>
2024-10-11 19:11:15 -0500

    Add service to help extract mentions & notify on new/updated comments

--- src/components/comments/mentioned-notification/comment-mentioned-notification.module.ts
+++ src/components/comments/mentioned-notification/comment-mentioned-notification.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { NotificationModule } from '../../notifications';
 import { CommentMentionedNotificationResolver } from './comment-mentioned-notification.resolver';
+import { CommentMentionedNotificationService } from './comment-mentioned-notification.service';
 import { CommentMentionedNotificationStrategy } from './comment-mentioned-notification.strategy';
 
 @Module({
@@ -8,6 +9,8 @@ import { CommentMentionedNotificationStrategy } from './comment-mentioned-notifi
   providers: [
     CommentMentionedNotificationResolver,
     CommentMentionedNotificationStrategy,
+    CommentMentionedNotificationService,
   ],
+  exports: [CommentMentionedNotificationService],
 })
 export class CommentMentionedNotificationModule {}
