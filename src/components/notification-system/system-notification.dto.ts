60215db3cf319d6c773c5650ef66d3d3806a4223
Carson Full <carson_full@tsco.org>
2024-10-15 16:54:47 -0500

    Change SystemNotification message type: String -> Markdown

--- src/components/notification-system/system-notification.dto.ts
+++ src/components/notification-system/system-notification.dto.ts
@@ -1,6 +1,7 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { keys as keysOf } from 'ts-transformer-keys';
 import { SecuredProps } from '~/common';
+import { MarkdownScalar } from '~/common/markdown.scalar';
 import { Notification } from '../notifications';
 
 @ObjectType({
@@ -10,6 +11,6 @@ export class SystemNotification extends Notification {
   static readonly Props = keysOf<SystemNotification>();
   static readonly SecuredProps = keysOf<SecuredProps<SystemNotification>>();
 
-  @Field()
+  @Field(() => MarkdownScalar)
   readonly message: string;
 }
