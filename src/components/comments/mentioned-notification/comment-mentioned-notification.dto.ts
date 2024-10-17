fa828ebd7e80c555b6984d5c7910c82089b80a53
Carson Full <carson_full@tsco.org>
2024-10-16 20:58:13 -0500

    Declare EdgeDB types for resources

--- src/components/comments/mentioned-notification/comment-mentioned-notification.dto.ts
+++ src/components/comments/mentioned-notification/comment-mentioned-notification.dto.ts
@@ -1,9 +1,11 @@
 import { ObjectType } from '@nestjs/graphql';
 import { keys as keysOf } from 'ts-transformer-keys';
 import { SecuredProps } from '~/common';
-import { LinkTo } from '~/core/resources';
+import { e } from '~/core/edgedb';
+import { LinkTo, RegisterResource } from '~/core/resources';
 import { Notification } from '../../notifications';
 
+@RegisterResource({ db: e.Notification.CommentMentioned })
 @ObjectType({
   implements: [Notification],
 })
@@ -13,4 +15,13 @@ export class CommentMentionedNotification extends Notification {
     keysOf<SecuredProps<CommentMentionedNotification>>();
 
   readonly comment: LinkTo<'Comment'>;
+}
+
+declare module '~/core/resources/map' {
+  interface ResourceMap {
+    CommentMentionedNotification: typeof CommentMentionedNotification;
+  }
+  interface ResourceDBMap {
+    CommentMentionedNotification: typeof e.Notification.CommentMentioned;
+  }
 }
