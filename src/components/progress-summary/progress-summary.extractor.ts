66872b45eccf90d317ab70c26d13666fc509ad5b
Andre Turner <79720641+atGit2021@users.noreply.github.com>
2024-11-07 14:30:50 -0600

    PnP: Mismatched Reporting Quarter / Missing Summary % -> 0 (#3324)
    Co-authored-by: Carson Full <carson_full@tsco.org>

--- src/components/progress-summary/progress-summary.extractor.ts
+++ src/components/progress-summary/progress-summary.extractor.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { Nil } from '@seedcompany/common';
+import { oneLine } from 'common-tags';
 import { clamp, round } from 'lodash';
-import { CalendarDate, fiscalQuarter, fiscalYear } from '~/common';
+import {
+  CalendarDate,
+  fiscalQuarter,
+  fiscalQuarterLabel,
+  fiscalYear,
+} from '~/common';
 import { Column, Row } from '~/common/xlsx.util';
 import { Downloadable } from '../file/dto';
 import { Pnp, ProgressSheet } from '../pnp';
@@ -18,26 +23,33 @@ export class ProgressSummaryExtractor {
     const pnp = await Pnp.fromDownloadable(file);
     const sheet = pnp.progress;
 
+    if (!(sheet.reportingQuarter && date <= sheet.reportingQuarter.end)) {
+      const cells = sheet.reportingQuarterCells;
+      result.addProblem({
+        severity: 'Error',
+        groups: 'Mismatched Reporting Quarter',
+        message: oneLine`
+          The PnP's Reporting Quarter
+            (_${
+              sheet.reportingQuarter
+                ? fiscalQuarterLabel(sheet.reportingQuarter.start)
+                : 'undetermined'
+            }_ \`${cells.quarter.ref}\`/\`${cells.year.ref}\`)
+          needs to be *at least* the quarter of this CORD report
+            (_${fiscalQuarterLabel(date)}_).
+        `,
+        source: cells.quarter,
+      });
+      return {};
+    }
+
     const currentFiscalYear = fiscalYear(date);
     const yearRow = findFiscalYearRow(sheet, currentFiscalYear);
     const quarterCol = sheet.columnForQuarterSummary(fiscalQuarter(date));
     return {
-      reportPeriod: summaryFrom(
-        yearRow,
-        quarterCol,
-        quarterCol,
-        result,
-        'Quarterly',
-        currentFiscalYear,
-      ),
-      fiscalYear: summaryFrom(
-        yearRow,
-        ...sheet.columnsForFiscalYear,
-        result,
-        'Yearly',
-        currentFiscalYear,
-      ),
-      cumulative: findLatestCumulative(yearRow, result, currentFiscalYear),
+      reportPeriod: summaryFrom(yearRow, quarterCol, quarterCol),
+      fiscalYear: summaryFrom(yearRow, ...sheet.columnsForFiscalYear),
+      cumulative: findLatestCumulative(yearRow),
     };
   }
 }
@@ -51,21 +63,11 @@ const findFiscalYearRow = (sheet: ProgressSheet, fiscalYear: number) => {
   throw new Error('Unable to find fiscal year in pnp file');
 };
 
-const findLatestCumulative = (
-  currentYear: Row<ProgressSheet>,
-  result: PnpProgressExtractionResult,
-  currentFiscalYear: number,
-) => {
+const findLatestCumulative = (currentYear: Row<ProgressSheet>) => {
   const { sheet } = currentYear;
   // eslint-disable-next-line no-constant-condition
   while (true) {
-    const summary = summaryFrom(
-      currentYear,
-      ...sheet.columnsForCumulative,
-      result,
-      'Cumulative',
-      currentFiscalYear,
-    );
+    const summary = summaryFrom(currentYear, ...sheet.columnsForCumulative);
     if (summary) {
       return summary;
     }
@@ -80,41 +82,10 @@ const summaryFrom = (
   fiscalYear: Row<ProgressSheet>,
   plannedColumn: Column,
   actualColumn: Column,
-  result: PnpProgressExtractionResult,
-  period: string,
-  currentFiscalYear: number,
-): Progress | null => {
-  const plannedCell = fiscalYear.cell(plannedColumn);
-  const actualCell = fiscalYear.cell(actualColumn);
-  let planned = plannedCell.asNumber;
-  let actual = actualCell.asNumber;
-  if (!planned && +plannedColumn !== +actualColumn) {
-    result.addProblem({
-      severity: 'Error',
-      groups: [
-        'Missing progress summary percents',
-        `The _${period}_ summary percents for _FY${currentFiscalYear}_ is missing`,
-      ],
-      message: `The _${period} Planned_ percent for _FY${currentFiscalYear}_ \`${plannedCell.ref}\` is missing`,
-      source: plannedCell,
-    });
-  }
-  if (!actual) {
-    result.addProblem({
-      severity: 'Error',
-      groups: [
-        'Missing progress summary percents',
-        `The _${period}_ summary percents for _FY${currentFiscalYear}_ is missing`,
-      ],
-      message: `The _${period} Actual_ percent for _FY${currentFiscalYear}_ \`${actualCell.ref}\` is missing`,
-      source: actualCell,
-    });
-  }
-  if (!planned && !actual) {
-    return null;
-  }
-  const normalize = (val: number | Nil) => {
-    if (!val) return 0;
+): Progress => {
+  let planned = fiscalYear.cell(plannedColumn).asNumber ?? 0;
+  let actual = fiscalYear.cell(actualColumn).asNumber ?? 0;
+  const normalize = (val: number) => {
     if (round(val, 4) > 1) {
       // The PnP has the macro option to do calculations as percents or verse equivalents.
       // We need to standardize as percents here.
