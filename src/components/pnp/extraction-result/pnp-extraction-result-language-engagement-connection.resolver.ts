06e463a88cbd6b5c17a1b4db40306d5a36cc67ca
Andre Turner <andre_turner@tsco.org>
2024-11-20 12:57:29 -0600

    modified resolver to return specific result type

--- src/components/pnp/extraction-result/pnp-extraction-result-language-engagement-connection.resolver.ts
+++ src/components/pnp/extraction-result/pnp-extraction-result-language-engagement-connection.resolver.ts
@@ -1,19 +1,19 @@
 import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
 import { Loader, LoaderOf } from '@seedcompany/data-loader';
 import { LanguageEngagement } from '../../engagement/dto';
-import { PnpExtractionResult } from './extraction-result.dto';
+import { PnpPlanningExtractionResult } from './extraction-result.dto';
 import { PnpExtractionResultLoader } from './pnp-extraction-result.loader';
 
 @Resolver(LanguageEngagement)
 export class PnpExtractionResultLanguageEngagementConnectionResolver {
-  @ResolveField(() => PnpExtractionResult, {
+  @ResolveField(() => PnpPlanningExtractionResult, {
     nullable: true,
   })
   async pnpExtractionResult(
     @Parent() engagement: LanguageEngagement,
     @Loader(() => PnpExtractionResultLoader)
     loader: LoaderOf<PnpExtractionResultLoader>,
-  ): Promise<PnpExtractionResult | null> {
+  ): Promise<PnpPlanningExtractionResult | null> {
     const fileId = engagement.pnp.value;
     if (!fileId) {
       return null;
