dc46d81b1494a7c7ddcce5e8fcf306911763c835
Carson Full <carson_full@tsco.org>
2024-12-16 12:55:48 -0600

    Bump to rextract pnps

--- src/components/progress-report/migrations/reextract-all-progress-reports.migration.ts
+++ src/components/progress-report/migrations/reextract-all-progress-reports.migration.ts
@@ -8,7 +8,7 @@ import { FileVersion } from '../../file/dto';
 import { PeriodicReportUploadedEvent } from '../../periodic-report/events';
 import { ProgressReport } from '../dto';
 
-@Migration('2024-11-11T16:00:06')
+@Migration('2024-12-16T13:00:00')
 export class ReextractPnpProgressReportsMigration extends BaseMigration {
   constructor(
     private readonly eventBus: IEventBus,
