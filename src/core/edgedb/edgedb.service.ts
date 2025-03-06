7dfc123ddac99b3c47d854606d3faa5e12cfd666
Carson Full <carson_full@tsco.org>
2025-03-05 19:08:12 -0600

    Adapt to new `Options` state

--- src/core/edgedb/edgedb.service.ts
+++ src/core/edgedb/edgedb.service.ts
@@ -127,7 +127,7 @@ export class EdgeDB {
       segment.sql = {
         // eslint-disable-next-line @typescript-eslint/naming-convention
         database_version: 'EdgeDB',
-        user: this.optionsContext.current.session.globals.currentActorId,
+        user: this.optionsContext.current.globals.get('currentActorId'),
       };
 
       try {
