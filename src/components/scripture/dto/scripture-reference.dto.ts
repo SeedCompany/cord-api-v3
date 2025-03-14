SeedCompany/cord-api-v3

commit a7187c6198fcae89af27e5cd7d1ccba4f0d11514

Migrate to new ECMA class fields

src/components/scripture/dto/scripture-reference.dto.ts
@@ -47,10 +47,10 @@ export abstract class ScriptureReference extends ScriptureReferenceInput {
   @Field(() => Int, {
     description: `The chapter number.`,
   })
-  readonly chapter: number;
+  declare readonly chapter: number;
 
   @Field(() => Int, {
     description: `The verse number.`,
   })
-  readonly verse: number;
+  declare readonly verse: number;
 }

src/components/authentication/dto/register.dto.ts
@@ -6,7 +6,7 @@ import { CreatePerson } from '../../user/dto';
 @InputType()
 export abstract class RegisterInput extends CreatePerson {
   @Field()
-  readonly email: string;
+  declare readonly email: string;
 
   @Field()
   @MinLength(6)

src/components/budget/dto/budget-record.dto.ts
@@ -25,7 +25,7 @@ export class BudgetRecord extends IntersectionType(ChangesetAware, Resource) {
   static readonly Parent = import('./budget.dto').then((m) => m.Budget);
 
   @Field(() => Budget)
-  readonly parent: BaseNode;
+  declare readonly parent: BaseNode;
 
   readonly organization: Secured<ID>;
 
@@ -42,5 +42,5 @@ export class BudgetRecord extends IntersectionType(ChangesetAware, Resource) {
 
   // A list of non-global roles the requesting user has available for this object.
   // This is just a cache, to prevent extra db lookups within the same request.
-  readonly scope: ScopedRole[];
+  declare readonly scope: ScopedRole[];
 }

src/components/budget/dto/budget.dto.ts
@@ -29,7 +29,7 @@ export class Budget extends IntersectionType(ChangesetAware, Resource) {
   static readonly Parent = import('../../project/dto').then((m) => m.IProject);
 
   @Field(() => IProject)
-  readonly parent: BaseNode;
+  declare readonly parent: BaseNode;
 
   @Field()
   @DbLabel('BudgetStatus')
@@ -47,7 +47,7 @@ export class Budget extends IntersectionType(ChangesetAware, Resource) {
 
   // A list of non-global roles the requesting user has available for this object.
   // This is just a cache, to prevent extra db lookups within the same request.
-  readonly scope: ScopedRole[];
+  declare readonly scope: ScopedRole[];
 }
 
 @ObjectType({

src/components/changeset/dto/changeset.dto.ts
@@ -9,7 +9,7 @@ import { Resource, SecuredProps } from '../../../common';
 export class Changeset extends Resource {
   static readonly Props: string[] = keysOf<Changeset>();
   static readonly SecuredProps: string[] = keysOf<SecuredProps<Changeset>>();
-  __typename: string;
+  declare __typename: string;
 
   @Field({
     description: 'Whether this changeset is editable',

src/components/comments/dto/commentable.dto.ts
@@ -14,5 +14,5 @@ export abstract class Commentable extends Resource {
     commentThreads: [CommentThread],
   };
 
-  __typename: string;
+  declare __typename: string;
 }

src/components/engagement/dto/engagement.dto.ts
@@ -60,12 +60,12 @@ class Engagement extends ChangesetAwareResource {
   static readonly SecuredProps: string[] = keysOf<SecuredProps<Engagement>>();
   static readonly Parent = import('../../project/dto').then((m) => m.IProject);
 
-  readonly __typename: 'LanguageEngagement' | 'InternshipEngagement';
+  declare readonly __typename: 'LanguageEngagement' | 'InternshipEngagement';
 
   readonly project: ID;
 
   @Field(() => IProject)
-  readonly parent: BaseNode;
+  declare readonly parent: BaseNode;
 
   @Field(() => SecuredEngagementStatus, {
     middleware: [parentIdMiddleware],
@@ -129,7 +129,7 @@ class Engagement extends ChangesetAwareResource {
 
   // A list of non-global roles the requesting user has available for this object.
   // This is just a cache, to prevent extra db lookups within the same request.
-  readonly scope: ScopedRole[];
+  declare readonly scope: ScopedRole[];
 
   @Field()
   readonly description: SecuredRichTextNullable;
@@ -154,7 +154,7 @@ export class LanguageEngagement extends Engagement {
   );
 
   @Field(() => TranslationProject)
-  readonly parent: BaseNode;
+  declare readonly parent: BaseNode;
 
   readonly language: Secured<ID>;
 
@@ -192,7 +192,7 @@ export class InternshipEngagement extends Engagement {
   );
 
   @Field(() => InternshipProject)
-  readonly parent: BaseNode;
+  declare readonly parent: BaseNode;
 
   readonly countryOfOrigin: Secured<ID>;
 

src/components/file/dto/node.ts
@@ -94,7 +94,7 @@ export class FileVersion extends BaseFile {
   static readonly Props = keysOf<FileVersion>();
   static readonly SecuredProps = keysOf<SecuredProps<FileVersion>>();
 
-  readonly type: FileNodeType.FileVersion;
+  declare readonly type: FileNodeType.FileVersion;
 }
 
 @ObjectType({
@@ -104,7 +104,7 @@ export class File extends BaseFile {
   static readonly Props = keysOf<File>();
   static readonly SecuredProps = keysOf<SecuredProps<File>>();
 
-  readonly type: FileNodeType.File;
+  declare readonly type: FileNodeType.File;
 
   readonly latestVersionId: ID;
 
@@ -121,7 +121,7 @@ export class Directory extends FileNode {
   static readonly Props = keysOf<Directory>();
   static readonly SecuredProps = keysOf<SecuredProps<Directory>>();
 
-  readonly type: FileNodeType.Directory;
+  declare readonly type: FileNodeType.Directory;
 
   @Field(() => Int, {
     description:

commit b5ca2bedbeeb017caf3b637d622c9c0c5a529174

Fix default last chapter/verse in scripture input
I'm guessing this regressed with a7187c6.
Fixed by using an actual getter/setter for the `book` in the initial `ScriptureReferenceInput` class.
This allows for overriding in `ScriptureReferenceEndInput`.
The getter/setter is re-defined to be a normal property on the first set.
So this makes it function like it did before at runtime.

This issue was exacerbated by the fact that this failure to produce a valid scripture reference
still produced a `Verse` object that happened to be invalid.
This invalid `Verse` object caused an infinite loop in `mergeVerseRanges`.
This was fixed here:
https://github.com/SeedCompany/libs/commit/b02ae4033c5bc573eb3d392c4e424051b3df45b8
Where runtime checks were added to prevent those invalid objects from being created, thus avoiding the infinite loop.

For some reason Gel doesn't like this getter/setter change.
Typecasting to a `Simplify`ed version restores the type compat.

src/components/scripture/dto/scripture-reference.dto.ts
@@ -17,7 +17,17 @@ export abstract class ScriptureReferenceInput {
     description: 'The Bible book',
   })
   @IsValidBook()
-  readonly book: string;
+  get book(): string {
+    return undefined as any;
+  }
+  protected set book(book: string) {
+    Object.defineProperty(this, 'book', {
+      value: book,
+      writable: true,
+      configurable: true,
+      enumerable: true,
+    });
+  }
 
   @Field(() => Int, {
     description: stripIndent`

src/components/scripture/dto/scripture-reference.transformer.ts
@@ -11,21 +11,17 @@ class ScriptureReferenceStartInput extends ScriptureReferenceInput {
 }
 
 class ScriptureReferenceEndInput extends ScriptureReferenceInput {
-  private bookName: string;
-
-  // @ts-expect-error Yes we are clobbering the property definition from the parent.
-  // It's ok for this use case and the parent decorators are still applied.
   get book() {
-    return this.bookName;
+    return super.book;
   }
+  protected set book(name: string) {
+    super.book = name;
 
-  set book(value: string) {
-    this.bookName = value;
     // When the book is set, default the chapter & verse to last.
     // If they are also given on input, then they will just be overridden
     // after this. Also ignoring errors, as the validator reports better.
     try {
-      const book = Book.named(value);
+      const book = Book.named(name);
       (this as Mutable<this>).chapter = book.lastChapter.index;
       (this as Mutable<this>).verse = book.lastChapter.lastVerse.index;
     } catch (e) {

src/components/scripture/gel.utils.ts
@@ -4,10 +4,11 @@ import {
   labelOfVerseRanges,
   Verse,
 } from '@seedcompany/scripture';
+import { Simplify } from 'type-fest';
 import { $, e } from '~/core/gel';
 import { $expr_Param } from '~/core/gel/generated-client/params';
 import { $expr_PathNode } from '~/core/gel/generated-client/path';
-import { ScriptureRangeInput } from './dto';
+import { ScriptureRangeInput, ScriptureReferenceInput } from './dto';
 
 const verse = e.tuple({
   book: e.str,
@@ -44,11 +45,11 @@ export const value = (input: readonly ScriptureRangeInput[]) => ({
   verses: input.map((verseRange) => ({
     label: labelOfVerseRange(verseRange),
     start: {
-      ...verseRange.start,
+      ...(verseRange.start as Simplify<ScriptureReferenceInput>),
       verseId: Verse.from(verseRange.start).id,
     },
     end: {
-      ...verseRange.end,
+      ...(verseRange.end as Simplify<ScriptureReferenceInput>),
       verseId: Verse.from(verseRange.end).id,
     },
   })),