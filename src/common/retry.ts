b343b8db7bd604f0b99dfad05c52857a9224610b
Carson Full <carson_full@tsco.org>
2025-04-03 10:10:55 -0500

    Create Retry decorator

--- src/common/retry.ts
+++ src/common/retry.ts
@@ -1,6 +1,9 @@
-import { Duration, DurationLike } from 'luxon';
+import { DurationIn as DurationLike } from '@seedcompany/common/temporal/luxon';
+import { Duration } from 'luxon';
 import pRetry, { Options } from 'p-retry';
 import { Merge } from 'type-fest';
+
+type AsyncFn = (...args: any[]) => Promise<any>;
 
 export { AbortError } from 'p-retry';
 
@@ -25,19 +28,37 @@ export type RetryOptions = Merge<
   }
 >;
 
+const parseOptions = (options: RetryOptions = {}): Options => ({
+  ...options,
+  maxRetryTime: options?.maxRetryTime
+    ? Duration.from(options.maxRetryTime).toMillis()
+    : undefined,
+  minTimeout: options?.minTimeout
+    ? Duration.from(options.minTimeout).toMillis()
+    : undefined,
+  maxTimeout: options?.maxTimeout
+    ? Duration.from(options.maxTimeout).toMillis()
+    : undefined,
+});
+
 export const retry = <T>(
   input: (attemptCount: number) => PromiseLike<T> | T,
   options?: RetryOptions,
-): Promise<T> =>
-  pRetry(input, {
-    ...options,
-    maxRetryTime: options?.maxRetryTime
-      ? Duration.from(options.maxRetryTime).toMillis()
-      : undefined,
-    minTimeout: options?.minTimeout
-      ? Duration.from(options.minTimeout).toMillis()
-      : undefined,
-    maxTimeout: options?.maxTimeout
-      ? Duration.from(options.maxTimeout).toMillis()
-      : undefined,
-  });
+): Promise<T> => pRetry(input, parseOptions(options));
+
+export const Retry =
+  (options?: RetryOptions) =>
+  (
+    target: unknown,
+    key: string | symbol,
+    descriptor: TypedPropertyDescriptor<AsyncFn>,
+  ) => {
+    const parsed = parseOptions(options);
+    const orig = descriptor.value!;
+    const next: Record<typeof key, AsyncFn> = {
+      [key]: function (...args: any[]) {
+        return pRetry(() => orig.apply(this, args), parsed);
+      },
+    };
+    descriptor.value = next[key];
+  };
