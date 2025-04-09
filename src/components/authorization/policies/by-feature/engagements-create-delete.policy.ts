ab8a31bb2cc58b9216b0a6768175ed80d82de754
Carson Full <carson_full@tsco.org>
2025-04-09 11:26:13 -0500

    Allow FPM's to add engagements while the project is discussing changes to plan

--- src/components/authorization/policies/by-feature/engagements-create-delete.policy.ts
+++ src/components/authorization/policies/by-feature/engagements-create-delete.policy.ts
@@ -1,9 +1,15 @@
-import { field, Hierarchies, member, Policy } from '../util';
+import { any, field, Hierarchies, member, Policy } from '../util';
 
 @Policy([...Hierarchies.Field, ...Hierarchies.Finance], (r) => [
   r.Project,
   r.Engagement.read,
-  r.Engagement.whenAll(member, field('project.status', 'InDevelopment')).create,
+  r.Engagement.whenAll(
+    member,
+    any(
+      field('project.status', 'InDevelopment'),
+      field('project.step', 'DiscussingChangeToPlan'),
+    ),
+  ).create,
   r.Engagement.whenAll(member, field('status', 'InDevelopment')).delete,
 ])
 export class EngagementsCreateDeletePolicy {}
