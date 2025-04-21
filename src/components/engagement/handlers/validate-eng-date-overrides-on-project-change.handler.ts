ee4db278948e1ef73221185b37216c29b01df17c
Carson Full <carson_full@tsco.org>
2025-04-21 17:51:36 -0500

    Validate engagement date overrides against the project

---
+++ src/components/engagement/handlers/validate-eng-date-overrides-on-project-change.handler.ts
@@ -0,0 +1,93 @@
+import { isNotFalsy, NonEmptyArray } from '@seedcompany/common';
+import { CalendarDate, ID, RangeException, type UnsecuredDto } from '~/common';
+import { EventsHandler, IEventHandler } from '~/core';
+import type { Project } from '../../project/dto';
+import { ProjectUpdatedEvent } from '../../project/events';
+import { EngagementService } from '../engagement.service';
+
+@EventsHandler(ProjectUpdatedEvent)
+export class ValidateEngDateOverridesOnProjectChangeHandlerSetLastStatusDate
+  implements IEventHandler<ProjectUpdatedEvent>
+{
+  constructor(private readonly engagements: EngagementService) {}
+
+  async handle(event: ProjectUpdatedEvent) {
+    const { changes, updated, session } = event;
+
+    if (changes.mouStart === undefined && changes.mouEnd === undefined) {
+      return;
+    }
+
+    const project = {
+      id: updated.id,
+      name: updated.name,
+      mouStart: updated.mouStart,
+      mouEnd: updated.mouEnd,
+    };
+    const engagements = await this.engagements.listAllByProjectId(
+      project.id,
+      session,
+    );
+    const errors = engagements
+      .flatMap<
+        EngagementDateOverrideConflictException['engagements'][0] | null
+      >((eng) => {
+        const common = {
+          id: eng.id,
+        } as const;
+        const { startDateOverride: start, endDateOverride: end } = eng;
+        return [
+          project.mouStart && start && project.mouStart > start
+            ? {
+                ...common,
+                point: 'start' as const,
+                date: start,
+              }
+            : null,
+          project.mouEnd && end && project.mouEnd < end
+            ? {
+                ...common,
+                point: 'end' as const,
+                date: end,
+              }
+            : null,
+        ];
+      })
+      .filter(isNotFalsy);
+    if (errors.length === 0) {
+      return;
+    }
+    throw new EngagementDateOverrideConflictException(project, [
+      errors[0]!,
+      ...errors.slice(1),
+    ]);
+  }
+}
+
+class EngagementDateOverrideConflictException extends RangeException {
+  constructor(
+    readonly project: Pick<
+      UnsecuredDto<Project>,
+      'id' | 'name' | 'mouStart' | 'mouEnd'
+    >,
+    readonly engagements: NonEmptyArray<
+      Readonly<{
+        id: ID<'Engagement'>;
+        point: 'start' | 'end';
+        date: CalendarDate;
+      }>
+    >,
+  ) {
+    const message = [
+      engagements.length === 1
+        ? 'An engagement has a date outside the new range'
+        : 'Some engagements have dates outside the new range',
+      ...engagements.map((eng) => {
+        const pointStr = eng.point === 'start' ? 'Start' : 'End';
+        const dateStr = eng.date.toISO();
+        return `  - ${pointStr} date of ${eng.id} is ${dateStr}`;
+      }),
+    ].join('\n');
+    super({ message });
+  }
+}
