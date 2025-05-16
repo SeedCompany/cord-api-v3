debf726c82ef3ffd974aba5eb5e7b6ca1f9001d0
Carson Full <carson_full@tsco.org>
2025-05-16 08:36:10 -0500

    Move LoaderFactory decorator to data-loader/ & use new metadata abstraction

--- src/core/data-loader/index.ts
+++ src/core/data-loader/index.ts
@@ -6,6 +6,7 @@ export {
 } from '@seedcompany/data-loader';
 
 export * from './options.type';
+export { LoaderFactory } from './loader-factory.decorator';
 export * from './session-aware-loader.strategy';
 export * from './ordered-data-loader.strategy';
 export * from './single-item-loader.strategy';
