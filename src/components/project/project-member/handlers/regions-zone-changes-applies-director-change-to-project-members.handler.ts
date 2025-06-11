12a182f2261fec00f6f29d1f9b53bee0423deae0
Carson Full <carson_full@tsco.org>
2025-06-11 15:31:59 -0500

    Replace FOD memberships on open projects when a FieldRegion's zone changes (#3495)

---
+++ src/components/project/project-member/handlers/regions-zone-changes-applies-director-change-to-project-members.handler.ts
@@ -0,0 +1,50 @@
+import { EventsHandler, ILogger, Logger, ResourceLoader } from '~/core';
+import { FieldRegionUpdatedEvent } from '../../../field-region/events/field-region-updated.event';
+import { ProjectMemberRepository } from '../project-member.repository';
+
+@EventsHandler(FieldRegionUpdatedEvent)
+export class RegionsZoneChangesAppliesDirectorChangeToProjectMembersHandler {
+  constructor(
+    private readonly repo: ProjectMemberRepository,
+    private readonly resources: ResourceLoader,
+    @Logger('project:members') private readonly logger: ILogger,
+  ) {}
+
+  async handle(event: FieldRegionUpdatedEvent) {
+    const oldZoneId = event.previous.fieldZone.id;
+    const newZoneId = event.input.fieldZoneId;
+    if (!newZoneId) {
+      return;
+    }
+    const [oldZone, newZone] = await Promise.all([
+      this.resources.load('FieldZone', oldZoneId),
+      this.resources.load('FieldZone', newZoneId),
+    ]);
+    const oldDirector = oldZone.director.value;
+    const newDirector = newZone.director.value;
+    if (!oldDirector || !newDirector) {
+      // Shouldn't really happen. I think everyone can see zones & directors rn.
+      throw new Error(
+        'Cannot read field zone directors to apply project membership changes',
+      );
+    }
+
+    const stats = await this.repo.replaceMembershipsOnOpenProjects(
+      oldDirector.id,
+      newDirector.id,
+      'FieldOperationsDirector',
+    );
+
+    this.logger.notice(
+      "FieldRegion's zone changed - Replaced FOD director memberships on open projects",
+      {
+        location: event.updated.id,
+        oldZone: oldZoneId,
+        newZone: newZoneId,
+        oldDirector: oldDirector.id,
+        newDirector: newDirector.id,
+        ...stats,
+      },
+    );
+  }
+}
