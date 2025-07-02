f98da5e239b5bf27ddbdf3cedf912363bb3f15b5
Carson Full <carson_full@tsco.org>
2025-07-02 13:52:06 -0500

    Bump Org.reach/types migration versions to execute them (#3523)

--- src/components/organization/migrations/add-reach.migration.ts
+++ src/components/organization/migrations/add-reach.migration.ts
@@ -1,7 +1,7 @@
 import { BaseMigration, Migration } from '~/core/database';
 import { Organization } from '../dto';
 
-@Migration('2023-08-04T00:00:00')
+@Migration('2025-07-02T18:11:16')
 export class AddOrganizationReachMigration extends BaseMigration {
   async up() {
     await this.addProperty(Organization, 'reach', []);
--- src/components/organization/migrations/add-type.migration.ts
+++ src/components/organization/migrations/add-type.migration.ts
@@ -1,7 +1,7 @@
 import { BaseMigration, Migration } from '~/core/database';
 import { Organization } from '../dto';
 
-@Migration('2023-08-04T00:00:00')
+@Migration('2025-07-02T18:11:16')
 export class AddOrganizationTypeMigration extends BaseMigration {
   async up() {
     await this.addProperty(Organization, 'types', []);
