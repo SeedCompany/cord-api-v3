fb0027f739191a3d773424eaeab9d904cb7fc3ef
Carson Full <carson_full@tsco.org>
2025-08-20 08:36:05 -0500

    Migrate EventsHandler

--- src/core/events/constants.ts
+++ src/core/events/constants.ts
@@ -1,9 +1,4 @@
-import { type ID } from '~/common';
-
 export const EVENT_METADATA = '__event__';
-export const EVENTS_HANDLER_METADATA = '__eventsHandler__';
-
-export type EventHandlerMetadata = Map<ID, Priority>;
 
 /**
  * Higher goes first.
