641d2ef29d476c5746ae0ba23e6c8fa630d496c8
Carson Full <carson_full@tsco.org>
2025-10-15 12:12:41 -0500

    Enhance LiveQueryStore to accept resource/id tuples

--- src/core/live-query/live-query-store.interface.ts
+++ src/core/live-query/live-query-store.interface.ts
@@ -1,18 +1,46 @@
-import type { Many } from '@seedcompany/common';
+import { EnhancedResource, type ID } from '~/common';
+import type { ResourceLike } from '~/core/resources';
 
 export abstract class LiveQueryStore {
   /**
    * Invalidate queries (and schedule their re-execution) via a resource identifier.
    *
-   * @param identifiers A single or list of resource identifiers that should be invalidated.
-   *
    * @example
-   *   // Invalidate all operations whose latest execution result contains the given user
-   *   `User:${user.id}`
-   *   // Invalidate query operations that select the Query, user field with the id argument
-   *   `Query.user(id:"${user.id}")`
-   *   // invalidate a list of all users (redundant with previous invalidations)
-   *   `Query.users`
+   * ```ts
+   * // Invalidate all operations whose latest execution result contains the given user
+   * `User:${user.id}`
+   * // or
+   * [User, user.id]
+   *
+   * // Invalidate query operations that select the Query, user field with the id argument
+   * `Query.user(id:"${user.id}")`
+   *
+   * // invalidate a list of all users (redundant with previous invalidations)
+   * `Query.users`
+   * ```
    */
-  abstract invalidate(identifiers: Many<string>): void;
+  invalidate(identifier: string | readonly [res: ResourceLike, id: ID]) {
+    this.invalidateAll([identifier]);
+  }
+
+  /**
+   * Invalidate multiple identifiers.
+   * @see invalidate
+   */
+  invalidateAll(
+    identifiers: ReadonlyArray<string | readonly [res: ResourceLike, id: ID]>,
+  ) {
+    this.doInvalidate(
+      identifiers.map((input) => {
+        if (typeof input === 'string') {
+          return input;
+        }
+        const [resLike, id] = input;
+        const res = EnhancedResource.resolve(resLike);
+        return `${res.name}:${id}`;
+      }),
+    );
+  }
+
+  protected abstract doInvalidate(identifiers: string[]): void;
 }
