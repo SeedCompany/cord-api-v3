66cca812c79411a9fa329f2589f08e2247d7e80a
Carson Full <carson_full@tsco.org>
2025-10-15 10:12:17 -0500

    Create a `LiveQueryStore` service to invalidate resources feeding `@live` data

---
+++ src/core/live-query/live-query-store.impl.ts
@@ -0,0 +1,34 @@
+import { InMemoryLiveQueryStore } from '@n1ru4l/in-memory-live-query-store';
+import { Injectable } from '@nestjs/common';
+import type { Many } from '@seedcompany/common';
+import { TransactionHooks } from '../database';
+import { LiveQueryStore } from './live-query-store.interface';
+
+/**
+ * Separated implementation because I imagine the interface will be imported all over,
+ * and I don't want to couple those imports to the database & graphql module/folder.
+ */
+@Injectable()
+export class LiveQueryStoreImpl extends LiveQueryStore {
+  constructor(
+    private readonly store: InMemoryLiveQueryStore,
+    private readonly txHooks: TransactionHooks,
+  ) {
+    super();
+  }
+
+  /**
+   * Wait to do invalidation until after the transaction is committed.
+   * The store re-runs the queries it determines to be stale inside this
+   * async execution, and this needs to be done outside the transaction.
+   *
+   * We also don't have an easy way to check if this execution is currently in a transaction.
+   * It would be nice to confirm this and warn if not.
+   * Currently, if this case is hit, it is highly likely the invalidation will just be dropped.
+   */
+  invalidate(identifiers: Many<string>) {
+    this.txHooks.afterCommit.add(async () => {
+      await this.store.invalidate(identifiers as string | string[]);
+    });
+  }
+}
