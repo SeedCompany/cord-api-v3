e17a366bf9d23d3b86a3d51079bf995781cfab20
Carson Full <carson_full@tsco.org>
2026-01-06 09:50:26 -0600

    Note why ID is exposed directly

--- src/components/project/dto/project-change.dto.ts
+++ src/components/project/dto/project-change.dto.ts
@@ -15,6 +15,18 @@ import { UpdateProject } from './update-project.dto';
 export class AnyProjectChangeOrDeletion extends DataObject {
   readonly __typename: string;
 
+  /**
+   * WHY HERE:
+   * We don't like exposing ID properties in output types,
+   * favoring the actual object instead which holds its own id property.
+   * We compromise here because a delete action cannot include the output type,
+   * since it doesn't exist anymore.
+   * This interface would be better as a union of the delete action or AnyProjectChange.
+   * This way the delete action can give the ID directly, and any other project
+   * change can give the project object.
+   * Unfortunately, the GraphQL spec does not allow interfaces in unions.
+   * https://github.com/graphql/graphql-js/issues/451
+   */
   @IdField()
   readonly projectId: ID<'Project'>;
 
