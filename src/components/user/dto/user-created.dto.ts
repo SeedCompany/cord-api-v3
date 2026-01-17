db462b37265bc6bb575e6c14bc7d590925edc9c6
Carson Full <carson_full@tsco.org>
2026-01-17 07:17:28 -0600

    Document UserCreated vs UserRegistered differences

--- src/components/user/dto/user-created.dto.ts
+++ src/components/user/dto/user-created.dto.ts
@@ -1,7 +1,13 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { stripIndent } from 'common-tags';
 import { User } from './user.dto';
 
-@ObjectType()
+@ObjectType({
+  description: stripIndent`
+    A user/person was created via another logged in user.
+    They do not have login credentials set yet.
+  `,
+})
 export abstract class UserCreated {
   @Field()
   readonly user: User; // intentionally user
