f8a2ee1c5837f1c8cb7bca9b20fd1bfe7322cf81
Carson Full <carson_full@tsco.org>
2026-01-26 12:10:59 -0600

    Allow services to be given & overridden in test app

--- test/setup/create-app.ts
+++ test/setup/create-app.ts
@@ -1,5 +1,6 @@
 import { afterAll } from '@jest/globals';
-import { Test } from '@nestjs/testing';
+import type { ModuleMetadata, Provider } from '@nestjs/common';
+import { Test, type TestingModuleBuilder } from '@nestjs/testing';
 import type { DeepPartial } from 'ts-essentials';
 import { andCall } from '~/common';
 import { ConfigService } from '~/core';
@@ -20,23 +21,32 @@ afterAll(async () => {
 
 export const createApp = async ({
   config,
+  imports,
+  providers,
+  overrides,
 }: {
   config?: DeepPartial<ConfigService>;
-} = {}): Promise<TestApp> => {
+  providers?: Provider[];
+  overrides?: (builder: TestingModuleBuilder) => TestingModuleBuilder;
+} & Pick<ModuleMetadata, 'imports' | 'providers'> = {}): Promise<TestApp> => {
   const db = await ephemeralGel();
 
   let app;
   try {
-    const moduleFixture = await Test.createTestingModule({
-      imports: [AppModule],
+    let builder = Test.createTestingModule({
+      imports: [AppModule, ...(imports ?? [])],
+      providers,
     })
       .overrideProvider(LevelMatcher)
       .useValue(new LevelMatcher([], LogLevel.ERROR))
       .overrideProvider('GEL_CONNECT')
       .useValue(db?.options)
       .overrideProvider('CONFIG_PART')
-      .useValue(config)
-      .compile();
+      .useValue(config);
+    if (overrides) {
+      builder = overrides?.(builder);
+    }
+    const moduleFixture = await builder.compile();
 
     app = moduleFixture.createNestApplication<NestHttpApplication>(
       new HttpAdapter(),
