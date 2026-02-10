8acb02e59155edd5012b1a63f95f4b4e1bc743bf
Carson Full <carson_full@tsco.org>
2026-02-10 16:23:27 -0600

    Fix `EngagmentMutation.engagement` specialization
    Now a single interface resolver that handles runtime resolution.
    And DTOs declare specialization types, only for schema generation.

--- src/components/engagement/engagement-mutation-links.resolver.ts
+++ src/components/engagement/engagement-mutation-links.resolver.ts
@@ -1,47 +1,11 @@
 import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
 import { Loader, type LoaderOf } from '@seedcompany/data-loader';
-import {
-  type Engagement,
-  EngagementMutation,
-  IEngagement,
-  InternshipEngagement,
-  InternshipEngagementMutation,
-  LanguageEngagement,
-  LanguageEngagementMutation,
-} from './dto';
+import { type Engagement, EngagementMutation, IEngagement } from './dto';
 import { EngagementLoader } from './engagement.loader';
 
 @Resolver(EngagementMutation)
 export class EngagementMutationLinksResolver {
   @ResolveField(() => IEngagement)
-  async engagement(
-    @Parent() change: EngagementMutation,
-    @Loader(EngagementLoader) engagements: LoaderOf<EngagementLoader>,
-  ): Promise<Engagement> {
-    return await engagements.load({
-      id: change.engagementId,
-      view: { active: true },
-    });
-  }
-}
-
-@Resolver(LanguageEngagementMutation)
-export class LanguageEngagementMutationLinksResolver {
-  @ResolveField(() => LanguageEngagement)
-  async engagement(
-    @Parent() change: EngagementMutation,
-    @Loader(EngagementLoader) engagements: LoaderOf<EngagementLoader>,
-  ): Promise<Engagement> {
-    return await engagements.load({
-      id: change.engagementId,
-      view: { active: true },
-    });
-  }
-}
-
-@Resolver(InternshipEngagementMutation)
-export class InternshipEngagementMutationLinksResolver {
-  @ResolveField(() => InternshipEngagement)
   async engagement(
     @Parent() change: EngagementMutation,
     @Loader(EngagementLoader) engagements: LoaderOf<EngagementLoader>,
--- src/components/project/dto/update-project.dto.ts
+++ src/components/project/dto/update-project.dto.ts
@@ -1,4 +1,4 @@
-import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
 import { type DateTime } from 'luxon';
 import {
   type CalendarDate,
