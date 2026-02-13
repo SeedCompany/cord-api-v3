695258a5062fba063c84e13be1410740ee7644e3
Carson Full <carson_full@tsco.org>
2026-02-12 19:23:58 -0600

    Add a JMES path filter for the `Queue.jobs` list
    ```
    jobs(filter: "[?progress != `0`]")
    jobs(filter: "[?attemptsMade > `1`]")
    ```

--- src/core/queue/management/resolvers/queue.resolver.ts
+++ src/core/queue/management/resolvers/queue.resolver.ts
@@ -1,3 +1,4 @@
+import { search } from '@aws-lambda-powertools/jmespath';
 import {
   Args,
   ArgsType,
@@ -8,10 +9,12 @@ import {
   ResolveField,
   Resolver,
 } from '@nestjs/graphql';
+import { mapEntries } from '@seedcompany/common';
 import { GraphQLJSONObject as JsonObject } from 'graphql-scalars';
-import { IdArg, Order } from '~/common';
+import { IdArg, JmesPathScalar, Order } from '~/common';
 import { Job, JobType, Queue } from '../dto';
 import { QueueManagementService } from '../queue-management.service';
+import { JobResolver } from './job.resolver';
 
 @ArgsType()
 class JobArgs {
@@ -23,6 +26,8 @@ class JobArgs {
   end?: number;
   @Field(() => Order, { nullable: true })
   order?: Order;
+  @Field(() => JmesPathScalar, { nullable: true })
+  filter?: string;
 }
 
 @ArgsType()
@@ -35,7 +40,10 @@ class MetricArgs {
 
 @Resolver(() => Queue)
 export class QueueResolver {
-  constructor(private readonly service: QueueManagementService) {}
+  constructor(
+    private readonly service: QueueManagementService,
+    private readonly jobResolver: JobResolver,
+  ) {}
 
   @Query(() => Queue)
   async queue(@Args('name') name: string): Promise<Queue> {
@@ -59,12 +67,45 @@ export class QueueResolver {
 
   @ResolveField(() => [Job])
   async jobs(@Parent() queue: Queue, @Args() args: JobArgs) {
-    return await queue.getJobs(
+    const jobs: Job[] = await queue.getJobs(
       args.types,
       args.start,
       args.end,
       args.order === Order.ASC,
     );
+
+    const filterPath = args.filter;
+    if (!filterPath) {
+      return jobs;
+    }
+
+    let states: ReadonlyMap<Job, `${JobType}` | 'unknown'> = new Map();
+    if (filterPath.includes('state')) {
+      const list = await Promise.all(jobs.map((job) => job.getState()));
+      states = mapEntries(jobs.entries(), ([i, job]) => [job, list[i]!]).asMap;
+    }
+
+    return jobs.filter((job) => {
+      const gqlJob = new Proxy(job, {
+        has: (target, p) => {
+          if (Reflect.has(this.jobResolver, p)) {
+            return true;
+          }
+          return Reflect.has(target, p);
+        },
+        get: (target, prop) => {
+          if (prop === 'state') {
+            return states.get(job);
+          }
+          if (Reflect.has(this.jobResolver, prop)) {
+            return this.jobResolver[prop as keyof JobResolver](job, {});
+          }
+          return Reflect.get(target, prop);
+        },
+      });
+      const result = search(filterPath, [gqlJob]);
+      return !!result && (Array.isArray(result) ? result.length > 0 : true);
+    });
   }
 
   @ResolveField(() => Job, { nullable: true })
