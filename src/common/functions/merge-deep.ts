ed832140127f790c7cd22163cd4d5de8e21deaef
Carson Full <carson_full@tsco.org>
2026-02-12 21:53:01 -0600

    Adjust mergeDeep() types

--- src/common/functions/merge-deep.ts
+++ src/common/functions/merge-deep.ts
@@ -1,5 +1,5 @@
 import { isPlainObject, mapValues } from '@seedcompany/common';
-import type { Merge } from 'type-fest';
+import type { MergeDeep } from 'type-fest';
 
 /**
  * This merges _plain_ objects deeply, everything else is replaced.
@@ -7,12 +7,13 @@ import type { Merge } from 'type-fest';
 export const mergeDeep = <A extends object, B extends object>(
   a: A,
   b: B,
-): Merge<A, B> => ({
-  ...a,
-  ...(mapValues(b, (key, bVal) => {
-    const aVal = a[key];
-    return isPlainObject(aVal) && isPlainObject(bVal)
-      ? mergeDeep(aVal, bVal)
-      : bVal;
-  }).asRecord as B),
-});
+): MergeDeep<A, B> =>
+  ({
+    ...a,
+    ...mapValues(b, (key, bVal) => {
+      const aVal = a[key];
+      return isPlainObject(aVal) && isPlainObject(bVal)
+        ? mergeDeep(aVal, bVal)
+        : bVal;
+    }).asRecord,
+  }) as any;
