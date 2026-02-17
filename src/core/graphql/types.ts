4acc65f09e1cf74fbe57cdcdeeddf6559153b4f8
Carson Full <carson_full@tsco.org>
2026-02-17 10:12:53 -0600

    Provide way to declare all ExecutionContextTypes enabled
    Ported from seed-api.
    I renamed `ContextTypeOverrides` to `DeclareContextTypes`

--- src/core/graphql/types.ts
+++ src/core/graphql/types.ts
@@ -1,20 +1,11 @@
 /* eslint-disable @typescript-eslint/method-signature-style */
 
-import type { GqlContextType as GqlRequestType } from '@nestjs/graphql/dist/services/gql-execution-context';
 import type { GraphQLResolveInfo } from 'graphql';
 import type { GqlContextType } from '~/common';
 
-type AllRequestTypes = GqlRequestType;
-
-declare module '@nestjs/common/interfaces/features/arguments-host.interface' {
-  export interface ArgumentsHost {
-    getType(): AllRequestTypes;
-  }
-}
-
-declare module '@nestjs/core/helpers/execution-context-host' {
-  export interface ExecutionContextHost {
-    getType(): AllRequestTypes;
+declare module '~/core/exe-ctx.type' {
+  interface DeclareContextTypes {
+    graphql: true;
   }
 }
 
