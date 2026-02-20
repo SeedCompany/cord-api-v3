08ee4acc25106b02dd3ad7d8f68a8a4dacd3ec7f
Carson Full <carson_full@tsco.org>
2026-02-20 15:19:57 -0600

    Move notification delivery to a queue/worker

--- src/components/notifications/notification.service.ts
+++ src/components/notifications/notification.service.ts
@@ -4,15 +4,7 @@ import {
   Injectable,
   type OnModuleInit,
 } from '@nestjs/common';
-import {
-  asNonEmptyArray,
-  asyncPool,
-  mapEntries,
-  mapValues,
-  type Nil,
-  type NonEmptyArray,
-} from '@seedcompany/common';
-import { type EmailMessage, MailerService } from '@seedcompany/nestjs-email';
+import { mapEntries, type Nil } from '@seedcompany/common';
 import Event from 'gel/dist/primitives/event.js';
 import { from, mergeMap } from 'rxjs';
 import {
@@ -21,26 +13,23 @@ import {
   ServerException,
   type UnsecuredDto,
 } from '~/common';
-import { Identity } from '~/core/authentication';
 import { Broadcaster } from '~/core/broadcast';
 import { MetadataDiscovery } from '~/core/discovery';
-import { UserService } from '../user';
 import {
   type MarkNotificationReadArgs,
   type Notification,
-  NotificationChannel,
   type NotificationList,
   type NotificationListInput,
   type NotificationType,
 } from './dto';
 import { NotificationAdded } from './dto/notification-added.hook';
+import { NotificationDeliveryQueue } from './notification-delivery.queue';
 import { NotificationRepository } from './notification.repository';
 import {
   INotificationStrategy,
   type InputOf,
   NotificationStrategy,
 } from './notification.strategy';
-import { NotificationPreferencesService } from './preferences/notification-preferences.service';
 
 @Injectable()
 export abstract class NotificationService {
@@ -110,11 +99,7 @@ export class NotificationServiceImpl
     private readonly discovery: MetadataDiscovery,
     @Inject(forwardRef(() => Broadcaster))
     private readonly broadcaster: Broadcaster & {},
-    private readonly mailer: MailerService,
-    private readonly identity: Identity,
-    private readonly users: UserService,
-    @Inject(forwardRef(() => NotificationPreferencesService))
-    private readonly preferencesService: NotificationPreferencesService & {},
+    private readonly deliveryQueue: NotificationDeliveryQueue,
   ) {
     super();
   }
@@ -124,80 +109,16 @@ export class NotificationServiceImpl
     recipients: ReadonlyArray<ID<'User'>> | Nil,
     input: T extends { Input: infer Input } ? Input : InputOf<T['prototype']>,
   ) {
-    const strategy = this.getStrategy(type);
-
     const out = await super.create(type, recipients, input);
     const { notification } = out;
 
-    // User IDs that received the notification (for preference lookup)
-    const userRecipients: ReadonlyArray<ID<'User'>> =
-      recipients ?? out.recipients ?? [];
-
-    // Partition the recipients into channels based on their preferences
-    const overridesMap = await this.preferencesService.getOverridesMap(
-      this.typeClassToName.get(type)!,
-      userRecipients,
-    );
-    const availabilities = strategy.channelAvailabilities();
-    const channelsForUsers = mapValues.fromList(
-      NotificationChannel,
-      (channel) => {
-        const availability = availabilities[channel];
-        if (availability === 'AlwaysOff') return undefined;
-        return asNonEmptyArray(
-          availability === 'AlwaysOn'
-            ? userRecipients
-            : userRecipients.filter(
-                (user) =>
-                  overridesMap.get(user)?.[channel] ??
-                  availability === 'DefaultOn',
-              ),
-        );
-      },
-    ).asRecord;
-
-    this.deliverToAppChannel(notification, [
-      // Always broadcast to static targets (they're not users with prefs)
-      ...strategy.broadcastTo(),
-      ...(channelsForUsers.App ?? []),
-    ]);
-
-    await this.deliverToEmailChannel(
-      strategy,
+    await this.deliveryQueue.add('deliver', {
+      typeName: this.typeClassToName.get(type)!,
       notification,
-      channelsForUsers.Email,
-    );
+      recipients: recipients ?? out.recipients ?? [],
+    });
 
     return out;
-  }
-
-  deliverToAppChannel(notification: Notification, targets: readonly string[]) {
-    const appPayload = { notification }; // maintain object identity for webhook batching
-    for (const target of targets) {
-      this.broadcaster.channel(NotificationAdded, target).publish(appPayload);
-    }
-  }
-
-  async deliverToEmailChannel(
-    strategy: INotificationStrategy<Notification>,
-    notification: Notification,
-    userIds: NonEmptyArray<ID<'User'>> | undefined,
-  ) {
-    if (!strategy.renderEmail || !userIds) {
-      return;
-    }
-    const users = await this.identity.asRole(
-      'Administrator',
-      async () => await this.users.readMany(userIds),
-    );
-    await asyncPool(Infinity, users, async (user) => {
-      const email = user.email.value;
-      if (!email) return;
-      await this.identity.asUser(user.id, async () => {
-        const msg: EmailMessage<any> = strategy.renderEmail!(notification);
-        await this.mailer.send(msg.withHeaders({ to: email }));
-      });
-    });
   }
 
   async list(input: NotificationListInput): Promise<NotificationList> {
