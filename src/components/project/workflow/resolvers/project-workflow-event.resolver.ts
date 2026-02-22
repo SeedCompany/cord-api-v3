3c50d4e3f824def68558b396875537f035f172c9
Carson Full <carson_full@tsco.org>
2026-02-22 09:27:04 -0600

    Expose WorkflowEvent.project

--- src/components/project/workflow/resolvers/project-workflow-event.resolver.ts
+++ src/components/project/workflow/resolvers/project-workflow-event.resolver.ts
@@ -3,10 +3,23 @@ import { mapSecuredValue } from '~/common';
 import { Loader, type LoaderOf } from '~/core/data-loader';
 import { ActorLoader } from '../../../user/actor.loader';
 import { SecuredActor } from '../../../user/dto';
+import { IProject, type Project } from '../../dto';
+import { ProjectLoader } from '../../project.loader';
 import { ProjectWorkflowEvent as WorkflowEvent } from '../dto';
 
 @Resolver(WorkflowEvent)
 export class ProjectWorkflowEventResolver {
+  @ResolveField(() => IProject)
+  async project(
+    @Parent() event: WorkflowEvent,
+    @Loader(ProjectLoader) projects: LoaderOf<ProjectLoader>,
+  ): Promise<Project> {
+    return await projects.load({
+      id: event.project.id,
+      view: { active: true },
+    });
+  }
+
   @ResolveField(() => SecuredActor)
   async who(
     @Parent() event: WorkflowEvent,
