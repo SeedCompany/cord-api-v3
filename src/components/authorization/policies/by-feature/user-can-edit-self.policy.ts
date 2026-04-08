f322dacae8c9f625d13680d951830b9be0b9a826
Rob Donigian <rob_donigian@tsco.org>
2026-04-08 10:29:12 -0400

    fix status and role policies to strictly admin

--- src/components/authorization/policies/by-feature/user-can-edit-self.policy.ts
+++ src/components/authorization/policies/by-feature/user-can-edit-self.policy.ts
@@ -1,4 +1,6 @@
 import { Policy, self } from '../util';
 
-@Policy('all', (r) => r.User.when(self).edit)
+@Policy('all', (r) =>
+  r.User.when(self).edit.specifically((p) => [p.status.none, p.roles.none]),
+)
 export class UserCanEditSelfPolicy {}
