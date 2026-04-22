fd8126210798eaacc28ad56524f6ff3daa9ae378
Rob Donigian <rob_donigian@tsco.org>
2026-04-22 17:32:25 -0400

    Add Drizzle transactional mutations interceptor

---
+++ src/core/database/drizzle/drizzle-transactional-mutations.interceptor.ts
@@ -0,0 +1,31 @@
+import {
+  type CallHandler,
+  type ExecutionContext,
+  Injectable,
+} from '@nestjs/common';
+import { ConfigService } from '~/core/config';
+import { TransactionalMutationsInterceptor } from '../abstract-transactional-mutations.interceptor';
+import { TransactionHooks } from '../transaction-hooks';
+import { DrizzleService } from './drizzle.service';
+
+@Injectable()
+export class DrizzleTransactionalMutationsInterceptor extends TransactionalMutationsInterceptor {
+  constructor(
+    txHooks: TransactionHooks,
+    private readonly config: ConfigService,
+    private readonly drizzle: DrizzleService,
+  ) {
+    super(txHooks);
+  }
+
+  async intercept(context: ExecutionContext, next: CallHandler) {
+    if (this.config.databaseEngine !== 'postgres') {
+      return next.handle();
+    }
+    return await super.intercept(context, next);
+  }
+
+  protected async inTx<R>(fn: () => Promise<R>): Promise<R> {
+    return await this.drizzle.inTx(fn);
+  }
+}
