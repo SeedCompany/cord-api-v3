2f9556b168db1b47de1216c5dfeb5566ef1f8289
Bryan Nelson <bryan_nelson@tsco.org>
2026-04-23 10:49:44 -0400

    Adds `.env.local` capability for Postgres env vars

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,7 @@
+import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
+
+config({ path: '.env.local' });
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
