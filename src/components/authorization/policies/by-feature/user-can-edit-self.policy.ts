4795ca446ce6b6b0041a98c00a9bcd9d53919d84
sethmcknight <7377802+sethmcknight@users.noreply.github.com>
2026-05-05 07:41:37 -0600

    Fix: Update UserCanEditSelfPolicy to include organization checks

--- src/components/authorization/policies/by-feature/user-can-edit-self.policy.ts
+++ src/components/authorization/policies/by-feature/user-can-edit-self.policy.ts
@@ -1,6 +1,10 @@
 import { Policy, self } from '../util';
 
 @Policy('all', (r) =>
-  r.User.when(self).edit.specifically((p) => [p.status.none, p.roles.none]),
+  r.User.when(self).edit.specifically((p) => [
+    p.status.none,
+    p.roles.none,
+    p.organizations.none,
+  ]),
 )
 export class UserCanEditSelfPolicy {}
